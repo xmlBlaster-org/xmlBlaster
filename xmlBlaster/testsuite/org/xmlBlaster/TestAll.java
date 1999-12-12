95eedf0673580f55f08157b2fda2bbe7926f2041
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
1999-12-12 15:19:48 +0000

    Removed unneeded import statements
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@391 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/org/xmlBlaster/TestAll.java
+++ xmlBlaster/testsuite/org/xmlBlaster/TestAll.java
@@ -3,23 +3,16 @@ Name:      TestAll.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Start all tests
-Version:   $Id: TestAll.java,v 1.1 1999/12/12 15:18:54 ruff Exp $
+Version:   $Id: TestAll.java,v 1.2 1999/12/12 15:19:48 ruff Exp $
 ------------------------------------------------------------------------------*/
 package testsuite.org.xmlBlaster;
 
-import org.xmlBlaster.client.CorbaConnection;
-import org.xmlBlaster.client.UpdateKey;
-import org.xmlBlaster.client.UpdateQoS;
 import org.xmlBlaster.util.Log;
-import org.xmlBlaster.util.Args;
-import org.xmlBlaster.util.StopWatch;
-import org.xmlBlaster.serverIdl.*;
-import org.xmlBlaster.clientIdl.*;
 import test.framework.*;
 
 
 /**
- * This test client starts all known tests. 
+ * This test client starts all known tests.
  * <p>
  * Invoke examples:
  *    ${JacORB_HOME}/bin/jaco testsuite.org.xmlBlaster.TestAll
