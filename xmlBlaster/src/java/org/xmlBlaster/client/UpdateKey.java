d2499c4ee253e47c5253b5fa0960309b9d77ef13
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
1999-12-16 09:43:11 +0000

    May be nicer used as a base class
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@452 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/client/UpdateKey.java
+++ xmlBlaster/src/java/org/xmlBlaster/client/UpdateKey.java
@@ -3,11 +3,12 @@ Name:      UpdateKey.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Handling one xmlKey, knows how to parse it with DOM
-Version:   $Id: UpdateKey.java,v 1.3 1999/12/15 00:45:10 ruff Exp $
+Version:   $Id: UpdateKey.java,v 1.4 1999/12/16 09:43:11 ruff Exp $
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.client;
 
 import org.xmlBlaster.util.Log;
+import org.xmlBlaster.util.SaxHandlerBase;
 import org.xmlBlaster.serverIdl.XmlBlasterException;
 import java.io.*;
 import org.xml.sax.*;
@@ -38,12 +39,10 @@ import org.xml.sax.helpers.*;
  * <p />
  * see http://www.w3.org/TR/xpath
  */
-public class UpdateKey extends HandlerBase
+public class UpdateKey extends SaxHandlerBase
 {
    private String ME = "UpdateKey";
 
-   // private static final String DEFAULT_PARSER_NAME =  // com.ibm.xml.parsers.SAXParser // com.sun.xml.parser.ValidatingParser
-   protected StringBuffer character = new StringBuffer();
    protected boolean inKey = false;     // parsing inside <key> ? </key>
 
    /** value from attribute <key oid="..."> */
@@ -51,12 +50,6 @@ public class UpdateKey extends HandlerBase
 
    /** value from attribute <key oid="" contentMime="..."> */
    protected String contentMime = null;
-
-   /**
-    * The original key in XML syntax, for example:
-    * <code>   &lt;key>&lt;/key>"</code>
-    */
-   protected String xmlKey_literal;
 
 
    /**
@@ -66,33 +59,6 @@ public class UpdateKey extends HandlerBase
    public UpdateKey()
    {
       if (Log.CALLS) Log.trace(ME, "Creating new UpdateKey");
-   }
-
-
-   /**
-    * Constructs an un initialized UpdateKey object.
-    * @deprecated Use the empty constructor
-    */
-   public UpdateKey(String xmlKey_literal)
-   {
-      if (Log.CALLS) Log.trace(ME, "Creating new UpdateKey");
-   }
-
-
-   /*
-    * This method parses the given key XML string using the SAX parser.
-    * @param xmlKey_literal xmlBlaster key in XML notation
-    */
-   public void init(String xmlKey_literal) throws XmlBlasterException
-   {
-      if (xmlKey_literal == null)
-         xmlKey_literal = "";
-
-      this.xmlKey_literal = xmlKey_literal;
-
-      if (xmlKey_literal.length() > 0) {
-         parse(xmlKey_literal);
-      }
    }
 
 
@@ -107,7 +73,7 @@ public class UpdateKey extends HandlerBase
 
 
    /**
-    * Find out which mime type (syntax) the content of the message has. 
+    * Find out which mime type (syntax) the content of the message has.
     * @return e.g "text/xml" or "image/png"
     */
    public String getContentMime()
@@ -117,49 +83,17 @@ public class UpdateKey extends HandlerBase
 
 
    /**
-    * Does the actual parsing
-    * @param xmlData key in XML notation
+    * Start element callback, does handling of tag &lt;key> and its attributes. 
+    * <p />
+    * You may include this into your derived startElement() method like this:<br />
+    * <pre>
+    *  if (super.startElementBase(name, attrs) == true)
+    *     return;
+    * </pre>
+    * @return true if the tag is parsed here, the derived class doesn't need to look at this tag anymore
+    *         false this tag is not handled by this Base class
     */
-   private void parse(String xmlData) throws XmlBlasterException
-   {
-      try {
-         Parser parser = ParserFactory.makeParser(); // DEFAULT_PARSER_NAME
-         parser.setDocumentHandler(this);
-         parser.setErrorHandler(this);
-         parser.setDTDHandler(this);
-         parser.parse(new InputSource(new StringReader(xmlData)));
-      }
-      catch (Exception e) {
-         Log.error(ME, "Error while parsing Key: " + e.toString());
-         e.printStackTrace();
-         throw new XmlBlasterException(ME, "Error while parsing Key: " + e.toString());
-      }
-   }
-
-
-   /**
-    * @return returns the literal xml string
-    */
-   public String toString()
-   {
-      return xmlKey_literal;
-   }
-
-
-   /** Processing instruction. */
-   public void processingInstruction(String target, String data)
-   {
-   }
-
-
-   /** Start document. */
-   public void startDocument()
-   {
-   }
-
-
-   /** Start element. */
-   public void startElement(String name, AttributeList attrs)
+   protected final boolean startElementBase(String name, AttributeList attrs)
    {
       if (name.equalsIgnoreCase("key")) {
          inKey = true;
@@ -178,108 +112,61 @@ public class UpdateKey extends HandlerBase
             if (contentMime == null)
                Log.warning(ME, "The contentMime of the message is missing");
          }
-         return;
+         return true;
       }
+      return false;
    }
 
 
    /**
-    * Characters.
-    * The text between two tags, in the following example 'Hello':
-    * <key>Hello</key>
+    * Start element.
+    * <p />
+    * Default implementation, knows how to parse &lt;key> but knows nothing about the tags inside of key
     */
-   public void characters(char ch[], int start, int length)
+   public void startElement(String name, AttributeList attrs)
    {
-      character.append(new String(ch, start, length));
+      startElementBase(name, attrs);
    }
 
 
-   /** Ignorable whitespace. */
-   public void ignorableWhitespace(char ch[], int start, int length)
+   /**
+    * End element callback, does handling of tag &lt;key>.
+    * <p />
+    * You may include this into your derived endElement() method like this:<br />
+    * <pre>
+    *  if (super.endElementBase(name) == true)
+    *     return;
+    * </pre>
+    * @return true if the tag is parsed here, the derived class doesn't need to look at this tag anymore
+    *         false this tag is not handled by this Base class
+    */
+   protected final boolean endElementBase(String name)
    {
-   }
-
-
-   /** End element. */
-   public void endElement(String name) {
       if( name.equalsIgnoreCase("key") ) {
          inKey = false;
          character = new StringBuffer();
-         return;
+         return true;
       }
+      return false;
    }
 
 
-   /** End document. */
-   public void endDocument() {
-   }
-
-
-   //
-   // ErrorHandler methods
-   //
-
-   /** Warning. */
-   public void warning(SAXParseException ex)
+   /**
+    * End element callback, does handling of tag &lt;key>.
+    * <p />
+    * You may include this into your derived endElement() method like this:<br />
+    * <pre>
+    *  if (super.endElementBase(name) == true)
+    *     return;
+    * </pre>
+    * @return true if the tag is parsed here, the derived class doesn't need to look at this tag anymore
+    *         false this tag is not handled by this Base class
+    */
+   public void endElement(String name)
    {
-      Log.warning(ME, getLocationString(ex) + ": " + ex.getMessage());
+      endElementBase(name);
    }
 
-
-   /** Error. */
-   public void error(SAXParseException ex)
-   {
-      Log.warning(ME, getLocationString(ex) + ": " + ex.getMessage());
-   }
-
-
-   /** Fatal error. */
-   public void fatalError(SAXParseException ex) throws SAXException
-   {
-      if(inKey) {
-         Log.warning(ME, getLocationString(ex) + ": " + ex.getMessage());
-         throw ex;
-      }
-   }
-
-
-   /** Fatal error. */
-   public void notationDecl(String name, String publicId, String systemId)
-   {
-      if (Log.TRACE) Log.trace(ME, "notationDecl(name="+name+", publicId="+publicId+", systemId="+systemId+")");
-   }
-
-
-   /** Fatal error. */
-   public void unparsedEntityDecl(String name, String publicId, String systemId, String notationName)
-   {
-      if (Log.TRACE) Log.trace(ME, "unparsedEntityDecl(name="+name+
-                                          ", publicId="+publicId+
-                                          ", systemId="+systemId+
-                                          ", notationName="+notationName+")");
-   }
-
-
-   /** Returns a string of the location. */
-   private String getLocationString(SAXParseException ex)
-   {
-      StringBuffer str = new StringBuffer();
-
-      String systemId = ex.getSystemId();
-      if (systemId != null) {
-          int index = systemId.lastIndexOf('/');
-          if (index != -1)
-              systemId = systemId.substring(index + 1);
-          str.append(systemId);
-      }
-      str.append(':');
-      str.append(ex.getLineNumber());
-      str.append(':');
-      str.append(ex.getColumnNumber());
-
-      return str.toString();
-
-   }
 
    /**
     * Dump state of this object into XML.
