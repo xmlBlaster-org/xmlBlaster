2fa3296beaf7f6aab34cf39de5085ee6796f466b
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
1999-12-17 09:58:41 +0000

    Allows clients to query on command line xmlBlaster (XPath)
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@461 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/demo/javaclients/ClientQuery.java
@@ -0,0 +1,88 @@
+/*------------------------------------------------------------------------------
+Name:      ClientGet.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Demo code for a client using xmlBlaster
+Version:   $Id: ClientQuery.java,v 1.1 1999/12/17 09:58:41 ruff Exp $
+------------------------------------------------------------------------------*/
+package javaclients;
+
+import org.xmlBlaster.util.*;
+import org.xmlBlaster.client.CorbaConnection;
+import org.xmlBlaster.client.UpdateKey;
+import org.xmlBlaster.serverIdl.*;
+import org.xmlBlaster.clientIdl.*;
+
+
+/**
+ * This client allows you to query xmlBlaster with method get().
+ * <p>
+ * It doesn't implement a Callback server, since it only access xmlBlaster
+ * using the synchronous get() method.
+ * <p>
+ * Invoke examples:<br />
+ * <pre>
+ *    jaco javaclients.ClientQuery -queryXpath "//AGENT/DRIVER"
+ * </pre>
+ */
+public class ClientQuery
+{
+   private Server xmlBlaster = null;
+   private static String ME = "Heidi";
+   private String queryString;
+   private String queryType = "XPATH";
+
+   public ClientQuery(String args[])
+   {
+      try {
+         ME = Args.getArg(args, "-name", ME); // check if parameter -name <userName> is given at startup of client
+         String loginName = ME;
+
+         queryString = Args.getArg(args, "-queryXpath", null);
+         if (queryString != null)
+            queryType = "XPATH";
+         else
+            Log.panic(ME, "Please enter a query string, example:\n" +
+                          "   jaco javaclients.ClientQuery -queryXpath \"//DRIVER/AGENT\"");
+
+         CorbaConnection corbaConnection = new CorbaConnection(args);
+         String qos = "";
+         String passwd = "some";
+         xmlBlaster = corbaConnection.login(loginName, passwd, (BlasterCallback)null, qos);
+
+
+         String xmlKey = "<key oid='' queryType='" + queryType + "'>\n" +
+                            queryString +
+                         "</key>";
+         StopWatch stop = new StopWatch();
+         MessageUnit[] msgArr = null;
+         try {
+            msgArr = xmlBlaster.get(xmlKey, qos);
+            Log.info(ME, "Got " + msgArr.length + " messages for query '" + queryString + "':");
+            for (int ii=0; ii<msgArr.length; ii++) {
+               UpdateKey updateKey = new UpdateKey();
+               updateKey.init(msgArr[ii].xmlKey);
+               Log.dump("UpdateKey", "\n" + updateKey.printOn().toString());
+               Log.dump("content", "\n" + new String(msgArr[ii].content) + "\n");
+            }
+         } catch(XmlBlasterException e) {
+            Log.error(ME, "XmlBlasterException: " + e.reason);
+         }
+
+         corbaConnection.logout(xmlBlaster);
+      }
+      catch (XmlBlasterException e) {
+          Log.error(ME, "Error occurred: " + e.toString());
+          e.printStackTrace();
+      }
+   }
+
+
+   /**
+    */
+   public static void main(String args[])
+   {
+      new ClientQuery(args);
+      Log.exit(ClientQuery.ME, "Good bye");
+   }
+}
