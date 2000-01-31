1f5526d8fc7c2ccc759e25e1f187b5db066a1056
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-01-31 12:03:34 +0000

    Test case for publish() qos
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@714 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/testsuite/org/xmlBlaster/TestPub.java
@@ -0,0 +1,276 @@
+/*------------------------------------------------------------------------------
+Name:      TestPub.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Testing publish()
+Version:   $Id: TestPub.java,v 1.1 2000/01/31 12:03:34 ruff Exp $
+------------------------------------------------------------------------------*/
+package testsuite.org.xmlBlaster;
+
+import org.xmlBlaster.client.*;
+import org.xmlBlaster.util.*;
+import org.xmlBlaster.serverIdl.*;
+import org.xmlBlaster.clientIdl.*;
+import test.framework.*;
+
+
+/**
+ * This client tests the method publish() with its different qos variants. 
+ * <br />
+ * <p>
+ * This client may be invoked multiple time on the same xmlBlaster server,
+ * as it cleans up everything after his tests are done.
+ * <p>
+ * Invoke examples:<br />
+ * <code>
+ *    jaco test.textui.TestRunner testsuite.org.xmlBlaster.TestPub
+ *    jaco test.ui.TestRunner testsuite.org.xmlBlaster.TestPub
+ * </code>
+ */
+public class TestPub extends TestCase implements I_Callback
+{
+   private Server xmlBlaster = null;
+   private static String ME = "Tim";
+   private boolean messageArrived = false;
+
+   private String subscribeOid;
+   private String publishOid = "ReadonlyMessage";
+   private CorbaConnection senderConnection;
+   private String senderName;
+   private String senderContent;
+   private String receiverName;         // sender/receiver is here the same client
+
+   private int numReceived = 0;         // error checking
+   private final String contentMime = "text/xml";
+   private final String contentMimeExtended = "1.0";
+
+   /**
+    * Constructs the TestPub object.
+    * <p />
+    * @param testName  The name used in the test suite
+    * @param loginName The name to login to the xmlBlaster
+    */
+   public TestPub(String testName, String loginName)
+   {
+       super(testName);
+       this.senderName = loginName;
+       this.receiverName = loginName;
+   }
+
+
+   /**
+    * Sets up the fixture.
+    * <p />
+    * Connect to xmlBlaster and login
+    */
+   protected void setUp()
+   {
+      try {
+         senderConnection = new CorbaConnection(); // Find orb
+         String passwd = "secret";
+         String qos = "<qos></qos>";
+         xmlBlaster = senderConnection.login(senderName, passwd, qos, this); // Login to xmlBlaster
+      }
+      catch (Exception e) {
+          Log.error(ME, e.toString());
+          e.printStackTrace();
+      }
+   }
+
+
+   /**
+    * Tears down the fixture.
+    * <p />
+    * cleaning up .... erase() the previous message OID and logout
+    */
+   protected void tearDown()
+   {
+      String xmlKey = "<?xml version='1.0' encoding='ISO-8859-1' ?>\n" +
+                      "<key oid='" + publishOid + "' queryType='EXACT'>\n" +
+                      "</key>";
+      String qos = "<qos></qos>";
+      String[] strArr = null;
+      try {
+         strArr = xmlBlaster.erase(xmlKey, qos);
+      } catch(XmlBlasterException e) { Log.error(ME, "XmlBlasterException: " + e.reason); }
+      if (strArr.length != 1) Log.error(ME, "Erased " + strArr.length + " messages:");
+
+      senderConnection.logout(xmlBlaster);
+   }
+
+
+   /**
+    * TEST: Subscribe to messages with XPATH.
+    * <p />
+    * The returned subscribeOid is checked
+    */
+   public void testSubscribe()
+   {
+      if (Log.TRACE) Log.trace(ME, "Subscribing using XPath syntax ...");
+
+      String xmlKey = "<key oid='" + publishOid + "' queryType='EXACT'>\n</key>";
+      String qos = "<qos></qos>";
+      numReceived = 0;
+      subscribeOid = null;
+      try {
+         subscribeOid = xmlBlaster.subscribe(xmlKey, qos);
+         Log.info(ME, "Success: Subscribe on " + subscribeOid + " done");
+      } catch(XmlBlasterException e) {
+         Log.warning(ME, "XmlBlasterException: " + e.reason);
+         assert("subscribe - XmlBlasterException: " + e.reason, false);
+      }
+      assert("returned null subscribeOid", subscribeOid != null);
+      assertNotEquals("returned subscribeOid is empty", 0, subscribeOid.length());
+   }
+
+
+   /**
+    * TEST: Construct a message and publish it.
+    * <p />
+    * The returned publishOid is checked
+    */
+   public void testPublish(boolean first)
+   {
+      if (Log.TRACE) Log.trace(ME, "Publishing a message ...");
+
+      numReceived = 0;
+      String xmlKey = "<key oid='" + publishOid + "' contentMime='" + contentMime + "' contentMimeExtended='" + contentMimeExtended + "'>\n" +
+                      "   <TestPub-AGENT id='192.168.124.10' subId='1' type='generic'>" +
+                      "   </TestPub-AGENT>" +
+                      "</key>";
+      MessageUnit messageUnit = new MessageUnit(xmlKey, senderContent.getBytes());
+      PublishQosWrapper qosWrapper = new PublishQosWrapper();
+      qosWrapper.setReadonly();
+      String qos = qosWrapper.toXml(); // == "<qos><Readonly /></qos>"
+
+      if (first) {
+         try {
+            publishOid = xmlBlaster.publish(messageUnit, qos);
+            Log.info(ME, "Success: Publishing done, returned oid=" + publishOid);
+         } catch(XmlBlasterException e) {
+            Log.warning(ME, "XmlBlasterException: " + e.reason);
+            assert("publish - XmlBlasterException: " + e.reason, false);
+         }
+         assert("returned publishOid == null", publishOid != null);
+         assertNotEquals("returned publishOid", 0, publishOid.length());
+      }
+      else {
+         try {
+            publishOid = xmlBlaster.publish(messageUnit, qos);
+            assert("Publishing readonly protected message again should not be possible", false);
+         } catch(XmlBlasterException e) {
+            Log.info(ME, "Success: Publishing again throws an exception");
+         }
+      }
+   }
+
+
+   /**
+    * TEST: Construct a message and publish it,<br />
+    * the previous XPath subscription should match and send an update.
+    */
+   public void testPublishAfterSubscribeXPath()
+   {
+      testSubscribe();
+      Util.delay(1000L);                                            // Wait some time for callback to arrive ...
+      assertEquals("numReceived after subscribe", 0, numReceived);  // there should be no Callback
+
+      senderContent = "Yeahh, i'm the new content 1";
+      testPublish(true);
+      waitOnUpdate(5000L);
+      assertEquals("numReceived after publishing", 1, numReceived); // message arrived?
+
+      senderContent = "Yeahh, i'm the new content 2";
+      testPublish(false);
+      Util.delay(1000L);                                            // Wait some time for callback to arrive ...
+      assertEquals("numReceived after publishing", 0, numReceived); // message arrived?
+   }
+
+
+   /**
+    * This is the callback method (I_Callback) invoked from CorbaConnection
+    * informing the client in an asynchronous mode about a new message.
+    * <p />
+    * The raw CORBA-BlasterCallback.update() is unpacked and for each arrived message
+    * this update is called.
+    *
+    * @param loginName The name to whom the callback belongs
+    * @param updateKey The arrived key
+    * @param content   The arrived message content
+    * @param qos       Quality of Service of the MessageUnit
+    */
+   public void update(String loginName, UpdateKey updateKey, byte[] content, UpdateQoS updateQoS)
+   {
+      if (Log.CALLS) Log.calls(ME, "Receiving update of a message ...");
+
+      numReceived += 1;
+
+      assertEquals("Wrong receveiver", receiverName, loginName);
+      assertEquals("Wrong sender", senderName, updateQoS.getSender());
+      assertEquals("Wrong oid of message returned", publishOid, updateKey.getUniqueKey());
+      assertEquals("Message content is corrupted", new String(senderContent), new String(content));
+      assertEquals("Message contentMime is corrupted", contentMime, updateKey.getContentMime());
+      assertEquals("Message contentMimeExtended is corrupted", contentMimeExtended, updateKey.getContentMimeExtended());
+
+      messageArrived = true;
+   }
+
+
+   /**
+    * Little helper, waits until the variable 'messageArrive' is set
+    * to true, or returns when the given timeout occurs.
+    * @param timeout in milliseconds
+    */
+   private void waitOnUpdate(final long timeout)
+   {
+      long pollingInterval = 50L;  // check every 0.05 seconds
+      if (timeout < 50)  pollingInterval = timeout / 10L;
+      long sum = 0L;
+      while (!messageArrived) {
+         try {
+            Thread.currentThread().sleep(pollingInterval);
+         }
+         catch( InterruptedException i)
+         {}
+         sum += pollingInterval;
+         if (sum > timeout) {
+            Log.warning(ME, "Timeout of " + timeout + " occurred");
+            break;
+         }
+      }
+      messageArrived = false;
+   }
+
+
+   /**
+    * Method is used by TestRunner to load these tests
+    */
+   public static Test suite()
+   {
+       TestSuite suite= new TestSuite();
+       String loginName = "Tim";
+       suite.addTest(new TestPub("testPublishAfterSubscribeXPath", loginName));
+       return suite;
+   }
+
+
+   /**
+    * Invoke: jaco testsuite.org.xmlBlaster.TestPub
+    * <p />
+    * Note you need 'jaco' instead of 'java' to start the TestRunner, otherwise the JDK ORB is used
+    * instead of the JacORB ORB, which won't work.
+    * <br />
+    * @deprecated Use the TestRunner from the testsuite to run it:<p />
+    * <code>   jaco -Djava.compiler= test.textui.TestRunner testsuite.org.xmlBlaster.TestPub</code>
+    */
+   public static void main(String args[])
+   {
+      Log.setLogLevel(args);
+      TestPub testSub = new TestPub("TestPub", "Tim");
+      testSub.setUp();
+      testSub.testPublishAfterSubscribeXPath();
+      testSub.tearDown();
+      Log.exit(TestPub.ME, "Good bye");
+   }
+}
+
