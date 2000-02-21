b8ba3cfcd5b1d82522a7725b101625df5e4c572c
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-02-21 10:00:52 +0000

    Framework to use in a browser, containing a simple html based chat demo
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@848 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/http/javascript/callback.js
@@ -0,0 +1,274 @@
+/*------------------------------------------------------------------------------
+Name:      callback.js
+Project:   xmlBlaster.org
+Comment:   Implementing some Javascript callback objects for xmlBlaster
+Author:    ruff@swand.lake.de
+Version:   $Id: callback.js,v 1.1 2000/02/21 10:00:52 ruff Exp $
+------------------------------------------------------------------------------*/
+
+// First define the usual xmlBlaster access methods
+// @see xmlBlaster.idl
+
+var loginName = "";
+var isLoggedIn = false;
+
+/**
+ * Called from controlFrame when the user clicks on Login button
+ */
+function login(login, passwd)
+{
+   loginName = login;
+   top.target = "callbackFrame";
+   top.location.href = "/servlet/CallbackServletDriver?ActionType=login&loginName=" + loginName + "&passwd=" + passwd;
+   log.info("Leaving login...");
+   return true;
+}
+
+
+function logout()
+{
+   loginName = "";
+   isLoggedIn = false;
+   // self.onerror = default???;
+   self.close();
+   self = null;
+}
+
+// @param message An instance of MessageWrapper
+function publish(message)
+{
+   log.error("Publish implementation to xmlBlaster is missing");
+}
+
+function get(xmlKey, qos)
+{
+   log.error("Get implementation to xmlBlaster is missing");
+}
+
+function erase(xmlKey, qos)
+{
+   log.error("Erase implementation to xmlBlaster is missing");
+}
+
+
+/**
+ * Constructor for a XmlKey helper object.
+ * If you have own meta data, add it with the method wrap:
+ * Example: var key = new top.PublishKeyWrapper(null, "text/xml", null);
+ *          key.wrap("<Name id='Joe' />");
+ * @param oid:String The unique message identifier, is optional and will be generated if null
+ * @param contentMime:String The MIME type of the content e.g. "text/xml" or "image/gif"
+ * @param contentMimeExtended:String Use it for whatever, e.g. the version number or parser
+ *        infos for your content set to null if not needed
+ */
+function PublishKeyWrapper(oid, contentMime, contentMimeExtended)
+{
+   if (oid == null)
+      this.oid = '';
+   else
+      this.oid = oid;
+
+   if (contentMime == null)
+      this.contentMime = "text/plain";
+   else
+      this.contentMime = contentMime;
+
+   this.contentMimeExtended = contentMimeExtended;
+   this.wrap = PublishKeyWrapperWrap;
+   this.toXml = PublishKeyWrapperToXml;
+}
+function PublishKeyWrapperToXml()
+{
+   var str='';
+   str += "<key oid='" + this.oid + "'";
+   str += " contentMime='" + this.contentMime + "'";
+   if (this.contentMimeExtended != null)
+      str += " contentMimeExtended='" + this.contentMimeExtended + "'";
+   str += ">\n";
+   str += this.clientTags;
+   str += "\n</key>";
+   log.info(str);
+   return str;
+}
+function PublishKeyWrapperWrap(tags)
+{
+   if (tags == null)
+      this.tags = '';
+   else
+      this.tags = tags;
+}
+
+
+/**
+ * This class encapsulates the Message meta data and unique identifier of a received message.
+ * A typical <b>update</b> key could look like this:<br />
+ * <pre>
+ *     &lt;key oid='4711' contentMime='text/xml'>
+ *        &lt;AGENT id='192.168.124.20' subId='1' type='generic'>
+ *           &lt;DRIVER id='FileProof' pollingFreq='10'>
+ *           &lt;/DRIVER>
+ *        &lt;/AGENT>
+ *     &lt;/key>
+ * </pre>
+ * Note that the AGENT and DRIVER tags are application know how, which you have to supply.<br />
+ * A well designed xml hierarchy of your problem domain is essential for a proper working xmlBlaster
+ * This is exactly the key how it was published from the data source.
+ * Call updateKey.init(xmlKey_literal); to start parsing the received key
+ */
+function UpdateKey(xml)
+{
+   this.oid = null;                 // value from attribute <key oid="...">
+   this.contentMime = null;         // value from attribute <key oid="" contentMime="...">
+   this.contentMimeExtended = null; // value from attribute <key oid="" contentMimeExtended="...">
+
+   //  Analyzing e.g. <key oid='1234' contentMime='text/plain'>...</key>
+
+   this.root = top.Xparse(xml);     // The Javascript DOM tree
+   var keyNode = this.root.contents[0];
+   if (keyNode.name != "key") {
+      log.warning('Key tag is missing in new arrvied message, received an unknown tag &lt;' + keyNode.name + '>');
+      return;
+   }
+   for(attrib in keyNode.attributes) {
+      // log.info('Processing ' + attrib + '="' + keyNode.attributes[attrib] + '" ...');
+      if (attrib == "oid")
+         this.oid = keyNode.attributes[attrib];
+      else if (attrib == "contentMime")
+         this.contentMime = keyNode.attributes[attrib];
+      else if (attrib == "contentMimeExtended")
+         this.contentMimeExtended = keyNode.attributes[attrib];
+   }
+}
+function UpdateQos(xml)
+{
+   this.sender= null;               // Who sent the message (his login name)?
+
+   this.root = top.Xparse(xml);     // The Javascript DOM tree
+   var qosNode = this.root.contents[0];
+   if (qosNode.name != "qos") {
+      log.warning('Qos tag is missing in new arrvied message, received an unknown tag &lt;' + qosNode.name + '>');
+      return;
+   }
+
+   //  Analyzing <qos><sender>Fritz</sender></qos>
+   for (var i = 0; i < qosNode.contents.length; i++) {
+      var tag = qosNode.contents[i];
+      if (qosNode.contents[i].type == "element") {
+         var name = _strip(tag.name);
+         if (name.length < 1)
+            continue;
+         if (name == "sender")
+            this.sender = tag.contents[0].value;
+      }
+   }
+}
+
+
+/**
+ * Create a message object, which contains the xmlBlaster message as string literals
+ *
+ * @param xmlKey:String  The meta data
+ * @param content:String The message itself (binary/octet????!!!)
+ * @param qos:String     Some quality of service infos of this xmlBlaster update
+*/
+function MessageWrapper(xmlKey, content, qos)
+{
+   if (xmlKey != null) {
+      this.xmlKey = xmlKey;
+   }
+   else {
+      var key = new PublishKeyWrapper();
+      this.xmlKey = key.toXml();
+   }
+
+   this.content = content;
+
+   if (qos != null) {
+      this.qos = qos;
+   }
+   else {
+      this.qos = "<qos></qos>";
+   }
+}
+
+//----------------------------------------------------------------------------------------------
+// Create a message object, which contains the new received xmlBlaster message
+// @param xmlKey:UpdateKey  The meta data
+// @param content:String    The message itself (blob/binary/octet???!!!)
+// @param qos:UpdateQos     Some quality of service infos of this xmlBlaster update
+//----------------------------------------------------------------------------------------------
+function UpdateMessageWrapper(xmlKey, content, qos)
+{
+   if (xmlKey == null) {
+      log.error("Please specify an UpdateKey object");
+      return;
+   }
+   this.xmlKey = xmlKey;
+
+   this.content = content;
+
+   if (qos == null) {
+      log.error("Please specify an UpdateQos object");
+      return;
+   }
+   this.qos = qos;
+}
+
+//----------------------------------------------------------------------------------------------
+// add/remove Callback Listener
+// every frame can register itself to receive the callbacks
+//----------------------------------------------------------------------------------------------
+var listenerList = new Array();
+
+function addUpdateListener(listenerFrame)
+{
+   if (listenerFrame.update == null) {
+      log.error("Frame has no method update()");
+      return;
+   }
+   listenerList[listenerList.length] = listenerFrame;
+   log.info("Added listener frame");
+   return;
+}
+
+function removeUpdateListener(listenerFrame)
+{
+   if (listenerList == null) {
+      log.error("listenerList is empty");
+      return;
+   }
+   var tmpArr = new Array(listenerList.length);
+   for (var ii=0; ii < listenerList.length; ++ii) {
+      if (listenerList[ii] == listenerFrame)
+         continue;
+      tmpArr[ii] = listenerList[ii];
+   }
+   log.info("Removed listener frame");
+   return;
+}
+
+function fireMessageUpdateEvent(message)
+{
+   for (var ii=0; ii < listenerList.length; ++ii) {
+      listenerList[ii].update(message);
+   }
+}
+
+//----------------------------------------------------------------------------------------------
+// This is called by the updateFrame, delivering an object containing
+// the xmlKey, content, qos
+//----------------------------------------------------------------------------------------------
+function update(message)
+{
+   var key = new UpdateKey(xml);
+   // var qos = new UpdateKey(xml);
+
+   log.info("Received update message, dispatching it to " + listenerList.length + " frames");
+   for (var ii=0; ii < listenerList.length; ++ii) {
+      listenerList[ii].update(message);
+   }
+   return 0;
+}
+
+
+
---
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/http/javascript/util.js
@@ -0,0 +1,236 @@
+/*------------------------------------------------------------------------------
+Name:      util.js
+Project:   xmlBlaster.org
+Comment:   Implementing some Javascript utility objects
+Author:    ruff@swand.lake.de
+Version:   $Id: util.js,v 1.1 2000/02/21 10:00:52 ruff Exp $
+------------------------------------------------------------------------------*/
+
+/**
+ * Check the browser version
+ */
+var isNetscape;
+var isExplorer;
+if (navigator.appVersion.substring(0,1) < "4") {
+   alert("Please use a browser version higher than 4.x\n Let's try it nevertheless.");
+   isNetscape = (navigator.appName.indexOf("Netscape") != -1);
+   isExplorer = (navigator.appName.indexOf("Microsoft") != -1);
+}
+
+/**
+ * Get some browser infos formatted as HTML
+ */
+function getBrowserInfo()
+{
+   var appName     = "browser name: <i>" + navigator.appName + "</i><br>";
+   var appVersion  = "browser version: <i>" + navigator.appVersion + "</i><br>";
+   var platform    = "operating system: <i>" + navigator.platform + "</i><br>";
+   return "<hr>" + appName + appVersion + platform;
+}
+
+/**
+ * Logging of errors/warnings/infos
+ * Example:
+ *    log.error("The variable listenerList is empty.");
+ *    log.warning("Performance over internet is slow.");
+ *    log.info("Login granted.");
+*/
+var logWindow = null;         // the window handle - a popup with the log output table
+var MAX_LOG_ENTRIES = 60;     // max number of rows in window
+var levelColor = new Array(); // every log level is displayed in a characteristic color
+levelColor["ERROR"] = "red";
+levelColor["WARNING"] = "yellow";
+levelColor["INFO"] = "green";
+
+// The log object, containing infos about one logging output
+function logObject(level_, codePos_, text_)
+{
+   this.level = level_;
+   this.codePos = codePos_;
+   this.text = text_;
+}
+var logEntries = new Array();
+
+function internal_(codePos, str)
+{
+   logToWindow("ERROR", codePos, str);
+}
+
+function error_(str)
+{
+   var codePos = error_.caller.toString().substring(10, error_.caller.toString().indexOf(")") + 1);
+   logToWindow("ERROR", codePos, str);
+   // alert("ERROR in " + codePos + ": " + str);
+}
+function warning_(str)
+{
+   var codePos = warning_.caller.toString().substring(10, warning_.caller.toString().indexOf(")") + 1);
+   logToWindow("WARN", codePos, str);
+}
+function info_(str)
+{
+   var codePos = info_.caller.toString().substring(10, info_.caller.toString().indexOf(")") + 1);
+   logToWindow("INFO", codePos, str);
+}
+
+// Catch and handle browser errors
+function catchError(text, url, row)
+{
+   if (url.indexOf("http://") != -1) { // shorten the URL string
+      url = url.slice(7);
+      var ind = url.indexOf("/");
+      if (ind != -1) {                 // shorten the URL string again
+         url = url.slice(ind+1);
+      }
+   }
+   log.internal(url + ": " + row, text);
+   return true; // Suppress errors from browser
+}
+// Set event handler, to catch the internal errors as well
+top.onerror = catchError;
+for (var jj=0; jj<top.frames.length; jj++) {
+   top.frames[ii].onerror = catchError;
+}
+
+
+// This function logs to the status bar of netscape/explorer
+function logToStatusBar(level, codePos, strText)
+{
+   var text = codePos + ": " + str;
+   if(window.statusbar.visible == true) {
+      window.defaultStatus = str;
+      window.status = str;
+   }
+   else
+      alert(str);
+}
+
+// This function logs to a pop up window
+// Every logging output is a colored row in a table.
+function logToWindow(level, codePos, text)
+{
+   if (text.length <= 0)
+      return;
+
+   if (logWindow == null || logWindow.closed) {
+      logWindow = window.open("","XmlBlasterLog",
+                  "content=no-cache,scrollbars=yes,resizable=yes," +
+                  "width=500,height=400,screenX=300,screenY=200," +
+                  "menubar=no,status=no,toolbar=no,titlebar=no" +
+                  "alwaysraised=yes,dependent=yes");
+   }
+   var d = logWindow.document;
+
+   if (logEntries.length >= MAX_LOG_ENTRIES) {
+      // TODO: only half the logs, not empty them totally
+      logEntries[logEntries.length] = new logObject(level, codePos, text);
+   }
+
+   logEntries[logEntries.length] = new logObject(level, codePos, text);
+
+   var headerStr =
+      '<HTML>' +
+      '<HEAD>' +
+      '   <link REL="stylesheet" type="text/css" href="xmlBlaster.css">' +
+      '   <title>Log your code!</title>' +
+      '</HEAD>' +
+      '<BODY>' +
+      '<CENTER><H3>Logging output from XmlBlaster Javascript</H3></CENTER>';
+   d.writeln(headerStr);
+
+   var tableStr =
+         "<TABLE NAME='ChatTable' BORDER='2' WIDTH='100%'>" +
+         "   <TR>" +
+         "      <TD WIDTH='10%'>" +
+         "Level" +
+         "      </TD>" +
+         "      <TD WIDTH='10%'>" +
+         "Where" +
+         "      </TD>" +
+         "      <TD WIDTH='80%'>" +
+         "Logging text" +
+         "      </TD>" +
+         "   </TR>";
+   for (var ii=0; ii<logEntries.length; ii++) {
+      tableStr +=
+         "   <TR>" +
+         "      <TD BGCOLOR='" + levelColor[logEntries[ii].level] + "'>" +
+         logEntries[ii].level +
+         "      &nbsp;</TD>" +
+         "      <TD>" +
+         logEntries[ii].codePos +
+         "      &nbsp;</TD>" +
+         "      <TD>" +
+         logEntries[ii].text +
+         "      &nbsp;</TD>" +
+         "   </TR>";
+   }
+   tableStr += "</TABLE>";
+
+   d.writeln(tableStr);
+   d.writeln('</BODY></HTML>');
+   d.close();
+   logWindow.focus();
+   return;
+}
+
+// The log handler, use this to invoke your logging output
+function logHandler()
+{
+   this.internal = internal_; // errors from browser are caught
+   this.error = error_;       // log.error() output
+   this.warning = warning_;
+   this.info = info_;
+}
+
+// The log handler, use this to invoke your logging output
+// Example:
+//    log.warning("Performance over internet is slow.");
+var log = new logHandler();
+
+
+
+/*
+   Copyright 1997 Stefan Mintert / Addison-Wesley
+   Use like this:
+   var varA = "Hello, ";
+   var varB = "world!";
+   "http://localhost" + var_encode("varA", "varB");
+   // the variable names are passed as a string
+   // The values are URL-encoded: http://localhost#varA%3DHello%2C%20%3BvarB%3Dworld%21
+   // %21 is hex value for '!'
+   // %3B is hex value for ';'
+   // %3D is hex value for '='
+   // %2C is hex value for ','
+   // %26 is hex value for '&'
+   // %20 is hex value for ' '
+   // http://www.xy.com/?flag=jfl&frame=yes&id=38aa6c5ccece7
+*/
+function var_encode()
+{
+   var hashstring ="";
+   var wert
+
+   for (i = 0; i < arguments.length; i++)
+   {
+      hashstring += arguments[i]+"=";
+      wert = eval(arguments[i]);
+      hashstring += wert.toString();
+      if (i < arguments.length-1)
+        hashstring += ";";
+   }
+   return "#"+escape(hashstring);
+}
+
+function var_decode()
+{
+   var hashstring = self.location.hash;
+   var et_Position, name_wert, gleich_Position, name, wert;
+
+
+   if ( hashstring.charAt(0) == "#")
+     hashstring = hashstring.substr(1,hashstring.length);
+
+   eval(unescape(hashstring));
+
+}
