c0e9d8f6d60b289b750196177617dc6690b47790
jsrbirch <jsrbirch@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-02-23 06:01:29 +0000

    Initial Upload
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@877 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/build.bat
@@ -0,0 +1,26 @@
+@echo off
+
+echo xmlBlaster Build System
+echo -------------------
+
+if "%JAVA_HOME%" == "" goto error
+
+set LOCALCLASSPATH=%CLASSPATH%;%JAVA_HOME%\lib\tools.jar;.\lib\ant.jar:lib\xml.jar:.\lib\idl.jar:.\lib\jacorb.jar:.\lib\omquery.jar:.\lib\xtdash.jar:.\servlet-2.0.jar:.\lib\test.jar
+
+echo Building with classpath %LOCALCLASSPATH%
+
+echo Starting Ant...
+
+%JAVA_HOME%\bin\java.exe -Dant.home=. -classpath "%LOCALCLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5
+
+goto end
+
+:error
+
+echo "ERROR: JAVA_HOME not found in your environment."
+echo "Please, set the JAVA_HOME variable in your environment to match the"
+echo "location of the Java Virtual Machine you want to use."
+
+:end
+
+set LOCALCLASSPATH=
---
+++ xmlBlaster/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+CLASSPATH=lib/ant.jar:lib/xml.jar:lib/idl.jar:lib/jacorb.jar:lib/omquery.jar:lib/xtdash.jar:servlet-2.0.jar:lib/test.jar:lib/tools.jar
+
+JAVA_HOME=/usr/jdk1.2.2
+
+$JAVA_HOME/bin/java -Dant.home=$XMLBLASTER_HOME -classpath $CLASSPATH org.apache.tools.ant.Main $@
+
