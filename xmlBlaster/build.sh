679204d7a7d89f88def68f912bd02390ae4e082d
jsrbirch <jsrbirch@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-02-23 06:05:40 +0000

    *** empty log message ***
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@879 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/build.sh
+++ xmlBlaster/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CLASSPATH=lib/ant.jar:lib/xml.jar:lib/idl.jar:lib/jacorb.jar:lib/omquery.jar:lib/xtdash.jar:servlet-2.0.jar:lib/test.jar:lib/tools.jar
+CLASSPATH=lib/ant.jar:lib/xml.jar:lib/idl.jar:lib/jacorb.jar:lib/omquery.jar:lib/xtdash.jar:lib/servlet-2.0.jar:lib/test.jar:lib/tools.jar
 
 JAVA_HOME=/usr/jdk1.2.2
 
