64b81ef8a4d3ad58aa2c41b25273efe2db21f761
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-03-03 15:51:10 +0000

    Extended tests
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@1001 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/protocol/http/BrowserTest.java
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/http/BrowserTest.java
@@ -3,7 +3,7 @@ Name:      BrowserTest.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Handling callback over http
-Version:   $Id: BrowserTest.java,v 1.2 2000/02/21 11:30:25 ruff Exp $
+Version:   $Id: BrowserTest.java,v 1.3 2000/03/03 15:51:10 ruff Exp $
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.protocol.http;
 
@@ -19,15 +19,18 @@ import org.xmlBlaster.util.*;
  * keeping a permanent http connection
  * Use this class to test the behavior of your browser.
  * <br />
- * Not you need to set the boolean variables multipart and polling
- * to true/false and recompile to try out the three different variants
- * of callback!
+ * <pre>
+ * http://localhost/servlets/BrowserTest?mode=multi
+ * http://localhost/servlets/BrowserTest?mode=push
+ * http://localhost/servlets/BrowserTest?mode=poll
+ * </pre>
  * @author ruff@swand.lake.de
  */
 public class BrowserTest extends HttpServlet
 {
    private final String ME = "BrowserTest";
    private int globalVal = 1;
+   private String mode = "multi";
 
 
    /**
@@ -57,13 +60,13 @@ public class BrowserTest extends HttpServlet
 
       HttpSession session = req.getSession(true);
       String sessionId = req.getRequestedSessionId();
-      String loginName = req.getParameter("login");    // "Joe";
-      String password = req.getParameter("password");  // "secret";
+      String loginName = Util.getParameter(req, "login", null);    // "Joe";
+      String password = Util.getParameter(req, "password", null);  // "secret";
       Log.info(ME, "Entering BrowserTest servlet for '" + loginName + "', sessionId=" + sessionId);
 
       StringBuffer retStr = new StringBuffer();
       try {
-         String actionType = req.getParameter("ActionType");
+         String actionType = Util.getParameter(req, "ActionType", null);
          if (actionType!=null && actionType.equals("Login")) {
             Log.info(ME, "Login pressed ...");
 
@@ -96,28 +99,110 @@ public class BrowserTest extends HttpServlet
    public void doGet(HttpServletRequest req, HttpServletResponse res)
                                  throws ServletException, IOException
    {
-      // set to true to selecting the push method you want to use:
-      boolean multipart = true;
-      boolean polling = false;
+      Log.info(ME, "Entering doGet()");
+      String tmp = Util.getParameter(req, "mode", null);
+      if (tmp != null) {
+         mode = tmp;
+         Log.info(ME, "Testing mode=" + mode);
+      }
 
-      if (multipart) {
-         /* Every line which is sent to the browser overwrites the former one
-            Problems: (Linux/netscape)
-            1. The watch-wait cursor is displayed, until the doGet() leaves.
-            2. Resizing the browser window doesn't resize the content.
-         */
-         ServletOutputStream out = res.getOutputStream();
+      try {
 
-         res.setContentType("multipart/x-mixed-replace;boundary=End");
-         out.println();         // An empty line an
-         out.println("--End");  // the end marker (from the boundary above)
-                                // are finishing the multipart
+         if (mode.equals("multi")) {
+            /* Every line which is sent to the browser overwrites the former one
+               Problems: (Linux/netscape)
+               1. The watch-wait cursor is displayed, until the doGet() leaves.
+               2. Resizing the browser window doesn't resize the content.
+            */
+            ServletOutputStream out = res.getOutputStream();
 
-         int val = 1;
+            res.setContentType("multipart/x-mixed-replace;boundary=End");
+            out.println();         // An empty line an
+            out.println("--End");  // the end marker (from the boundary above)
+                                   // are finishing the multipart
 
-         while (true) {
-            out.println("Content-Type: text/html");
+            int val = 1;
+
+            while (true) {
+               Log.info(ME, "Sending next multipart");
+
+               out.println("Content-Type: text/html");
+               out.println();
+
+               out.println("<HTML>");
+               out.println("<HEAD>");
+               out.println("<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>");
+               out.println("<meta http-equiv='Pragma' content='no-cache'>");
+               out.println("<meta http-equiv='Expires' content='Tue, 31 Dec 1997 23:59:59 GMT'>");
+               out.println("<TITLE>Hello World</TITLE>");
+               out.println("</HEAD>");
+               out.println("<BODY>");
+               out.println("<BIG>Hello World - GET - multipart No." + val++ + "</BIG>");
+               out.println("<A HREF='http://www.doubleslash.de'>DOUBLESLASH</A>");
+               /*
+               out.println("<script language='JavaScript'>");
+               out.println("alert('Konrad Konradowitsch');");
+               out.println("</script>");
+               */
+               out.println("</BODY></HTML>");
+
+               out.println();
+               out.println("--End");
+               out.flush();
+
+               try { Thread.currentThread().sleep(2000); } catch(Exception e) {}
+               if (val > 10)
+                  break;
+            }
+            /* deletes the last page
             out.println();
+            out.println("--End");
+            out.flush();
+            */
+            out.close();
+         }
+         else if (mode.equals("poll")) {
+            /* Every line which is sent to the browser overwrites the former one
+               Problems: (Linux/netscape)
+               None, but it is a polling mode.
+            */
+            PrintWriter out = res.getWriter();
+            res.setContentType("text/html");
+            if (globalVal % 2 == 0) {
+               globalVal++;
+               Log.info(ME, "SC_NO_CONTENT globalVal=" + globalVal);
+               res.setStatus(HttpServletResponse.SC_NO_CONTENT);
+               return;
+            }
+            Log.info(ME, "globalVal=" + globalVal);
+            out.println("<HTML>");
+            out.println("<HEAD>");
+            out.println("<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>");
+            out.println("<meta http-equiv='Pragma' content='no-cache'>");
+            // the polling variant every 2 sec:
+            out.println("<meta http-equiv='refresh' content='2;URL=BrowserTest'>");
+            out.println("<TITLE>Hello World</TITLE>");
+            out.println("</HEAD>");
+            out.println("<BODY>");
+            out.println("<BIG>Hello World - GET - Polling No." + globalVal++ + "</BIG>");
+            out.println("</BODY></HTML>");
+            out.flush();
+            out.close();
+         }
+         else if (mode.equals("push")) {
+            /*
+               Problems: (Linux/netscape)
+               1. The watch-wait cursor is displayed, until the doGet() leaves.
+               2. Resizing the browser window doesn't resize the content.
+               3. Every line which is sent again to the browser is written after
+                  the previous one resulting in a list of ten rows.
+            */
+            PrintWriter out = res.getWriter();
+            res.setContentType("text/html");
+            res.setHeader("Cache-Control", "no-cache");
+            res.setHeader("Expires","1 Jan 2000");
+
+            int val = 1;
 
             out.println("<HTML>");
             out.println("<HEAD>");
@@ -127,80 +212,55 @@ public class BrowserTest extends HttpServlet
             out.println("<TITLE>Hello World</TITLE>");
             out.println("</HEAD>");
             out.println("<BODY>");
-            out.println("<BIG>Hello World - GET - multipart No." + val++ + "</BIG>");
+            while (true) {
+               out.println("<BIG>Hello World - GET - Simple server push No." + val++ + "</BIG>");
+               out.println("<P>"); // This newline forces a refresh everytime!
+               out.flush();
+               try { Thread.currentThread().sleep(2000); } catch(Exception e) {}
+               if (val > 10)
+                  break;
+            }
             out.println("</BODY></HTML>");
-
-            out.println();
-            out.println("--End");
             out.flush();
-
-            try { Thread.currentThread().sleep(2000); } catch(Exception e) {}
-            if (val > 10)
-               break;
+            out.close();
          }
-         /* deletes the last page
-         out.println();
-         out.println("--End");
-         out.flush();
-         */
-         out.close();
-      }
-      else if (polling) {
-         /* Every line which is sent to the browser overwrites the former one
-            Problems: (Linux/netscape)
-            None, but it is a polling mode.
-         */
-         PrintWriter out = res.getWriter();
-         res.setContentType("text/html");
-         out.println("<HTML>");
-         out.println("<HEAD>");
-         out.println("<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>");
-         out.println("<meta http-equiv='Pragma' content='no-cache'>");
-         // the polling variant every 2 sec:
-         out.println("<meta http-equiv='refresh' content='2;URL=BrowserTest'>");
-         out.println("<TITLE>Hello World</TITLE>");
-         out.println("</HEAD>");
-         out.println("<BODY>");
-         out.println("<BIG>Hello World - GET - Polling No." + globalVal++ + "</BIG>");
-         out.println("</BODY></HTML>");
-         out.flush();
-         out.close();
-      }
-      else {
-         /*
-            Problems: (Linux/netscape)
-            1. The watch-wait cursor is displayed, until the doGet() leaves.
-            2. Resizing the browser window doesn't resize the content.
-            3. Every line which is sent again to the browser is written after
-               the previous one resulting in a list of ten rows.
-         */
-         PrintWriter out = res.getWriter();
-         res.setContentType("text/html");
-         res.setHeader("Cache-Control", "no-cache");
-         res.setHeader("Expires","1 Jan 2000");
+         else if (mode.equals("pushall")) {
+            /*
+               Problems: (Linux/netscape)
+               see push
+               The behavior is the same as with 'push', sending the HTML header doesn't change
+               anything.
+            */
+            PrintWriter out = res.getWriter();
+            res.setContentType("text/html");
+            res.setHeader("Cache-Control", "no-cache");
+            res.setHeader("Expires","1 Jan 2000");
 
-         int val = 1;
+            int val = 1;
 
-         out.println("<HTML>");
-         out.println("<HEAD>");
-         out.println("<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>");
-         out.println("<meta http-equiv='Pragma' content='no-cache'>");
-         out.println("<meta http-equiv='Expires' content='Tue, 31 Dec 1997 23:59:59 GMT'>");
-         out.println("<TITLE>Hello World</TITLE>");
-         out.println("</HEAD>");
-         out.println("<BODY>");
-         while (true) {
-            out.println("<BIG>Hello World - GET - Simple server push No." + val++ + "</BIG>");
-            out.println("<P>"); // This newline forces a refresh everytime!
-            out.flush();
-            try { Thread.currentThread().sleep(2000); } catch(Exception e) {}
-            if (val > 10)
-               break;
+            while (true) {
+               out.println("<HTML>");
+               out.println("<HEAD>");
+               out.println("<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>");
+               out.println("<meta http-equiv='Pragma' content='no-cache'>");
+               out.println("<meta http-equiv='Expires' content='Tue, 31 Dec 1997 23:59:59 GMT'>");
+               out.println("<TITLE>Hello World</TITLE>");
+               out.println("</HEAD>");
+               out.println("<BODY>");
+               out.println("<BIG>Hello World - GET - Simple server push No." + val++ + "</BIG>");
+               out.println("<P>"); // This newline forces a refresh everytime!
+               out.println("</BODY></HTML>");
+               out.flush();
+               try { Thread.currentThread().sleep(2000); } catch(Exception e) {}
+               if (val > 10)
+                  break;
+            }
+            out.close();
          }
-         out.println("</BODY></HTML>");
-         out.flush();
-         out.close();
+         Log.info(ME, "doGet() done");
+      }
+      catch(Exception e) {  // if browser closes in multipart: java.io.IOException
+         Log.error(ME, "doGet() failed, " + e.toString());
       }
    }
-
 }
