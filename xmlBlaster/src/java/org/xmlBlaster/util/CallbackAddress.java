6598d3a020827cac8cee988a546869d5b79575c5
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-05-17 07:01:52 +0000

    Container for address and protocol
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@1466 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/util/CallbackAddress.java
@@ -0,0 +1,101 @@
+/*------------------------------------------------------------------------------
+Name:      CallbackAddress.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Holding callback address string and protocol string
+Version:   $Id: CallbackAddress.java,v 1.1 2000/05/17 07:01:52 ruff Exp $
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.util;
+
+
+/**
+ * Helper class holding callback address string and protocol string.
+ * <p />
+ */
+public class CallbackAddress
+{
+   private String address;
+   private String type;
+
+   /**
+    * @param type    The protocol type, e.g. "IOR", "EMAIL", "XML-RPC"
+    */
+   public CallbackAddress(String type)
+   {
+      this.type = type;
+   }
+
+
+   /**
+    * @param type    The protocol type, e.g. "IOR", "EMAIL", "XML-RPC"
+    * @param address A callback address for your client, suitable to the protocol
+    */
+   public CallbackAddress(String type, String address)
+   {
+      this.type = type;
+      setAddress(address);
+   }
+
+
+   /**
+    * Set the callback address, it should fit to the protocol-type.
+    *
+    * @param address The callback address, e.g. "et@mars.univers"
+    */
+   public final void setAddress(String address)
+   {
+      this.address = address;
+   }
+
+
+   /**
+    * Returns the address.
+    * @return e.g. "IOR:00001100022...."
+    */
+   public final String getAddress()
+   {
+      return address;
+   }
+
+
+   /**
+    * Returns the protocol type.
+    * @return e.g. "EMAIL" or "IOR"
+    */
+   public final String getType()
+   {
+      return type;
+   }
+
+
+   /**
+    * Dump state of this object into a XML ASCII string.
+    */
+   public final String toXml()
+   {
+      return toXml((String)null);
+   }
+
+
+   /**
+    * Dump state of this object into a XML ASCII string.
+    * <br>
+    * @param extraOffset indenting of tags for nice output
+    * @return The xml representation
+    */
+   public final String toXml(String extraOffset)
+   {
+      StringBuffer sb = new StringBuffer();
+      String offset = "\n   ";
+      if (extraOffset == null) extraOffset = "";
+      offset += extraOffset;
+
+      sb.append(offset + "<callback type='").append(getType()).append("'>");
+      sb.append(offset + "   ").append(getAddress());
+      sb.append(offset + "</callback>");
+
+      return sb.toString();
+   }
+}
+
+
