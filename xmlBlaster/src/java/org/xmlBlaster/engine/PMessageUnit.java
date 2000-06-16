858a35fa35750fb3968a7f45addf2aeec6ec3cc4
kron <kron@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-06-16 17:44:53 +0000

    Has oid by RE searching.
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@1734 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/engine/PMessageUnit.java
+++ xmlBlaster/src/java/org/xmlBlaster/engine/PMessageUnit.java
@@ -3,16 +3,20 @@ Name:      PMessageUnit.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Ccapsulate a MessageUnit and QoS for persistence and engine 
-Version:   $Id: PMessageUnit.java,v 1.5 2000/06/13 17:50:29 kron Exp $
+Version:   $Id: PMessageUnit.java,v 1.6 2000/06/16 17:44:53 kron Exp $
 Author:    manuel.kron@gmx.net 
 ------------------------------------------------------------------------------*/
 
 package org.xmlBlaster.engine;
+import java.io.*;
   
 import org.xmlBlaster.protocol.corba.serverIdl.MessageUnit;
 import org.xmlBlaster.engine.xml2java.PublishQoS;
+import org.xmlBlaster.util.Log;
 
-public class PMessageUnit
+import gnu.regexp.*;
+
+public class PMessageUnit implements Serializable
 {
    private static final String ME = "PMessageUnit";
    public final MessageUnit msgUnit; 
@@ -20,6 +24,7 @@ public class PMessageUnit
    public final String      sender;
    public final long        timeStamp;
    public long        size = 0L;
+   public String oid = "";
 
    public PMessageUnit(MessageUnit mu,PublishQoS qos, String send)
    {
@@ -29,6 +34,36 @@ public class PMessageUnit
       sender    = send;
      
       size = mu.xmlKey.length() + mu.content.length  + qos.size + 2200;
+      oid = getOid();
+
+   }
+
+   private String getOid()
+   {
+     RE expression = null;
+     String oid = null;
+     try{
+        expression = new RE("oid=(\'|\"|\\s)(.*)(\'|\")");
+        REMatch match = expression.getMatch(msgUnit.xmlKey);
+        if(match != null)
+        {
+            /** matches OID pure */
+            RE re = new RE("[^oid=\'\"]");
+            REMatch[] matches = re.getAllMatches(match.toString());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < matches.length; i++) {
+              sb.append(matches[i]);
+            }
+            oid = sb.toString();
+            
+         }else{
+            Log.error(ME,"Invalid xmlKey.");
+         }   
+            
+      }catch(REException e){
+        Log.error(ME,"Can't create RE."+e.toString());
+      }
+     return oid; 
    }
 
 }
