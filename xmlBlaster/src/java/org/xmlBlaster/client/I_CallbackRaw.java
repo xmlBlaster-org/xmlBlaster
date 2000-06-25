209a506d2c84c6de5596f194d5096aec8ba34e7c
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-06-25 18:25:18 +0000

    Needed for InvocationRecorder
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@1796 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/client/I_CallbackRaw.java
@@ -0,0 +1,32 @@
+/*------------------------------------------------------------------------------
+Name:      I_CallbackRaw.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Helper to easy get the callback messages
+Version:   $Id: I_CallbackRaw.java,v 1.1 2000/06/25 18:25:18 ruff Exp $
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.client;
+
+
+/**
+ * This is a little helper class is the same as the CORBA generated
+ * <pre>
+ *    org.xmlBlaster.protocol.corba.clientIdl.BlasterCallbackOperations
+ * </pre>
+ * but it is independent of the protocol type.
+ * <p>
+ * Its purpose is to deliver Java clients the asynchronous message updates
+ * through the update() method.
+ * @author ruff@swand.lake.de
+ */
+public interface I_CallbackRaw
+{
+   /**
+    * This is the callback method invoked from xmlBlaster server
+    * informing the client in an asynchronous mode about a new message.
+    * <p />
+    * @param msgUnitArr Array of MessageUnit, containing xmlKey,content,qos
+    */
+   public void update(org.xmlBlaster.engine.helper.MessageUnit[] msgUnitArr);
+}
+
