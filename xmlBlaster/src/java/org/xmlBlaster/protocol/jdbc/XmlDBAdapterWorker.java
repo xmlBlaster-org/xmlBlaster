1452877b5a1d2f73d1f3ceec228c4f8d78def955
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-07-11 18:07:23 +0000

    Bugfix for notify=false
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@1934 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/protocol/jdbc/XmlDBAdapterWorker.java
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/jdbc/XmlDBAdapterWorker.java
@@ -4,7 +4,7 @@
  * Project:   xmlBlaster.org
  * Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
  * Comment:   The thread that does the actual connection and interaction
- * Version:   $Id: XmlDBAdapterWorker.java,v 1.13 2000/07/11 08:53:15 ruff Exp $
+ * Version:   $Id: XmlDBAdapterWorker.java,v 1.14 2000/07/11 18:07:23 ruff Exp $
  * ------------------------------------------------------------------------------
  */
 package org.xmlBlaster.protocol.jdbc;
@@ -64,20 +64,23 @@ public class XmlDBAdapterWorker extends Thread {
    {
       XmlDBAdapter adap = new XmlDBAdapter(content, namedPool);
       MessageUnit[] msgArr = adap.query();
-      if (msgArr.length > 0) {
-         PublishKeyWrapper key = new PublishKeyWrapper("__sys_jdbc."+ME, "text/xml", "SQLQuery");
-         PublishQosWrapper qos = new PublishQosWrapper(new Destination(cust));
-         msgArr[0].xmlKey = key.toXml();
-         msgArr[0].qos = qos.toXml();
-      }
       try {
-         String      oid = callback.publish(msgArr[0]);
-         if (Log.DUMP) Log.plain("Delivered Results...\n" + new String(content));
+         if (msgArr.length > 0) {
+            PublishKeyWrapper key = new PublishKeyWrapper("__sys_jdbc."+ME, "text/xml", "SQLQuery");
+            PublishQosWrapper qos = new PublishQosWrapper(new Destination(cust));
+            msgArr[0].xmlKey = key.toXml();
+            msgArr[0].qos = qos.toXml();
+            String  oid = callback.publish(msgArr[0]);
+            if (Log.DUMP) Log.plain("Delivered Results...\n" + new String(content));
+         }
+         else
+            if (Log.TRACE) Log.trace(ME, "No result message returned to client");
       }
       catch (XmlBlasterException e) {
          Log.error(ME, "Exception in notify: " + e.reason);
       }
       catch (Exception e) {
+         e.printStackTrace();
          Log.error(ME, "Exception in notify: " + e);
       }
    }
