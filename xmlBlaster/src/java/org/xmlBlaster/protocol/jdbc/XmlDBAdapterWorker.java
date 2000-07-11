37745dcdfb3b539fd3b68c63f38fdc33b160dab3
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-07-11 08:53:15 +0000

    Changed to be only a worker thread, the query is done in XmlDBAdatper.
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@1919 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/protocol/jdbc/XmlDBAdapterWorker.java
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/jdbc/XmlDBAdapterWorker.java
@@ -4,7 +4,7 @@
  * Project:   xmlBlaster.org
  * Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
  * Comment:   The thread that does the actual connection and interaction
- * Version:   $Id: XmlDBAdapterWorker.java,v 1.12 2000/07/09 13:55:16 ruff Exp $
+ * Version:   $Id: XmlDBAdapterWorker.java,v 1.13 2000/07/11 08:53:15 ruff Exp $
  * ------------------------------------------------------------------------------
  */
 package org.xmlBlaster.protocol.jdbc;
@@ -32,7 +32,7 @@ import java.io.OutputStream;
 import java.io.IOException;
 
 /**
- * For every database access, an instance of this class does the work in a dedicated thread. 
+ * For every database access, an instance of this class does the work in a dedicated thread.
  */
 public class XmlDBAdapterWorker extends Thread {
 
@@ -43,7 +43,7 @@ public class XmlDBAdapterWorker extends Thread {
    private NamedConnectionPool   namedPool = null;
 
    /**
-    * Create the worker instance to handle a single RDBMS request. 
+    * Create the worker instance to handle a single RDBMS request.
     * @param cust       The sender of the SQL message
     * @param content    The SQL statement
     * @param callback   Interface to publish the XML based result set
@@ -58,120 +58,20 @@ public class XmlDBAdapterWorker extends Thread {
    }
 
    /**
-    * Parse the XML encoded SQL statement. 
+    * Query the database.
     */
-   private XmlDocument createDocument() throws Exception {
-      XmlDocument          document = null;
-      ByteArrayInputStream bais = new ByteArrayInputStream(content);
-
-      try {
-         document = XmlDocument.createXmlDocument(bais, false);
-      }
-      catch (Exception e) {
-         Log.warning(ME, "Exception in create: " + e);
-
-         throw (e);
-      }
-
-      return document;
-   }
-
-   /**
-    * Query the database. 
-    */
-   private XmlDocument queryDB(ConnectionDescriptor descriptor) throws XmlBlasterException
+   public void run()
    {
-      if (Log.CALLS) Log.calls(ME, "Entering queryDB() ...");
-      Connection  conn = null;
-      Statement   stmt = null;
-      ResultSet   rs = null;
-      XmlDocument doc = null;
-
-      try {
-         conn =  namedPool.reserve(descriptor.getUrl(), descriptor.getUsername(), descriptor.getPassword()); // using default connection pool properties
-         stmt = conn.createStatement();
-
-         String   command = descriptor.getCommand();
-
-         try {
-            if (descriptor.getInteraction().equalsIgnoreCase("update")) {
-               if (Log.TRACE) Log.trace(ME, "Trying DB update '" + command + "' ...");
-
-               int   rowsAffected = stmt.executeUpdate(command);
-
-               doc = createUpdateDocument(rowsAffected, descriptor);
-            }
-            else {
-               if (Log.TRACE) Log.trace(ME, "Trying SQL query '" + command + "' ...");
-               rs = stmt.executeQuery(command);
-               doc =
-                  DBAdapterUtils.createDocument(descriptor.getDocumentrootnode(),
-                                                descriptor.getRowrootnode(),
-                                                descriptor.getRowlimit(), rs);
-            }
-         } finally {
-            if (rs!=null) rs.close();
-            if (stmt!=null) stmt.close();
-            if (conn!=null) namedPool.release(descriptor.getUrl(), descriptor.getUsername(), descriptor.getPassword(), conn);
-            if (Log.TRACE) Log.trace(ME, "Query successful done, connection released");
-         }
+      XmlDBAdapter adap = new XmlDBAdapter(content, namedPool);
+      MessageUnit[] msgArr = adap.query();
+      if (msgArr.length > 0) {
+         PublishKeyWrapper key = new PublishKeyWrapper("__sys_jdbc."+ME, "text/xml", "SQLQuery");
+         PublishQosWrapper qos = new PublishQosWrapper(new Destination(cust));
+         msgArr[0].xmlKey = key.toXml();
+         msgArr[0].qos = qos.toXml();
       }
-      catch (SQLException e) {
-         Log.warning(ME, "Exception in query '" + descriptor.getCommand() + "' : " + e);
-         throw new XmlBlasterException(ME, e.getMessage());
-      }
-
-      return doc;
-   }
-
-   /**
-    * @param rowsAffected
-    * @param descriptor
-    */
-   private XmlDocument createUpdateDocument(int rowsAffected,
-                                            ConnectionDescriptor descriptor) {
-      XmlDocument document = new XmlDocument();
-      ElementNode root =
-         (ElementNode) document.createElement(descriptor.getDocumentrootnode());
-
-      document.appendChild(root);
-
-      ElementNode row =
-         (ElementNode) document.createElement(descriptor.getRowrootnode());
-
-      root.appendChild(row);
-
-      Text  rows =
-         (Text) document.createTextNode(rowsAffected
-                                        + "row(s) were affected during the update.");
-
-      row.appendChild(rows);
-
-      return document;
-   }
-
-
-   /**
-    */
-   private void notifyCust(XmlDocument doc)
-   {
-      ByteArrayOutputStream bais = new ByteArrayOutputStream();
-      try { doc.write(bais); } catch(IOException e) { Log.error(ME, e.getMessage()); }
-      notifyCust(bais.toByteArray(), "QueryResults");
-   }
-
-   /**
-    * @param content
-    * @param contentMimeExtended Informative only, "XmlBlasterException" or "QueryResults"
-    */
-   private void notifyCust(byte[] content, String contentMimeExtended)
-   {
-      PublishKeyWrapper key = new PublishKeyWrapper("__sys_jdbc."+ME, "text/xml", contentMimeExtended);
-      PublishQosWrapper qos = new PublishQosWrapper(new Destination(cust));
-
       try {
-         MessageUnit mu = new MessageUnit(key.toXml(), content, qos.toXml());
-         String      oid = callback.publish(mu);
+         String      oid = callback.publish(msgArr[0]);
          if (Log.DUMP) Log.plain("Delivered Results...\n" + new String(content));
       }
       catch (XmlBlasterException e) {
@@ -179,42 +79,6 @@ public class XmlDBAdapterWorker extends Thread {
       }
       catch (Exception e) {
          Log.error(ME, "Exception in notify: " + e);
-      }
-   }
-
-   /**
-    * Query the database. 
-    */
-   public void run() {
-      XmlDocument document = null;
-
-      try {
-         document = createDocument();
-      }
-      catch (Exception e) {
-         Log.error(ME+".SqlInitError", e.getMessage());
-         XmlBlasterException ex = new XmlBlasterException(ME+".SqlInitError", e.getMessage());
-         notifyCust(ex.toXml().getBytes(), "XmlBlasterException");
-         return;
-      }
-
-      ConnectionDescriptor descriptor = null;
-
-      if (Log.TRACE) Log.trace(ME, "Get connection ...");
-      descriptor = new ConnectionDescriptor(document);
-
-      try {
-         if (Log.TRACE) Log.trace(ME, "Access DB ...");
-         document = queryDB(descriptor);
-      }
-      catch (XmlBlasterException e) {
-         Log.error(e.id, e.reason);
-         notifyCust(e.toXml().getBytes(), "XmlBlasterException");
-         return;
-      }
-
-      if (descriptor.getConfirmation()) {
-         notifyCust(document);
       }
    }
 }
