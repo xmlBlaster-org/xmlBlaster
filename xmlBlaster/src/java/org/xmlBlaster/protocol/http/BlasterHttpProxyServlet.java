80c14e1aa989de5ed36c5f7af780a5add6fff1f0
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-07-11 09:27:27 +0000

    Removed trace
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@1926 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/protocol/http/BlasterHttpProxyServlet.java
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/http/BlasterHttpProxyServlet.java
@@ -3,7 +3,7 @@ Name:      BlasterHttpProxyServlet.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Handling callback over http
-Version:   $Id: BlasterHttpProxyServlet.java,v 1.39 2000/07/11 09:26:24 ruff Exp $
+Version:   $Id: BlasterHttpProxyServlet.java,v 1.40 2000/07/11 09:27:27 ruff Exp $
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.protocol.http;
 
@@ -39,7 +39,7 @@ import javax.servlet.http.*;
  * Invoke for testing:<br />
  *    http://localhost/servlet/BlasterHttpProxyServlet?ActionType=login&xmlBlaster.loginName=martin&xmlBlaster.passwd=secret
  * @author Marcel Ruff ruff@swand.lake.de
- * @version $Revision: 1.39 $
+ * @version $Revision: 1.40 $
  */
 public class BlasterHttpProxyServlet extends HttpServlet implements org.jutils.log.LogListener
 {
@@ -55,7 +55,7 @@ public class BlasterHttpProxyServlet extends HttpServlet implements org.jutils.l
       // Redirect xmlBlaster logs to servlet log file (see method log() below)
       Log.setDefaultLogLevel();
       // Log.addLogLevel("DUMP");  // Use this to see all messages!
-      Log.addLogLevel("TRACE"); // Use this to trace the code
+      // Log.addLogLevel("TRACE"); // Use this to trace the code
       // Log.addLogLevel("CALLS");
       // Log.addLogLevel("TIME");
 
