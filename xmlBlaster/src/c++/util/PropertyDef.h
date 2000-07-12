9bbcff5ec43a2109f3f7b0da0db2d022a22a6794
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-07-12 21:35:25 +0000

    corrected example
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@1948 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/c++/util/PropertyDef.h
+++ xmlBlaster/src/c++/util/PropertyDef.h
@@ -3,7 +3,7 @@ Name:      PropertyDef.h
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Handling the Client data
-Version:   $Id: PropertyDef.h,v 1.2 2000/07/06 23:42:27 laghi Exp $
+Version:   $Id: PropertyDef.h,v 1.3 2000/07/12 21:35:25 ruff Exp $
 ---------------------------------------------------------------------------*/
 
 #ifndef _UTIL_PROPERTYDEF_H
