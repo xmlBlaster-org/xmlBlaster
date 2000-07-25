a080eafd007d45e32f3104182f519638a8e40feb
kron <kron@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-07-25 14:28:06 +0000

    added isDurable
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@2025 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/engine/PMessageUnit.java
+++ xmlBlaster/src/java/org/xmlBlaster/engine/PMessageUnit.java
@@ -3,7 +3,7 @@ Name:      PMessageUnit.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Ccapsulate a MessageUnit and QoS for persistence and engine
-Version:   $Id: PMessageUnit.java,v 1.8 2000/06/25 18:32:41 ruff Exp $
+Version:   $Id: PMessageUnit.java,v 1.9 2000/07/25 14:28:06 kron Exp $
 Author:    manuel.kron@gmx.net
 ------------------------------------------------------------------------------*/
 
@@ -11,7 +11,7 @@ package org.xmlBlaster.engine;
 import java.io.*;
 
 import org.xmlBlaster.engine.helper.MessageUnit;
-import org.xmlBlaster.engine.xml2java.PublishQoS;
+import org.xmlBlaster.util.XmlBlasterException;
 import org.jutils.log.Log;
 
 import gnu.regexp.*;
@@ -19,24 +19,24 @@ import gnu.regexp.*;
 public class PMessageUnit implements Serializable
 {
    private static final String ME = "PMessageUnit";
-   public final MessageUnit msgUnit;
-   public final PublishQoS  pubQos;
-   public final String      sender;
-   public final long        timeStamp;
-   public long        size = 0L;
-   public String oid = "";
 
-   public PMessageUnit(MessageUnit mu,PublishQoS qos, String send)
+   public final MessageUnit msgUnit;
+   public final long        timeStamp;
+   public final boolean     isDurable;
+   public long              size = 0L;
+   public String            oid = "";
+
+
+   public PMessageUnit(MessageUnit mu,boolean durable)
    {
+      isDurable = durable;
       timeStamp = System.currentTimeMillis();
       msgUnit   = mu;
-      pubQos    = qos;
-      sender    = send;
-
-      size = mu.xmlKey.length() + mu.content.length  + qos.size + 2200;
+      // PublishQoS is now in the MessageUnit  
+      size = mu.xmlKey.length() + mu.content.length  + mu.qos.length() + 2200;
       oid = getOid();
-
    }
+
 
    private String getOid()
    {
