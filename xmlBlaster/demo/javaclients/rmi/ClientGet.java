90219e352998d03e56a833184b9868300109c0a8
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-10-22 16:54:33 +0000

    usage
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@2415 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/demo/javaclients/rmi/ClientGet.java
+++ xmlBlaster/demo/javaclients/rmi/ClientGet.java
@@ -3,7 +3,7 @@ Name:      ClientGet.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Demo code for a client using xmlBlaster with RMI
-Version:   $Id: ClientGet.java,v 1.9 2000/10/22 13:50:01 ruff Exp $
+Version:   $Id: ClientGet.java,v 1.10 2000/10/22 16:54:33 ruff Exp $
 ------------------------------------------------------------------------------*/
 package javaclients.rmi;
 
@@ -64,7 +64,7 @@ public class ClientGet
          XmlBlasterProperty.init(args);
       } catch(org.jutils.JUtilsException e) {
          Log.plain("\nAvailable options:");
-         Log.plain("   -name               The login name [ClientSub].");
+         Log.plain("   -name               The login name [ClientGet].");
          Log.plain("   -passwd             The password [secret].");
          // !!! RmiConnection.usage();
          Log.usage();
