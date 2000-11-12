ad052d7f47bde39e3c4e90aa24d450f566b5e241
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-11-12 13:20:33 +0000

    Corrected to use Subscription as a template
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@2582 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/client/GetKeyWrapper.java
+++ xmlBlaster/src/java/org/xmlBlaster/client/GetKeyWrapper.java
@@ -3,23 +3,29 @@ Name:      GetKeyWrapper.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Handling one xmlKey
-Version:   $Id: GetKeyWrapper.java,v 1.1 2000/07/11 08:50:17 ruff Exp $
+Version:   $Id: GetKeyWrapper.java,v 1.2 2000/11/12 13:20:33 ruff Exp $
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.client;
 
+import org.xmlBlaster.util.XmlBlasterException;
+
 
 /**
- * This class encapsulates the Message meta data and unique identifier (key) of a get() message. 
+ * This class encapsulates the Message meta data and unique identifier (key) of a get() message.
  * <p />
- * Currently it is implemented identical to PublishKeyWrapper.java, see there for javadoc info.
+ * Currently it is implemented identical to SubscribeKeyWrapper.java, see there for javadoc info.
  */
-public class GetKeyWrapper extends PublishKeyWrapper
+public class GetKeyWrapper extends SubscribeKeyWrapper
 {
    /**
-    * @see org.xmlBlaster.client.PublishKeyWrapper
+    * @see org.xmlBlaster.client.SubscribeKeyWrapper
     */
-   public GetKeyWrapper(String oid, String contentMime, String contentMimeExtended)
+   public GetKeyWrapper(String oid)
    {
-      super(oid, contentMime, contentMimeExtended);
+      super(oid);
+   }
+   public GetKeyWrapper(String queryString, String queryType) throws XmlBlasterException
+   {
+      super(queryString, queryType);
    }
 }
