f0018040750e26ba64f1be67a7124a4f7a524f38
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2000-11-12 13:19:04 +0000

    Forwarding all env variables into build.xml
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@2580 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/build.sh
+++ xmlBlaster/build.sh
@@ -9,6 +9,14 @@ fi
 
 CLASSPATH=lib/ant.jar:lib/parser.jar:lib/jaxp.jar:lib/idl.jar:lib/jacorb.jar:lib/omquery.jar:lib/xtdash.jar:lib/servlet-2.0.jar:lib/test.jar:$JAVA_HOME/lib/tools.jar:lib/xmlrpc.jar
 
+ALL_ENV=`env`
+MY_DEF=""
+for i in ${ALL_ENV} ; do
+    #echo "Processing -D$i"
+    MY_DEF="${MY_DEF} -D$i"
+done
+#echo ${MY_DEF}
 
-$JAVA_HOME/bin/java -Dant.home=$XMLBLASTER_HOME -classpath $CLASSPATH org.apache.tools.ant.Main $@
+
+$JAVA_HOME/bin/java -Dant.home=$XMLBLASTER_HOME ${MY_DEF} -classpath $CLASSPATH org.apache.tools.ant.Main $@
 
