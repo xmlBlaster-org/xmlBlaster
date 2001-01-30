4d2d49d21da8e4d68f0f60f5dd4a55c05e2c3f11
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2001-01-30 14:05:03 +0000

    Heaps of new qos elements
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@2744 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/authentication/ClientQoS.java
+++ xmlBlaster/src/java/org/xmlBlaster/authentication/ClientQoS.java
@@ -3,7 +3,7 @@ Name:      ClientQoS.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Handling login QoS (quality of service), knows how to parse with SAX
-Version:   $Id: ClientQoS.java,v 1.9 2000/10/22 19:35:52 ruff Exp $
+Version:   $Id: ClientQoS.java,v 1.10 2001/01/30 14:05:03 ruff Exp $
 Author:    ruff@swand.lake.de
 -----------------------------------------------------------------------------*/
 package org.xmlBlaster.authentication;
@@ -11,8 +11,10 @@ package org.xmlBlaster.authentication;
 import org.xmlBlaster.util.Log;
 import org.xmlBlaster.engine.helper.CallbackAddress;
 import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.util.XmlBlasterProperty;
 import org.xml.sax.AttributeList;
 import java.util.Vector;
+import java.io.Serializable;
 
 
 /**
@@ -21,21 +23,21 @@ import java.util.Vector;
  * QoS Informations sent from the client to the server via the login() method<br />
  * They inform the server about client preferences and wishes
  */
-public class ClientQoS extends org.xmlBlaster.util.XmlQoSBase
+public class ClientQoS extends org.xmlBlaster.util.XmlQoSBase implements Serializable
 {
    private static String ME = "ClientQoS";
 
    // helper flags for SAX parsing
 
-   /** PtP messages wanted? */
-   private boolean noPtP = false;    // <noPtP />    <!-- Don't send me any PtP messages (prevents spamming) -->
-
    /** Contains CallbackAddress objects */
    private boolean inCallback = false;
    private CallbackAddress tmpAddr = null;
-   private CallbackAddress[] addressArr = null;
+   transient private CallbackAddress[] addressArr = null;
    private Vector addressVec = new Vector();  // <callback type="IOR">IOR:000122200...</callback>
 
+   private boolean inBurstMode = false;
+   private boolean inCompress = false;
+   private boolean inPtpAllowed = false;
 
    /**
     * Constructs the specialized quality of service object for a publish() call.
@@ -45,17 +47,6 @@ public class ClientQoS extends org.xmlBlaster.util.XmlQoSBase
       if (Log.DUMP) Log.dump(ME, "Creating ClientQoS(" + xmlQoS_literal + ")");
       addressArr = null;
       init(xmlQoS_literal);
-   }
-
-
-   /**
-    * Does client wants to receive PtP messages?
-    *
-    * @return true/false
-    */
-   public final boolean wantsPtP()
-   {
-      return !noPtP;
    }
 
 
@@ -95,15 +86,20 @@ public class ClientQoS extends org.xmlBlaster.util.XmlQoSBase
       if (super.startElementBase(name, attrs) == true)
          return;
 
-      //if (Log.TRACE) Log.trace(ME, "Entering startElement for " + name);
+      if (Log.TRACE) Log.trace(ME, "Entering startElement for " + name);
 
-      if (!inQos) return;
-
-      if (name.equalsIgnoreCase("noPtP"))
-         noPtP = true;
+      if (inCallback && !inBurstMode && !inCompress && !inPtpAllowed) {
+         String tmp = character.toString().trim(); // The address
+         if (tmp.length() > 0) {
+            tmpAddr.setAddress(tmp);
+            Log.info(ME, "Setting address '" + tmp + "'");
+            character.setLength(0);
+         }
+      }
 
       if (name.equalsIgnoreCase("callback")) {
          inCallback = true;
+         String tmp = character.toString().trim(); // The address (if before inner tags)
          String type = null;
          if (attrs != null) {
             int len = attrs.getLength();
@@ -119,9 +115,86 @@ public class ClientQoS extends org.xmlBlaster.util.XmlQoSBase
             type = "IOR";
          }
          tmpAddr = new CallbackAddress(type);
+         if (tmp.length() > 0) {
+            tmpAddr.setAddress(tmp);
+            character.setLength(0);
+         }
+         return;
+      }
+
+      if (name.equalsIgnoreCase("burstMode")) {
+         inBurstMode = true;
+         if (tmpAddr == null || !inCallback) {
+            Log.error(ME, "<burstMode> tag is not in <callback> tag, element ignored.");
+            character.setLength(0);
+            return;
+         }
+         if (attrs != null) {
+            int len = attrs.getLength();
+            int ii=0;
+            for (ii = 0; ii < len; ii++) {
+               if (attrs.getName(ii).equalsIgnoreCase("collectTime")) {
+                  String tmp = attrs.getValue(ii).trim();
+                  try {
+                     tmpAddr.setCollectTime(new Long(tmp).longValue());
+                  } catch (NumberFormatException e) {
+                     Log.error(ME, "Wrong format of <burstMode collectTime='" + tmp + "'>, expected a long in milliseconds, burst mode is switched off.");
+                  }
+                  break;
+               }
+            }
+            if (ii >= len)
+               Log.error(ME, "Missing 'collectTime' attribute in login-qos <burstMode>");
+         }
+         else {
+            Log.error(ME, "Missing 'collectTime' attribute in login-qos <burstMode>");
+         }
          character.setLength(0);
          return;
       }
+
+      if (name.equalsIgnoreCase("compress")) {
+         inCompress = true;
+         if (tmpAddr == null || !inCallback) {
+            Log.error(ME, "<compress> tag is not in <callback> tag, element ignored.");
+            character.setLength(0);
+            return;
+         }
+         if (attrs != null) {
+            int len = attrs.getLength();
+            int ii=0;
+            for (ii = 0; ii < len; ii++) {
+               if (attrs.getName(ii).equalsIgnoreCase("type")) {
+                  tmpAddr.setCompressType(attrs.getValue(ii).trim());
+               }
+               else if (attrs.getName(ii).equalsIgnoreCase("minSize")) {
+                  String tmp = attrs.getValue(ii).trim();
+                  try {
+                     tmpAddr.setMinSize(new Long(tmp).longValue());
+                  } catch (NumberFormatException e) {
+                     Log.error(ME, "Wrong format of <compress minSize='" + tmp + "'>, expected a long in bytes, compress is switched off.");
+                  }
+               }
+            }
+         }
+         else {
+            Log.error(ME, "Missing 'type' attribute in login-qos <compress>");
+         }
+         character.setLength(0);
+         return;
+      }
+
+      if (name.equalsIgnoreCase("PtP")) {
+         inPtpAllowed = true;
+         if (tmpAddr == null || !inCallback) {
+            Log.error(ME, "<PtP> tag is not in <callback> tag, element ignored.");
+            character.setLength(0);
+            return;
+         }
+         character.setLength(0);
+         return;
+      }
+
    }
 
 
@@ -132,20 +205,108 @@ public class ClientQoS extends org.xmlBlaster.util.XmlQoSBase
     */
    public void endElement(String name)
    {
-      super.endElement(name);
+      if (super.endElementBase(name) == true)
+         return;
 
-      //if (Log.TRACE) Log.trace(ME, "Entering endElement for " + name);
+      if (Log.TRACE) Log.trace(ME, "Entering endElement for " + name);
 
-      if(name.equalsIgnoreCase("callback")) {
+      if (name.equalsIgnoreCase("callback")) {
          inCallback = false;
+         String tmp = character.toString().trim(); // The address (if after inner tags)
          if (tmpAddr != null) {
-            tmpAddr.setAddress(character.toString().trim());
+            if (tmp.length() > 0) tmpAddr.setAddress(tmp);
             addressVec.addElement(tmpAddr);
          }
          character.setLength(0);
          return;
       }
+
+      if (name.equalsIgnoreCase("burstMode")) {
+         inBurstMode = false;
+         character.setLength(0);
+         return;
+      }
+
+      if (name.equalsIgnoreCase("compress")) {
+         inCompress = false;
+         character.setLength(0);
+         return;
+      }
+
+      if (name.equalsIgnoreCase("PtP")) {
+         inPtpAllowed = false;
+         if (tmpAddr != null) {
+            String tmp = character.toString().trim();
+            tmpAddr.setPtpAllowed(new Boolean(tmp).booleanValue());
+         }
+         else
+            Log.error(ME, "Internal problem, ignoring <PtP> element");
+         character.setLength(0);
+         return;
+      }
+   }
+
+   /**
+    * Dump state of this object into a XML ASCII string.
+    */
+   public final String toXml()
+   {
+      return toXml((String)null);
    }
 
 
+   /**
+    * Dump state of this object into a XML ASCII string.
+    * <br>
+    * @param extraOffset indenting of tags for nice output
+    * @return The xml representation
+    */
+   public final String toXml(String extraOffset)
+   {
+      StringBuffer sb = new StringBuffer();
+      String offset = "\n   ";
+      if (extraOffset == null) extraOffset = "";
+      offset += extraOffset;
+
+      sb.append(offset + "<qos>");
+      CallbackAddress[] addr = getCallbackAddresses();
+      for (int ii=0; ii<addr.length; ii++)
+         sb.append(addr[ii].toXml(extraOffset + "   "));
+      sb.append(offset + "</qos>");
+
+      return sb.toString();
+   }
+
+
+   /** For testing: java org.xmlBlaster.authentication.ClientQoS */
+   public static void main(String[] args)
+   {
+      try {
+         XmlBlasterProperty.init(args);
+         String xml =
+            "<qos>\n" +
+            "   <callback type='IOR'>\n" +
+            "      <PtP>true</PtP>\n" +
+            "      IOR:00011200070009990000....\n" +
+            "      <compress type='gzip' minSize='1000' />\n" +
+            "      <burstMode collectTime='400' />\n" +
+            "   </callback>\n" +
+            "   <callback type='EMAIL'>\n" +
+            "      et@mars.universe\n" +
+            "      <PtP>false</PtP>\n" +
+            "   </callback>\n" +
+            "   <callback type='XML-RPC'>\n" +
+            "      <PtP>true</PtP>\n" +
+            "      http:/www.mars.universe:8080/RPC2\n" +
+            "   </callback>\n" +
+            "   <offlineQueuing timeout='3600' />\n" +
+            "</qos>\n";
+
+         ClientQoS qos = new ClientQoS(xml);
+         System.out.println(qos.toXml());
+      }
+      catch(Throwable e) {
+         Log.error("TestFailed", e.toString());
+      }
+   }
 }
--- xmlBlaster/src/java/org/xmlBlaster/engine/helper/CallbackAddress.java
+++ xmlBlaster/src/java/org/xmlBlaster/engine/helper/CallbackAddress.java
@@ -3,10 +3,11 @@ Name:      CallbackAddress.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Holding callback address string and protocol string
-Version:   $Id: CallbackAddress.java,v 1.2 2000/06/26 12:01:16 ruff Exp $
+Version:   $Id: CallbackAddress.java,v 1.3 2001/01/30 14:03:14 ruff Exp $
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.engine.helper;
 
+import org.xmlBlaster.util.Log;
 
 /**
  * Helper class holding callback address string and protocol string.
@@ -14,10 +15,20 @@ package org.xmlBlaster.engine.helper;
  */
 public class CallbackAddress
 {
+   private static final String ME = "CallbackAddress";
+
    /** The unique address, e.g. the CORBA IOR string */
    private String address;
    /** The unique protocol type, e.g. "IOR" */
    private String type;
+   /** BurstMode: The time to collect messages for update */
+   private long collectTime = 0L;
+   /** Compress messages if set to "gzip" or "zip" */
+   private String compressType = "";
+   /** Messages bigger this size in bytes are compressed */
+   private long minSize = 0L;
+   /** PtP messages wanted? defaults to true */
+   private boolean ptpAllowed = true;    // <PtP>false</PtP>    <!-- Don't send me any PtP messages (prevents spamming) -->
 
    /**
     * @param type    The protocol type, e.g. "IOR", "EMAIL", "XML-RPC"
@@ -71,6 +82,87 @@ public class CallbackAddress
 
 
    /**
+    * BurstMode: Access the time to collect messages for update. 
+    * @return The time to collect in milliseconds
+    */
+   public long getCollectTime()
+   {
+      return collectTime;
+   }
+
+   /**
+    * BurstMode: The time to collect messages for one update. 
+    * @param The time to collect in milliseconds
+    */
+   public void setCollectTime(long collectTime)
+   {
+      if (collectTime < 0L)
+         this.collectTime = 0L;
+      else
+         this.collectTime = collectTime;
+   }
+
+
+   public void setCompressType(String compressType)
+   {
+      if (compressType == null) compressType = "";
+      this.compressType = compressType;
+
+      // TODO !!!
+      if (compressType.length() > 0)
+         Log.warn(ME, "Compression of messages is not yet supported");
+   }
+
+
+   /**
+    * Get the compression method. 
+    * @return "" No compression
+    */
+   public String getCompressType()
+   {
+      return compressType;
+   }
+
+   /** 
+    * Messages bigger this size in bytes are compressed. 
+    * <br />
+    * Note: This value is only used if compressType is set to a supported value
+    * @return size in bytes
+    */
+   public long getMinSize()
+   {
+      return minSize;
+   }
+
+   /** 
+    * Messages bigger this size in bytes are compressed. 
+    * <br />
+    * Note: This value is only evaluated if compressType is set to a supported value
+    * @return size in bytes
+    */
+   public void setMinSize(long minSize)
+   {
+      this.minSize = minSize;
+   }
+
+   /**
+    * @return true if we may send PtP messages
+    */
+   public boolean isPtpAllowed()
+   {
+      return this.ptpAllowed;
+   }
+
+   /**
+    * @param Set if we accept point to point messages
+    */
+   public void setPtpAllowed(boolean ptpAllowed)
+   {
+      this.ptpAllowed = ptpAllowed;
+   }
+
+
+   /**
     * Dump state of this object into a XML ASCII string.
     */
    public final String toXml()
@@ -94,6 +186,9 @@ public class CallbackAddress
 
       sb.append(offset + "<callback type='").append(getType()).append("'>");
       sb.append(offset + "   ").append(getAddress());
+      sb.append(offset + "   ").append("<burstMode collectTime='").append(getCollectTime()).append("' />");
+      sb.append(offset + "   ").append("<compress type='").append(getCompressType()).append("' minSize='").append(getMinSize()).append("' />");
+      sb.append(offset + "   ").append("<PtP>").append(isPtpAllowed()).append("</PtP>");
       sb.append(offset + "</callback>");
 
       return sb.toString();
