e307de8e2311dfb478dbb507703d9accd7035271
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2001-02-16 19:51:30 +0000

    Corrected import statement
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@2865 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/demo/javaclients/j2ee/k2/TestClient.java
+++ xmlBlaster/demo/javaclients/j2ee/k2/TestClient.java
@@ -6,12 +6,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -19,7 +19,7 @@
 package javaclients.j2ee.k2;
 
 import org.xmlBlaster.j2ee.k2.client.*;
-import org.xmlBlaster.j2ee.k2;
+import org.xmlBlaster.j2ee.k2.*;
 
 import org.xmlBlaster.engine.helper.MessageUnit;
 /**
@@ -28,41 +28,41 @@ import org.xmlBlaster.engine.helper.MessageUnit;
  *
  * Created: Wed Feb  7 21:11:49 2001
  *
- * @author 
+ * @author
  * @version
  */
 
 public class TestClient {
-    
+
     public TestClient() {
-	
+
     }
-    
+
     public static void main(String[] args) {
-	try {
-	    BlasterManagedConnectionFactory f = new BlasterManagedConnectionFactory();
-	    f.setClientProtocol("IOR");
-	    f.setRmiRegistryPort("1199");
-	    f.setLogging("true");
-	    BlasterConnectionFactory cf = (BlasterConnectionFactory)f.createConnectionFactory();
+        try {
+            BlasterManagedConnectionFactory f = new BlasterManagedConnectionFactory();
+            f.setClientProtocol("IOR");
+            f.setRmiRegistryPort("1199");
+            f.setLogging("true");
+            BlasterConnectionFactory cf = (BlasterConnectionFactory)f.createConnectionFactory();
 
 
 
-	    BlasterConnection con = cf.getConnection("pra", "passw");
-	    
-	    String key ="<key oid=\"News-35\" contentMime=\"text/xml\">" +
-		"<head> <title>Blodiga strider i Kongo-Kinshasa</title></head></key>";
-	    String qos = "<qos></qos>";
-	    String msg = "Meddelande";
-	    con.publish( new MessageUnit(key,msg.getBytes(),qos));
-	    
-	    con.close();
+            BlasterConnection con = cf.getConnection("pra", "passw");
+
+            String key ="<key oid=\"News-35\" contentMime=\"text/xml\">" +
+                "<head> <title>Blodiga strider i Kongo-Kinshasa</title></head></key>";
+            String qos = "<qos></qos>";
+            String msg = "Meddelande";
+            con.publish( new MessageUnit(key,msg.getBytes(),qos));
+
+            con.close();
 
 
-	}catch(Exception ex) {
-	    System.err.println("Error: " + ex);
-	    ex.printStackTrace();
-	}
+        }catch(Exception ex) {
+            System.err.println("Error: " + ex);
+            ex.printStackTrace();
+        }
     }
-    
+
 } // TestClient
