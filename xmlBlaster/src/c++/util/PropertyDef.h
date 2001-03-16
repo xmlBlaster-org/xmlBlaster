e9d425cf0a731361165a4b41605436da0c11ae95
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2001-03-16 10:15:58 +0000

    Removed ID string
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@2981 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/c++/util/PropertyDef.h
+++ xmlBlaster/src/c++/util/PropertyDef.h
@@ -3,7 +3,6 @@ Name:      PropertyDef.h
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Handling the Client data
-Version:   $Id: PropertyDef.h,v 1.19 2001/03/11 18:26:20 ruff Exp $
 ---------------------------------------------------------------------------*/
 
 #ifndef _UTIL_PROPERTYDEF_H
