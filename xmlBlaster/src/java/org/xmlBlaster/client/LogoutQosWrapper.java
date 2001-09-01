177b21c07119c05617f50cc54d0fbc9fd5742829
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2001-09-01 09:35:33 +0000

    Generating XML string for logout or disconnect
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@3439 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/client/LogoutQosWrapper.java
@@ -0,0 +1,97 @@
+/*------------------------------------------------------------------------------
+Name:      LogoutQosWrapper.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Handling one xmlQoS
+Version:   $Id: LogoutQosWrapper.java,v 1.1 2001/09/01 09:35:33 ruff Exp $
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.client;
+
+import org.xmlBlaster.util.Log;
+import org.xmlBlaster.engine.helper.CallbackAddress;
+import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.client.PluginLoader;
+import org.xmlBlaster.authentication.plugins.I_ClientHelper;
+import org.xmlBlaster.authentication.plugins.I_InitQos;
+import java.util.Vector;
+
+
+/**
+ * This class encapsulates the qos of a logout() or disconnect()
+ * <p />
+ * So you don't need to type the 'ugly' XML ASCII string by yourself.
+ * After construction access the ASCII-XML string with the toXml() method.
+ * <br />
+ * A typical <b>logout</b> qos could look like this:<br />
+ * <pre>
+ *     &lt;qos>
+ *     &lt;/qos>
+ * </pre>
+ * <p />
+ * see xmlBlaster/src/dtd/XmlQoS.xml
+ */
+public class LogoutQosWrapper extends QosWrapper
+{
+   private String ME = "LogoutQosWrapper";
+
+   /**
+    * Default constructor
+    */
+   public LogoutQosWrapper()
+   {
+   }
+
+   /**
+    * Converts the data into a valid XML ASCII string.
+    * @return An XML ASCII string
+    */
+   public String toString()
+   {
+      return toXml();
+   }
+
+
+   /**
+    * Dump state of this object into a XML ASCII string.
+    * <br>
+    * The default is to include the security string
+    * @return internal state of the RequestBroker as a XML ASCII string
+    */
+   public final String toXml()
+   {
+      return toXml("");
+   }
+
+
+   /**
+    * Dump state of this object into a XML ASCII string.
+    * <br>
+    * @param extraOffset indenting of tags for nice output
+    * @return internal state of the RequestBroker as a XML ASCII string
+    */
+   public final String toXml(String extraOffset)
+   {
+      StringBuffer sb = new StringBuffer(30);
+      String offset = "\n   ";
+      if (extraOffset == null) extraOffset = "";
+      offset += extraOffset;
+
+      sb.append("<qos>\n");
+      sb.append("</qos>");
+
+      return sb.toString();
+   }
+
+   /** For testing: java org.xmlBlaster.client.LogoutQosWrapper */
+   public static void main(String[] args)
+   {
+      try {
+         org.xmlBlaster.util.XmlBlasterProperty.init(args);
+         LogoutQosWrapper qos = new LogoutQosWrapper();
+         System.out.println(qos.toXml());
+      }
+      catch(Throwable e) {
+         Log.error("TestFailed", e.toString());
+      }
+   }
+}
