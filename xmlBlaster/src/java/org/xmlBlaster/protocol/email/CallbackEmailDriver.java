7cba4a5add171c045d8beb86754d165c85b32453
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2001-11-23 17:57:14 +0000

    Added some first support to send emails
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@3534 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/protocol/email/CallbackEmailDriver.java
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/email/CallbackEmailDriver.java
@@ -3,25 +3,30 @@ Name:      CallbackEmailDriver.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   This singleton sends messages to clients using email
-Version:   $Id: CallbackEmailDriver.java,v 1.10 2000/09/15 17:16:18 ruff Exp $
+Version:   $Id: CallbackEmailDriver.java,v 1.11 2001/11/23 17:57:14 ruff Exp $
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.protocol.email;
 
 import org.xmlBlaster.util.Log;
 
 import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.util.XmlBlasterProperty;
 import org.xmlBlaster.protocol.I_CallbackDriver;
 import org.xmlBlaster.engine.ClientInfo;
 import org.xmlBlaster.engine.MessageUnitWrapper;
+import org.xmlBlaster.engine.helper.MessageUnit;
 import org.xmlBlaster.engine.helper.CallbackAddress;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
 
 
 /**
- * This singleton sends a MessageUnit back to a client using Email.
+ * Sends a MessageUnit back to a client using Email.
  * <p>
- * THIS DRIVER IS NOT YET IMPLEMENTED
+ * One instance of this for each client callback is used
  *
- * @version $Revision: 1.10 $
  * @author $Author: ruff $
  */
 public class CallbackEmailDriver implements I_CallbackDriver
@@ -48,10 +53,62 @@ public class CallbackEmailDriver implements I_CallbackDriver
    /**
     * This sends the update to the client.
     */
-   public final void sendUpdate(ClientInfo clientInfo, MessageUnitWrapper msgUnitWrapper, org.xmlBlaster.engine.helper.MessageUnit[] messageUnitArr) throws XmlBlasterException
+   public final void sendUpdate(ClientInfo clientInfo, MessageUnitWrapper msgUnitWrapper, MessageUnit[] messageUnitArr) throws XmlBlasterException
    {
-      Log.error(ME + ".NoEmailProtocol", "Sorry, email callbacks are not yet supported, no mail sent to " + callbackAddress.getAddress());
-      throw new XmlBlasterException(ME + ".NoEmailProtocol", "Sorry, email callbacks are not yet supported, no mail sent to " + callbackAddress.getAddress());
+      if (Log.TRACE) Log.trace(ME, "xmlBlaster.update(" + msgUnitWrapper.getUniqueKey() + ") to " + clientInfo.toString());
+      try {
+         String smtpHost = XmlBlasterProperty.get("EmailDriver.smtpHost", "localhost");
+         String from = XmlBlasterProperty.get("EmailDriver.from", "xmlblast@localhost"); //clientInfo.getLoginName();
+         String to = callbackAddress.getAddress();
+
+         // Get system properties
+         Properties props = System.getProperties();
+
+         // Setup mail server
+         props.put("mail.smtp.host", smtpHost);
+
+         // Get session
+         Session session = Session.getDefaultInstance(props, null);
+
+         // Define message
+         MimeMessage message = new MimeMessage(session);
+         message.setFrom(new InternetAddress(from));
+         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+         message.setSubject("XmlBlaster Generated Email");
+         String text = getMailBody(msgUnitWrapper, messageUnitArr);
+         message.setText(text);
+         Log.info(ME + ".sendUpdate", "Sending email from " + from + " to " + to + ", smtpHost=" + smtpHost);
+         Log.info(ME + ".sendUpdate", "\n"+text);
+         // Send message
+         Transport.send(message);
+      } catch (Exception e) {
+         String str = "Sorry, email callback failed, no mail sent to " + callbackAddress.getAddress() + ": " + e.toString();
+         Log.warn(ME + ".EmailSendError", str);
+         throw new XmlBlasterException(ME + ".EmailSendError", str);
+      }
+   }
+
+
+   private String getMailBody(MessageUnitWrapper msgUnitWrapper, MessageUnit[] arr) throws XmlBlasterException
+   {
+      StringBuffer sb = new StringBuffer();
+      String offset = "\n";
+
+      sb.append(offset).append("<xmlBlaster>");
+      sb.append(offset).append(msgUnitWrapper.getXmlKey().toXml()).append("\n");
+      sb.append(offset).append("   <content>").append(new String(msgUnitWrapper.getMessageUnit().getContent())).append("</content>");
+      sb.append(offset).append(msgUnitWrapper.getPublishQoS().toXml());
+      if (arr.length > 1)
+         Log.error(ME, "Sendung more than one callback email (burst mode) is not supported, mails are lost!");
+      /*
+      for (int ii=0; ii<arr.length; ii++) {
+         sb.append(offset).append(arr[ii].xmlKey);
+         sb.append(offset).append("<content>").append(arr[ii].content).append("</content>");
+         sb.append(offset).append(arr[ii].qos);
+      }
+      */
+      sb.append(offset).append("</xmlBlaster>\n");
+      return sb.toString();
    }
 
 
