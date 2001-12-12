73ce4215962962ec6e12f6190ed8036655e60c0f
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2001-12-12 17:48:14 +0000

    Added Windows directive
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@3625 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/c++/util/PropertyDef.h
+++ xmlBlaster/src/c++/util/PropertyDef.h
@@ -9,7 +9,12 @@ Comment:   Handling the Client data, defines operating system specific stuff.
 #ifndef _UTIL_PROPERTYDEF_H
 #define _UTIL_PROPERTYDEF_H
 
-#define FILE_SEP "/"
-#define PATH_SEP ":"
+#ifdef _WIN32
+#  define FILE_SEP "\\"
+#  define PATH_SEP ";"
+#else
+#  define FILE_SEP "/"
+#  define PATH_SEP ":"
+#endif
 
 #endif
