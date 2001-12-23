10a253b6cf62d282b1918042d28d83634308364c
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2001-12-23 19:52:54 +0000

    Added notify QoS
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@3725 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/engine/xml2java/EraseQoS.java
+++ xmlBlaster/src/java/org/xmlBlaster/engine/xml2java/EraseQoS.java
@@ -3,7 +3,7 @@ Name:      EraseQoS.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Handling QoS (quality of service), knows how to parse it with SAX
-Version:   $Id: EraseQoS.java,v 1.6 2001/02/12 00:05:53 ruff Exp $
+Version:   $Id: EraseQoS.java,v 1.7 2001/12/23 19:52:54 ruff Exp $
 Author:    ruff@swand.lake.de
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.engine.xml2java;
@@ -24,6 +24,10 @@ public class EraseQoS extends org.xmlBlaster.util.XmlQoSBase
 {
    private static String ME = "EraseQoS";
 
+   private boolean inNotify = false; // parsing inside <notify> ?
+   /** Default is to notify subscribers when their topic is erased */
+   private boolean notify = true;
+
 
    /**
     * Constructs the specialized quality of service object for a publish() call.
@@ -32,6 +36,15 @@ public class EraseQoS extends org.xmlBlaster.util.XmlQoSBase
    {
       if (Log.CALL) Log.call(ME, "Creating EraseQoS(" + xmlQoS_literal + ")");
       init(xmlQoS_literal);
+   }
+
+
+   /**
+    * @return true - notify subscribers, false - do nothing
+    */
+   public final boolean isNotify()
+   {
+      return notify;
    }
 
 
@@ -49,6 +62,20 @@ public class EraseQoS extends org.xmlBlaster.util.XmlQoSBase
       if (Log.TRACE) Log.trace(ME, "Entering startElement for " + name);
 
       if (!inQos) return;
+
+      if (name.equalsIgnoreCase("notify")) {
+         if (!inQos)
+            return;
+         inNotify = true;
+         if (attrs != null) {
+            int len = attrs.getLength();
+            for (int i = 0; i < len; i++) {
+               Log.warn(ME, "Ignoring sent <notify> attribute " + attrs.getQName(i) + "=" + attrs.getValue(i).trim());
+            }
+            // if (Log.TRACE) Log.trace(ME, "Found notify tag");
+         }
+         return;
+      }
    }
 
 
@@ -63,6 +90,19 @@ public class EraseQoS extends org.xmlBlaster.util.XmlQoSBase
          return;
 
       if (Log.TRACE) Log.trace(ME, "Entering endElement for " + name);
+
+      if(name.equalsIgnoreCase("notify")) {
+         inNotify = false;
+         String tmp = character.toString().trim();
+         try {
+            notify = new Boolean(tmp).booleanValue();
+         } catch (NumberFormatException e) {
+            Log.error(ME, "Wrong format of <notify>" + tmp + "</notify>, expected true or false.");
+         }
+         // if (Log.TRACE) Log.trace(ME, "Found notify = " + notify);
+         character.setLength(0);
+         return;
+      }
    }
 
 
@@ -71,9 +111,9 @@ public class EraseQoS extends org.xmlBlaster.util.XmlQoSBase
     * <br>
     * @return internal state of the RequestBroker as a XML ASCII string
     */
-   public final StringBuffer printOn() throws XmlBlasterException
+   public final String toXml()
    {
-      return printOn((String)null);
+      return toXml((String)null);
    }
 
 
@@ -83,16 +123,35 @@ public class EraseQoS extends org.xmlBlaster.util.XmlQoSBase
     * @param extraOffset indenting of tags for nice output
     * @return internal state of the RequestBroker as a XML ASCII string
     */
-   public final StringBuffer printOn(String extraOffset) throws XmlBlasterException
+   public final String toXml(String extraOffset)
    {
       StringBuffer sb = new StringBuffer();
       String offset = "\n   ";
       if (extraOffset == null) extraOffset = "";
       offset += extraOffset;
 
-      sb.append(offset + "<" + ME + ">");
-      sb.append(offset + "</" + ME + ">\n");
+      sb.append(offset).append("<").append(ME).append(">");
+      sb.append(offset).append("</").append(ME).append(">\n");
 
-      return sb;
+      return sb.toString();
+   }
+
+
+   /** For testing: java org.xmlBlaster.engine.xml2java.PublishQoS */
+   public static void main(String[] args)
+   {
+      try {
+         org.xmlBlaster.util.XmlBlasterProperty.init(args);
+         String xml =
+            "<qos>\n" +
+            "   <notify>true</notify>\n" +
+            "</qos>\n";
+
+         EraseQoS qos = new EraseQoS(xml);
+         System.out.println(qos.toXml());
+      }
+      catch(Throwable e) {
+         Log.error("TestFailed", e.toString());
+      }
    }
 }
