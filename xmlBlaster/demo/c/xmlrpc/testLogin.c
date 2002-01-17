0f0847a34a532f8e36365de41949789905f642b8
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-01-17 18:15:28 +0000

    Added further example
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@3837 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/demo/c/xmlrpc/testLogin.c
+++ xmlBlaster/demo/c/xmlrpc/testLogin.c
@@ -18,12 +18,13 @@
 
 void usage()
 {
-   fprintf(stderr, "--------------------------------------------------\n"
-                  "Usage:\n"
-                  "   testLogin -xmlrpc.serverUrl <serverURL>\n"
-                  "Example:\n"
-                  "   testLogin -xmlrpc.serverUrl http://myHost:8080/\n"
-                  "--------------------------------------------------\n");
+   fprintf(stderr,
+    "--------------------------------------------------\n"
+    "Usage:\n"
+    "   testLogin -xmlrpc.serverUrl <serverURL> -xmlrpc.cbUrl <myCallbackServerURL> -name <loginName> -passwd <Password> -dest <loginName> -msgFile <xmlFile>\n"
+    "Example:\n"
+    "   testLogin -xmlrpc.serverUrl http://anotherHost:8080/ -xmlrpc.cbUrl http://myHost:8080/ -name ben -passwd secret -dest gesa -msgFile xy.xml\n"
+    "--------------------------------------------------\n");
 }
 
 void die_if_fault_occurred (xmlrpc_env *env)
@@ -42,7 +43,11 @@ int main (int argc, char** argv)
    xmlrpc_env env;
    xmlrpc_value *result;
    char *sessionId;
+   char key[256], *content, qos[256], *pubReturn, *loginName, *passwd, loginQos[256], *cbUrl;
+   char *keyOid = "PIB_REQUEST";
    char *serverUrl = NULL; //SERVER_URL;
+   char *msgFile = NULL;
+   char *destination = NULL;
    char serverHostName[126];
    char tmp[256];
    int iarg;
@@ -50,11 +55,32 @@ int main (int argc, char** argv)
    for (iarg=0; iarg < argc-1; iarg++) {
      if (strcmp(argv[iarg], "-xmlrpc.serverUrl") == 0)
         serverUrl = argv[++iarg];
+     else if (strcmp(argv[iarg], "-xmlrpc.cbUrl") == 0) // URL of our here instantiated callback server
+        cbUrl = argv[++iarg];
+     else if (strcmp(argv[iarg], "-dest") == 0)
+        destination = argv[++iarg];
+     else if (strcmp(argv[iarg], "-msgFile") == 0)
+        msgFile = argv[++iarg];
+     else if (strcmp(argv[iarg], "-name") == 0)
+        loginName = argv[++iarg];
+     else if (strcmp(argv[iarg], "-passwd") == 0)
+        passwd = argv[++iarg];
      else if (strcmp(argv[iarg], "--help") == 0 || strcmp(argv[iarg], "-help") == 0 || strcmp(argv[iarg], "-?") == 0) {
         usage();
         exit(0);
      }
    }
+
+   if (destination == NULL || msgFile == NULL) {
+      printf("Missing arguments\n");
+      usage();
+      exit(0);
+   }
+
+   /*
+    !!! Missing !!!
+    Startup the XmlRpc server for callbacks
+   */
 
    if (serverUrl == NULL) {
       gethostname(serverHostName, 125);
@@ -67,10 +93,11 @@ int main (int argc, char** argv)
    xmlrpc_env_init(&env);
 
    /* Call our XML-RPC server. */
-   printf("Login to %s ...\n", serverUrl);
+   sprintf(loginQos, "<qos><callback type='XML-RPC'>%.100s</callback></qos>", cbUrl);
+   printf("Login to %s as user %s ...\n", serverUrl, loginName);
    result = xmlrpc_client_call(&env, serverUrl,
                                "authenticate.login", "(ssss)",
-                               "ben", "secret", "<qos></qos>", "");
+                               loginName, passwd, loginQos, "");
    die_if_fault_occurred(&env);
    
    /* Parse our result value. */
@@ -78,6 +105,23 @@ int main (int argc, char** argv)
    die_if_fault_occurred(&env);
    printf("Login success, got sessionId=%s\n", sessionId);
    
+   /* Send message to destination. */
+   printf("publish %s to %s ...\n", keyOid, destination);
+   sprintf(key, "<key oid='%.100s' contentMime='text/xml'/>", keyOid);
+   sprintf(qos, "<qos><destination queryType='EXACT'>%.100s</destination></qos>", destination);
+   content = "Hello world";
+   printf("Key=%s Qos=%s\n", key, qos);
+   result = xmlrpc_client_call(&env, serverUrl,
+                               "xmlBlaster.publish", "(ssss)",
+                               sessionId, key, content, qos);
+   die_if_fault_occurred(&env);
+
+   /* Parse our result value. */
+   xmlrpc_parse_value(&env, result, "s", &pubReturn);
+   die_if_fault_occurred(&env);
+   printf("Publish success, return value is %s\n", pubReturn);
+
+
    /* Dispose of our result value. */
    xmlrpc_DECREF(result);
 
