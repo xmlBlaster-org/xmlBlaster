30a0d7570a83c2ffb9293ab569b5695da02c4f94
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-02-14 19:05:24 +0000

    backup, needs to be cleaned
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@4067 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/protocol/I_Authenticate.java
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/I_Authenticate.java
@@ -3,7 +3,7 @@ Name:      I_Authenticate.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Native Interface to xmlBlaster
-Version:   $Id: I_Authenticate.java,v 1.3 2001/09/05 12:21:27 ruff Exp $
+Version:   $Id: I_Authenticate.java,v 1.4 2002/02/14 19:05:24 ruff Exp $
 Author:    ruff@swand.lake.de
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.protocol;
@@ -25,9 +25,28 @@ import org.xmlBlaster.util.ConnectReturnQos;
  */
 public interface I_Authenticate
 {
+   /*
+      I don't know which is the better way
+      1) passing raw strings as QoS
+         + The driver doesn't need to parse
+      2) passing QoS objects
+         + The Socket driver needs the object to extract the sessionId
+         - The drivers have a reference to the internal object (should we copy it)?
+         - Every driver needs to know how to parse
+   */
+
+   public String connect(String qos_literal) throws XmlBlasterException;
    public ConnectReturnQos connect(ConnectQos qos) throws XmlBlasterException;
+   /*
+    * I_CallbackDriver is an optional argument to use for callbacks instead
+    * of instantiating one via xmlBlaster.properties
+    */
+   //public ConnectReturnQos connect(ConnectQos qos, I_CallbackDriver cbDriver) throws XmlBlasterException;
+
+   public String connect(String qos_literal, String sessionId) throws XmlBlasterException;
    public ConnectReturnQos connect(ConnectQos qos, String sessionId) throws XmlBlasterException;
-   public void disconnect(String sessionId, String qos_literal) throws XmlBlasterException;
+
+   public String disconnect(String sessionId, String qos_literal) throws XmlBlasterException;
 }
 
 
