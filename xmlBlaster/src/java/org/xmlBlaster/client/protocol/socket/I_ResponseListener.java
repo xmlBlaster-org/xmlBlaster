7953f656e720c1bb7b9cf8ba13ad3c0eb2770777
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-02-15 12:55:36 +0000

    Changed to generic Object type
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@4078 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/client/protocol/socket/I_ResponseListener.java
+++ xmlBlaster/src/java/org/xmlBlaster/client/protocol/socket/I_ResponseListener.java
@@ -3,7 +3,7 @@ Name:      I_ResponseListener.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Event for asynchronous response from server
-Version:   $Id: I_ResponseListener.java,v 1.1 2002/02/14 15:01:15 ruff Exp $
+Version:   $Id: I_ResponseListener.java,v 1.2 2002/02/15 12:55:36 ruff Exp $
 Author:    michele.laghi@attglobal.net
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.client.protocol.socket;
@@ -18,8 +18,6 @@ import org.xmlBlaster.client.protocol.ConnectionException;
  */
 public interface I_ResponseListener
 {
-   public void responseEvent(String requestId, String qos);
-   public void responseEvent(String requestId, MessageUnit[] msgArr);
-   public void responseEvent(String requestId, XmlBlasterException e);
+   public void responseEvent(String requestId, Object response);
 }
 
