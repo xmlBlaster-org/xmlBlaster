4242b5f87839bc185fdbde1b5ea169907e3a5c05
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-02-18 17:36:12 +0000

    Javadoc
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@4122 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/protocol/socket/SocketDriver.java
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/socket/SocketDriver.java
@@ -3,7 +3,7 @@ Name:      SocketDriver.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   SocketDriver class to invoke the xmlBlaster server in the same JVM.
-Version:   $Id: SocketDriver.java,v 1.9 2002/02/16 16:33:12 ruff Exp $
+Version:   $Id: SocketDriver.java,v 1.10 2002/02/18 17:36:12 ruff Exp $
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.protocol.socket;
 
@@ -222,6 +222,14 @@ public class SocketDriver extends Thread implements I_Driver
    /**
     * Command line usage.
     * <p />
+    * <ul>
+    *  <li><i>-socket.port</i>        The SOCKET web server port [7607]</li>
+    *  <li><i>-socket.hostname</i>    Specify a hostname where the SOCKET web server runs
+    *                          Default is the localhost.</li>
+    *  <li><i>-socket.backlog</i>     Queue size for incoming connection request [50]</li>
+    *  <li><i>-socket.debug</i>       true switches on detailed SOCKET debugging [false]</li>
+    * </ul>
+    * <p />
     * Enforced by interface I_Driver.
     */
    public String usage()
@@ -231,7 +239,7 @@ public class SocketDriver extends Thread implements I_Driver
       text += "   -socket.port        The SOCKET web server port [7607].\n";
       text += "   -socket.hostname    Specify a hostname where the SOCKET web server runs.\n";
       text += "                       Default is the localhost.\n";
-      text += "   -socket.backlog     Queue size for incomming connection request [50].\n";
+      text += "   -socket.backlog     Queue size for incoming connection request [50].\n";
       text += "   -socket.debug       true switches on detailed SOCKET debugging [false].\n";
       text += "\n";
       return text;
