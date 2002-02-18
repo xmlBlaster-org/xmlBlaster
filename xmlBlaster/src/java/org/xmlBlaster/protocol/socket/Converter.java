7ff1d0f6eee69dc0945deff8242a6f82a0bd15d4
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-02-18 21:38:32 +0000

    Bugfix in toByte Logging
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@4123 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/protocol/socket/Converter.java
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/socket/Converter.java
@@ -3,7 +3,7 @@ Name:      Converter.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Converter class for raw socket messages
-Version:   $Id: Converter.java,v 1.5 2002/02/16 11:51:37 ruff Exp $
+Version:   $Id: Converter.java,v 1.6 2002/02/18 21:38:32 ruff Exp $
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.protocol.socket;
 
@@ -19,6 +19,8 @@ public class Converter
    public static final int NUM_FIELD_LEN = 10;
    public static final int FLAG_FIELD_LEN = 6;
    public static final int MAX_STRING_LEN = Integer.MAX_VALUE;
+   /** Set debug level */
+   public boolean SOCKET_DEBUG=false;
 
    protected long index = 0L;
 
@@ -39,12 +41,17 @@ public class Converter
     */
    public final long toLong(InputStream in) throws IOException {
       byteArray.reset();
+      /*
+      int ret = in.read(byteArray.getByteArray(), 0, NUM_FIELD_LEN);
+      if (ret != NUM_FIELD_LEN)
+         throw new IOException("Can't read expected " + NUM_FIELD_LEN + " bytes from socket, only " + ret + " received");
+      */
       for (int ii=0; ii<NUM_FIELD_LEN; ii++) {
          int val = readNext(in);
          if (val == 0)
             break;
          if (val == -1)
-            throw new IOException("Can't read expected " + NUM_FIELD_LEN + " bytes from socket");
+            throw new IOException("Can't read expected " + NUM_FIELD_LEN + " bytes from socket, only " + ii + " received");
          byteArray.write(val);
       }
       try {
@@ -56,13 +63,16 @@ public class Converter
    }
 
    /**
-    * Converts 10 bytes from InputStream to a long
+    * Reads the binary content of a message. First we parse the long value which
+    * holds the content length, than we retrieve the binary content. 
     */
    public final byte[] toByte(InputStream in) throws IOException {
       byteArray.reset();
-      long len = toLong(in);
+      long len = toLong0(in, 0L);
       if (len >= Integer.MAX_VALUE) throw new IllegalArgumentException("Length of data is bigger " + Integer.MAX_VALUE + " bytes");
       byte[] b = new byte[(int)len];
+      if (len == 0L)
+         return b;
       {
          in.read(b, 0, (int)len);
          index += len;
