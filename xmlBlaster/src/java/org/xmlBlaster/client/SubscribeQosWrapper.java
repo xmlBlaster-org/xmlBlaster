ff271b34f605bc1b6e8f842a3d3bf2954a147090
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-03-15 13:10:16 +0000

    Added support for SubscribeQosWrapper
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@4355 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/client/SubscribeQosWrapper.java
+++ xmlBlaster/src/java/org/xmlBlaster/client/SubscribeQosWrapper.java
@@ -3,12 +3,13 @@ Name:      SubscribeQosWrapper.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Handling one xmlQoS
-Version:   $Id: SubscribeQosWrapper.java,v 1.7 2002/03/13 16:41:08 ruff Exp $
+Version:   $Id: SubscribeQosWrapper.java,v 1.8 2002/03/15 13:10:16 ruff Exp $
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.client;
 
 import org.xmlBlaster.util.Log;
 import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.engine.helper.SubscribeFilterQos;
 import java.util.Vector;
 
 
@@ -21,6 +22,7 @@ import java.util.Vector;
  *        &lt;meta>false<&lt;/meta>      &lt;!-- Don't send me the xmlKey meta data on updates -->
  *        &lt;content>false&lt;/content> &lt;!-- Don't send me the content data on updates (notify only) -->
  *        &lt;local>false&lt;/local>     &lt;!-- Inhibit the delivery of messages to myself if i have published it -->
+ *        &lt;filter type='myPlugin' version='1.0'>a!=100&lt;/filter>  &lt;!-- Filters messages i have subscribed as implemented in your plugin -->
  *     &lt;/qos>
  * </pre>
  * <p />
@@ -33,10 +35,12 @@ public class SubscribeQosWrapper extends QosWrapper
    /** not yet supported */
    private boolean meta = true;
 
+   /** not yet supported */
    private boolean content = true;
 
    private boolean local = true;
 
+   private Vector filterVec = null;
 
 
    /**
@@ -60,13 +64,30 @@ public class SubscribeQosWrapper extends QosWrapper
 
 
    /**
-    * Inhibit the delivery of messages to myself if i have published it.
+    * false Inhibit the delivery of messages to myself if i have published it.
     */
-   public void setNoLocal()
+   public void setLocal(boolean local)
    {
-      this.local = false;
+      this.local = local;
    }
 
+   /**
+    * If false, the update contains not the content (it is a notify of change only)
+    * TODO: Implement in server!!!
+    */
+   public void setContent(boolean content)
+   {
+      this.content = content;
+   }
+
+   /**
+    * Adds your subplied subscribe filter
+    */
+   public void addSubscribeFilter(SubscribeFilterQos filter)
+   {
+      if (filterVec == null) filterVec = new Vector();
+      this.filterVec.addElement(filter);
+   }
 
    /**
     * Converts the data into a valid XML ASCII string.
@@ -85,11 +106,32 @@ public class SubscribeQosWrapper extends QosWrapper
    public String toXml()
    {
       StringBuffer sb = new StringBuffer();
-      sb.append("<qos>\n");
-      if (!meta) sb.append("   <meta>false</meta>\n");
-      if (!content) sb.append("   <content>false</content>\n");
-      if (!local) sb.append("   <local>false</local>\n");
-      sb.append("</qos>");
+      sb.append("<qos>");
+      if (!meta) sb.append("\n   <meta>false</meta>");
+      if (!content) sb.append("\n   <content>false</content>");
+      if (!local) sb.append("\n   <local>false</local>");
+      if (filterVec != null && filterVec.size() > 0) {
+         for (int ii=0; ii<filterVec.size(); ii++) {
+            SubscribeFilterQos filter = (SubscribeFilterQos)filterVec.elementAt(ii);
+            sb.append(filter.toXml());
+         }
+      }
+      sb.append("\n</qos>");
       return sb.toString();
+   }
+
+   /** For testing: java org.xmlBlaster.client.SubscribeQosWrapper */
+   public static void main(String[] args)
+   {
+      try {
+         SubscribeQosWrapper qos = new SubscribeQosWrapper();
+         qos.setContent(false);
+         qos.addSubscribeFilter(new SubscribeFilterQos("ContentLenFilter", "1.0", "800"));
+         qos.addSubscribeFilter(new SubscribeFilterQos("ContentLenFilter", "3.2", "a<10"));
+         System.out.println(qos.toXml());
+      }
+      catch (Throwable e) {
+         System.out.println("Test failed: " + e.toString());
+      }
    }
 }
