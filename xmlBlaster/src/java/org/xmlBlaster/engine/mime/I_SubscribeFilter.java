e3b374f95cca87a723914ea8eddf4f3c5fb4d034
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-03-15 17:24:35 +0000

    Javadoc
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@4360 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/engine/mime/I_SubscribeFilter.java
+++ xmlBlaster/src/java/org/xmlBlaster/engine/mime/I_SubscribeFilter.java
@@ -3,7 +3,7 @@ Name:      I_SubscribeFilter.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Interface hiding the real callback protocol
-Version:   $Id: I_SubscribeFilter.java,v 1.2 2002/03/15 13:04:02 ruff Exp $
+Version:   $Id: I_SubscribeFilter.java,v 1.3 2002/03/15 17:24:35 ruff Exp $
 Author:    ruff@swand.lake.de
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.engine.mime;
@@ -30,17 +30,17 @@ import org.xmlBlaster.engine.Global;
  *    <li>Code the plugin.<br />
  *        Code a plugin which inherits from this interface.
  *        In the match() method code your specific filter rule.
- *        You get the MessageUnit object, which contains the content
+ *        You get passed the MessageUnitWrapper object, which contains the content
  *        of a message. You look into the content and decide if the
  *        message matches your rule or not.
  *    </li>
  *    <li>Register the plugin.<br />
  *        Register the plugin in xmlBlaster.properties file, for example<br />
- *        MimeSubscribePlugin[ContentLenFilter][1.0]=org.xmlBlaster.engine.mime.demo.ContentLenFilter
+ *        MimeSubscribePlugin[ContentLenFilter][1.0]=org.xmlBlaster.engine.mime.demo.ContentLenFilter,DEFAULT_MAX_LEN=80000,VERBOSE=false
  *    </li>
  * </ul>
  *
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  * @author ruff@swand.lake.de
  */
 public interface I_SubscribeFilter
@@ -62,6 +62,9 @@ public interface I_SubscribeFilter
 
    /**
     * Check if the filter rule matches for this message. 
+    * <p />
+    * You may manipulate the content of the message, but not the key and qos or other attributes
+    * of the MessageUnitWrapper object.
     * @return true If the filter matches this message, else false
     * @param msgUnitWrapper  The message to check (access the raw message with msgUnitWrapper.getMessageUnit())
     * @param query   The query string containing the filter rule on subscribe usually
