f5fbbc4736c4d45ab3fb109cc27537175c5feba7
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-03-15 12:54:56 +0000

    Javadoc how to add options
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@4344 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/util/I_Plugin.java
+++ xmlBlaster/src/java/org/xmlBlaster/util/I_Plugin.java
@@ -4,7 +4,7 @@ import org.xmlBlaster.util.XmlBlasterException;
 
 /**
  * @author  goetzger
- * @version $Revision: 1.2 $ (State: $State) (Date: $Date: 2002/02/08 00:48:19 $)
+ * @version $Revision: 1.3 $ (State: $State) (Date: $Date: 2002/03/15 12:54:56 $)
  */
 
 public interface I_Plugin
@@ -12,6 +12,15 @@ public interface I_Plugin
    /**
     * This method is called by the PluginManager.
     * <p/>
+    * Example how options are evaluated:
+    * <pre>
+    *   MimeSubscribePlugin[ContentLenFilter][1.0]=org.xmlBlaster.engine.mime.demo.ContentLenFilter,DEFAULT_MAX_LEN=200,DEFAULT_MIN_LEN=20
+    *
+    *   options[0]=DEFAULT_MAX_LEN
+    *   options[1]=200
+    *   options[2]=DEFAULT_MAX_LEN
+    *   options[3]=20
+    * </pre>
     * @param String[] Some arguments from xmlBlaster.properties.
     */
    public void init(String[] options) throws XmlBlasterException;
