19903285bac2e2ad1b1c559ab3e51a629e489d1d
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-03-17 13:38:44 +0000

    Changed to support cbSessionId
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@4396 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/client/I_Callback.java
+++ xmlBlaster/src/java/org/xmlBlaster/client/I_Callback.java
@@ -3,7 +3,7 @@ Name:      I_Callback.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Helper to easy get the callback messages
-Version:   $Id: I_Callback.java,v 1.6 2000/10/18 20:45:42 ruff Exp $
+Version:   $Id: I_Callback.java,v 1.7 2002/03/17 13:38:44 ruff Exp $
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.client;
 
@@ -17,7 +17,7 @@ import org.xmlBlaster.util.XmlBlasterException;
  * You may use this, if you don't want to program with the rawer CORBA BlasterCallback.update()
  * or RMI or XML-RPC.
  *
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  * @author <a href="mailto:ruff@swand.lake.de">Marcel Ruff</a>.
  */
 public interface I_Callback
@@ -29,15 +29,14 @@ public interface I_Callback
     * So you should implement in your client the I_Callback interface -
     * suppling the update() method where you can do with the message whatever you want.
     * <p />
-    * If you do multiple logins with the same I_Callback implementation, the loginName
-    * which is delivered with this update() method may be used to dispatch the message
-    * to the correct client.
+    * The raw driver specific update() method (e.g. CORBA-BlasterCallback.update())
+    * is unpacked and for each arrived message this update is called.
     *
-    * @param loginName The name to whom the callback belongs
-    * @param updateKey The arrived key
-    * @param content   The arrived message content
-    * @param qos       Quality of Service of the MessageUnit
+    * @param cbSessionId The session ID specified by the client which registered the callback
+    * @param updateKey   The arrived key
+    * @param content     The arrived message content
+    * @param qos         Quality of Service of the MessageUnit
     */
-   public void update(String loginName, UpdateKey updateKey, byte[] content, UpdateQoS updateQoS) throws XmlBlasterException;
+   public void update(String cbSessionId, UpdateKey updateKey, byte[] content, UpdateQoS updateQoS) throws XmlBlasterException;
 }
 
