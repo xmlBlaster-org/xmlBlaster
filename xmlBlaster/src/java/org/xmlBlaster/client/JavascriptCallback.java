a0f6466d82acdbda1aa3731c6fdadc11e1c04840
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-03-28 15:57:43 +0000

    Changed to new update syntax
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@4499 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/client/JavascriptCallback.java
+++ xmlBlaster/src/java/org/xmlBlaster/client/JavascriptCallback.java
@@ -3,7 +3,7 @@ Name:      JavascriptCallback.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Helper to easy get the callback messages to invoke a javascript
-Version:   $Id: JavascriptCallback.java,v 1.1 2002/03/28 10:25:20 laghi Exp $
+Version:   $Id: JavascriptCallback.java,v 1.2 2002/03/28 15:57:43 ruff Exp $
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.client;
 
@@ -21,7 +21,7 @@ import org.mozilla.javascript.JavaScriptException;
  * You may use this, if you don't want to program with the rawer CORBA BlasterCallback.update()
  * or RMI or XML-RPC.
  *
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  * @author <a href="mailto:laghi@swissinfo.org">Michele Laghi</a>.
  */
 public class JavascriptCallback implements I_Callback
@@ -34,35 +34,31 @@ public class JavascriptCallback implements I_Callback
    }
 
    /**
-    * This is the callback method invoked from XmlBlasterConnection
-    * informing the client in an asynchronous mode about a new message.
-    * <p />
-    * So you should implement in your client the I_Callback interface -
-    * suppling the update() method where you can do with the message whatever you want.
-    * <p />
-    * If you do multiple logins with the same I_Callback implementation, the loginName
-    * which is delivered with this update() method may be used to dispatch the message
-    * to the correct client.
+    * This is the callback method invoked from xmlBlaster
+    * delivering us a new asynchronous message. 
     *
-    * @param loginName The name to whom the callback belongs
-    * @param updateKey The arrived key
-    * @param content   The arrived message content
-    * @param qos       Quality of Service of the MessageUnit
+    * @param cbSessionId The session ID specified by the client which registered the callback
+    * @param updateKey   The arrived key
+    * @param content     The arrived message content
+    * @param qos         Quality of Service of the MessageUnit
+    *
+    * @see org.xmlBlaster.client.I_Callback#update(String, UpdateKey, byte[], UpdateQoS)
     */
-   public void update(String loginName, UpdateKey updateKey, byte[] content, UpdateQoS updateQoS) throws XmlBlasterException
+   public String update(String cbSessionId, UpdateKey updateKey, byte[] content, UpdateQoS updateQoS) throws XmlBlasterException
    {
       try {
-      Object[] args = new Object[4];
-      args[0] = loginName;
-      args[1] = updateKey;
-      args[2] = new String(content);
-      args[3] = updateQoS;
+         Object[] args = new Object[4];
+         args[0] = cbSessionId;
+         args[1] = updateKey;
+         args[2] = new String(content);
+         args[3] = updateQoS;
 
-      ScriptableObject.callMethod(this.script, "update", args);
+         ScriptableObject.callMethod(this.script, "update", args);
       }
       catch (JavaScriptException ex) {
-         throw new Error(ex.toString());
+         throw new XmlBlasterException("JavascriptCallback.update()", ex.toString());
       }
+      return "";
    }
 
 }
