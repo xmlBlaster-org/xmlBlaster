03e9a0d3e5aad1368b12edfc8b76bcd9cf73ca41
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-05-09 15:18:45 +0000

    Starting tests on class level
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@4906 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/testsuite/org/xmlBlaster/classtest/AllTests.java
@@ -0,0 +1,22 @@
+package classtest;
+
+import junit.framework.*;
+
+/**
+ * TestSuite that runs all the sample tests. 
+ * <pre>
+ * java classtest.AllTests
+ * java -Djava.compiler= classtest.AllTests
+ * java -Djava.compiler= junit.ui.TestRunner -noloading classtest.AllTests
+ * </pre>
+ */
+public class AllTests {
+   public static void main (String[] args) {
+      junit.textui.TestRunner.run (suite());
+   }
+   public static Test suite ( ) {
+      TestSuite suite= new TestSuite("All xmlBlaster class tests");
+      suite.addTest(new TestSuite(classtest.XmlKeySaxTest.class));
+      return suite;
+   }
+}
