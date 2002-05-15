f125c4d2e9593ad652c1f501a2326b10b0a7e67f
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-05-15 17:32:34 +0000

    Under construction
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@4971 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/demo/javaclients/HelloWorld4.java
@@ -0,0 +1,105 @@
+// xmlBlaster/demo/javaclients/HelloWorld4.java
+import org.jutils.log.LogChannel;
+import org.xmlBlaster.util.*;
+import org.xmlBlaster.client.*;
+import org.xmlBlaster.client.protocol.XmlBlasterConnection;
+import org.xmlBlaster.engine.helper.MessageUnit;
+
+
+/**
+ * This client connects to xmlBlaster in fail save mode and uses specific update handlers. 
+ * <p />
+ * In fail save mode the client will poll for the xmlBlaster server and
+ * queue messages until the server is available.
+ * <p />
+ * Invoke: java HelloWorld4
+ * <p />
+ * Invoke: java HelloWorld4 -loginName joe -passwd secret
+ */
+public class HelloWorld4
+{
+   private final String ME = "HelloWorld4";
+
+   public HelloWorld4(final Global glob) {
+      
+      final LogChannel log = glob.getLog(null);
+      
+      XmlBlasterConnection con = null;
+
+      try {
+         con = new XmlBlasterConnection(glob);
+
+         con.initFailSave(new I_ConnectionProblems() {
+               public void reConnected() {
+                  log.info(ME, "I_ConnectionProblems: We were lucky, reconnected to " + glob.getId());
+               }
+               public void lostConnection() {
+                  log.warn(ME, "I_ConnectionProblems: Lost connection to " + glob.getId());
+               }
+            });
+
+         ConnectQos qos = new ConnectQos(glob);
+         ConnectReturnQos conRetQos = con.connect(qos, new I_Callback() {
+            public String update(String cbSessionId, UpdateKey updateKey, byte[] content, UpdateQos updateQos) {
+               log.info(ME, "Reveiving asynchronous message in default handler '" + updateKey.getOid() + "'");
+               return "";
+            }
+         });  // Login to xmlBlaster, default handler for updates
+
+         log.info(ME, "Connected to xmlBlaster.");
+
+         SubscribeKeyWrapper sk = new SubscribeKeyWrapper("HelloWorld4");
+         SubscribeQosWrapper sq = new SubscribeQosWrapper();
+         String subId = con.subscribe(sk.toXml(), sq.toXml(), new I_Callback() {
+            public String update(String cbSessionId, UpdateKey updateKey, byte[] content, UpdateQos updateQos) {
+               log.info(ME, "Reveiving asynchronous message in HelloWorld4 handler '" + updateKey.getOid() + "'");
+               return "";
+            }
+         });  // subscribe with our specific update handler
+
+
+         PublishKeyWrapper pk = new PublishKeyWrapper("HelloWorld4", "text/plain", "1.0");
+         PublishQosWrapper pq = new PublishQosWrapper();
+         MessageUnit msgUnit = new MessageUnit(pk.toXml(), "Hi".getBytes(), pq.toXml());
+         String retQos = con.publish(msgUnit);
+         log.info(ME, "Published message '" + pk.getOid() + "'");
+
+         pk = new PublishKeyWrapper("SomeOtherMessage", "text/plain", "1.0");
+         pq = new PublishQosWrapper();
+         msgUnit = new MessageUnit(pk.toXml(), "Ho".getBytes(), pq.toXml());
+         retQos = con.publish(msgUnit);
+         log.info(ME, "Published message '" + pk.getOid() + "'");
+      }
+      catch (XmlBlasterException e) {
+         log.error(ME, "Houston, we have a problem: " + e.toString());
+      }
+      finally {
+         if (con != null) {
+            con.disconnect(null);
+         }
+      }
+
+      log.info(ME, "Success, hit a key to exit");
+      char c;
+      try { System.in.read(); } catch(java.io.IOException e) {}
+   }
+
+   /**
+    * Try
+    * <pre>
+    *   java HelloWorld4 -help
+    * </pre>
+    * for usage help
+    */
+   public static void main(String args[]) {
+      Global glob = new Global();
+      
+      if (glob.init(args) != 0) { // Get help with -help
+         XmlBlasterConnection.usage();
+         Log.usage();
+         Log.exit("", "Example: java HelloWorld4 -loginName Jeff\n");
+      }
+
+      new HelloWorld4(glob);
+   }
+}
