cee0aef43b37cab3170ea2e932c6a33d21ac16f3
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-05-26 16:32:36 +0000

    Introduced checkForVolatileErase() Changed to use "cb" LogChannel
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@5081 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/engine/callback/CbWorker.java
+++ xmlBlaster/src/java/org/xmlBlaster/engine/callback/CbWorker.java
@@ -3,13 +3,13 @@ Name:      CbWorker.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Holding messages waiting on client callback.
-Version:   $Id: CbWorker.java,v 1.6 2002/03/22 14:12:02 ruff Exp $
+Version:   $Id: CbWorker.java,v 1.7 2002/05/26 16:32:36 ruff Exp $
 Author:    ruff@swand.lake.de
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.engine.callback;
 
 import org.jutils.runtime.Sleeper;
-import org.xmlBlaster.util.Log;
+import org.jutils.log.LogChannel;
 import org.xmlBlaster.util.XmlBlasterException;
 import org.xmlBlaster.engine.Global;
 import org.xmlBlaster.engine.queue.MsgQueue;
@@ -25,17 +25,19 @@ public class CbWorker implements Runnable
    public final String ME;
    private MsgQueue msgQueue;
    private Global glob;
+   private final LogChannel log;
 
    public CbWorker(Global glob, MsgQueue q)
    {
       ME = "CbWorker:" + q.getName(); 
       this.glob = glob;
+      this.log = glob.getLog("cb");
       this.msgQueue = q;
    }
 
    public void run()
    {
-      if (Log.CALL) Log.call(ME, "Starting callback job with " + msgQueue.size() + " entries.");
+      if (log.CALL) log.call(ME, "Starting callback job with " + msgQueue.size() + " entries.");
       MsgQueueEntry[] entries = null;
       try {
          CbInfo cbInfo = this.msgQueue.getCbInfo();
@@ -44,55 +46,40 @@ public class CbWorker implements Runnable
                entries = this.msgQueue.takeMsgs();
             }
             if (entries == null || entries.length < 1) {
-               Log.warn(ME, "Got zero messages from queue, expected at least one");
+               log.warn(ME, "Got zero messages from queue, expected at least one");
                return;
             }
             
-            //Log.info(ME, "Sending now " + entries.length + " messages back ...");
+            //log.info(ME, "Sending now " + entries.length + " messages back ...");
             
             cbInfo.sendUpdate(entries, this.msgQueue.getErrorCounter()); // redeliver == errorCounter
             
             this.msgQueue.resetErrorCounter(); // callback is fine
 
             // Delete volatile messages ...
-            for (int ii=0; ii<entries.length; ii++) {
-               entries[ii].getMessageUnitWrapper().addEnqueueCounter(-1);
-               if (entries[ii].getMessageUnitWrapper().getEnqueueCounter() == 0 &&
-                     entries[ii].getMessageUnitWrapper().getPublishQos().isVolatile()) {
-                  if (msgQueue instanceof SessionMsgQueue) {
-                     SessionMsgQueue q = (SessionMsgQueue)msgQueue;
-                     try {
-                        glob.getRequestBroker().eraseVolatile(q.getSessionInfo(), entries[ii].getMessageUnitWrapper().getMessageUnitHandler());
-                     }
-                     catch (XmlBlasterException ex) {
-                        ex.printStackTrace();
-                        Log.error(ME, "Erasing of volatile message failed");
-                     }
-                  }
-               }
-            }
+            this.msgQueue.checkForVolatileErase(entries);
 
-            //Log.info(ME, "Sending of " + entries.length + " messages done");
+            //log.info(ME, "Sending of " + entries.length + " messages done");
          }
          else
-            Log.error(ME, "No CbInfo available");
+            log.error(ME, "No CbInfo available");
       }
       catch(Throwable e) {
          if (e instanceof XmlBlasterException) {
-            Log.warn(ME, "Callback failed: " + e.toString());
+            log.warn(ME, "Callback failed: " + e.toString());
          }
          else {
-            Log.error(ME, "Callback failed: " + e.toString());
+            log.error(ME, "Callback failed: " + e.toString());
             e.printStackTrace();
          }
          if (entries != null) {
-            Log.info(ME, "Recovering " + entries.length + " messages");
+            if (log.TRACE) log.trace(ME, "Recovering " + entries.length + " messages");
             try {
                this.msgQueue.putMsgs(entries); // recover the messages
             }
             catch (Throwable e2) {
                e2.printStackTrace();
-               Log.error(ME, "Disaster: Can't recover " + entries.length + " messages - the messages are lost: " + e2.toString());
+               log.error(ME, "Disaster: Can't recover " + entries.length + " messages - the messages are lost: " + e2.toString());
             }
          }
          msgQueue.connectionLost();  // TODO: We should distinguish which address in CbInfo failed
@@ -101,14 +88,14 @@ public class CbWorker implements Runnable
          //synchronized (this.msgQueue.getMonitor()) {
             this.msgQueue.setCbWorkerIsActive(false);
             if (msgQueue.size() > 0 && !msgQueue.isShutdown()) {
-               // Log.info(ME, "Finished callback job. Giving a kick to send the remaining " + msgQueue.size() + " messages.");
-               if (Log.TRACE) Log.trace(ME, "Finished callback job. Giving a kick to send the remaining " + msgQueue.size() + " messages.");
-               try{ msgQueue.activateCallbackWorker(); } catch(Throwable e) { Log.error(ME, e.toString()); }// Assure the queue is flushed with another worker
+               // log.info(ME, "Finished callback job. Giving a kick to send the remaining " + msgQueue.size() + " messages.");
+               if (log.TRACE) log.trace(ME, "Finished callback job. Giving a kick to send the remaining " + msgQueue.size() + " messages.");
+               try{ msgQueue.activateCallbackWorker(); } catch(Throwable e) { log.error(ME, e.toString()); }// Assure the queue is flushed with another worker
             }
          //}
       }
 
-      //Log.info(ME, "Finished callback job. " + entries.length + " sent, " + msgQueue.size() + " messages in the queue. " + msgQueue.getCbWorkerPoolStatistic());
+      //log.info(ME, "Finished callback job. " + entries.length + " sent, " + msgQueue.size() + " messages in the queue. " + msgQueue.getCbWorkerPoolStatistic());
    }
 }
 
