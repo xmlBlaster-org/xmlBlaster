e212f4dc953d122f2b0c603ddd378675a364810b
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-05-27 16:23:47 +0000

    Added handling methods and inherit from I_XmlBlaster
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@5108 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/util/I_InvocationRecorder.java
+++ xmlBlaster/src/java/org/xmlBlaster/util/I_InvocationRecorder.java
@@ -3,12 +3,13 @@ Name:      I_InvocationRecorder.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Interface
-Version:   $Id: I_InvocationRecorder.java,v 1.2 2000/06/25 18:32:43 ruff Exp $
+Version:   $Id: I_InvocationRecorder.java,v 1.3 2002/05/27 16:23:47 ruff Exp $
 Author:    ruff@swand.lake.de
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.util;
 
 import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.client.protocol.I_XmlBlaster;
 import org.xmlBlaster.engine.helper.MessageUnit;
 
 /**
@@ -16,12 +17,11 @@ import org.xmlBlaster.engine.helper.MessageUnit;
  * <p />
  * @see InvocationRecorder
  */
-public interface I_InvocationRecorder
+public interface I_InvocationRecorder extends I_XmlBlaster
 {
-   public java.lang.String subscribe(java.lang.String xmlKey, java.lang.String qos) throws XmlBlasterException;
-   public org.xmlBlaster.engine.helper.MessageUnit[] get(java.lang.String xmlKey, java.lang.String qos) throws XmlBlasterException;
-   public void unSubscribe(java.lang.String xmlKey, java.lang.String qos) throws XmlBlasterException;
-   public java.lang.String publish(org.xmlBlaster.engine.helper.MessageUnit msgUnit) throws XmlBlasterException;
-   public java.lang.String[] publishArr(org.xmlBlaster.engine.helper.MessageUnit[] msgUnitArr) throws XmlBlasterException;
-   public java.lang.String[] erase(java.lang.String xmlKey, java.lang.String qos) throws XmlBlasterException;
+   public boolean isFull() throws XmlBlasterException;
+   public int size();
+   public long getNumLost();
+   public void playback(long startDate, long endDate, double motionFactor) throws XmlBlasterException;
+   public void reset();
 }
