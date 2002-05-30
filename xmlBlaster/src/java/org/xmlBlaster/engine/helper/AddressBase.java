48f160eb73dc85daa3db23135b29ddd9bed2dda5
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-05-30 16:29:06 +0000

    Added getName() Added getOnExhaust() Added check for pingInterval < 10L Changed to use LogChannel
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@5172 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/engine/helper/AddressBase.java
+++ xmlBlaster/src/java/org/xmlBlaster/engine/helper/AddressBase.java
@@ -3,11 +3,11 @@ Name:      AddressBase.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Holding connect address and callback address string including protocol
-Version:   $Id: AddressBase.java,v 1.12 2002/05/19 17:53:47 ruff Exp $
+Version:   $Id: AddressBase.java,v 1.13 2002/05/30 16:29:06 ruff Exp $
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.engine.helper;
 
-import org.xmlBlaster.util.Log;
+import org.jutils.log.LogChannel;
 import org.xmlBlaster.util.Global;
 import org.xml.sax.Attributes;
 
@@ -24,6 +24,7 @@ public abstract class AddressBase
 {
    private static final String ME = "AddressBase";
    protected final Global glob;
+   protected final LogChannel log;
 
    /** The root xml element: &lt;callback> or &lt;address>, is set from the derived class */
    protected String rootTag = null;
@@ -94,13 +95,21 @@ public abstract class AddressBase
     */
    public AddressBase(Global glob, String rootTag) {
       this.glob = glob;
+      this.log = glob.getLog("core");
       setRootTag(rootTag);
+   }
+
+   /**
+    * A nice human readable name for this address (used for logging)
+    */
+   public final String getName() {
+      return hostname+":"+port;
    }
 
    /**
     * Check if supplied address would connect to the address of this instance
     */
-   public final boolean isSameAddress(Address other) {
+   public final boolean isSameAddress(AddressBase other) {
       String oa = other.getAddress();
       if (oa != null && oa.length() > 1 && oa.equals(getAddress()))
          return true;
@@ -205,10 +214,21 @@ public abstract class AddressBase
    }
 
    /**
+    * What to do if max retries is exhausted. 
+    * <p />
+    * This mode is currently not configurable, we always destroy the login session. 
+    * This is interpreted only server side if callback fails.
+    * @return Constants.ONEXHAUST_KILL_SESSION="killSession"
+    */
+   public final String getOnExhaust() {
+      return Constants.ONEXHAUST_KILL_SESSION; // in future possibly Constants.ONEXHAUST_KILL_CALLBACK
+   }
+
+   /**
     * BurstMode: The time span to collect messages before sending. 
     * @return The time to collect in milliseconds
     */
-   public long getCollectTime() {
+   public final long getCollectTime() {
       return collectTime;
    }
 
@@ -216,7 +236,7 @@ public abstract class AddressBase
     * BurstMode: The time span to collect oneway messages before sending. 
     * @return The time to collect in milliseconds
     */
-   public long getCollectTimeOneway() {
+   public final long getCollectTimeOneway() {
       return collectTimeOneway;
    }
 
@@ -246,7 +266,7 @@ public abstract class AddressBase
     * How long to wait between pings to the callback server. 
     * @return The pause time between pings in millis
     */
-   public long getPingInterval() {
+   public final long getPingInterval() {
       return pingInterval;
    }
 
@@ -257,6 +277,10 @@ public abstract class AddressBase
    public void setPingInterval(long pingInterval) {
       if (pingInterval < 0L)
          this.pingInterval = 0L;
+      else if (pingInterval < 10L) {
+         log.warn(ME, "pingInterval=" + pingInterval + " msec is too short, setting it to 10 millis");
+         this.pingInterval = 10L;
+      }
       else
          this.pingInterval = pingInterval;
    }
@@ -265,7 +289,7 @@ public abstract class AddressBase
     * How often shall we retry callback attempt on callback failure
     * @return -1 forever, 0 no retry, > 0 number of retries
     */
-   public int getRetries() {
+   public final int getRetries() {
       return retries;
    }
 
@@ -284,7 +308,7 @@ public abstract class AddressBase
     * Delay between callback retries in milliseconds, defaults to one minute
     * @return The delay in millisconds
     */
-   public long getDelay() {
+   public final long getDelay() {
       return delay;
    }
 
@@ -303,7 +327,7 @@ public abstract class AddressBase
     * Is only with CORBA and our native SOCKET protocol supported
     * @return true if you want to force oneway sending
     */
-   public boolean oneway() {
+   public final boolean oneway() {
       return oneway;
    }
 
@@ -316,11 +340,17 @@ public abstract class AddressBase
       this.oneway = oneway;
    }
 
+   /**
+    * @param Set if we accept point to point messages
+    */
    public void setPtpAllowed(boolean ptpAllowed) {
       this.ptpAllowed = ptpAllowed;
    }
 
-   public boolean isPtpAllowed() {
+   /**
+    * @return true if we may send PtP messages
+    */
+   public final boolean isPtpAllowed() {
       return this.ptpAllowed;
    }
 
@@ -330,14 +360,14 @@ public abstract class AddressBase
 
       // TODO !!!
       if (compressType.length() > 0)
-         Log.warn(ME, "Compression of messages is not yet supported");
+         log.warn(ME, "Compression of messages is not yet supported");
    }
 
    /**
     * The identifier sent to the callback client, the client can decide if he trusts this invocation
     * @return never null
     */
-   public String getSessionId() {
+   public final String getSessionId() {
       return sessionId;
    }
 
@@ -350,7 +380,7 @@ public abstract class AddressBase
     * Get the compression method. 
     * @return "" No compression
     */
-   public String getCompressType() {
+   public final String getCompressType() {
       return compressType;
    }
 
@@ -378,7 +408,7 @@ public abstract class AddressBase
     * Called for SAX callback start tag
     */
    public final void startElement(String uri, String localName, String name, StringBuffer character, Attributes attrs) {
-      // Log.info(ME, "startElement(rootTag=" + rootTag + "): name=" + name + " character='" + character.toString() + "'");
+      // log.info(ME, "startElement(rootTag=" + rootTag + "): name=" + name + " character='" + character.toString() + "'");
 
       String tmp = character.toString().trim(); // The address
       if (tmp.length() > 0) {
@@ -401,7 +431,7 @@ public abstract class AddressBase
                   try {
                      setPort(new Integer(ll).intValue());
                   } catch (NumberFormatException e) {
-                     Log.error(ME, "Wrong format of <" + rootTag + " port='" + ll + "'>, expected an integer number.");
+                     log.error(ME, "Wrong format of <" + rootTag + " port='" + ll + "'>, expected an integer number.");
                   }
                }
                else if( attrs.getQName(i).equalsIgnoreCase("sessionId") ) {
@@ -412,7 +442,7 @@ public abstract class AddressBase
                   try {
                      setPingInterval(new Long(ll).longValue());
                   } catch (NumberFormatException e) {
-                     Log.error(ME, "Wrong format of <" + rootTag + " pingInterval='" + ll + "'>, expected a long in milliseconds.");
+                     log.error(ME, "Wrong format of <" + rootTag + " pingInterval='" + ll + "'>, expected a long in milliseconds.");
                   }
                }
                else if( attrs.getQName(i).equalsIgnoreCase("retries") ) {
@@ -420,7 +450,7 @@ public abstract class AddressBase
                   try {
                      setRetries(new Integer(ll).intValue());
                   } catch (NumberFormatException e) {
-                     Log.error(ME, "Wrong format of <" + rootTag + " retries='" + ll + "'>, expected an integer number.");
+                     log.error(ME, "Wrong format of <" + rootTag + " retries='" + ll + "'>, expected an integer number.");
                   }
                }
                else if( attrs.getQName(i).equalsIgnoreCase("delay") ) {
@@ -428,7 +458,7 @@ public abstract class AddressBase
                   try {
                      setDelay(new Long(ll).longValue());
                   } catch (NumberFormatException e) {
-                     Log.error(ME, "Wrong format of <" + rootTag + " delay='" + ll + "'>, expected a long in milliseconds.");
+                     log.error(ME, "Wrong format of <" + rootTag + " delay='" + ll + "'>, expected a long in milliseconds.");
                   }
                }
                else if( attrs.getQName(i).equalsIgnoreCase("oneway") ) {
@@ -438,16 +468,16 @@ public abstract class AddressBase
                   this.useForSubjectQueue = new Boolean(attrs.getValue(i).trim()).booleanValue();
                }
                else {
-                  Log.error(ME, "Ignoring unknown attribute " + attrs.getQName(i) + " in " + rootTag + " section.");
+                  log.error(ME, "Ignoring unknown attribute " + attrs.getQName(i) + " in " + rootTag + " section.");
                }
             }
          }
          if (getType() == null) {
-            Log.error(ME, "Missing '" + rootTag + "' attribute 'type' in QoS");
+            log.error(ME, "Missing '" + rootTag + "' attribute 'type' in QoS");
             setType("IOR");
          }
          if (getSessionId() == null) {
-            Log.warn(ME, "Missing '" + rootTag + "' attribute 'sessionId' QoS");
+            log.warn(ME, "Missing '" + rootTag + "' attribute 'sessionId' QoS");
          }
          return;
       }
@@ -462,7 +492,7 @@ public abstract class AddressBase
                   try {
                      setCollectTime(new Long(ll).longValue());
                   } catch (NumberFormatException e) {
-                     Log.error(ME, "Wrong format of <burstMode collectTime='" + ll + "'>, expected a long in milliseconds, burst mode is switched off sync messages.");
+                     log.error(ME, "Wrong format of <burstMode collectTime='" + ll + "'>, expected a long in milliseconds, burst mode is switched off sync messages.");
                   }
                }
                else if (attrs.getQName(ii).equalsIgnoreCase("collectTimeOneway")) {
@@ -470,13 +500,13 @@ public abstract class AddressBase
                   try {
                      setCollectTimeOneway(new Long(ll).longValue());
                   } catch (NumberFormatException e) {
-                     Log.error(ME, "Wrong format of <burstMode collectTimeOneway='" + ll + "'>, expected a long in milliseconds, burst mode is switched off for oneway messages.");
+                     log.error(ME, "Wrong format of <burstMode collectTimeOneway='" + ll + "'>, expected a long in milliseconds, burst mode is switched off for oneway messages.");
                   }
                }
             }
          }
          else {
-            Log.error(ME, "Missing 'collectTime' or 'collectTimeOneway' attribute in login-qos <burstMode>");
+            log.error(ME, "Missing 'collectTime' or 'collectTimeOneway' attribute in login-qos <burstMode>");
          }
          return;
       }
@@ -493,13 +523,13 @@ public abstract class AddressBase
                   try {
                      setMinSize(new Long(ll).longValue());
                   } catch (NumberFormatException e) {
-                     Log.error(ME, "Wrong format of <compress minSize='" + ll + "'>, expected a long in bytes, compress is switched off.");
+                     log.error(ME, "Wrong format of <compress minSize='" + ll + "'>, expected a long in bytes, compress is switched off.");
                   }
                }
             }
          }
          else {
-            Log.error(ME, "Missing 'type' attribute in qos <compress>");
+            log.error(ME, "Missing 'type' attribute in qos <compress>");
          }
          return;
       }
@@ -518,7 +548,7 @@ public abstract class AddressBase
          if (tmp.length() > 0)
             setAddress(tmp);
          else if (getAddress() == null)
-            Log.error(ME, rootTag + " QoS contains no address data");
+            log.error(ME, rootTag + " QoS contains no address data");
       }
       else if (name.equalsIgnoreCase("burstMode")) {
       }
