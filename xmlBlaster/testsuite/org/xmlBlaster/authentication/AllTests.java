5de79fb3a8f672b8ce33cb9878a6c3a922ca4de8
stelzl <stelzl@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-06-06 13:39:32 +0000

    Collect tests
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@5284 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/testsuite/org/xmlBlaster/authentication/AllTests.java
@@ -0,0 +1,27 @@
+package authentication;
+
+import junit.framework.*;
+
+/**
+ * TestSuite that runs all the sample tests. 
+ * <pre>
+ * java authentication.AllTests
+ * java -Djava.compiler= authentication.AllTests
+ * java -Djava.compiler= junit.ui.TestRunner -noloading authentication.AllTests
+ * </pre>
+ */
+public class AllTests {
+   public static void main (String[] args) {
+      junit.textui.TestRunner.run (suite());
+   }
+   public static Test suite ( ) {
+      TestSuite suite= new TestSuite("All xmlBlaster authentication tests");
+      suite.addTest(new TestSuite(authentication.TestAuthenticationHtPassWd.class));
+      return suite;
+   }
+   // To force compilation:
+   public void dummy() {
+      new TestAuthenticationHtPassWd(null); 
+   }
+}
+
