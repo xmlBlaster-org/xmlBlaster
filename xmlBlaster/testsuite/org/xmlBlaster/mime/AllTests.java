5eb5545c503cd6124396f3d907b55753efa15d1b
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-06-08 20:00:14 +0000

    Mime specific tests
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@5304 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/testsuite/org/xmlBlaster/mime/AllTests.java
@@ -0,0 +1,30 @@
+package testsuite.org.xmlBlaster.mime;
+
+import junit.framework.*;
+
+/**
+ * TestSuite that runs all the sample tests. 
+ * <pre>
+ * java clustertest.AllTests
+ * java -Djava.compiler= clustertest.AllTests
+ * java -Djava.compiler= junit.ui.TestRunner -noloading clustertest.AllTests
+ * </pre>
+ */
+public class AllTests {
+   public static void main (String[] args) {
+      junit.textui.TestRunner.run (suite());
+   }
+   public static Test suite ( ) {
+      TestSuite suite= new TestSuite("All xmlBlaster cluster tests");
+      suite.addTest(TestGetRegexFilter.suite());
+      suite.addTest(TestGetFilter.suite());
+      suite.addTest(TestSubscribeFilter.suite());
+      return suite;
+   }
+   // To force compilation:
+   public void dummy() {
+      new TestGetRegexFilter(null,null,null); 
+      new TestGetFilter(null,null,null); 
+      new TestSubscribeFilter(null,null,null); 
+   }
+}
