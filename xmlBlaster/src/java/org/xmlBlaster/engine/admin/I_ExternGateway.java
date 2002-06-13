b1caf1b414c0742630889782a9a387cc24f39306
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-06-13 10:41:00 +0000

    Interface to decouple external gateways like SNMP or telnet
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@5437 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/engine/admin/I_ExternGateway.java
@@ -0,0 +1,38 @@
+/*------------------------------------------------------------------------------
+Name:      I_ExternGateway.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Interface to hide extern gateway implementations
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.engine.admin;
+
+import org.xmlBlaster.client.protocol.XmlBlasterConnection;
+import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.engine.Global;
+import org.xmlBlaster.engine.cluster.ClusterManager;
+
+import java.util.Set;
+
+/**
+ * Interface to allow different external gateway implementations. 
+ * <p />
+ * @author ruff@swand.lake.de
+ * @since 0.79f
+ */
+public interface I_ExternGateway {
+   /**
+    * This is called after creation of the plugin. 
+    * <p />
+    * You should register yourself with commandManager.register() during initialization.
+    *
+    * @param glob The Global handle of this xmlBlaster server instance.
+    * @param commandManager My manager
+    * @return false Ignore this implementation (gateway is switched off)
+    */
+   public boolean initialize(Global glob, CommandManager commandManager) throws XmlBlasterException;
+
+   /** Get a human readable name of this filter implementation */
+   public String getName();
+
+   public void shutdown();
+}
