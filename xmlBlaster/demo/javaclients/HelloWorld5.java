e408c0632cad75c2d140c4aa2680d86ebcdafdc4
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-07-07 21:23:40 +0000

    Typo
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@5838 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/demo/javaclients/HelloWorld5.java
+++ xmlBlaster/demo/javaclients/HelloWorld5.java
@@ -35,7 +35,7 @@ public class HelloWorld5
             ConnectQos qos = new ConnectQos(glob, senderName, "secret");
             ConnectReturnQos conRetQos = sender.connect(qos, new I_Callback() {
                public String update(String cbSessionId, UpdateKey updateKey, byte[] content, UpdateQos updateQos) {
-                  log.info(senderName, "Reveiving asynchronous message '" + updateKey.getOid() + "' in default handler");
+                  log.info(senderName, "Receiving asynchronous message '" + updateKey.getOid() + "' in default handler");
                   return "";
                }
             });  // Login to xmlBlaster, default handler for updates
@@ -50,7 +50,7 @@ public class HelloWorld5
             ConnectQos qos = new ConnectQos(glob, receiverName, "secret");
             ConnectReturnQos conRetQos = receiver.connect(qos, new I_Callback() {
                public String update(String cbSessionId, UpdateKey updateKey, byte[] content, UpdateQos updateQos) {
-                  log.info(receiverName, "Reveiving asynchronous message '" + updateKey.getOid() + "' in default handler");
+                  log.info(receiverName, "Receiving asynchronous message '" + updateKey.getOid() + "' in default handler");
 
                   try {
                      // Send an ACK back ...
