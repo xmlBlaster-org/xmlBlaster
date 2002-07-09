d3829f193b4854ba6de9257776d61e8c43716d0e
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-07-09 18:34:41 +0000

    First start to test ConnectQos
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@5864 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/org/xmlBlaster/classtest/AllTests.java
+++ xmlBlaster/testsuite/org/xmlBlaster/classtest/AllTests.java
@@ -25,6 +25,7 @@ public class AllTests {
       suite.addTest(new TestSuite(classtest.EraseRetQosTest.class));
       suite.addTest(new TestSuite(classtest.SubscribeRetQosTest.class));
       suite.addTest(new TestSuite(classtest.PublishQosTest.class));
+      suite.addTest(new TestSuite(classtest.ConnectQosTest.class));
       suite.addTest(new TestSuite(classtest.DisconnectQosTest.class));
       return suite;
    }
