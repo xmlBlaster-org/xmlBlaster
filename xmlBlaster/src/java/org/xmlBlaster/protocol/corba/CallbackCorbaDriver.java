eea83d763a4334c1159f2269fafb534a31fbd940
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-08-04 16:29:58 +0000

    Added log
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@5968 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/protocol/corba/CallbackCorbaDriver.java
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/corba/CallbackCorbaDriver.java
@@ -3,7 +3,7 @@ Name:      CallbackCorbaDriver.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   This singleton sends messages to clients using CORBA
-Version:   $Id: CallbackCorbaDriver.java,v 1.26 2002/06/25 17:43:28 ruff Exp $
+Version:   $Id: CallbackCorbaDriver.java,v 1.27 2002/08/04 16:29:58 ruff Exp $
 Author:    ruff@swand.lake.de
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.protocol.corba;
@@ -23,7 +23,7 @@ import org.xmlBlaster.protocol.corba.clientIdl.BlasterCallbackHelper;
  * <p>
  * The BlasterCallback.update() method of the client will be invoked
  *
- * @version $Revision: 1.26 $
+ * @version $Revision: 1.27 $
  * @author $Author: ruff $
  */
 public class CallbackCorbaDriver implements I_CallbackDriver
@@ -173,6 +173,7 @@ public class CallbackCorbaDriver implements I_CallbackDriver
     */
    public void shutdown()
    {
+      if (log.CALL) log.call(ME, "Entering shutdown ...");
       // CorbaDriver.getOrb().disconnect(cb); TODO: !!! must be called delayed, otherwise the logout() call from the client is aborted with a CORBA exception
       cb._release();
       //cbfactory.releaseCb(cb);
