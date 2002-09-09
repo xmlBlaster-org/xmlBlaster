97ab6c7d2f36f77c67f3c7e220be8739c5bdb787
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-09-09 13:37:49 +0000

    Main interface for EmbeddedXmlBlaster
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@6200 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/I_Main.java
@@ -0,0 +1,20 @@
+/*------------------------------------------------------------------------------
+Name:      I_Main.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+package org.xmlBlaster;
+
+
+/**
+ * I_Main interface is mimized interface to control Main.java. 
+ * <p />
+ * It allows instantiating xmlBlaster in EmbeddedXmlBlaster with a specific classloader.
+ */
+public interface I_Main
+{
+   public void init(java.util.Properties p);
+   public void shutdown();
+   public boolean isHalted();
+   public org.xmlBlaster.engine.Global getGlobal();
+}
