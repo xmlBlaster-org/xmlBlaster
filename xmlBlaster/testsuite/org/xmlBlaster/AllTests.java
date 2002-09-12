a9ff745e8935ec4f1b04a6e1ef9b83d7429f24ad
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-09-12 09:45:03 +0000

    Added stress directory
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@6236 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/org/xmlBlaster/AllTests.java
+++ xmlBlaster/testsuite/org/xmlBlaster/AllTests.java
@@ -23,6 +23,7 @@ public class AllTests extends junit.framework.TestCase
     suite.addTest(classtest.AllTests.suite());
     suite.addTest(testsuite.org.xmlBlaster.TestAll.suite());
     suite.addTest(authentication.AllTests.suite());
+    suite.addTest(testsuite.org.xmlBlaster.stress.AllTests.suite());
     suite.addTest(testsuite.org.xmlBlaster.mime.AllTests.suite());
     suite.addTest(testsuite.org.xmlBlaster.jdbc.AllTests.suite());
     suite.addTest(clustertest.AllTests.suite());
