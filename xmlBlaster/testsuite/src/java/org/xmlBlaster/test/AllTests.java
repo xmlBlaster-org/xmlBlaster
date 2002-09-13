a9c3586af379238edc847687734ad6398967d0d2
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-09-13 07:55:08 +0000

    Combining all tests
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@6262 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/testsuite/src/java/org/xmlBlaster/test/AllTests.java
@@ -0,0 +1,34 @@
+/*------------------------------------------------------------------------------
+Name:      AllTests.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Start all tests
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.test;
+
+import junit.framework.*;
+
+/**
+ * java -Djava.compiler= junit.swingui.TestRunner -noloading org.xmlBlaster.test.AllTests
+ */
+public class AllTests extends junit.framework.TestCase
+{
+
+  public AllTests(String s) {
+    super(s);
+  }
+
+  public static junit.framework.Test suite() {
+    junit.framework.TestSuite suite = new junit.framework.TestSuite("Run all xmlBlaster tests");
+    suite.addTest(org.xmlBlaster.test.classtest.AllTests.suite());
+    suite.addTest(org.xmlBlaster.test.qos.TestAll.suite());
+    suite.addTest(org.xmlBlaster.test.authentication.AllTests.suite());
+    suite.addTest(org.xmlBlaster.test.mime.AllTests.suite());
+    suite.addTest(org.xmlBlaster.test.jdbc.AllTests.suite());
+    suite.addTest(org.xmlBlaster.test.snmp.AllTests.suite());
+    suite.addTest(org.xmlBlaster.test.persistence.AllTests.suite());
+    suite.addTest(org.xmlBlaster.test.cluster.AllTests.suite());
+    suite.addTest(org.xmlBlaster.test.stress.AllTests.suite());
+    return suite;
+  }
+}
