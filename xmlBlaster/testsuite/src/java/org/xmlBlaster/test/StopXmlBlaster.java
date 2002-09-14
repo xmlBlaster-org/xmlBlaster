3f571f92f71cf3efbdd1daedd838c69e9f626c51
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-09-14 13:42:34 +0000

    Stops xmlBlaster with a command message
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@6292 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/testsuite/src/java/org/xmlBlaster/test/StopXmlBlaster.java
@@ -0,0 +1,53 @@
+/*------------------------------------------------------------------------------
+Name:      StopXmlBlaster.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Stop xmlBlaster
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.test;
+
+import org.xmlBlaster.util.ConnectQos;
+import org.xmlBlaster.util.DisconnectQos;
+import org.xmlBlaster.client.protocol.XmlBlasterConnection;
+import org.xmlBlaster.engine.helper.MessageUnit;
+
+import junit.framework.*;
+
+/**
+ * This client connects to xmlBlaster and stops it with a command message. 
+ * <p />
+ * Invoke: java org.xmlBlaster.test.StopXmlBlaster
+ */
+public class StopXmlBlaster extends TestCase
+{
+   String[] args = new String[0];
+
+   public StopXmlBlaster() { // For Junit invoke
+   }
+
+   public StopXmlBlaster(String[] args) { // Used by our main
+      this.args = args;
+      testStop();
+   }
+
+   /** Stop xmlBlaster server (invoked by junit automatically as name starts with 'test') */
+   public void testStop() {
+      try {
+         XmlBlasterConnection con = new XmlBlasterConnection(args);
+
+         ConnectQos qos = new ConnectQos(null, "joe", "secret");
+         con.connect(qos, null);
+
+         con.publish(new MessageUnit("<key oid='__cmd:?exit=0'/>", "".getBytes(), "<qos/>"));
+
+         con.disconnect(null);
+      }
+      catch (Exception e) {
+         System.err.println(e.toString());
+      }
+   }
+
+   public static void main(String args[]) {
+      new StopXmlBlaster(args);
+   }
+}
