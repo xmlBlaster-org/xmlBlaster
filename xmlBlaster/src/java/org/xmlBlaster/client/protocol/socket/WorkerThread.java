01f28c4fb4bf93cc1aa770376e94a23d076ad71f
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-09-14 23:11:44 +0000

    Allows to dispatch callback methods in seperate thread to avoid deadlock for recursive remote calls
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@6311 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/client/protocol/socket/WorkerThread.java
@@ -0,0 +1,65 @@
+/*------------------------------------------------------------------------------
+Name:      WorkerThread.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.client.protocol.socket;
+
+import org.jutils.log.LogChannel;
+import org.xmlBlaster.util.Global;
+import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.protocol.socket.Parser;
+
+import java.io.IOException;
+
+/**
+ * Takes a received messages and invokes the desired action. 
+ * <p />
+ * Usually this is an update() or probably a ping()
+ */
+public class WorkerThread extends Thread
+{
+   private final String ME;
+   
+   /** The global handle */
+   private final Global glob;
+
+   private final LogChannel log;
+
+   /** Contains the received message */
+   private final Parser parser;
+
+   /** The manager class, my creator and my destiny */
+   private final SocketCallbackImpl cbHandler;
+
+   /**
+    * Creates the thread. 
+    */
+   public WorkerThread(Global glob, SocketCallbackImpl cbHandler, Parser receiver) {
+      super("protocol.socket.workerThread.forCallbackMessages");
+      this.glob = glob;
+      this.log = glob.getLog("socket");
+      this.cbHandler = cbHandler;
+      this.ME = "WorkerThread-" + cbHandler.getSocketConnection().getLoginName();
+      this.parser = receiver;
+   }
+
+   /**
+    * Starts the thread which calls update() into client code. 
+    */
+   public void run() {
+      try {
+         if (log.TRACE) log.trace(ME, "Starting worker thread, invoking client code with received message");
+         cbHandler.receive(this.parser);  // Parse the message and invoke callback to client code
+         if (log.TRACE) log.trace(ME, "Worker thread done");
+      }
+      catch (XmlBlasterException e) {
+         log.error(ME, e.toString());
+      }
+      catch (Throwable e) {
+         if (!(e instanceof IOException)) e.printStackTrace();
+         log.error(ME, e.toString());
+         cbHandler.getSocketConnection().shutdown();
+      }
+   }
+}
