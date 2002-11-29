a84aabd17f1a11452dc535b5b1ad43aabb871ac0
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-11-29 20:06:51 +0000

    added authentication stuff
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7365 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/c++/authentication/SecurityQos.cpp
@@ -0,0 +1,219 @@
+#include <authentication/SecurityQos.h>
+#include <string>
+
+#include <util/PlatformUtils.hpp>
+
+namespace org { namespace xmlBlaster { namespace authentication {
+
+SecurityQos::SecurityQos(int args, char *argc[])
+   : SaxHandlerBase(args, argc),
+     ME("SecurityQos-simple"),
+     trim_()
+{
+   log_.call(ME, "first constructor");
+   prep();
+}
+
+SecurityQos::SecurityQos(const string& xmlQoS_literal, int args=0, char *argc[]=0)
+   : SaxHandlerBase(args, argc),
+     ME("SecurityQos-simple"),
+     trim_()
+{
+   log_.call(ME, "second constructor");
+   prep();
+   parse(xmlQoS_literal);
+}
+
+SecurityQos::SecurityQos(const string& loginName,
+                         const string& password,
+                         int args=0,
+                         char *argc[]=0)
+   : SaxHandlerBase(args, argc), ME("SecurityQos-simple"), trim_()
+{
+   log_.call(ME, "third constructor");
+   prep();
+   user_   = loginName;
+   passwd_ = password;
+}
+
+
+string SecurityQos::getPluginVersion() const
+{
+   return version_;
+}
+
+
+string SecurityQos::getPluginType() const
+{
+   return type_;
+}
+
+
+void SecurityQos::setUserId(const string& userId)
+{
+   user_ = userId;
+}
+
+
+string SecurityQos::getUserId() const
+{
+   return user_;
+}
+
+
+/**
+ * @param cred The password
+ */
+void SecurityQos::setCredential(const string& cred)
+{
+   passwd_ = cred;
+}
+
+
+/**
+ * @return "" (empty string) (no password is delivered)
+ */
+string SecurityQos::getCredential() const
+{
+   return "";
+}
+
+/**
+ * Start element, event from SAX parser.
+ * <p />
+ * @param name Tag name
+ * @param attrs the attributes of the tag
+ */
+void SecurityQos::startElement(const XMLCh* const name, AttributeList& attrs)
+{
+   log_.call(ME, "startElement");
+   if (name == XMLString::transcode("securityService")) {
+      inSecurityService_ = true;
+      unsigned int len = attrs.getLength();
+
+      unsigned int ii=0;
+      for (ii = 0; ii < len; ii++) {
+         if (attrs.getName(ii) == XMLString::transcode("type")) {
+            type_ = trim_.trim(XMLString::transcode(attrs.getValue(ii))); // .trim();
+         }
+         else if (attrs.getName(ii) == XMLString::transcode("version")) {
+            version_ = trim_.trim(XMLString::transcode(attrs.getValue(ii))); //.trim();
+         }
+      }
+      character_.erase();
+      return;
+   }
+
+   if (name == XMLString::transcode("user")) {
+      inUser_ = true;
+      character_.erase();
+      return;
+   }
+
+   if (name == XMLString::transcode("passwd")) {
+      inPasswd_ = true;
+      character_.erase();
+      return;
+   }
+}
+
+/**
+ * End element, event from SAX parser.
+ * <p />
+ * @param name Tag name
+ */
+ void SecurityQos::endElement(const XMLCh* const name)
+{
+   log_.call(ME, "endElement");
+   if (name == XMLString::transcode("user")) {
+      inUser_ = false;
+      user_ = trim_.trim(character_.c_str()); // .toString().trim();
+      character_.erase();
+      return;
+   }
+
+   if (name == XMLString::transcode("passwd")) {
+      inPasswd_ = false;
+      passwd_ = trim_.trim(character_.c_str()); // .toString().trim();
+      character_.erase();
+      return;
+   }
+
+   if (name == XMLString::transcode("securityService")) {
+      inSecurityService_ = false;
+      character_.erase();
+      return;
+   }
+}
+
+
+string SecurityQos::toXml()
+{
+   return toXml("");
+}
+
+
+/**
+ * Dump state of this object into a XML ASCII string.
+ * <br>
+ * @param extraOffset indenting of tags for nice output
+ * @return The xml representation
+ */
+string SecurityQos::toXml(const string& extraOffset)
+{
+   log_.call(ME, "toXml");
+   string ret;
+   string offset = "\n   ";
+   offset += extraOffset;
+
+   ret += offset + "<securityService type=\"";
+   ret += getPluginType() + "\" version=\"" + getPluginVersion()  + "\">";
+   ret += offset + "   <![CDATA[";
+   ret += offset + "   <user>" + user_ + "</user>";
+   ret += offset + "   <passwd>" + passwd_ + "</passwd>";
+   ret += offset + "   ]]>";
+   ret += offset + "</securityService>";
+   return ret;
+}
+
+}}} // namespaces
+
+
+#ifndef _XMLBLASTER_CLASSTEST
+using namespace std;
+
+/** For testing: java org.xmlBlaster.authentication.plugins.simple.SecurityQos */
+int main(int args, char* argv[])
+{
+
+    // Init the XML platform
+    try
+    {
+  XMLPlatformUtils::Initialize();
+    }
+
+    catch(const XMLException& toCatch)
+    {
+  cout << "Error during platform init! Message:\n"
+ << toCatch.getMessage() << endl;
+  return 1;
+    }
+
+   string xml =
+      string("<securityService type=\"  simple \" version=\"1.0\">\n") +
+      string("   <![CDATA[\n") +
+      string("   <passwd>theUsersPwd</passwd>\n") +
+      string("   <user>aUser</user>\n") +
+      string("   ]]>\n") +
+      string("</securityService>");
+
+   cout << "Original:\n" << xml << endl;
+   org::xmlBlaster::authentication::SecurityQos qos(xml, args, argv);
+   cout << "Result:\n" << qos.toXml() << endl;
+   qos.setUserId("AnotherUser");
+   qos.setCredential("AnotherPassword");
+   cout << qos.toXml() << endl;
+   return 0;
+}
+
+#endif
---
+++ xmlBlaster/src/c++/authentication/SecurityQos.h
@@ -0,0 +1,132 @@
+/**
+ * Parse the default security handling with loginName and password
+ * from the login qos xml string:
+ * <pre>
+ *  &lt;securityService type="simple" version="1.0">
+ *     &lt;user>aUser&lt;/user>
+ *     &lt;passwd>theUsersPwd&lt;/passwd>
+ *  &lt;/securityService>
+ * </pre>
+ */
+
+#ifndef _AUTHENTICATION_SECURITY_QOS_H
+#define _AUTHENTICATION_SECURITY_QOS_H
+
+#include <string>
+#include <util/SaxHandlerBase.h>
+#include <util/StringTrim.h>
+
+// #include <util/XmlBlasterException>
+
+namespace org { namespace xmlBlaster { namespace authentication {
+
+   class SecurityQos: public util::SaxHandlerBase
+   {
+   private:
+      const string ME;
+
+      // helper flags for SAX parsing
+      bool inSecurityService_;
+      bool inUser_;
+      bool inPasswd_;
+
+      string type_;
+      string version_;
+      string user_;
+      string passwd_;
+      util::StringTrim<char> trim_;
+
+      void prep()
+      {
+         inSecurityService_ = false;
+         inUser_            = false;
+         inPasswd_          = false;
+         type_              = "simple";
+         version_           = "1.0";
+         user_              = "";
+         passwd_            = "";
+      }
+
+   public:
+      SecurityQos(int args=0, char *argc[]=0);
+
+      void parse(const string& xmlQoS_literal)
+      {
+         log_.call(ME, "parse");
+         // Strip CDATA tags that we are able to parse it:
+         string ret = xmlQoS_literal;
+
+//         xmlQoS_literal = StringHelper.replaceAll(xmlQoS_literal, "<![CDATA[", "");
+         string::size_type pos = 0;
+         while (pos != ret.npos) {
+            pos = ret.find("<![CDATA[");
+            if (pos == ret.npos) break;
+            ret = ret.erase(pos, 9);
+         }
+
+//         xmlQoS_literal = StringHelper.replaceAll(xmlQoS_literal, "]]>", "");
+         pos = 0;
+         while (pos != ret.npos) {
+            pos = ret.find("]]>");
+            if (pos == ret.npos) break;
+            ret = ret.erase(pos, 3);
+         }
+
+         init(ret);
+      }
+
+      SecurityQos(const string& xmlQoS_literal, int args=0, char *argc[]=0);
+
+      SecurityQos(const string& loginName,
+                  const string& password,
+                  int args=0,
+                  char *argc[]=0);
+
+      string getPluginVersion() const;
+
+      string getPluginType() const;
+
+      void setUserId(const string& userId);
+
+      string getUserId() const;
+
+      /**
+       * @param cred The password
+       */
+      void setCredential(const string& cred);
+
+      /**
+       * @return "" (empty string) (no password is delivered)
+       */
+      string getCredential() const;
+
+      /**
+       * Start element, event from SAX parser.
+       * <p />
+       * @param name Tag name
+       * @param attrs the attributes of the tag
+       */
+      void startElement(const XMLCh* const name, AttributeList& attrs);
+
+      /**
+       * End element, event from SAX parser.
+       * <p />
+       * @param name Tag name
+       */
+
+       void endElement(const XMLCh* const name);
+
+      string toXml();
+
+      /**
+       * Dump state of this object into a XML ASCII string.
+       * <br>
+       * @param extraOffset indenting of tags for nice output
+       * @return The xml representation
+       */
+      string toXml(const string& extraOffset);
+   };
+
+}}} // namespaces
+
+#endif
