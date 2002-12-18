87f1de2c665836ae91f7c6d715c913e387b3c774
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-12-18 10:12:15 +0000

    Entry for history queue which holds a reference on the real message (in msgstore)
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7429 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/engine/queuemsg/MsgQueueHistoryEntry.java
@@ -0,0 +1,56 @@
+/*------------------------------------------------------------------------------
+Name:      MsgQueueHistoryEntry.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.engine.queuemsg;
+
+import org.xmlBlaster.engine.Global;
+import org.xmlBlaster.engine.MsgUnitWrapper;
+import org.xmlBlaster.engine.TopicHandler;
+import org.xmlBlaster.util.key.MsgKeyData;
+import org.xmlBlaster.util.qos.MsgQosData;
+import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.util.enum.ErrorCode;
+import org.xmlBlaster.util.SessionName;
+import org.xmlBlaster.util.MsgUnit;
+import org.xmlBlaster.util.Timestamp;
+import org.xmlBlaster.util.enum.PriorityEnum;
+import org.xmlBlaster.util.queue.StorageId;
+
+/**
+ * Wraps an publish() message into an entry for a sorted queue.
+ * @author laghi@swissinfo.org
+ * @author xmlBlaster@marcelruff.info
+ */
+public final class MsgQueueHistoryEntry extends ReferenceEntry
+{
+   private final static String ME = "MsgQueueHistoryEntry";
+
+   /**
+    * A new message object is fed by method publish(). 
+    * @param msgUnit The raw data, we keep a weak reference only on this data so it can be garbage collected
+    */
+   public MsgQueueHistoryEntry(Global glob, MsgUnitWrapper msgUnitWrapper, StorageId storageId) throws XmlBlasterException {
+      super(ME, glob, ServerEntryFactory.ENTRY_TYPE_HISTORY_REF, msgUnitWrapper, storageId, (SessionName)null);
+   }
+
+   /**
+    * For persistence recovery
+    */
+   public MsgQueueHistoryEntry(Global glob, PriorityEnum priority, StorageId storageId, Timestamp updateEntryTimestamp,
+                              String keyOid, long msgUnitWrapperUniqueId, boolean isDurable) {
+      super(ME, glob, ServerEntryFactory.ENTRY_TYPE_UPDATE_REF, priority, storageId,
+            updateEntryTimestamp, keyOid, msgUnitWrapperUniqueId, isDurable, (SessionName)null);
+   }
+
+   /**
+    * Returns a shallow clone
+    */
+   public Object clone() {
+      MsgQueueHistoryEntry entry = null;
+      entry = (MsgQueueHistoryEntry)super.clone();
+      return entry;
+   }
+}
+
