7653f06eeac5a32b6284af4694ab9eb829de5b53
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-12-18 11:57:30 +0000

    Renamed SwapSize to SwapBytes
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7508 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/util/queue/cache/CacheControlParam.java
+++ xmlBlaster/src/java/org/xmlBlaster/util/queue/cache/CacheControlParam.java
@@ -12,7 +12,7 @@ import org.xmlBlaster.engine.helper.QueuePropertyBase;
  * of the cache queue.
  * 
  * @author laghi@swissinfo.org
- * @author ruff@swand.lake.de
+ * @author xmlBlaster@marcelruff.info
  */
 public class CacheControlParam
 {
@@ -21,7 +21,7 @@ public class CacheControlParam
     * The maximum size of the queue. It is used as a reference for the other
     * parameter. The levels and the sizes should never exceed this value.
     */
-   public long maxSize;
+   public long maxBytes;
 
    /**
     * The storeSwapLevel is the limit size in bytes over which the cache queue
@@ -31,10 +31,10 @@ public class CacheControlParam
    public long storeSwapLevel;
 
    /**
-    * The storeSwapSize is the size in bytes to take each time data is swapped
+    * The storeSwapBytes is the size in bytes to take each time data is swapped
     * from the transient queue to the persistent queue.
     */
-   public long storeSwapSize;
+   public long storeSwapBytes;
 
    /**
     * The reloadSwapLevel is the limit size in bytes under which the cache queue
@@ -44,10 +44,10 @@ public class CacheControlParam
    public long reloadSwapLevel;
 
    /**
-    * The reloadSwapSize is the size in bytes to take each time data is swapped
+    * The reloadSwapBytes is the size in bytes to take each time data is swapped
     * from the persistent queue to reload the transient queue.
     */
-   public long reloadSwapSize;
+   public long reloadSwapBytes;
 
    /**
     * The number of milliseconds to wait during a put action. This would delay
@@ -63,9 +63,9 @@ public class CacheControlParam
     */
    public CacheControlParam(QueuePropertyBase property) {
       this.storeSwapLevel = property.getStoreSwapLevel();
-      this.storeSwapSize = property.getStoreSwapSize();
+      this.storeSwapBytes = property.getStoreSwapBytes();
       this.reloadSwapLevel = property.getReloadSwapLevel();
-      this.reloadSwapSize = property.getReloadSwapSize();
+      this.reloadSwapBytes = property.getReloadSwapBytes();
    }
 
 
