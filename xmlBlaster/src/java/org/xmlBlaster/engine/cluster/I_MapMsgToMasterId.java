0c8d881d43d4d0c750a2541fac316b16c5c68826
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-12-18 10:46:46 +0000

    Changed to MsgUnit
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7457 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/engine/cluster/I_MapMsgToMasterId.java
+++ xmlBlaster/src/java/org/xmlBlaster/engine/cluster/I_MapMsgToMasterId.java
@@ -3,17 +3,17 @@ Name:      I_MapMsgToMasterId.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Interface top the load balancing implementation
-Author:    ruff@swand.lake.de
+Author:    xmlBlaster@marcelruff.info
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.engine.cluster;
 
 import org.xmlBlaster.util.XmlBlasterException;
 import org.xmlBlaster.engine.Global;
-import org.xmlBlaster.engine.MessageUnitWrapper;
+import org.xmlBlaster.util.MsgUnit;
 
 /**
  * Interface to implementation which finds the master cluster node of a given message. 
- * @author ruff@swand.lake.de
+ * @author xmlBlaster@marcelruff.info
  */
 public interface I_MapMsgToMasterId
 {
@@ -53,11 +53,11 @@ public interface I_MapMsgToMasterId
     * <p />
     * Here you code your clustering logic.
     * @param nodeDomainInfo The mapping rule to find the master
-    * @param msgWrapper The message
+    * @param msgUnit The message
     * @return The nodeDomainInfo (same as you passed as parameter) it this is a possible master
     *         or null if not suitable.<br />
     * You can access the master ClusterNode with <code>nodeDomainInfo.getClusterNode()</code> and the xmlBlasterConnection
     * to the master node with <code>nodeDomainInfo.getClusterNode().getXmlBlasterConnection()</code>
     */
-   public NodeDomainInfo getMasterId(NodeDomainInfo nodeDomainInfo, MessageUnitWrapper msgWrapper) throws XmlBlasterException;
+   public NodeDomainInfo getMasterId(NodeDomainInfo nodeDomainInfo, MsgUnit msgUnit) throws XmlBlasterException;
 }
