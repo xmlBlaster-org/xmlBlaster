6c7ac4fb652b85bae1938b1fbba9b58062f6fe4c
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-12-19 21:21:26 +0000

    added AccessFilterQos
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7616 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/c++/util/qos/AccessFilterQos.cpp
@@ -0,0 +1,162 @@
+/*------------------------------------------------------------------------------
+Name:      AccessFilterQos.cpp
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Holding filter address string and protocol string
+------------------------------------------------------------------------------*/
+
+/**
+ * Helper class holding filter markup from a subscribe() or get() QoS. 
+ * <p />
+ * <pre>
+ * &lt;filter type='ContentLength' version='1.0'>
+ *    800
+ * &lt;/filter>
+ * </pre>
+ * This example addresses the plugin in xmlBlaster.properties file
+ * <pre>
+ *   MimeAccessPlugin[ContentLenFilter][1.0]=org.xmlBlaster.engine.mime.demo.ContentLenFilter
+ * </pre>
+ * The filter rules apply for cluster configuration as well.
+ *
+ * @see <a href="http://www.xmlblaster.org/xmlBlaster/doc/requirements/mime.plugin.accessfilter.html">MIME based access filter plugin framework</a>
+ */
+
+#include <util/qos/AccessFilterQos.h>
+#include <util/Global.h>
+
+using namespace org::xmlBlaster::util;
+using namespace std;
+using org::xmlBlaster::util::mime::Query;
+
+namespace org { namespace xmlBlaster { namespace util { namespace qos {
+
+
+Dll_Export const char* DEFAULT_version = "1.0";
+Dll_Export const char* DEFAULT_type    = "";
+  
+   void AccessFilterQos::copy(const AccessFilterQos& qos) 
+   {
+      type_    = qos.type_;
+      version_ = qos.version_;
+   }
+
+   /**
+    */
+   AccessFilterQos::AccessFilterQos(Global& global) 
+      : ME("AccessFilterQos"), global_(global), log_(global.getLog("mime")), query_(global)
+   {
+      type_ = "";
+      setVersion(global_.getProperty().getStringProperty("accessFilter.version", DEFAULT_version));
+      setType(global_.getProperty().getStringProperty("accessFilter.type", DEFAULT_type));
+    }
+
+   /**
+    * @param glob The global handle holding environment and logging objects
+    * @param type The plugin name, as used in xmlBlaster.properties e.g. "ContentLenFilter".
+    * @param version The plugin version, defaults to "1.0"
+    * @param query   Your filter rule
+    */
+   AccessFilterQos::AccessFilterQos(Global& global, const string& type, const string& version, const string& query)
+      : ME("AccessFilterQos"), global_(global), log_(global.getLog("mime")), query_(global, query)
+   {
+      setType(type);
+      setVersion(version);
+   }
+
+   /**
+    * @param glob The global handle holding environment and logging objects
+    * @param type The plugin name, as used in xmlBlaster.properties e.g. "ContentLenFilter".
+    * @param version The plugin version, defaults to "1.0"
+    * @param query   Your filter rule
+    */
+   AccessFilterQos::AccessFilterQos(Global& global, const string& type, const string& version, const Query& query)
+      : ME("AccessFilterQos"), global_(global), log_(global.getLog("mime")), query_(query)
+   {
+      setType(type);
+      setVersion(version);
+   }
+
+   /**
+    * @param type The plugin name, as used in xmlBlaster.properties e.g. "ContentLenFilter".
+    */
+   void AccessFilterQos::setType(const string& type)
+   {
+      type_ = type;
+   }
+
+   /**
+    * Returns the plugins name. 
+    * @return e.g. "ContentLenFilter"
+    */
+   string AccessFilterQos::getType()
+   {
+      return type_;
+   }
+
+   /**
+    * @param version The version of the plugin, defaults to "1.0", but can anything you like. 
+    */
+   void AccessFilterQos::setVersion(const string& version)
+   {
+      version_ = version;
+   }
+
+   /**
+    * Returns the plugins version. 
+    * @return e.g. "1.0"
+    */
+   string AccessFilterQos::getVersion()
+   {
+      return version_;
+   }
+
+   /**
+    * Set the filter query, it should fit to the protocol-type.
+    *
+    * @param query The filter query, e.g. "8000" for max length of a content with "ContentLenFilter" plugin
+    */
+   void AccessFilterQos::setQuery(const Query& query)
+   {
+      query_ = query;
+   }
+
+   /**
+    * Returns the query, the syntax is depending on what your plugin supports.
+    * @return e.g. "a>12 AND b<15"
+    */
+   Query AccessFilterQos::getQuery()
+   {
+      return query_;
+   }
+
+
+   /**
+    * Dump state of this object into a XML ASCII string.
+    * <br>
+    * Only none default values are dumped for performance reasons
+    * @param extraOffset indenting of tags for nice output
+    * @return The xml representation
+    */
+   string AccessFilterQos::toXml(const string& extraOffset="")
+   {
+      string ret;   
+      string offset = "\n " + extraOffset;
+
+      ret += offset + "<filter type='" + getType() + "'";
+      if (DEFAULT_version != getVersion())
+          ret += " version='" + getVersion() + "'";
+      ret += ">";
+      string help = getQuery().toString();
+      if (help.find("<![CDATA[") != help.npos)
+         ret += offset + " " + help;
+      else
+         ret += offset + " <![CDATA[" + help + "]]>";
+      ret += offset + "</filter>";
+
+      return ret;
+   }
+
+}}}}
+
+
---
+++ xmlBlaster/src/c++/util/qos/AccessFilterQos.h
@@ -0,0 +1,132 @@
+/*------------------------------------------------------------------------------
+Name:      AccessFilterQos.h
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Holding filter address string and protocol string
+------------------------------------------------------------------------------*/
+
+/**
+ * Helper class holding filter markup from a subscribe() or get() QoS. 
+ * <p />
+ * <pre>
+ * &lt;filter type='ContentLength' version='1.0'>
+ *    800
+ * &lt;/filter>
+ * </pre>
+ * This example addresses the plugin in xmlBlaster.properties file
+ * <pre>
+ *   MimeAccessPlugin[ContentLenFilter][1.0]=org.xmlBlaster.engine.mime.demo.ContentLenFilter
+ * </pre>
+ * The filter rules apply for cluster configuration as well.
+ *
+ * @see <a href="http://www.xmlblaster.org/xmlBlaster/doc/requirements/mime.plugin.accessfilter.html">MIME based access filter plugin framework</a>
+ */
+
+#ifndef _UTIL_QOS_ACCESSFILTERQOS_H
+#define _UTIL_QOS_ACCESSFILTERQOS_H
+
+#include <util/xmlBlasterDef.h>
+#include <util/mime/Query.h>
+#include <util/Log.h>
+#include <util/Property.h>
+
+#include <vector>
+#include <string>
+
+using namespace org::xmlBlaster::util;
+using namespace std;
+using org::xmlBlaster::util::mime::Query;
+
+namespace org { namespace xmlBlaster { namespace util { namespace qos {
+
+extern Dll_Export const char* DEFAULT_version;
+extern Dll_Export const char* DEFAULT_type;
+
+class Dll_Export AccessFilterQos
+{
+   const string ME;
+   Global&      global_;
+   Log&         log_;
+
+   /** The filter rule string and an object to hold the prepared query on demand  */
+   Query query_;
+
+   /** The plugin name e.g. "ContentLength" */
+   string type_;
+   
+   /** The version of the plugin */
+   string version_; // = DEFAULT_version;
+   
+   void copy(const AccessFilterQos& qos);
+
+public:
+   /**
+    */
+   AccessFilterQos(Global& global);
+
+   /**
+    * @param glob The global handle holding environment and logging objects
+    * @param type The plugin name, as used in xmlBlaster.properties e.g. "ContentLenFilter".
+    * @param version The plugin version, defaults to "1.0"
+    * @param query   Your filter rule
+    */
+   AccessFilterQos(Global& global, const string& type, const string& version, const string& query);
+
+   /**
+    * @param glob The global handle holding environment and logging objects
+    * @param type The plugin name, as used in xmlBlaster.properties e.g. "ContentLenFilter".
+    * @param version The plugin version, defaults to "1.0"
+    * @param query   Your filter rule
+    */
+   AccessFilterQos(Global& global, const string& type, const string& version, const Query& query);
+
+   /**
+    * @param type The plugin name, as used in xmlBlaster.properties e.g. "ContentLenFilter".
+    */
+   void setType(const string& type);
+
+   /**
+    * Returns the plugins name. 
+    * @return e.g. "ContentLenFilter"
+    */
+   string getType();
+
+   /**
+    * @param version The version of the plugin, defaults to "1.0", but can anything you like. 
+    */
+   void setVersion(const string& version);
+
+   /**
+    * Returns the plugins version. 
+    * @return e.g. "1.0"
+    */
+   string getVersion();
+
+   /**
+    * Set the filter query, it should fit to the protocol-type.
+    *
+    * @param query The filter query, e.g. "8000" for max length of a content with "ContentLenFilter" plugin
+    */
+   void setQuery(const Query& query);
+
+   /**
+    * Returns the query, the syntax is depending on what your plugin supports.
+    * @return e.g. "a>12 AND b<15"
+    */
+   Query getQuery();
+
+   /**
+    * Dump state of this object into a XML ASCII string.
+    * <br>
+    * Only none default values are dumped for performance reasons
+    * @param extraOffset indenting of tags for nice output
+    * @return The xml representation
+    */
+   string toXml(const string& extraOffset="");
+};
+
+}}}}
+
+#endif
+
+
