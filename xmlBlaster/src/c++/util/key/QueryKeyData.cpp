e371f29d602febe0f8d0f703858190840200b89f
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-12-21 14:20:27 +0000

    added MsgKeyData: all util/key completed
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7654 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/c++/util/key/QueryKeyData.cpp
@@ -0,0 +1,105 @@
+/*------------------------------------------------------------------------------
+Name:      QueryKeyData.cpp
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+
+#include <util/key/QueryKeyData.h>
+#include <util/Constants.h>
+#include <util/XmlBlasterException.h>
+#include <util/Global.h>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+using namespace org::xmlBlaster::util;
+
+namespace org { namespace xmlBlaster { namespace util { namespace key {
+
+QueryKeyData::QueryKeyData(Global& global) : KeyData(global), accessFilterVector_()
+{
+}
+
+QueryKeyData::QueryKeyData(const QueryKeyData& key) : KeyData(key), accessFilterVector_(key.accessFilterVector_)
+{
+}
+
+QueryKeyData& QueryKeyData::operator =(const QueryKeyData& key) 
+{
+   accessFilterVector_ = key.accessFilterVector_;
+   return *this;
+}
+
+void QueryKeyData::setQueryType(const string& queryType)
+{
+   string tmp = queryType;
+   transform (tmp.begin(), tmp.end(), tmp.begin(), toupper);
+ 
+   if (Constants::EXACT != tmp && Constants::XPATH !=tmp)
+      throw XmlBlasterException(USER_ILLEGALARGUMENT, ME + "::setQueryType",
+                                "Your queryType=" + queryType_ + " is invalid, use one of '" + 
+				Constants::EXACT + "' , '" + Constants::XPATH + "'");
+   queryType_ = tmp;
+}
+
+/**
+ * Your XPath query string. 
+ * @param str Your tags in ASCII XML syntax
+ */
+void QueryKeyData::setQueryString(const string& tags)
+{
+   queryString_ = tags;
+}
+
+string QueryKeyData::getQueryString() const
+{
+   return queryString_;
+}
+
+/**
+ * Return the filters or array with size==0 if none is specified. 
+ * <p />
+ * For subscribe() and get() and cluster messages.
+ * @return never null
+ */
+AccessFilterVector QueryKeyData::getAccessFilterVector() const 
+{
+   return accessFilterVector_;
+}
+
+void QueryKeyData::addFilter(const AccessFilterQos& qos) 
+{
+   accessFilterVector_.insert(accessFilterVector_.end(), qos);
+}
+
+string QueryKeyData::toXml(const string& extraOffset) const
+{
+   string ret;
+   string offset = Constants::OFFSET + extraOffset;
+
+   ret += offset + "<key oid='" + oid_ + "'";
+   if (!getContentMime().empty())
+      ret += " contentMime='" + getContentMime() + "'";
+   if (!getContentMimeExtended().empty())
+      ret += " contentMimeExtended='" + getContentMimeExtended() + "'";
+   if (!getDomain().empty())
+      ret += " domain='" + getDomain() + "'";
+
+   if (!getQueryType().empty() && Constants::EXACT != getQueryType())
+         ret += " queryType='" + getQueryType() + "'";
+   ret += ">";
+   if (!queryString_.empty()) {
+      ret += offset + Constants::INDENT + getQueryString();
+   }
+
+   AccessFilterVector::const_iterator iter = accessFilterVector_.begin();
+   while (iter != accessFilterVector_.end()) {
+      ret += (*iter).toXml(extraOffset + Constants::INDENT);
+      iter++;
+   }
+   ret += "<key/>";
+  return ret;
+}
+
+}}}} // namespace
+
---
+++ xmlBlaster/src/c++/util/key/QueryKeyData.h
@@ -0,0 +1,92 @@
+/*------------------------------------------------------------------------------
+Name:      QueryKeyData.h
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+
+/**
+ * This class encapsulates the Message key information of query invocations. 
+ * <p />
+ * <ul>
+ * <li>SubscribeKey Client side access facade</i>
+ * <li>UnSubscribeKey Client side access facade</i>
+ * <li>GetKey Client side access facade</i>
+ * <li>EraseKey Client side access facade</i>
+ * </ul>
+ * <p>
+ * For the xml representation see MsgKeySaxFactory.
+ * </p>
+ * @see org.xmlBlaster.util.key.QueryKeySaxFactory
+ */
+
+#ifndef _UTIL_KEY_QUERYKEYDATA_H
+#define _UTIL_KEY_QUERYKEYDATA_H
+
+#include <util/key/KeyData.h>
+#include <util/qos/AccessFilterQos.h>
+#include <string>
+
+using namespace std;
+using namespace org::xmlBlaster::util;
+using org::xmlBlaster::util::qos::AccessFilterQos;
+
+namespace org { namespace xmlBlaster { namespace util { namespace key {
+
+typedef vector<AccessFilterQos> AccessFilterVector;
+
+class Dll_Export QueryKeyData : public KeyData
+{
+protected:
+
+   /**
+    * subscribe(), get() and cluster configuration keys may contain a filter rule
+    */
+   AccessFilterVector accessFilterVector_;
+
+public:
+
+   /**
+    * Minimal constructor.
+    */
+   QueryKeyData(Global& global);
+   
+   QueryKeyData(const QueryKeyData& key);
+
+   QueryKeyData& operator =(const QueryKeyData& key);
+
+   void setQueryType(const string& queryType);
+
+   /**
+    * Your XPath query string. 
+    * @param str Your tags in ASCII XML syntax
+    */
+   void setQueryString(const string& tags);
+
+   string getQueryString() const;
+
+   /**
+    * Return the filters or array with size==0 if none is specified. 
+    * <p />
+    * For subscribe() and get() and cluster messages.
+    * @return never null
+    */
+   AccessFilterVector getAccessFilterVector() const;
+
+   void addFilter(const AccessFilterQos& qos);
+
+   /**
+    * Dump state of this object into a XML ASCII string.
+    * <br>
+    * @param extraOffset indenting of tags for nice output
+    * @return internal state of the query as a XML ASCII string
+    */
+   string toXml(const string& extraOffset="") const;
+
+};
+
+}}}} // namespace
+
+#endif
+
+
+
