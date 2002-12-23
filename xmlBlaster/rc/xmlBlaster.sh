e7c885a287c504814c511bc4006fc0d5f8229b4c
david <david@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-12-23 20:40:23 +0000

    admin.start.asService
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7684 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/rc/xmlBlaster.sh
@@ -0,0 +1,161 @@
+#! /bin/sh
+
+#
+# Skeleton sh script suitable for starting and stopping 
+# wrapped Java apps on the Solaris platform. 
+#
+# This script expects to find the 'realpath' executable
+# in the same directory. 
+#
+# Make sure that PIDFILE points to the correct location,
+# if you have changed the default location set in the 
+# wrapper configuration file.
+#
+
+#-----------------------------------------------------------------------------
+# These settings can be modified to fit the needs of your application
+
+# Application
+APP_NAME="xmlBlaster"
+APP_LONG_NAME="xmlBlaster"
+
+# Wrapper
+WRAPPER_CMD="$XMLBLASTER_HOME/rc/wrapper"
+WRAPPER_CONF="$XMLBLASTER_HOME/config/wrapper.conf"
+
+# Priority (see the start() method if you want to use this) 
+PRIORITY=
+
+# Do not modify anything beyond this point
+#-----------------------------------------------------------------------------
+
+# Get to the actual location of this script
+#SCRIPT_DIR=`dirname $0`
+#SCRIPT=`$SCRIPT_DIR/realpath $0`
+#cd `dirname $SCRIPT`
+
+# Process ID
+PIDDIR="/var/run"
+PIDFILE="$PIDDIR/$APP_NAME.pid"
+pid=""
+
+getpid() {
+    if [ -f $PIDFILE ]
+    then
+    if [ -r $PIDFILE ]
+    then
+        pid=`cat $PIDFILE`
+        if [ "X$pid" != "X" ]
+        then
+        # Verify that a process with this pid is still running.
+        pid=`/usr/bin/ps -p $pid | grep $pid | grep -v grep | awk '{print $1}' | tail -1`
+        if [ "X$pid" = "X" ]
+        then
+            # This is a stale pid file.
+            rm -f $PIDFILE
+            echo "Removed stale pid file: $PIDFILE"
+        fi
+        fi
+    else
+        echo "Cannot read $PIDFILE."
+        rm -f $PIDFILE
+        exit 1
+    fi
+    fi
+}
+ 
+start() {
+    echo "Starting $APP_LONG_NAME..."
+    getpid
+    if [ "X$pid" = "X" ]
+    then
+        # If you wanted to specify the priority with which
+        # your app runs, you could use nice here:
+        # exec nice -$PRIORITY $WRAPPER_CMD $WRAPPER_CONF &
+        # See "man nice" for more details.
+        exec $WRAPPER_CMD $WRAPPER_CONF &
+    else
+        echo "$APP_LONG_NAME is already running."
+        exit 1
+    fi
+}
+ 
+stop() {
+    echo "Stopping $APP_LONG_NAME..."
+    getpid
+    if [ "X$pid" = "X" ]
+    then
+        echo "$APP_LONG_NAME was not running."
+
+    else
+        kill $pid
+        sleep 6
+
+        pid=`/usr/bin/ps -p $pid | grep $pid | grep -v grep | awk '{print $1}' | tail -1`
+
+        if [ "X$pid" != "X" ]
+        then
+        # SIGTERM didn't work, send SIGKILL.
+            kill -9 $pid
+        rm -f $PIDFILE
+
+            pid=`/usr/bin/ps -p $pid | grep $pid | grep -v grep | awk '{print $1}' | tail -1`
+        fi
+
+        if [ "X$pid" != "X" ]
+        then
+            echo "Failed to stop $APP_LONG_NAME."
+            exit 1
+        else
+            echo "Stopped $APP_LONG_NAME."
+        fi
+    fi
+}
+
+dump() {
+    echo "Dumping $APP_LONG_NAME..."
+    getpid
+    if [ "X$pid" = "X" ]
+    then
+        echo "$APP_LONG_NAME was not running."
+
+    else
+        kill -3 $pid
+
+        if [ $? -ne 0 ]
+        then
+            echo "Failed to dump $APP_LONG_NAME."
+            exit 1
+        else
+            echo "Dumped $APP_LONG_NAME."
+        fi
+    fi
+}
+
+case "$1" in
+
+    'start')
+        start
+        ;;
+
+    'stop')
+        stop
+        ;;
+
+    'restart')
+        stop
+        start
+        ;;
+
+    'dump')
+        dump
+        ;;
+
+    *)
+        echo "Usage: $0 { start | stop | restart | dump }"
+        exit 1
+        ;;
+esac
+
+exit 0
+
---
+++ xmlBlaster/src/java/org/xmlBlaster/MainSvc.java
@@ -0,0 +1,87 @@
+/*------------------------------------------------------------------------------
+Name:      MainSvc.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Main class to invoke the xmlBlaster server
+Version:   $Id: MainSvc.java,v 1.1 2002/12/23 20:40:23 david Exp $
+------------------------------------------------------------------------------*/
+package org.xmlBlaster;
+
+import com.silveregg.wrapper.WrapperManager;
+import com.silveregg.wrapper.WrapperListener;
+
+//import org.jutils.log.LogChannel;
+//import org.jutils.JUtilsException;
+//import org.jutils.io.FileUtil;
+//import org.jutils.runtime.Memory;
+//import org.jutils.runtime.ThreadLister;
+
+import org.xmlBlaster.engine.*;
+//import org.xmlBlaster.engine.helper.Constants;
+//import org.xmlBlaster.util.XmlBlasterException;
+//import org.xmlBlaster.util.SignalCatcher;
+//import org.xmlBlaster.protocol.I_XmlBlaster;
+//import org.xmlBlaster.authentication.Authenticate;
+
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.io.IOException;
+
+public class MainSvc implements WrapperListener 
+{
+	private Main main = null;
+    
+    /**************************************************************************
+     * WrapperListener Methods
+     *************************************************************************/
+    public Integer start(String[] args) {
+        System.out.println("start()");
+        
+        final String[] myArgs = args;
+        WrapperManager.signalStarting(20000);
+        
+        Thread startThread = new Thread() {
+            public void run() {
+                Global glob = new Global(myArgs);
+                try {
+                    glob.getProperty().set("doBlocking", "false");
+                }catch(org.jutils.JUtilsException ex) {
+                }
+                main = new Main(glob);
+                while (!main.isHalted()) try { Thread.sleep(60*1000); } catch (Exception e) {}
+            }
+        };
+        startThread.start();
+        
+        return null;
+    }
+    
+    public int stop(int exitCode) {
+        System.out.println("stop(" + exitCode + ")");
+
+	  if (main != null) main.shutdown();
+        main = null;
+        
+        return exitCode;
+    }
+    
+    public void controlEvent(int event) {
+        System.out.println("controlEvent(" + event + ")");
+        if (event == WrapperManager.WRAPPER_CTRL_C_EVENT) {
+            WrapperManager.stop(0);
+        }
+    }
+    
+    /**************************************************************************
+     * Main Method
+     *************************************************************************/
+    public static void main(String[] args) {
+        System.out.println("Initializing...");
+        
+        // Start the application.  If the JVM was launched from the native
+        //  Wrapper then the application will wait for the native Wrapper to
+        //  call the application's start method.  Otherwise the start method
+        //  will be called immediately.
+        WrapperManager.start(new MainSvc(), args);
+    }
+}
