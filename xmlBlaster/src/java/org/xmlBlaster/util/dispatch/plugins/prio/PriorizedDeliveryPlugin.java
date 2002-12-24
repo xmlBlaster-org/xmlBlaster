abf8e33c1371829405aa5b66e605c64a3db51697
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-12-24 14:11:58 +0000

    Javadoc only
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7695 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/util/dispatch/plugins/prio/PriorizedDeliveryPlugin.java
+++ xmlBlaster/src/java/org/xmlBlaster/util/dispatch/plugins/prio/PriorizedDeliveryPlugin.java
@@ -278,6 +278,13 @@ public final class PriorizedDeliveryPlugin implements I_MsgDeliveryInterceptor, 
    }
 
    /**
+    * Enforced by I_MsgDeliveryInterceptor
+    * <p>
+    * NOTE: When copying entries from one queue to another one we have
+    * to take care that the reference counter in msgstore is not temporary zero (and is
+    * garbage collected). This is avoided by a peek() and later remove() - which is
+    * necessary for durable messages anyhow to ensure 100% crash safety.
+    * </p>
     * @see I_MsgDeliveryInterceptor#handleNextMessages(DeliveryManager, ArrayList)
     */
    public final ArrayList handleNextMessages(DeliveryManager deliveryManager, ArrayList entries) throws XmlBlasterException {
