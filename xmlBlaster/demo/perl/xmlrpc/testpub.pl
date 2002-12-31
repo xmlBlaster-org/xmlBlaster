2fe0629aede07093d0ea87d146e3f5764e183c41
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2002-12-31 13:52:11 +0000

    Added topic QoS
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7804 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/demo/perl/xmlrpc/testpub.pl
+++ xmlBlaster/demo/perl/xmlrpc/testpub.pl
@@ -1,7 +1,5 @@
 # Invoke: perl testpub.pl http://myHost:8080
 use Frontier::Client;
-use MIME::Base64 ();
-
     
 #$server_url = 'http://MyHost:8080/';
 $server_url =  @ARGV[0];
@@ -9,7 +7,6 @@ print "\nTrying to connect to xmlBlaster server on $server_url ...\n";
 
 # Make an object to represent the XML-RPC server.
 $server = Frontier::Client->new(url => $server_url);
-print "Connected to xmlBlaster server on $server_url \n";
 
 $connectQos = "<qos>".
               "  <securityService type='htpasswd' version='1.0'>".
@@ -22,7 +19,7 @@ $connectQos = "<qos>".
 
 # Call the remote server and get our result (we need to port to connect(), see XmlBlaster.pm).
 $sessionId = $server->call('authenticate.login', "testpubUser", "secret", $connectQos, "");
-print "\nLogin success, got secret sessionId=$sessionId \n";
+print "\nLogin success on $server_url, got secret sessionId=$sessionId \n";
 
 my $publishKey="<key oid='' contentMime='text/xml'>".
                "  <service>post</service>".
@@ -33,7 +30,7 @@ my $cdata="<event>testing</event>";
 my $publishQos="<qos>".
                "  <expiration lifeTime='0'/>".
                "  <isDurable>false</isDurable>".
-               "  <topic destroyDelay='0'/>".
+               "  <topic destroyDelay='0' createDomEntry='true'/>".
                "</qos>";
 
 $message = $server->call('xmlBlaster.publish', $sessionId,
