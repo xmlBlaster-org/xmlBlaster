f092a0c2b74cda2984f368cb24ab66f444b4513c
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-01-03 17:27:04 +0000

    Test all topic related things
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7870 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/testsuite/src/java/org/xmlBlaster/test/topic/AllTests.java
@@ -0,0 +1,30 @@
+/*------------------------------------------------------------------------------
+Name:      AllTests.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Start all tests
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.test.topic;
+
+import org.jutils.JUtilsException;
+import junit.framework.*;
+
+
+/**
+ * This test client starts all known tests in directory xmlBlaster/testsuite/src/java/org/xmlBlaster/topic
+ * these are remote tests between clients and a running xmlBlaster. 
+ * <p />
+ * Invoke examples:<br />
+ * <pre>
+ *    java junit.textui.TestRunner -noloading org.xmlBlaster.test.topic.AllTests
+ *    java junit.swingui.TestRunner -noloading org.xmlBlaster.test.topic.AllTests
+ * </pre>
+ */
+public class AllTests
+{
+   public static Test suite() {
+      TestSuite suite= new TestSuite("All xmlBlaster message expiry tests");
+      suite.addTest(TestTopicLifeCycle.suite());
+      return suite;
+   }
+}
