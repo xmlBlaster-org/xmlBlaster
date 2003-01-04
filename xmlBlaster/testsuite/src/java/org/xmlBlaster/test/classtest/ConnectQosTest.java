3f90156d2141f07754d97b4917da29c2ccb419aa
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-01-04 22:58:51 +0000

    Added missing tests
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7908 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/src/java/org/xmlBlaster/test/classtest/ConnectQosTest.java
+++ xmlBlaster/testsuite/src/java/org/xmlBlaster/test/classtest/ConnectQosTest.java
@@ -5,6 +5,17 @@ import org.xmlBlaster.util.Global;
 import org.xmlBlaster.util.XmlBlasterException;
 import org.xmlBlaster.util.ConnectQos;
 
+import org.xmlBlaster.util.qos.address.Address;
+import org.xmlBlaster.util.qos.address.AddressBase;
+import org.xmlBlaster.util.qos.address.CallbackAddress;
+import org.xmlBlaster.util.enum.Constants;
+import org.xmlBlaster.util.qos.storage.QueueProperty;
+import org.xmlBlaster.util.qos.storage.CbQueueProperty;
+import org.xmlBlaster.util.qos.address.ServerRef;
+import org.xmlBlaster.protocol.I_CallbackDriver;
+import org.xmlBlaster.authentication.plugins.I_ClientPlugin;
+import org.xmlBlaster.authentication.plugins.I_SecurityQos;
+
 import junit.framework.*;
 
 /**
@@ -20,6 +31,7 @@ import junit.framework.*;
  */
 public class ConnectQosTest extends TestCase {
    protected Global glob;
+   protected LogChannel log;
    int counter = 0;
 
    public ConnectQosTest(String name) {
@@ -28,6 +40,7 @@ public class ConnectQosTest extends TestCase {
 
    protected void setUp() {
       this.glob = Global.instance();
+      this.log = this.glob.getLog("test");
    }
 
    public void testParse() {
@@ -37,21 +50,17 @@ public class ConnectQosTest extends TestCase {
          long sessionTimeout = 3600001L;
          String xml =
          "<qos>\n" +
-         /*
          "   <securityService type='htpasswd' version='1.0'>\n" +
          "      <![CDATA[\n" +
-         "         <user>aUser</user>\n" +
+         "         <user>joe</user>\n" +
          "         <passwd>theUsersPwd</passwd>\n" +
          "      ]]>\n" +
          "   </securityService>\n" +
-         */
-         "   <ptp>true</ptp>\n" +
+         "   <ptp>false</ptp>\n" +
          "   <isClusterNode>true</isClusterNode>\n" +
          "   <duplicateUpdates>false</duplicateUpdates>\n" +
-         "   <session name='/node/avalon/client/joe/2' timeout='" + sessionTimeout + "' maxSessions='20' clearSessions='false'>\n" +
-         "      <sessionId>anId</sessionId>\n" +
-         "   </session>\n" +
-         "   <queue relating='subject' maxMsg='1000' maxBytes='4000' onOverflow='deadMessage'>\n" +
+         "   <session name='/node/avalon/client/joe/2' timeout='" + sessionTimeout + "' maxSessions='27' clearSessions='true' sessionId='xyz'/>\n" +
+         "   <queue relating='subject' maxMsg='1009' maxBytes='4000' onOverflow='deadMessage'>\n" +
          "      <callback type='IOR' sessionId='4e56890ghdFzj0' pingInterval='60000' retries='1' delay='60000' useForSubjectQueue='true'>\n" +
          "         <ptp>true</ptp>\n" +
          "         IOR:00011200070009990000....\n" +
@@ -59,6 +68,7 @@ public class ConnectQosTest extends TestCase {
          "         <burstMode collectTime='400' />\n" +
          "      </callback>\n" +
          "   </queue>\n" +
+         /*
          "   <callback type='IOR'>\n" +
          "      IOR:00000461203\n" +
          "   </callback>\n" +
@@ -66,12 +76,13 @@ public class ConnectQosTest extends TestCase {
          "      et@mars.universe\n" +
          "      <ptp>false</ptp>\n" +
          "   </callback>\n" +
-         "   <callback type='XML-RPC'>\n" +
-         "      <ptp>true</ptp>\n" +
-         "      http:/www.mars.universe:8080/RPC2\n" +
-         "   </callback>\n" +
-         "   <queue relating='session' maxMsg='1600' maxBytes='2000'/>\n" +
-         "   <queue relating='subject' maxMsg='1600' maxBytes='2000' expires='360000000'/>\n" +
+         */
+         "   <queue relating='session' maxMsg='1600' maxBytes='2000'>\n" +
+         "      <callback type='XML-RPC'>\n" +
+         "         <ptp>true</ptp>\n" +
+         "         http:/www.mars.universe:8080/RPC2\n" +
+         "      </callback>\n" +
+         "   </queue>\n" +
          "   <serverRef type='IOR'>\n" +
          "      IOR:00011200070009990000....\n" +
          "   </serverRef>\n" +
@@ -83,11 +94,59 @@ public class ConnectQosTest extends TestCase {
          "   </serverRef>\n" +
          "</qos>\n";
 
-         ConnectQos qos = new ConnectQos(glob, xml);
+         ConnectQos qos = new ConnectQos(glob, xml); // parse
+         String newXml = qos.toXml();                // dump
+         qos = new ConnectQos(glob, newXml);         // parse again
+
+         log.info("ConnectQosTest", "ORIG=\n" + xml + "\n NEW=\n" + newXml);
+
          assertEquals("sessionTimeout failed", sessionTimeout, qos.getSessionTimeout());
          assertEquals("", "/node/avalon/client/joe/2", qos.getSessionName().getAbsoluteName());
+         assertEquals("", true, qos.hasPublicSessionId());
+         assertEquals("", 2L, qos.getPublicSessionId());
+         assertEquals("", sessionTimeout, qos.getSessionTimeout());
+         assertEquals("", 27, qos.getMaxSessions());
+         assertEquals("", true, qos.clearSessions());
+         assertEquals("", "xyz", qos.getSessionId());
 
-         // TODO: check all methods
+         assertEquals("", false, qos.isPtpAllowed());
+         assertEquals("", true, qos.isClusterNode());
+         assertEquals("", false, qos.duplicateUpdates());
+
+         {
+            CbQueueProperty prop = qos.getSubjectCbQueueProperty();
+            assertEquals("", 1009L, prop.getMaxMsg());
+            assertEquals("", true, qos.subjectCbQueuePropertyIsInitialized());
+         }
+
+
+         {
+            CbQueueProperty prop = qos.getSessionCbQueueProperty();
+            assertEquals("", 1600L, prop.getMaxMsg());
+         }
+         
+         // TODO: check all methods !!!
+
+         /*
+         protected I_ClientPlugin getPlugin(String mechanism, String version) throws XmlBlasterException
+         public String getSecurityData() {
+         */
+         ServerRef[] refArr = qos.getServerRefs();
+         assertEquals("", 3, refArr.length);
+         ServerRef ref = qos.getServerRef();
+         assertTrue("", ref != null);
+         /*
+         public QueueProperty getClientQueueProperty() {
+         public final I_SecurityQos getSecurityQos() throws XmlBlasterException
+         public final String getSecurityPluginType() throws XmlBlasterException
+         public final String getSecurityPluginVersion() throws XmlBlasterException
+         */
+         AddressBase[] addrArr = qos.getAddresses();
+         assertEquals("", 1, addrArr.length);
+         Address addr = qos.getAddress();
+         //assertEquals("", "http:...", addr.getAddress().trim()); // from client queue property
+         assertEquals("", false, qos.isPtpAllowed());
+         assertEquals("", "joe", qos.getUserId());
       }
       catch (XmlBlasterException e) {
          fail("testParse failed: " + e.toString());
