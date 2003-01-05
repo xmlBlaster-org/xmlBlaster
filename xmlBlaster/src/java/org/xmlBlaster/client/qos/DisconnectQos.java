1ff2fff6835e611210a6dd965674454cc17f0e59
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-01-05 23:05:26 +0000

    Decorator for disconnect() QoS to be used by client code
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7931 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/client/qos/DisconnectQos.java
@@ -0,0 +1,42 @@
+/*------------------------------------------------------------------------------
+Name:      DisconnectQos.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Handling one xmlQoS
+Version:   $Id: DisconnectQos.java,v 1.1 2003/01/05 23:05:26 ruff Exp $
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.client.qos;
+
+import org.xmlBlaster.util.Global;
+import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.util.qos.DisconnectQosData;
+
+/**
+ * This class encapsulates the qos of a logout() or disconnect()
+ * <p />
+ * So you don't need to type the 'ugly' XML ASCII string by yourself.
+ * After construction access the ASCII-XML string with the toXml() method.
+ * <br />
+ * A typical <b>logout</b> qos could look like this:<br />
+ * <pre>
+ *  &lt;qos>
+ *    &lt;deleteSubjectQueue>true&lt;/deleteSubjectQueue>
+ *    &lt;clearSessions>false&lt;/clearSessions>
+ *  &lt;/qos>
+ * </pre>
+ * <p />
+ * see xmlBlaster/src/dtd/XmlQoS.xml
+ * @see <a href="http://www.xmlBlaster.org/xmlBlaster/doc/requirements/interface.disconnect.html">The interface.disconnect requirement</a>
+ * @see org.xmlBlaster.test.classtest.DisconnectQosTest
+ */
+public class DisconnectQos extends DisconnectQosData
+{
+   public DisconnectQos(Global glob) {
+      super(glob);
+   }
+
+   /** @deprecated */
+   public DisconnectQos() {
+      this(Global.instance());
+   }
+}
