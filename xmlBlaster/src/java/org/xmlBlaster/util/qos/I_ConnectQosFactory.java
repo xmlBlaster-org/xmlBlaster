54e06c6c907f841c9381c15b7e3e5e721882dc70
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-01-05 23:02:57 +0000

    Interface to hide connect() QoS parser
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@7929 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/util/qos/I_ConnectQosFactory.java
@@ -0,0 +1,35 @@
+/*------------------------------------------------------------------------------
+Name:      I_ConnectQosFactory.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.util.qos;
+
+import org.xmlBlaster.util.XmlBlasterException;
+
+
+/**
+ * Parsing/Serializing QoS (quality of service) of publish() and update(). 
+ * @author xmlBlaster@marcelruff.info
+ */
+public interface I_ConnectQosFactory
+{
+   /**
+    * Parses the given Qos and returns a ConnectQosData holding the data. 
+    * Parsing of update() and publish() QoS is supported here.
+    * @param e.g. the XML based ASCII string
+    */
+   ConnectQosData readObject(String xmlQos) throws XmlBlasterException;
+
+   /**
+    * Serialize the given data object.  
+    * <br>
+    * @param The data object to serialize
+    * @param extraOffset Formatting hints
+    * @return The serialized representation
+    */
+   String writeObject(ConnectQosData msgQosData, String extraOffset);
+
+   /** A human readable name of this factory */
+   String getName();
+}
