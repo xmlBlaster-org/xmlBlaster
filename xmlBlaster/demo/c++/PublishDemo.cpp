e3767869c96fcc8758192e12aee455895edf9f1e
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-01-14 22:36:55 +0000

    added demos for publish and subscribe
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@8117 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/demo/c++/PublishDemo.cpp
@@ -0,0 +1,121 @@
+/*-----------------------------------------------------------------------------
+Name:      PublishDemo.cpp
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Little demo to show how a publish is done
+-----------------------------------------------------------------------------*/
+
+#include <client/XmlBlasterAccess.h>
+#include <util/Global.h>
+#include <util/PlatformUtils.hpp>
+#include <boost/lexical_cast.hpp>
+
+
+/**
+ *
+ */
+
+using boost::lexical_cast;
+using namespace std;
+using namespace org::xmlBlaster::client;
+using namespace org::xmlBlaster::client::qos;
+using namespace org::xmlBlaster::client::key;
+
+class PublishDemo
+{
+private:
+   string           ME;
+   Global&          global_;
+   Log&             log_;
+   XmlBlasterAccess connection_;
+
+public:
+   PublishDemo(Global& glob) 
+      : ME("PublishDemo"), 
+        global_(glob), 
+        log_(glob.getLog("demo")),
+        connection_(global_)
+   {
+   }
+
+   void connect()
+   {
+      ConnectQos connQos(global_);
+      log_.trace(ME, string("connecting to xmlBlaster. Connect qos: ") + connQos.toXml());
+      ConnectReturnQos retQos = connection_.connect(connQos, NULL); // no callback
+      log_.trace(ME, "successfully connected to xmlBlaster. Return qos: " + retQos.toXml());
+   }
+
+   void publish(const string& oid="c++-demo", const string& clientTags="<demo/>", const string& content="simple content")
+   {
+      PublishKey key(global_, oid, "text/xml", "1.0");
+      key.setClientTags("<org.xmlBlaster><demo/></org.xmlBlaster>");
+      PublishQos qos(global_);
+      MessageUnit msgUnit(key, content, qos);
+      log_.trace(ME, string("published message unit: ") + msgUnit.toXml());
+      PublishReturnQos tmp = connection_.publish(msgUnit);
+      log_.trace(ME, string("publish return qos: ") + tmp.toXml());
+   }
+
+/*
+      try {
+         EraseKey key(global_);
+         key.setOid(oid);
+         EraseQos qos(global_);
+         vector<EraseReturnQos> arr = connection_->erase(key, qos);
+         assertEquals(log_, ME, (size_t)1, arr.size(), "Erase");
+      } 
+      catch(XmlBlasterException& e) { 
+         assert(0);
+      }
+   }
+*/
+
+};
+
+/**
+ * Try
+ * <pre>
+ *   java TestSubXPath -help
+ * </pre>
+ * for usage help
+ */
+int main(int args, char ** argv)
+{
+   try {
+      XMLPlatformUtils::Initialize();
+      Global& glob = Global::getInstance();
+      glob.initialize(args, argv);
+
+      PublishDemo demo(glob);
+      demo.connect();
+      for (int i=0; i < 10; i++) {
+         demo.publish();
+         org::xmlBlaster::util::thread::Thread::sleepSecs(1);
+      }
+   }
+   catch (XmlBlasterException& ex) {
+      std::cout << ex.toXml() << std::endl;
+   }
+   catch (exception& ex) {
+      cout << " exception: " << ex.what() << endl;
+   }
+   catch (bad_exception& ex) {
+      cout << "bad_exception: " << ex.what() << endl;
+   }
+   catch (string& ex) {
+      cout << "string: " << ex << endl;
+   }
+   catch (char* ex) {
+      cout << "char* :  " << ex << endl;
+   }
+
+   catch (...)
+   {
+      cout << "unknown exception occured" << endl;
+      XmlBlasterException e(INTERNAL_UNKNOWN, "main", "main thread");
+      cout << e.toXml() << endl;
+   }
+
+   return 0;
+}
---
+++ xmlBlaster/demo/c++/SubscribeDemo.cpp
@@ -0,0 +1,116 @@
+/*-----------------------------------------------------------------------------
+Name:      SubscribeDemo.cpp
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Little demo to show how a publish is done
+-----------------------------------------------------------------------------*/
+
+#include <client/XmlBlasterAccess.h>
+#include <util/Global.h>
+#include <util/PlatformUtils.hpp>
+#include <boost/lexical_cast.hpp>
+
+
+/**
+ *
+ */
+
+using boost::lexical_cast;
+using namespace std;
+using namespace org::xmlBlaster::client;
+using namespace org::xmlBlaster::client::qos;
+using namespace org::xmlBlaster::client::key;
+
+class SubscribeDemo : public I_Callback
+{
+private:
+   string           ME;
+   Global&          global_;
+   Log&             log_;
+   XmlBlasterAccess connection_;
+
+public:
+   SubscribeDemo(Global& glob) 
+      : ME("SubscribeDemo"), 
+        global_(glob), 
+        log_(glob.getLog("demo")),
+        connection_(global_)
+   {
+   }
+
+   void connect()
+   {
+      ConnectQos connQos(global_);
+      log_.trace(ME, string("connecting to xmlBlaster. Connect qos: ") + connQos.toXml());
+      ConnectReturnQos retQos = connection_.connect(connQos, this);
+      log_.trace(ME, "successfully connected to xmlBlaster. Return qos: " + retQos.toXml());
+   }
+
+   void subscribe(const string& oid="c++-demo")
+   {
+      SubscribeKey key(global_);
+      key.setOid(oid);
+      SubscribeQos qos(global_);
+      log_.trace(ME, string("subscribing to xmlBlaster with key: ") + key.toXml() + " and qos: " + qos.toXml());
+      SubscribeReturnQos subRetQos = connection_.subscribe(key, qos);
+      log_.trace(ME, string("successfully subscribed to xmlBlaster. Return qos: ") + subRetQos.toXml());
+   }
+
+   string update(const string& sessionId, UpdateKey& updateKey, void *content, long contentSize, UpdateQos& updateQos)
+   {
+      if (log_.TRACE) log_.trace(ME, "update: key    : " + updateKey.toXml());
+      if (log_.TRACE) log_.trace(ME, "update: qos    : " + updateQos.toXml());
+      string help((char*)content, (char*)(content)+contentSize);
+      if (log_.TRACE) log_.trace(ME, "update: content: " + help);
+      if (updateQos.getState() == "ERASED" ) return "";
+
+      return "";
+   }
+
+
+};
+
+/**
+ * Try
+ * <pre>
+ *   java TestSubXPath -help
+ * </pre>
+ * for usage help
+ */
+int main(int args, char ** argv)
+{
+   try {
+      XMLPlatformUtils::Initialize();
+      Global& glob = Global::getInstance();
+      glob.initialize(args, argv);
+
+      SubscribeDemo demo(glob);
+      demo.connect();
+      demo.subscribe();
+      org::xmlBlaster::util::thread::Thread::sleepSecs(12);
+   }
+   catch (XmlBlasterException& ex) {
+      std::cout << ex.toXml() << std::endl;
+   }
+   catch (exception& ex) {
+      cout << " exception: " << ex.what() << endl;
+   }
+   catch (bad_exception& ex) {
+      cout << "bad_exception: " << ex.what() << endl;
+   }
+   catch (string& ex) {
+      cout << "string: " << ex << endl;
+   }
+   catch (char* ex) {
+      cout << "char* :  " << ex << endl;
+   }
+
+   catch (...)
+   {
+      cout << "unknown exception occured" << endl;
+      XmlBlasterException e(INTERNAL_UNKNOWN, "main", "main thread");
+      cout << e.toXml() << endl;
+   }
+
+   return 0;
+}
