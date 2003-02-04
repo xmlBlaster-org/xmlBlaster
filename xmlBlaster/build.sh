99ba53f9f0e550878632b39d04ea731d760ed765
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-02-04 13:22:30 +0000

    Removed BUILDCOMPILER as it is not portable, use build.properties instead
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@8382 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/build.sh
+++ xmlBlaster/build.sh
@@ -7,15 +7,6 @@ if [ ${JAVA_HOME:=""} = "" ] ; then
    exit
 fi
 
-#-------- Checkin for preset Buildcompiler -
-# if you want to switch, set in your own .profile
-# default is modern, possible values are classic, modern or jikes
-if [ "${BUILDCOMPILER}" = "" ] ; then
-   BUILDCOMPILER=modern
-fi
-export BUILDCOMPILER
-
-
 CLASSPATH=$JAVA_HOME/lib/tools.jar:lib/ant/ant.jar:lib/ant/cpptasks.jar:lib/ant/ant-contrib.jar:lib/ant/xerces.jar:lib/ant/optional.jar:lib/junit.jar:lib/ant/xalan.jar:lib/ant/xml-apis.jar
 
 ALL_ENV=`env`
@@ -26,8 +17,9 @@ for i in ${ALL_ENV} ; do
 done
 #echo ${MY_DEF}
 
-# -Dbuild.compiler=jikes  or  modern  or classic
+# Set in your $HOME/build.properties
+# build.compiler=jikes  or  modern  or classic
 
-$JAVA_HOME/bin/java -Dbuild.compiler=$BUILDCOMPILER -Dant.home=$XMLBLASTER_HOME ${MY_DEF} -classpath $CLASSPATH org.apache.tools.ant.Main $@
+$JAVA_HOME/bin/java -Dant.home=$XMLBLASTER_HOME ${MY_DEF} -classpath $CLASSPATH org.apache.tools.ant.Main $@
 
 # end of file
