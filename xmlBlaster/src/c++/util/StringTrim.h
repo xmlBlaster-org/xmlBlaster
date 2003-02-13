c79f26a67382af3c9d33209f6f86c0a78c41cf87
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-02-13 13:54:15 +0000

    Ported to STD C++ string and added new variants
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@8489 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/c++/util/StringTrim.h
+++ xmlBlaster/src/c++/util/StringTrim.h
@@ -10,6 +10,10 @@ Author:    <Michele Laghi> laghi@swissinfo.org
 #define _UTIL_STRINGTRIM_H
 
 #include <util/XmlBCfg.h>
+#include <ctype.h>
+#include <string>
+
+#define  EMPTY_STRING string("")
 
 using namespace std;
 
@@ -23,9 +27,8 @@ namespace util {
     * This class is part of the package because the XMLString::trim(...) 
     * method in the xerces package does not fullfill our requirements.
     */
-   template <class CharT> class Dll_Export StringTrim
+   class Dll_Export StringTrim
    {
-      
    public:
       
       /** 
@@ -34,80 +37,165 @@ namespace util {
       StringTrim()
       {
       }
-      
+
+      /**
+       * @param str The checked string (is not modified)
+       * @return true if trimmed string is "TRUE" or "true" else false
+       */
+      static bool isTrue(const string& str) {
+         string tmp = trim(str.c_str());
+         return string("true")==tmp || string("TRUE")==tmp;
+      }
+
+      /**
+       * Use this method instead of isTrue(string&) if your input string may
+       * be manipulated (it performs a bit better then the other method). 
+       * @param str The checked string (is trimmed!!)
+       * @return true if trimmed string is "TRUE" or "true" else false
+       */
+      static bool isTrueTrim(string& str) {
+         trim(str);
+         return string("true")==str || string("TRUE")==str;
+      }
 
       /**
        * returns the length of the character string str. (by detecting the 
        * first occurence of zero.
        */
-      int stringLength(const CharT *str) const
+       /*
+      static int stringLength(const char *str)
       {
          int count = 0;
-         while (str[count] != (CharT)0) count++;
+         while (str[count] != (char)0) count++;
          return count;
       }
-      
+        */
 
       /**
-       * trims the start of the string (whitespaces, newlines and tabs are
-       * the trimmed characters). It creates a new string, so the caller is
-       * responsible for deleting the return string which has been allocated by
-       * this method.
+       * Trims the start of the string (whitespaces, newlines etc.). 
        */
-      CharT* trimStart(const CharT *str) const
+      static string trimStart(const char *str)
       {
-         int length = stringLength(str), start = 0;
-         while ( (start      != length     ) && 
-                 ( (str[start] == (CharT)' ' ) ||
-                   (str[start] == (CharT)'\t') ||
-                   (str[start] == (CharT)'\n') ) ) start++;
-         if (start == length) return 0;
-         int sizeOfNewString = length - start + 1; // zero terminated string !!
-         CharT *ret = new CharT[sizeOfNewString];
-         for (int i=0; i < sizeOfNewString; i++) ret[i] = str[start+i];
-         return ret;
+         if (str == static_cast<const char *>(0)) return EMPTY_STRING;
+
+         int start = 0;
+         while (str[start] != 0)
+            if (isspace(str[start++]))
+               continue;
+            else
+               return string(str + (start-1));
+
+         return EMPTY_STRING;
       }
       
 
       /**
-       * trims the end of the string (whitespaces, newlines and tabs are
-       * the trimmed characters). It creates a new string, so the caller is
-       * responsible for deleting the return string which has been allocated by
-       * this method.
+       * @param str The given string will be trimmed (in - out parameter)
+       * @param Returns the trimmed given parameter (the str instance)
+       * @see #trimStart(const char *)
        */
-      CharT* trimEnd(const CharT *str) const
+      static string& trimStart(string &str)
       {
-         int length = stringLength(str), end = length-1;
-         while ( (end        != -1     ) && 
-                 ( (str[end] == (CharT)' ' ) ||
-                   (str[end] == (CharT)'\t') ||
-                   (str[end] == (CharT)'\n') ) ) end--;
-         if (end == -1) return 0;
-         int sizeOfNewString = end + 2; // zero terminated string !!
-         CharT *ret = new CharT[sizeOfNewString];
-         for (int i=0; i < sizeOfNewString; i++) ret[i] = str[i];
-         ret[sizeOfNewString-1] = (CharT)0;
-         return ret;
+         if (str.capacity() < 1) return str;
+
+         if (str.length() < 1)
+            return str;
+         if (!isspace(str[0]))
+            return str;
+
+         for (string::size_type ii=1; ii<str.length(); ii++) {
+            if (!isspace(str[ii])) {
+               str = str.substr(ii);
+               return str;
+            }
+         }
+
+         return str;
+      }
+
+      /**
+       * Trims all spaces from the end of the string (whitespaces, newlines etc). 
+       */
+      static string trimEnd(const char *str)
+      {
+         if (str == static_cast<const char *>(0) || *str == 0) return EMPTY_STRING;
+         string strip(str);
+         trimEnd(strip);
+         return strip;
       }      
+
+      /**
+       * @param str The given string will be trimmed (in - out parameter)
+       * @param Returns the trimmed given parameter (the str instance)
+       * @see #trimEnd(const char *)
+       */
+      static string& trimEnd(string &str)
+      {
+         if (str.capacity() < 1) return str;
+
+         int i;
+         for (i=str.length()-1; i >= 0; i--) {
+             if (!isspace(str[i])) {
+                 str = str.substr(0, i+1);
+                 return str;
+             }
+         }
+         if (i<0) str = EMPTY_STRING;
+         return str;
+      }
       
 
       /**
-       * trims the start and the end of the string (whitespaces, newlines and 
-       * tabs are the trimmed characters). It creates a new string, so the 
-       * caller is responsible for deleting the return string which has been 
-       * allocated by this method.
+       * Trims all spaces (like blanks and newlines) at the start and the end of the string. 
+       * <pre>
+       * "  \t Hello \t World \n" -> "Hello \t World"
+       * </pre>
+       * @param str The given string
+       * @return The trimmed string
        */
-      CharT* trim(const CharT *str) const
+      static string trim(const char *str)
       {
-         CharT* buffer = trimStart(str);
-         if (buffer == 0) return 0;
-         CharT* ret = trimEnd(buffer);
-         if (buffer) delete buffer;
-         return ret;
+         string buffer = trimStart(str);
+         if (buffer.empty()) return EMPTY_STRING;
+         return trimEnd(buffer.c_str());
       }
-      
+
+      /**
+       * @param str The given string will be trimmed (in - out parameter)
+       * @param Returns the trimmed given parameter (the str instance)
+       * @see #trim(const char *)
+       */
+      static string& trim(string &str)
+      {
+         if (str.capacity() < 1 || str.size() < 1) return str;
+
+         int jj=0;
+         if (isspace(str[str.size()-1])) {
+            for (jj=str.size()-2; jj >= 0; jj--) {
+                if (!isspace(str[jj])) {
+                   str.resize(jj+1);
+                   break;
+                }
+            }
+         }
+         if (jj<0) {
+            str = EMPTY_STRING;
+            return str;
+         }
+
+         if (!isspace(str[0]))
+            return str;
+         for (string::size_type ii=1; ii<str.size(); ii++) {
+            if (!isspace(str[ii])) {
+               str = str.substr(ii);
+               return str;
+            }
+         }
+         return str;
+      }
    };
 }}} // namespace
 
 #endif
+
 
