62a450098b690c3d5ff0390478e24348dee458ef
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-02-17 21:15:39 +0000

    Increased the default msgstore size to unlimited, the cache has still only 2 MB
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@8548 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/util/qos/storage/MsgUnitStoreProperty.java
+++ xmlBlaster/src/java/org/xmlBlaster/util/qos/storage/MsgUnitStoreProperty.java
@@ -8,12 +8,15 @@ package org.xmlBlaster.util.qos.storage;
 import org.jutils.log.LogChannel;
 import org.xmlBlaster.util.Global;
 import org.xmlBlaster.util.enum.Constants;
+import org.xmlBlaster.util.property.PropEntry;
 import org.xml.sax.Attributes;
 
 /**
  * Helper class holding properties of the MsgUnit storage. 
- * <p />
- * See ConnectQos for XML sysntax.
+ * <p>
+ * Here the real message including the message content is stored.
+ * </p>
+ * See ConnectQos for XML syntax.
  * @see org.xmlBlaster.client.qos.ConnectQos
  */
 public class MsgUnitStoreProperty extends QueuePropertyBase
@@ -22,13 +25,20 @@ public class MsgUnitStoreProperty extends QueuePropertyBase
    private final LogChannel log;
 
    /**
+    * Increases the default bytes in the storage to 25 MB for each topic
     * @see QueuePropertyBase#QueuePropertyBase(Global, String)
     */
    public MsgUnitStoreProperty(Global glob, String nodeId) {
       super(glob, nodeId);
       this.log = glob.getLog("core");
       relating = Constants.RELATING_MSGUNITSTORE;
+
       super.initialize(Constants.RELATING_MSGUNITSTORE);
+      
+      //super.maxMsgCache.setDefaultValue(2000);
+      super.maxMsg.setDefaultValue(Integer.MAX_VALUE);
+      //super.maxBytesCache.setDefaultValue(Integer.MAX_VALUE);
+      super.maxBytes.setDefaultValue(Integer.MAX_VALUE);
    }
 
    public final boolean onOverflowDeadMessage() {
