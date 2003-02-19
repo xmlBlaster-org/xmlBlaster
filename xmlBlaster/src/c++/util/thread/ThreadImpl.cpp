68498d46790790cf049b94fb8318c56ab61dab71
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-02-19 10:17:02 +0000

    Comment only
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@8571 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/c++/util/thread/ThreadImpl.cpp
+++ xmlBlaster/src/c++/util/thread/ThreadImpl.cpp
@@ -1,6 +1,15 @@
+/*
+ * xmlBlaster/src/c++/util/thread/ThreadImpl.cpp
+ *
+ * Used to switch between the http://www.boost.org C++ multi threading library
+ * and http://omniorb.sourceforge.net/ omniORB omnithread C++ multi threading library (default)
+ * Add 
+ *  thread.impl   = BOOST
+ * to your build.properties (or as command line switch) to force using boost
+ */
 #ifdef BOOST
 #include "ThreadImplBoost.cc"
-#else
+#else // OMNITHREAD
 #include "ThreadImplOmniOrb.cc"
 #endif
 
