fbb93680f10f1fd4cf14a4caa7d6401839d87845
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-03-24 19:59:26 +0000

    deprecated
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9120 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/client/I_ConnectionProblems.java
+++ xmlBlaster/src/java/org/xmlBlaster/client/I_ConnectionProblems.java
@@ -3,7 +3,7 @@ Name:      I_ConnectionProblems.java
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Helper to easy get the callback messages
-Version:   $Id: I_ConnectionProblems.java,v 1.5 2003/03/24 16:12:57 ruff Exp $
+Version:   $Id: I_ConnectionProblems.java,v 1.6 2003/03/24 19:59:26 ruff Exp $
 Author:    xmlBlaster@marcelruff.info
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.client;
@@ -13,9 +13,9 @@ package org.xmlBlaster.client;
  * Callback the client from XmlBlasterConnection if the connection to xmlBlaster is lost
  * or was reestablished (fail save mode).
  * <p>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  * @author $Author: ruff $
- * @deprecated Please use I_ConnectionStateListener instead
+ * @deprecated Please use I_ConnectionStateListener and I_XmlBlasterAccess instead
  */
 public interface I_ConnectionProblems
 {
