29934b7a1d92e980f9b64a28a1f701f15ea06cd7
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-03-27 23:21:34 +0000

    args check
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9245 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/util/Global.java
+++ xmlBlaster/src/java/org/xmlBlaster/util/Global.java
@@ -839,6 +839,14 @@ public class Global implements Cloneable
     */
    public final String[] getArgs()
    {
+      if (args != null) {
+         for(int i=0; i<args.length; i++) {
+            if (args[i] == null) {
+               if (log.TRACE) log.trace(ME, "args[" + i + "] is null");
+               args[i] = "";
+            }
+         }
+      }
       return this.args;
    }
 
