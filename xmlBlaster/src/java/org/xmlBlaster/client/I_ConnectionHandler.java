ecaf58885cd017e310530c0c7f9649604d6b9020
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-03-31 09:25:55 +0000

    Removed getGlobal(), flushQueue(), isFailSafe(), getConnectReturnQos(), getConnectQos(): Contains now only dispatch framework specific methods
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9267 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/client/I_ConnectionHandler.java
+++ xmlBlaster/src/java/org/xmlBlaster/client/I_ConnectionHandler.java
@@ -13,40 +13,16 @@ import org.xmlBlaster.client.qos.ConnectReturnQos;
 
 
 /**
- * Access the remote connection object to access information or manipulate it. 
+ * Access the connection handler to access connection status information or manipulate queued messages. 
  * <p>
  * @author xmlBlaster@marcelruff.info
  */
 public interface I_ConnectionHandler
 {
    /**
-    * Access the environment settings of this connection. 
-    * @return The global handle (like a stack with local variables for this connection)
-    */
-   public Global getGlobal();
-
-   /**
-    * Flushes all entries in the queue, i.e. the entries of the queue are sent to xmlBlaster. 
-    * <p>
-    * This method blocks until all entries in the queue have been sent.
-    * </p>
-    * @return The number of sent messages. <br />
-    *         If the queue is empty or NULL, then 0 is returned.<br />
-    *         If the state is in POLLING or DEAD, then -1 is returned.<br />
-    */
-   long flushQueue() throws XmlBlasterException;
-
-   /**
     * @return The queue used to store tailback messages. 
     */
    I_Queue getQueue();
-
-   /**
-    * You can activate this mode by invoking initFailsafe
-    * in XmlBlasterAccess.
-    * @return Returns true if the connection is in failsafe mode
-    */
-   boolean isFailSafe();
 
    /**
     * @return true if the connection to xmlBlaster is operational
@@ -62,15 +38,5 @@ public interface I_ConnectionHandler
     * @return true if we have definitely lost the connection to xmlBlaster and gave up
     */
    boolean isDead();
-
-   /**
-    * Access the returned QoS of a connect() call. 
-    */
-   ConnectReturnQos getConnectReturnQos();
-
-   /**
-    * Access the current ConnectQos
-    */
-   ConnectQos getConnectQos();
 }
 
