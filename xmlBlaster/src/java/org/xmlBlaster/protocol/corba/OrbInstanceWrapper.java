09bf7fbe0d4e8003c05d818968b6dddba7e72f20
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-04-04 15:41:24 +0000

    Implements reference counting for a CORBA orb
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9361 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/corba/OrbInstanceWrapper.java
@@ -0,0 +1,64 @@
+/*------------------------------------------------------------------------------
+Name:      OrbInstanceWrapper.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   OrbInstanceWrapper class to invoke the xmlBlaster server using CORBA.
+Version:   $Id: OrbInstanceWrapper.java,v 1.1 2003/04/04 15:41:24 ruff Exp $
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.protocol.corba;
+
+import org.jutils.log.LogChannel;
+import org.xmlBlaster.util.Global;
+import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.util.enum.ErrorCode;
+import org.xmlBlaster.util.enum.Constants;
+import java.util.Properties;
+
+/**
+ * OrbInstanceWrapper wraps an org.omg.CORBA.ORB instance/singleton in Global scope. 
+ * The first call to getOrb() creates an ORB and following calls increment a reference counter.
+ * Calls to shutdown() reduce the reference counter. If the counter reaches 0 the
+ * orb is destroyed. 
+ */
+public class OrbInstanceWrapper
+{
+   private String ME = "OrbInstanceWrapper";
+   private org.omg.CORBA.ORB orb;
+   private int referenceCounter;
+
+   /**
+    * On first call an orb is created, further calls return the same orb instance. 
+    * @param glob
+    * @param args command line args, see org.omg.CORBA.ORB.init(), use glob.getArgs(String[], Properties)
+    * @param props application-specific properties; may be <code>null</code>, see org.omg.CORBA.ORB.init(String[], Properties)
+    * @param forCB set to true if used to configure the callback ORB (adds 'CB' to command line properties)
+    * @return Access to a new created orb handle
+    * @see org.omg.CORBA.ORB#init(String[], Properties)
+    */
+   public synchronized org.omg.CORBA.ORB getOrb(Global glob, String[] args, Properties props, boolean forCB) {
+      if (this.orb == null) {
+         this.orb = OrbInstanceFactory.createOrbInstance(glob, args, (Properties)null, forCB);
+      }
+      this.referenceCounter++;
+      return this.orb;
+   }
+
+   /**
+    * When the same amount releasOrb() is called as getOrb(), the internal orb is shutdown. 
+    */
+   public synchronized void releaseOrb(boolean wait_for_completion) {
+      this.referenceCounter--;
+      if (this.referenceCounter <= 0) {
+         this.referenceCounter = 0;
+         if (this.orb != null) {
+            try {
+               this.orb.shutdown(wait_for_completion);
+               this.orb = null;
+            }
+            catch (Throwable ex) {
+               System.err.println(ME+".releaseOrb: Exception occured during orb.shutdown("+wait_for_completion+"): " + ex.toString());
+            }
+         }
+      }
+   }
+}
