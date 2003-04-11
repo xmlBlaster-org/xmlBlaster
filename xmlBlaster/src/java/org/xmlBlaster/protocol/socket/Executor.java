57c309dbf9e9d7dade1d75d76b9471c4fe1662e7
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-04-11 06:55:16 +0000

    logging only
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9398 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/protocol/socket/Executor.java
+++ xmlBlaster/src/java/org/xmlBlaster/protocol/socket/Executor.java
@@ -458,7 +458,7 @@ public abstract class Executor implements ExecutorBase
     * use this method to free blocking threads which wait on responses
     */
    public final void freePendingThreads() {
-      if (log != null && log.TRACE) log.trace(ME, "Freeing " + latchSet.size() + " pending threads (waiting on responses) from their ugly blocking situation");
+      if (log != null && log.TRACE) log.trace(ME, "Freeing " + ((latchSet==null) ? 0 : latchSet.size()) + " pending threads (waiting on responses) from their ugly blocking situation");
       if (latchSet != null) {
          while (true) {
             Latch l = null;
