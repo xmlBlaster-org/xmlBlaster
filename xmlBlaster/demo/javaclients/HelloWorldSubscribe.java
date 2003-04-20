e60fc2ce74d5c23f4b3df44fc3d5135a37aa08b1
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-04-20 11:12:54 +0000

    Added updateSleep switch, add domain specific query
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9457 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/demo/javaclients/HelloWorldSubscribe.java
+++ xmlBlaster/demo/javaclients/HelloWorldSubscribe.java
@@ -62,8 +62,9 @@ public class HelloWorldSubscribe implements I_Callback
    private final String ME = "HelloWorldSubscribe";
    private final Global glob;
    private final LogChannel log;
-   private int updateCounter = 0;
+   private int updateCounter;
    private boolean interactiveUpdate;
+   private long updateSleep;
 
    public HelloWorldSubscribe(Global glob) {
       this.glob = glob;
@@ -71,12 +72,14 @@ public class HelloWorldSubscribe implements I_Callback
       try {
          boolean interactive = glob.getProperty().get("interactive", true);
          this.interactiveUpdate = glob.getProperty().get("interactiveUpdate", false);
+         this.updateSleep = glob.getProperty().get("updateSleep", 0L);
          String oid = glob.getProperty().get("oid", "");
+         String domain = glob.getProperty().get("domain", "");
          String xpath = glob.getProperty().get("xpath", "");
          boolean multiSubscribe = glob.getProperty().get("multiSubscribe", true);
          boolean local = glob.getProperty().get("local", true);
          boolean initialUpdate = glob.getProperty().get("initialUpdate", true);
-         boolean content = glob.getProperty().get("content", true);
+         boolean wantContent = glob.getProperty().get("wantContent", true);
          int historyNumUpdates = glob.getProperty().get("historyNumUpdates", 1);
          String filterType = glob.getProperty().get("filter.type", "GnuRegexFilter");// XPathFilter | ContentLenFilter
          String filterVersion = glob.getProperty().get("filter.version", "1.0");
@@ -97,16 +100,23 @@ public class HelloWorldSubscribe implements I_Callback
             */
          }
 
+         if (this.updateSleep > 0L && interactiveUpdate == true) {
+            log.warn(ME, "You can't set 'updateSleep' and  'interactiveUpdate' simultaneous, we reset interactiveUpdate to false");
+            this.interactiveUpdate = false;
+         }
+
          log.info(ME, "Used settings are:");
          log.info(ME, "   -interactive       " + interactive);
-         log.info(ME, "   -interactiveUpdate " + interactiveUpdate);
+         log.info(ME, "   -interactiveUpdate " + this.interactiveUpdate);
+         log.info(ME, "   -updateSleep       " + this.updateSleep);
          log.info(ME, "   -oid               " + oid);
+         log.info(ME, "   -domain            " + domain);
          log.info(ME, "   -xpath             " + xpath);
          log.info(ME, "   -multiSubscribe    " + multiSubscribe);
          log.info(ME, "   -local             " + local);
          log.info(ME, "   -initialUpdate     " + initialUpdate);
          log.info(ME, "   -historyNumUpdates " + historyNumUpdates);
-         log.info(ME, "   -content           " + content);
+         log.info(ME, "   -wantContent       " + wantContent);
          log.info(ME, "   -unSubscribe       " + unSubscribe);
          log.info(ME, "   -disconnect        " + disconnect);
          log.info(ME, "   -filter.type       " + filterType);
@@ -124,12 +134,26 @@ public class HelloWorldSubscribe implements I_Callback
          ConnectReturnQos crq = con.connect(qos, this);  // Login to xmlBlaster, register for updates
          log.info(ME, "Connect success as " + crq.toXml());
 
-         SubscribeKey sk = (oid.length() > 0) ? new SubscribeKey(glob, oid) : new SubscribeKey(glob, xpath, Constants.XPATH);
+         SubscribeKey sk = null;
+         String qStr = null;
+         if (domain.length() > 0) {
+            sk = new SubscribeKey(glob, "", Constants.DOMAIN);
+            sk.setDomain(domain);
+            qStr = domain;
+         }
+         else if (oid.length() > 0) {
+            sk = new SubscribeKey(glob, oid);
+            qStr = oid;
+         }
+         else if (xpath.length() > 0) {
+            sk = new SubscribeKey(glob, xpath, Constants.XPATH);
+            qStr = xpath;
+         }
          SubscribeQos sq = new SubscribeQos(glob);
          sq.setWantInitialUpdate(initialUpdate);
          sq.setMultiSubscribe(multiSubscribe);
          sq.setWantLocal(local);
-         sq.setWantContent(content);
+         sq.setWantContent(wantContent);
          
          HistoryQos historyQos = new HistoryQos(glob);
          historyQos.setNumEntries(historyNumUpdates);
@@ -144,7 +168,7 @@ public class HelloWorldSubscribe implements I_Callback
          log.info(ME, "SubscribeQos=\n" + sq.toXml());
 
          if (interactive) {
-            log.info(ME, "Hit a key to subscribe '" + ((oid.length() > 0) ? oid : xpath) + "'");
+            log.info(ME, "Hit a key to subscribe '" + qStr + "'");
             try { System.in.read(); } catch(java.io.IOException e) {}
          }
 
@@ -210,7 +234,11 @@ public class HelloWorldSubscribe implements I_Callback
       System.out.println("============= END #" + updateCounter + " '" + updateKey.getOid() + "' =========================");
       System.out.println("");
 
-      if (interactiveUpdate) {
+      if (this.updateSleep > 0L) {
+         log.info(ME, "Sleeping for " + this.updateSleep + " millis ...");
+         try { Thread.currentThread().sleep(this.updateSleep); } catch( InterruptedException i) {}
+         log.info(ME, "Waking up.");
+      } else if (this.interactiveUpdate) {
          log.info(ME, "Hit a key to return from update() (we are blocking the server callback) ...");
          try { System.in.read(); } catch(java.io.IOException e) {}
          log.info(ME, "Returning update() - control goes back to server");
