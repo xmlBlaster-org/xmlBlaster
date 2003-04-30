b3bb4c46b4dcfa257b23e473fbefe77b6b17613d
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-04-30 21:31:02 +0000

    Basic JMX support from branch
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9568 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/engine/RequestBrokerMBean.java
@@ -0,0 +1,16 @@
+/*------------------------------------------------------------------------------
+Name:      RequestBrokerMBean.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.engine;
+import org.xmlBlaster.engine.admin.I_AdminNode;
+
+/**
+ * Export methods for JMX on cluster node level.
+ *
+ */
+
+public interface RequestBrokerMBean extends I_AdminNode {
+}
+
