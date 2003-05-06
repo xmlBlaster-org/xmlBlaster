70354f751c23323512655964fdf1ecfdd4a18397
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-05-06 16:23:52 +0000

    added objman
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9594 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/c++/util/objman.cpp
@@ -0,0 +1,131 @@
+
+#include <util/xmlBlasterDef.h>
+#include <util/objman.h>
+#include <util/Global.h>
+
+using namespace std;
+using namespace org::xmlBlaster::client::protocol;
+using namespace org::xmlBlaster::util::dispatch;
+using namespace org::xmlBlaster::util;
+
+#define PREALLOCATE_OBJECT(TYPE, ID)\
+{\
+  TYPE* t = new TYPE;\
+  ManagedObject<TYPE> *obj_p;\
+  obj_p = new  ManagedObject<TYPE>(t);\
+  preallocated_object[ID] = obj_p;\
+}
+
+int gObjectManagerState = 0;
+// Cleanup routine for atexit
+void object_manager_cleanup()
+{ 
+  if(gObjectManagerState != Object_Lifetime_Manager_Base::OBJ_MAN_SHUT_DOWN &&
+     gObjectManagerState != Object_Lifetime_Manager_Base::OBJ_MAN_SHUTTING_DOWN)
+    ;//Object_Lifetime_Manager::instance()->fini();
+}
+
+namespace org { namespace xmlBlaster { namespace util {
+
+// Static declaratons for Object_Lifetime_Manager.
+Object_Lifetime_Manager * Object_Lifetime_Manager::instance_ = 0;
+managed_list              Object_Lifetime_Manager::managed_objects_list_;
+void *                    Object_Lifetime_Manager::preallocated_object[PREALLOCATED_OBJECTS];
+
+
+void Object_Lifetime_Manager::init (void)
+{
+  Object_Lifetime_Manager::instance()->startup();
+}
+
+void Object_Lifetime_Manager::fini (void)
+{
+  Object_Lifetime_Manager::instance()->shutdown();
+}
+
+Object_Lifetime_Manager * Object_Lifetime_Manager::instance (void)
+{
+// This function should be called during
+// construction of static instances, or
+// before any other threads have been created
+// in the process. So, it’s not thread safe.
+  if (instance_ == 0) 
+  {
+    Object_Lifetime_Manager *instance_pointer =
+    new Object_Lifetime_Manager;
+    
+    instance_pointer->dynamically_allocated_ = 1;
+  }
+  return instance_;
+}
+
+int Object_Lifetime_Manager::startup ()
+{
+  if (starting_up_i ()) 
+  {
+    // First, indicate that this
+    // Object_Lifetime_Manager instance
+    // is being initialized.
+    object_manager_state_ = OBJ_MAN_INITIALIZING;
+    if (this == instance_) 
+    {      
+      // Create Global as part of ObjectManager startup.
+      PREALLOCATE_OBJECT(Global, XMLBLASTER_GLOBAL);
+      object_manager_state_ = OBJ_MAN_INITIALIZED;    
+    }
+    return 0;
+  } 
+  else 
+  {
+    // Hads already initialized.
+    return 1;
+  }
+}
+
+int Object_Lifetime_Manager::shutdown ()
+{
+  if (shutting_down_i ())
+    // Too late. Or, maybe too early. Either
+    // <fini> has already been called, or
+    // <init> was never called.
+    return object_manager_state_ == OBJ_MAN_SHUT_DOWN ? 1 : -1;
+
+  // Indicate that the Object_Lifetime_Manager
+  // instance is being shut down.
+  // This object manager should be the last one
+  // to be shut down.
+  gObjectManagerState = object_manager_state_ = OBJ_MAN_SHUTTING_DOWN;
+  
+  // Only clean up Preallocated Objects when
+  // the singleton Instance is being destroyed.
+  if (this == instance_) 
+  {
+   
+    managed_list::iterator i;
+
+    for(i = managed_objects_list_.begin(); i != managed_objects_list_.end(); i++)
+    {
+      (dynamic_cast<Cleanup_Adaptor*>(*i))->cleanup();
+      delete *i;
+    }
+
+    // Delete the Global post of list method as a dependancy may exist
+    // between objects existing in both constructs.
+    // Array is last as all objects placed here are intentional and
+    // it is assumed user is aware of such semantics.
+    delete (Cleanup_Adaptor*)preallocated_object[XMLBLASTER_GLOBAL];
+    // continue to remove any other array based objects
+
+  }
+
+  // Indicate that this Object_Lifetime_Manager
+  // instance has been shut down.
+  gObjectManagerState = object_manager_state_ = OBJ_MAN_SHUT_DOWN;
+  if (dynamically_allocated_)
+    delete this;
+  if (this == instance_)
+    instance_ = 0;
+  return 0;
+}
+
+}}}; // namespace
---
+++ xmlBlaster/src/c++/util/objman.h
@@ -0,0 +1,207 @@
+
+/**
+*
+* Author: Martin Johnson
+*
+* Based upon http://www.cs.wustl.edu/~schmidt/PDF/ObjMan.pdf
+* with some modification.
+*
+* Synopsis:
+* Support for Preallocated Managed Objects and Managed Objects.
+* Managed Objects are required to grant friendship to the following
+*
+*  template <class TYPE> friend class ManagedObject;
+*  friend class Object_Lifetime_Manager;
+*
+* Applications utilising Object_Lifetime_Manager are required to call
+*
+**/
+
+#ifndef _Object_Lifetime_Manager_
+#define _Object_Lifetime_Manager_
+
+#include <util/XmlBCfg.h>
+#include <stdlib.h>
+#include <list>
+
+extern "C"
+{
+  void object_manager_cleanup();
+};
+
+namespace org { namespace xmlBlaster { namespace util {
+
+class Cleanup_Adaptor
+{
+public:
+  Cleanup_Adaptor(){;}
+  virtual ~Cleanup_Adaptor(){;}
+
+  virtual void cleanup(){;}
+
+};
+
+template <class TYPE>
+class ManagedObject : public Cleanup_Adaptor
+{
+public:
+
+  ManagedObject(TYPE* p):obj_(p) {;}
+  virtual ~ManagedObject(){if(obj_ != 0) delete obj_;}
+
+  virtual void cleanup(){ delete obj_; obj_ = 0;}
+
+protected:
+  TYPE* obj_;
+private:
+  ManagedObject();
+  ManagedObject(const ManagedObject& in){ obj_ = in.obj_; }
+
+};
+
+typedef std::list<Cleanup_Adaptor*> managed_list;
+
+class Dll_Export Object_Lifetime_Manager_Base
+{
+public:
+  // Explicitly initialize. Returns 0 on success,
+  // -1 on failure due to dynamic allocation
+  // failure (in which case errno is set to
+  // ENOMEM), or 1 if it had already been called.
+  virtual int startup (void) = 0;
+
+  // Explicitly destroy. Returns 0 on success,
+  // -1 on failure because the number of <fini>
+  // calls hasn’t reached the number of <init>
+  // calls, or 1 if it had already been called.
+  virtual int shutdown (void) = 0;
+
+  enum Object_Lifetime_Manager_State 
+  {
+    OBJ_MAN_UNINITIALIZED,
+    OBJ_MAN_INITIALIZING,
+    OBJ_MAN_INITIALIZED,
+    OBJ_MAN_SHUTTING_DOWN,
+    OBJ_MAN_SHUT_DOWN
+  };
+
+protected:
+
+  Object_Lifetime_Manager_Base (void) :
+    object_manager_state_ (OBJ_MAN_UNINITIALIZED),
+    dynamically_allocated_ (0) {}
+
+  virtual ~Object_Lifetime_Manager_Base (void) 
+  {
+    // Clear the flag so that fini
+    // doesn’t delete again.
+    dynamically_allocated_ = 0;
+  }
+
+  // Returns 1 before Object_Lifetime_Manager_Base
+  // has been constructed. This flag can be used
+  // to determine if the program is constructing
+  // static objects. If no static object spawns
+  // any threads, the program will be
+  // single-threaded when this flag returns 1.
+  int starting_up_i (void) 
+  {
+    return object_manager_state_ < OBJ_MAN_INITIALIZED;
+  }
+
+  // Returns 1 after Object_Lifetime_Manager_Base
+  // has been destroyed.  
+  int shutting_down_i (void) 
+  {
+    return object_manager_state_ > OBJ_MAN_INITIALIZED;
+  }
+
+  // State of the Object_Lifetime_Manager;
+  Object_Lifetime_Manager_State object_manager_state_;
+
+  // Flag indicating whether the
+  // Object_Lifetime_Manager instance was
+  // dynamically allocated by the library.
+  // (If it was dynamically allocated by the
+  // application, then the application is
+  // responsible for deleting it.)
+  int dynamically_allocated_;  
+
+};
+
+class Dll_Export Object_Lifetime_Manager : public Object_Lifetime_Manager_Base
+{
+public:
+
+  static void init (void);
+  static void fini (void);
+
+  virtual int startup (void);
+  virtual int shutdown (void);
+
+  static int starting_up (void) 
+  {
+    return instance_ ? instance_->starting_up_i () : 1;
+  }
+
+  static int shutting_down (void) 
+  {
+    return instance_ ? instance_->shutting_down_i () : 1;
+  }
+
+  // Unique identifiers for Preallocated Objects.
+  enum Preallocated_Object
+  {
+    XMLBLASTER_GLOBAL,
+    PREALLOCATED_OBJECTS
+  };
+
+  // Accessor to singleton instance.
+  static Object_Lifetime_Manager *instance (void);
+public:
+ 
+  Object_Lifetime_Manager (void) 
+  {
+    // Make sure that no further instances are
+    // created via instance.
+    if (instance_ == 0)
+    {
+      instance_ = this;
+
+      // shown to be useless though if some one else has a better
+      // opinion. Doesnt work in win32 land.
+      //atexit(object_manager_cleanup);
+    }
+    init ();
+  }
+
+  ~Object_Lifetime_Manager (void) 
+  {
+    // Don’t delete this again in fini.
+    dynamically_allocated_ = 0;
+    fini ();
+  }
+
+  template <class T> void manage_object(T* obj)
+  {
+    ManagedObject<T>* mobj = new ManagedObject<T>(obj);
+
+    managed_objects_list_.push_back(mobj);
+  }
+
+private:
+
+  // Singleton instance pointer.
+  static Object_Lifetime_Manager *instance_;
+
+  // Array of Preallocated Objects.
+  static void * preallocated_object[PREALLOCATED_OBJECTS];
+
+  static managed_list managed_objects_list_;
+};
+
+}}}; //namespace
+
+#endif
+
+
