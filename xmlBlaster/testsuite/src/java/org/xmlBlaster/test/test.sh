33d7188841d53a45e6f85ec8fc6953587bd33d5a
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-05-20 20:20:45 +0000

    fileRecorder fix
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9659 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/src/java/org/xmlBlaster/test/test.sh
+++ xmlBlaster/testsuite/src/java/org/xmlBlaster/test/test.sh
@@ -3,7 +3,7 @@
 # Helper to start the different test groups in sequence (UNIX only)
 # You need to manually close each of the GUI windows to continue
 #
-rm -r $HOME/tmp/fileRecorder
+rm -rf $HOME/tmp/fileRecorder
 echo "STARTING classtest TESTS ..."
 java -Djava.compiler= junit.swingui.TestRunner -noloading org.xmlBlaster.test.classtest.AllTests
 echo "STARTING xmlBlaster and wait for 10 sec to startup ..."
