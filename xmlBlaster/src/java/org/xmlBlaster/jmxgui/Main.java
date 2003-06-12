ee21eb74d9d72336c95b63444071a6f11990f3c9
neubert <neubert@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-06-12 07:53:32 +0000

    Starting class for the GUI
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9928 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/jmxgui/Main.java
@@ -0,0 +1,34 @@
+/*------------------------------------------------------------------------------
+Name:      Main.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.jmxgui;
+
+import org.jutils.log.LogChannel;
+import org.xmlBlaster.util.Global;
+
+
+/**
+ * Class that starts GUI
+ */
+public class Main {
+
+
+  public static void main(String[] args) {
+    //get Globals and Log
+    Global glob = null;
+    LogChannel log = null;
+    if (glob == null) glob = new Global().instance();
+    log = glob.getLog("jmxGUI");
+
+    SplashWindow sw = new SplashWindow("tims_rainbowfish.gif",null, 5000);
+
+
+    MainFrame f = new MainFrame(glob);
+//    cc.start();
+    f.setVisible(true);
+    sw.setVisible(true);
+
+  }
+}
