b1d4024952d0eb93e720f4e58b121d234c2362d4
neubert <neubert@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-06-12 07:51:27 +0000

    class that wraps JMX-functionality / xmlBlasterfunctionality from client-classes
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9923 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/jmxgui/ConnectorClient.java
@@ -0,0 +1,118 @@
+/*------------------------------------------------------------------------------
+Name:      Main.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.jmxgui;
+
+import javax.management.*;
+
+import org.jutils.log.LogChannel;
+import org.xmlBlaster.util.Global;
+
+import org.xmlBlaster.client.jmx.AsyncMBeanServer;
+import org.xmlBlaster.client.jmx.ConnectorFactory;
+
+import org.xmlBlaster.client.jmx.ConnectorException;
+import java.rmi.RemoteException;
+
+/**
+ * Class that encapsulates Communication to xmlBlaster / MBeanServer from GUI
+ */
+public class ConnectorClient {
+
+  private LogChannel log = null;
+  private Global glob = null;
+  private static AsyncMBeanServer aServer = null;
+  final String ME = "ConnectorClient";
+
+  public ConnectorClient(Global glob, String serverName) {
+    if (glob == null) glob = new Global().instance();
+    log = glob.getLog("jmxGUI");
+    //trying to establish connection to JmxServer
+    log.info(ME,"New Connector client - trying to establish connection to JmxServer....");
+    if (aServer == null) {
+      try {
+        aServer = ConnectorFactory.createAsyncConnector("xmlBlaster", serverName);
+      }
+      catch (ConnectorException ex) {
+        log.error(ME,"Error when creating AsyncMBeanServerInstance " + ex.toString());
+        ex.printStackTrace();
+      }
+    }
+//    if ( aServer != null ) {log.info(ME,"Asynchron MBeanServer available: " + aServer.getDefaultDomain());}
+//    else {log.error(ME,"Error when creating AsyncMBeanServerInstance - Reference to Object Null");}
+  }
+
+  /**
+   * Returns AsyncMBeanServer Object. For basic access to MBean-functions within the clients
+   * @return
+   * @throws ConnectorException
+   */
+  public AsyncMBeanServer getServer() throws ConnectorException {
+    if ( this.aServer != null ) { return this.aServer; }
+    else {
+      throw new ConnectorException("MBeanServer not available");
+    }
+  }
+
+/**
+ * Returns a list of servers that have a JMX-Server running
+ * TODO: implement cluster-lookup from xmlBlaster-lib.
+ * JMX-implementation not working correctly
+ */
+  public String[] getServerList() {
+    return ConnectorFactory.getMBeanServerList();
+  }
+
+  /**
+   * logout from Server
+   */
+  public void logout() {
+    aServer.close();
+  }
+
+  /**
+   * Returns Information about all attributes that are handeled by the given
+   * MBean
+   * @param name name of the MBean
+   * @return Array of MBeanAttributeInfo
+   */
+  public MBeanAttributeInfo[] exploreMBeansByObjectName(String name) {
+    MBeanInfo mbInfo = getMBeanInfo(name);
+    return mbInfo.getAttributes();
+
+  }
+
+  /**
+   * Discovers the attributes and operations that an MBean exposes for management.
+   * @param name name of the Bean
+   * @return MBeanInfo
+   */
+  private MBeanInfo getMBeanInfo(String name) {
+    MBeanInfo mbInfo = null;
+    ObjectName RequestBroker = null;
+    if (aServer != null ) {
+      try {
+      RequestBroker = new ObjectName(name);
+      mbInfo = (MBeanInfo) aServer.getMBeanInfo(RequestBroker).get();
+      }
+      catch (Exception ex) {
+        log.error(ME,"Error when retrieving information about " + name  +" >> " + ex.toString());
+        ex.printStackTrace();
+      }
+    }
+    else log.error(ME,"Lost AsyncServer!");
+    return mbInfo;
+  }
+
+  /**
+   * Discovers the Operations that an MBEan exposes for management
+   * @param name name of the Bean
+   * @return Array of MBeanOperationInfo
+   */
+  public MBeanOperationInfo[] exploreMBeanOperationsByObjectName(String name) {
+    MBeanInfo mbInfo = getMBeanInfo(name);
+    return mbInfo.getOperations();
+  }
+}
