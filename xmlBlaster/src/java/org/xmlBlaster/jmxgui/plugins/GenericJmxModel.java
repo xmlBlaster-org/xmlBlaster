80bd64a844bb9ef7ece015f3d4ad4ed1aed4675e
neubert <neubert@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-06-12 07:48:51 +0000

    Generic Model for tables. Helps to develop quickly a table with key-value pairs that are displayed within a JTable
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9919 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/jmxgui/plugins/GenericJmxModel.java
@@ -0,0 +1,134 @@
+/*------------------------------------------------------------------------------
+Name:      GenericJmxModel.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.jmxgui.plugins;
+
+import org.xmlBlaster.jmxgui.*;
+
+import javax.management.ObjectName;
+import javax.management.MBeanAttributeInfo;
+import javax.management.MalformedObjectNameException;
+import java.util.Vector;
+import java.util.HashMap;
+import org.xmlBlaster.client.jmx.*;
+import java.rmi.*;
+
+/**
+ * Model that wraps key-value pairs from MBeans that are exposed for management
+ */
+public class GenericJmxModel extends javax.swing.table.AbstractTableModel{
+  protected Vector vecMBeans;
+  protected ConnectorClient cc = null;
+  protected AsyncMBeanServer aServer = null;
+  protected ObjectName RequestBroker = null;
+  private HashMap hmtable = new HashMap();
+  private String objectName ="";
+  private String className="";
+
+  public GenericJmxModel(ConnectorClient cc, String objectName, String className) {
+    this.objectName = objectName;
+    this.cc = cc;
+    this.className = className;
+    loadData();
+  }
+
+  public void loadData() {
+    try {
+      ObjectName name = new ObjectName(objectName);
+      boolean isLoaded = ((Boolean) (cc.getServer().isRegistered(name).get())).booleanValue();
+      if (!isLoaded) {
+        loadMBean(objectName, className);
+      }
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+    }
+    exploreMBeans();
+    fillHashMap();
+  }
+
+  public void fillHashMap() {
+    for (int i = 0; i<vecMBeans.size(); i++) {
+      try {
+        hmtable.put(new Integer(i), aServer.getAttribute(RequestBroker, (String) vecMBeans.get(i)).get());
+      }
+      catch (RemoteException ex) {
+      }
+    }
+  }
+
+  public void exploreMBeans() {
+    vecMBeans = new Vector();
+    try {
+      aServer= cc.getServer();
+    }
+    catch (ConnectorException ex) {
+      ex.printStackTrace();
+    }
+
+    try {
+      RequestBroker = new ObjectName(objectName);
+    }
+    catch (MalformedObjectNameException ex) {
+    }
+    MBeanAttributeInfo[] attrInfo = cc.exploreMBeansByObjectName(objectName);
+    for (int i =0; i<attrInfo.length; i++) {
+      vecMBeans.addElement(attrInfo[i].getName());
+    }
+  }
+
+  public void loadMBean(String myObjectName, String className) {
+    try {
+      AsyncMBeanServer server = cc.getServer();
+      server.createMBean(className, new ObjectName(myObjectName));
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+
+
+//Swing implementation
+  public boolean isCellEditable (int i, int j) {
+    return false;
+  }
+
+  public int getRowCount() {
+    return vecMBeans.size();
+  }
+
+  public int getColumnCount() {
+    return 2;
+  }
+  public Object getValueAt(int row, int column) {
+    Object obj = new Object();
+
+    if (column == 0) {
+      obj = vecMBeans.get(row);
+    }
+    if (column == 1) {
+      try {
+        obj = hmtable.get(new Integer(row));
+      }
+      catch (Exception ex) {
+        ex.printStackTrace();
+      }
+    }
+    return obj;
+  }
+
+  public String getColumnName(int columnIndex) {
+    String name = "";
+    if (columnIndex==0) {
+     name= "key";
+    }
+    if (columnIndex==1) {
+     name = "value";
+    }
+    return name;
+  }
+
+}
