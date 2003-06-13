eac9e3480b75af3f37868f67b98607766d2a6a2a
neubert <neubert@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-06-13 10:06:15 +0000

    Added log  / comments
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@9956 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/jmxgui/util/XmlUtil.java
+++ xmlBlaster/src/java/org/xmlBlaster/jmxgui/util/XmlUtil.java
@@ -6,19 +6,16 @@ Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 package org.xmlBlaster.jmxgui.util;
 
 import org.xml.sax.InputSource;
-import org.xml.sax.Parser;
 import org.xml.sax.SAXException;
 import org.w3c.dom.Document;
 
-import org.xml.sax.helpers.XMLReaderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.*;
+import org.jutils.log.LogChannel;
 
-import org.xmlBlaster.util.FileLocator;
 import org.xmlBlaster.util.Global;
 import org.xmlBlaster.util.*;
 
@@ -26,31 +23,46 @@ import org.xmlBlaster.util.*;
  * Helperclass for Configuration
  */
 public class XmlUtil {
+  private LogChannel log = null;
+  private Global glob = null;
+  private static String ME = "XmlUtil";
+
+  public XmlUtil() {
+    this.glob = new Global();
+    this.log = this.glob.getLog("jmxGUI");
+  }
 
   private DocumentBuilder docBuilder = null;
 
+  /**
+   * Loads configurationfile from local ressourcepath.
+   * Parses the configfile into a Document
+   * @param filename file where to find configuration
+   * @return
+   */
   public Document loadConfig (String filename) {
-  FileLocator locator = new FileLocator(new Global().instance());
-  java.net.URL oUrl = null;
-  oUrl = this.getClass().getResource(filename);
+    log.info(ME,"loading config from file " + filename);
+    java.net.URL oUrl = null;
+    oUrl = this.getClass().getResource(filename);
 
-  Document doc = null;
-  try {
+    Document doc = null;
     try {
-     docBuilder  = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-    } catch (Exception e) {
-    System.err.println(e.getMessage());
+      try {
+        docBuilder  = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
+      }
+      InputSource in = new InputSource(oUrl.openStream());
+      doc = docBuilder.parse(in);
     }
-    InputSource in = new InputSource(oUrl.openStream());
-    doc = docBuilder.parse(in);
+    catch (SAXException ex) {
+      log.error(ME,"Error parsing xmlString from file " + filename);
+      ex.printStackTrace();
+    }
+    catch (IOException ex) {
+      log.error(ME,"Error reading file " + filename);
+      ex.printStackTrace();
+    }
+    return doc;
   }
-  catch (SAXException ex) {
-    ex.printStackTrace();
-  }
-  catch (IOException ex) {
-    ex.printStackTrace();
-  }
-  return doc;
-}
-
 }
