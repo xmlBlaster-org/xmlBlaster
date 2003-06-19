36ec7092d8986c80cce1c0976cfc484b174bb082
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-06-19 13:34:55 +0000

    Run external processes
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@10023 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/util/Execute.java
@@ -0,0 +1,209 @@
+/*------------------------------------------------------------------------------
+Name:      Execute.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Starts a program
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.util;
+
+import java.lang.*;
+import java.util.Vector;
+import java.io.*;
+
+import org.xmlBlaster.util.XmlBlasterException;
+import org.jutils.log.LogChannel;
+
+
+/**
+ * Starts a program and delivers the exit value and some out parameter.
+ */
+public class Execute
+{
+   private static final String ME = "Execute";
+   private Global glob;
+   private LogChannel log;
+   private Process process = null;
+   private String[] commandArr;
+   private String[] envArr;
+
+   private int BUFFERED_READER_SIZE = 200000; // 200 kBytes, must be big enough to collect stdout/stderr
+   /** Thread to collect stdout of a process */
+   private OutputThread stdoutThread;
+   private StringBuffer stdout = new StringBuffer();
+   /** Thread to collect stderr of a process */
+   private OutputThread stderrThread;
+   private StringBuffer stderr = new StringBuffer();
+
+   private int exitValue;
+
+   /**
+    * Construct an instance which can execute a program with the given parameters.
+    */
+   public Execute(Global glob, String[] commandArr, String[] envArr) {
+      this.glob = glob;
+      this.log = glob.getLog("core");
+      this.commandArr = commandArr;
+      this.envArr = envArr;
+      if (this.commandArr == null || this.commandArr.length < 1) {
+         throw new IllegalArgumentException("Please provide the process to start");
+      }
+   }
+
+   /**
+    * Start
+    */
+   public void run() {
+      if (log.TRACE) log.trace( ME, "Entering Method Execute.run()");
+      try {
+         Runtime runtime = Runtime.getRuntime();
+         process = runtime.exec(commandArr, envArr); // start the process
+         // This exec() is not blocking, it returns even when the command still executes
+
+
+         // get command's output stream and collect it in stdout
+         InputStream istr = process.getInputStream();
+         BufferedReader ibr = new BufferedReader(new InputStreamReader(istr), BUFFERED_READER_SIZE);
+         stdoutThread = new OutputThread(this, ibr, stdout);
+         stdoutThread.start();
+
+         // get command's stderr stream and collect it in stderr
+         InputStream estr = process.getErrorStream();
+         BufferedReader ebr = new BufferedReader(new InputStreamReader(estr), BUFFERED_READER_SIZE);
+         stderrThread = new OutputThread(this, ebr, stderr);
+         stderrThread.start();
+
+         // Wait for the threads to be up and running (HACK, NOT THREAD SAFE!, needs to be resolved)
+         while (true) {
+            if (this.stdoutThread.isReady() && this.stderrThread.isReady())
+               break;
+         }
+
+         // wait for command to terminate
+         try {
+            process.waitFor();
+         }
+         catch (InterruptedException e) {
+            log.warn(ME, "Process [" + commandArr[0] + "] was interrupted");
+         }
+
+         if (log.TRACE) log.trace(ME, "Process [" + commandArr[0] + "] finished its work, good bye");
+
+         this.exitValue = this.process.exitValue();
+
+         this.stdoutThread.stopIt();
+         this.stderrThread.stopIt();
+         this.process.destroy();
+
+         stdoutThread.join();
+         stderrThread.join();
+
+         this.process = null;
+
+         return;
+      }
+      catch (Exception e) {
+         String text = "Process [" + commandArr[0] + "] could not be started: " + e.toString();
+         log.error(ME, text);
+         e.printStackTrace();
+      }
+      finally {
+         if (stdoutThread != null) { stdoutThread.stopIt(); stdoutThread=null; } // necessary?
+         if (stderrThread != null) { stderrThread.stopIt(); stderrThread=null; }
+      }
+   }
+
+   public void stop() {
+      try {
+         if (this.process != null)
+            this.process.destroy();
+      }
+      catch (Exception e) {
+         log.error(ME, "Process kill failed: " + e.toString());
+      }
+
+      if (stdoutThread != null) { stdoutThread.stopIt(); stdoutThread=null; } // necessary?
+      if (stderrThread != null) { stderrThread.stopIt(); stderrThread=null; }
+   }
+
+   public String getStdout() {
+      return this.stdout.toString();
+   }
+
+   public String getStderr() {
+      return this.stderr.toString();
+   }
+
+   public int getExitValue() {
+      return this.exitValue;
+   }
+
+   /**
+    * Inner class of Execute
+    */
+   private class OutputThread extends Thread
+   {
+      private final String ME = "OutputThread";
+      private Execute boss;
+      private BufferedReader br;
+      private StringBuffer result;
+      private boolean isReady = false;
+      private boolean stopIt = false;
+
+      /**
+       */
+      OutputThread(Execute boss, BufferedReader br, StringBuffer result) {
+         this.boss = boss;
+         this.br = br;
+         this.result = result;
+      }
+
+      public void run() {
+         if(log.TRACE) log.trace(ME, "Start reading lines from process ...");
+         String str;
+         try {
+            isReady = true;
+            while (true) {
+               if (br.ready()) { // some data here?
+                  while ((str = br.readLine()) != null) {
+                     result.append(str).append("\n");
+                     if (!br.ready())
+                        break;      // no more data, would possibly block
+                  }
+               }
+               if (stopIt)
+                  break;
+            }
+            br.close();
+            if(log.TRACE) log.trace(ME, "End reading lines from process.");
+         }
+         catch (IOException e) {
+            log.error(ME, "Could not read process output: " + e.toString());
+         }
+      }
+
+      void stopIt() {
+         this.stopIt = true;
+      }
+
+      boolean isReady() {
+         return this.isReady;
+      }
+   } // class OutputThread
+
+
+   /**
+    *  org.xmlBlaster.util.Execute ls
+    */
+   public static void main( String[] args )
+   {
+      String[] commandArr = { args[0] };
+      String[] envArr = new String[0];
+      String[] ar = { "-trace", "true" };
+      Global glob = new Global(ar);
+      Execute execute = new Execute(glob, commandArr, envArr);
+      execute.run();
+      System.out.println("Stdout of " + args[0] + " is:\n" + execute.getStdout());
+      System.out.println("Stderr of " + args[0] + " is:\n" + execute.getStderr());
+      System.out.println("Exit   of " + args[0] + " is: " + execute.getExitValue());
+   }
+}
