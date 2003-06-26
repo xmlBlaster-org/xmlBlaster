1f7d9ec9eb14c8a3c42b521a8f6a01e367b30eb4
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-06-26 20:51:01 +0000

    Added test function mu_fail()
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@10098 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/src/c/test.h
+++ xmlBlaster/testsuite/src/c/test.h
@@ -4,6 +4,7 @@
  * See: http://www.eskimo.com/~scs/C-faq/q10.4.html
  */
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
+#define mu_fail(message) do { return message; } while (0)
 #define mu_run_test(test) do { const char *message = test(); tests_run++; \
                               if (message) return message; } while (0)
 extern int tests_run;
