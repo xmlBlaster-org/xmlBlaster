0533fff13b2d366d0e84e89db9a440c3b16dc038
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-09-24 21:28:58 +0000

    Added j2ee
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@10389 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/src/java/org/xmlBlaster/test/AllTests.java
+++ xmlBlaster/testsuite/src/java/org/xmlBlaster/test/AllTests.java
@@ -31,8 +31,9 @@ public class AllTests extends junit.framework.TestCase
     suite.addTest(org.xmlBlaster.test.snmp.AllTests.suite());
     suite.addTest(org.xmlBlaster.test.persistence.AllTests.suite());
     suite.addTest(org.xmlBlaster.test.cluster.AllTests.suite());
-    suite.addTest(org.xmlBlaster.test.stress.AllTests.suite());
     suite.addTest(org.xmlBlaster.test.jmx.AllTests.suite());
+    suite.addTest(org.xmlBlaster.test.j2ee.AllTests.suite());
+    suite.addTest(org.xmlBlaster.test.stress.AllTests.suite());
     suite.addTest(new TestSuite(org.xmlBlaster.test.StopXmlBlaster.class));
     return suite;
   }
