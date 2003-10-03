400f38110895c3fa5091ac6f4891805822031b6f
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-10-03 19:21:39 +0000

    The Protector encapsulates the same named class and allows access to SubjectInfo only by delegate calls, we need it to prevent core access by protocol plugins or LOCAL clients
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@10452 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/authentication/SubjectInfoProtector.java
@@ -0,0 +1,63 @@
+/*------------------------------------------------------------------------------
+Name:      SubjectInfoProtector.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Author:    xmlBlaster@marcelruff.info
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.authentication;
+
+import org.xmlBlaster.engine.admin.I_AdminSubject;
+import org.xmlBlaster.engine.admin.I_AdminSession;
+import org.xmlBlaster.util.XmlBlasterException;
+
+/**
+ * SubjectInfoProtector protects SubjectInfo.java from direct access by administrative tasks. 
+ * <p>
+ * See javadoc of SubjectInfo.java for a description
+ * </p>
+ * @author <a href="mailto:xmlBlaster@marcelruff.info">Marcel Ruff</a>
+ */
+public final class SubjectInfoProtector implements I_AdminSubject
+{
+   private final SubjectInfo subjectInfo;
+
+   public SubjectInfoProtector(SubjectInfo subjectInfo) {
+      this.subjectInfo = subjectInfo;
+   }
+
+   public long getUptime() {
+      return this.subjectInfo.getUptime();
+   }
+
+   public long getNumUpdates() {
+      return this.subjectInfo.getNumUpdates();
+   }
+
+   public long getCbQueueNumMsgs() {
+      return this.subjectInfo.getCbQueueNumMsgs();
+   }
+
+   public long getCbQueueMaxMsgs() {
+      return this.subjectInfo.getCbQueueMaxMsgs();
+   }
+
+   public int getNumSessions() {
+      return this.subjectInfo.getNumSessions();
+   }
+
+   public int getMaxSessions() {
+      return this.subjectInfo.getMaxSessions();
+   }
+
+   public String getSessionList() {
+      return this.subjectInfo.getSessionList();
+   }
+
+   public I_AdminSession getSessionByPubSessionId(long pubSessionId) {
+      return this.subjectInfo.getSessionByPubSessionId(pubSessionId);
+   }
+
+   public String getKillClient() throws XmlBlasterException {
+      return this.subjectInfo.getKillClient();
+   }
+}
