5fd5038a3d7fb94dd217a37da46b369c64ae0095
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-10-05 18:33:42 +0000

    Formatting
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@10514 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/demo/javaclients/HelloWorldNative.java
+++ xmlBlaster/demo/javaclients/HelloWorldNative.java
@@ -17,7 +17,8 @@ import org.xmlBlaster.util.plugin.PluginInfo;
  * You need to add this plugin to xmlBlasterPlugins.xml, for example:
  * <pre>
  *  &lt;plugin id='HelloWorldNative' className='javaclients.HelloWorldNative'>
- *     &lt;action do='LOAD' onStartupRunlevel='9' sequence='5' onFail='resource.configuration.pluginFailed'/>
+ *     &lt;action do='LOAD' onStartupRunlevel='9' sequence='5'
+ *                          onFail='resource.configuration.pluginFailed'/>
  *     &lt;action do='STOP' onShutdownRunlevel='6' sequence='4'/>
  *  &lt;/plugin>
  * </pre>
@@ -25,7 +26,8 @@ import org.xmlBlaster.util.plugin.PluginInfo;
  * plugin works only if client and server is in the same virtual machine (JVM).
  * Other protocols like CORBA or SOCKET would work as well but carry the overhead
  * of sending the message over TCP/IP.
- * @see <a href="http://www.xmlBlaster.org/xmlBlaster/doc/requirements/engine.runlevel.html" target="others">run level requirement</a>
+ * @see <a href="http://www.xmlBlaster.org/xmlBlaster/doc/requirements/engine.runlevel.html"
+ *       target="others">run level requirement</a>
  * @see javaclients.HelloWorldNative2
  */
 public class HelloWorldNative implements I_Plugin
@@ -40,13 +42,13 @@ public class HelloWorldNative implements I_Plugin
 
    private final void queryServerMemory() {
       try {
-         System.err.println("HelloWorldNative: Connecting with protocol 'LOCAL' to xmlBlaster\n");
+         System.err.println("HelloWorldNative: Connecting with protocol 'LOCAL'\n");
          I_XmlBlasterAccess con = new XmlBlasterAccess(glob);
 
          ConnectQos qos = new ConnectQos(this.glob); /* Client side object */
          qos.setUserId("A-NATIVE-CLIENT-PLUGIN");
          qos.getSessionQos().setSessionTimeout(0L);
-         con.connect(qos, null);    // Login to xmlBlaster as "A-NATIVE-CLIENT-PLUGIN"
+         con.connect(qos, null);  // Login as "A-NATIVE-CLIENT-PLUGIN"
 
          MsgUnit[] msgs = con.get("<key oid='__cmd:?freeMem'/>", null);
 
@@ -56,13 +58,15 @@ public class HelloWorldNative implements I_Plugin
          con.disconnect(null);
       }
       catch (Exception e) {
-         System.err.println("HelloWorldNative: We have a problem: " + e.toString());
+         System.err.println("HelloWorldNative: Exception: "+e.toString());
       }
    }
 
-   public void init(org.xmlBlaster.util.Global glob, PluginInfo pluginInfo) throws XmlBlasterException {
+   public void init(org.xmlBlaster.util.Global glob, PluginInfo pluginInfo)
+                                                    throws XmlBlasterException {
       this.glob = glob.getClone(nativeConnectArgs);
-      this.glob.addObjectEntry("ServerNodeScope", glob.getObjectEntry("ServerNodeScope"));
+      this.glob.addObjectEntry("ServerNodeScope", 
+                               glob.getObjectEntry("ServerNodeScope"));
       System.out.println("\nHelloWorldNative: init(): The plugin is loaded");
       queryServerMemory();
    }
