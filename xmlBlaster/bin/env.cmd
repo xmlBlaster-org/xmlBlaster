206c9f20db82835e3ca7b2a259fcff255515e40b
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2003-10-31 18:58:40 +0000

    Env settings for MS DOS
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@10648 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/bin/env.cmd
@@ -0,0 +1,13 @@
+echo "Setting Marcels aliases"
+@echo off
+doskey l=dir
+doskey cdx=cd %XMLBLASTER_HOME%
+doskey cdxs=cd %XMLBLASTER_HOME%\src
+doskey cdxj=cd %XMLBLASTER_HOME%\src\java\org\xmlBlaster
+doskey cdxu=cd %XMLBLASTER_HOME%\src\java\org\jutils
+doskey cdxd=cd %XMLBLASTER_HOME%\demo
+doskey cdxdj=cd %XMLBLASTER_HOME%\demo\javaclients
+doskey cdxt=cd %XMLBLASTER_HOME%\testsuite\src
+doskey cdxr=cd %XMLBLASTER_HOME%\doc\requirements
+doskey cdxtj=cd %XMLBLASTER_HOME%\testsuite\src\java\org\xmlBlaster\test
+
