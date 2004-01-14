27c0df33c55d0cadf6280c2693db891fa139fe2b
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-01-14 15:05:43 +0000

    Fixed destuction sequence to be reversed to creation, added map to support lookup of registered objects by keys
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@10983 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/c++/util/objman.cpp
+++ xmlBlaster/src/c++/util/objman.cpp
@@ -30,28 +30,31 @@ namespace org { namespace xmlBlaster { namespace util {
 
 //namespace org { namespace xmlBlaster { namespace util {
 
-// Static declaratons for Object_Lifetime_Manager.
+// Static declarations for Object_Lifetime_Manager.
 Object_Lifetime_Manager * Object_Lifetime_Manager::instance_ = 0;
-managed_list              Object_Lifetime_Manager::managed_objects_list_;
-void *                    Object_Lifetime_Manager::preallocated_object[PREALLOCATED_OBJECTS];
+ManagedMap              Object_Lifetime_Manager::managedObjectMap_;
+ManagedList             Object_Lifetime_Manager::managedObjectList_;
+void *                  Object_Lifetime_Manager::preallocated_object[PREALLOCATED_OBJECTS];
 
 
 void Object_Lifetime_Manager::init (void)
 {
-  Object_Lifetime_Manager::instance()->startup();
+   Object_Lifetime_Manager::instance()->startup();
 }
 
 void Object_Lifetime_Manager::fini (void)
 {
-  Object_Lifetime_Manager::instance()->shutdown();
+   Object_Lifetime_Manager::instance()->shutdown();
 }
 
+/**
+ * This function should be called during
+ * construction of static instances, or
+ * before any other threads have been created
+ * in the process. So, it's not thread safe.
+ */
 Object_Lifetime_Manager * Object_Lifetime_Manager::instance (void)
 {
-// This function should be called during
-// construction of static instances, or
-// before any other threads have been created
-// in the process. So, it’s not thread safe.
   if (instance_ == 0) 
   {
     Object_Lifetime_Manager *instance_pointer =
@@ -87,48 +90,47 @@ int Object_Lifetime_Manager::startup ()
 
 int Object_Lifetime_Manager::shutdown ()
 {
-  if (shutting_down_i ())
-    // Too late. Or, maybe too early. Either
-    // <fini> has already been called, or
-    // <init> was never called.
-    return object_manager_state_ == OBJ_MAN_SHUT_DOWN ? 1 : -1;
+   if (shutting_down_i())
+      // Too late. Or, maybe too early. Either
+      // <fini> has already been called, or
+      // <init> was never called.
+      return object_manager_state_ == OBJ_MAN_SHUT_DOWN ? 1 : -1;
 
-  // Indicate that the Object_Lifetime_Manager
-  // instance is being shut down.
-  // This object manager should be the last one
-  // to be shut down.
-  gObjectManagerState = object_manager_state_ = OBJ_MAN_SHUTTING_DOWN;
-  
-  // Only clean up Preallocated Objects when
-  // the singleton Instance is being destroyed.
-  if (this == instance_) 
-  {
-   
-    managed_list::iterator i;
+   // Indicate that the Object_Lifetime_Manager
+   // instance is being shut down.
+   // This object manager should be the last one
+   // to be shut down.
+   gObjectManagerState = object_manager_state_ = OBJ_MAN_SHUTTING_DOWN;
 
-    for(i = managed_objects_list_.begin(); i != managed_objects_list_.end(); i++)
-    {
-      (dynamic_cast<Cleanup_Adaptor*>(*i))->cleanup();
-      delete *i;
-    }
+   // Only clean up Pre-allocated Objects when
+   // the singleton Instance is being destroyed.
+   // The sequence is reversed, the last created object is destroyed first.
+   if (this == instance_) {
+      ManagedList::reverse_iterator i;
+      for(i = managedObjectList_.rbegin(); i != managedObjectList_.rend(); ++i) {
+         Cleanup_Adaptor* adap = *i;
+         adap->cleanup();
+         delete adap;
+      }
+      managedObjectList_.clear();
 
-    // Delete the Global post of list method as a dependancy may exist
-    // between objects existing in both constructs.
-    // Array is last as all objects placed here are intentional and
-    // it is assumed user is aware of such semantics.
-    delete (Cleanup_Adaptor*)preallocated_object[XMLBLASTER_GLOBAL];
-    // continue to remove any other array based objects
+      managedObjectMap_.clear();
 
-  }
+      // Delete the Global post of list method as a dependency may exist
+      // between objects existing in both constructs.
+      // Array is last as all objects placed here are intentional and
+      // it is assumed user is aware of such semantics.
+      delete (Cleanup_Adaptor*)preallocated_object[XMLBLASTER_GLOBAL];
+   }
 
-  // Indicate that this Object_Lifetime_Manager
-  // instance has been shut down.
-  gObjectManagerState = object_manager_state_ = OBJ_MAN_SHUT_DOWN;
-  if (dynamically_allocated_)
-    delete this;
-  if (this == instance_)
-    instance_ = 0;
-  return 0;
+   // Indicate that this Object_Lifetime_Manager
+   // instance has been shut down.
+   gObjectManagerState = object_manager_state_ = OBJ_MAN_SHUT_DOWN;
+   if (dynamically_allocated_)
+      delete this;
+   if (this == instance_)
+      instance_ = 0;
+   return 0;
 }
 
 }}} // namespace
