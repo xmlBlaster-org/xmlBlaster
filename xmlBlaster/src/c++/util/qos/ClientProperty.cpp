684ad260d877c4541a57c1984860d36c52947511
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-01-19 14:56:15 +0000

    Handles client properties and base64 encoding/decoding
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@11044 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/c++/util/qos/ClientProperty.cpp
@@ -0,0 +1,280 @@
+/*------------------------------------------------------------------------------
+Name:      ClientProperty.cpp
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Handling one client property of QosData
+------------------------------------------------------------------------------*/
+#include <typeinfo>
+#include <vector>
+//#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <util/qos/ClientProperty.h>
+#include <util/Base64.h>
+#include <util/Constants.h>
+#include <util/lexical_cast.h>
+
+using namespace org::xmlBlaster::util::qos;
+using namespace org::xmlBlaster::util;
+using namespace std;
+
+namespace org { namespace xmlBlaster { namespace util { namespace qos {
+
+ClientProperty::ClientProperty(const std::string& name,
+                     const char *value,
+                     const std::string& type)
+   : name_(name),
+     value_(value),
+     encoding_(""),
+     type_(type)
+{
+   if (needsEncoding()) {
+      std::vector<unsigned char> vec;
+      vec.reserve(value_.size());
+      encoding_ = Constants::ENCODING_BASE64;
+      copy(value_.begin(), value_.end(), back_inserter(vec));
+      value_ = Base64::Encode(vec);
+   }
+}
+
+ClientProperty::ClientProperty(const std::string& name,
+                     const std::vector<unsigned char>& value,
+                     const std::string& type)
+   : name_(name),
+     value_(""),
+     encoding_(""),
+     type_(type)
+{
+   if (type_ == "") {
+      type_ = Constants::TYPE_BLOB;
+   }
+   encoding_ = Constants::ENCODING_BASE64;
+   value_ = Base64::Encode(value);
+}
+
+ClientProperty::ClientProperty(const std::string& name,
+                     const std::string& encoding,
+                     const std::string& type)
+   : name_(name),
+     value_(""),
+     encoding_(encoding),
+     type_(type)
+{
+}
+
+bool ClientProperty::needsEncoding() const {
+   if (type_ == Constants::TYPE_BLOB)
+      return true;
+   else if (
+        value_.find("<") != std::string::npos ||
+        value_.find("]]>") != std::string::npos
+      ) {
+      return true;
+   }
+   return false;
+}
+
+const std::string& ClientProperty::getName() const {
+   return name_;
+}
+
+std::string ClientProperty::getType() const {
+   return type_;
+}
+
+std::string ClientProperty::getEncoding() const {
+   return encoding_;
+}
+
+bool ClientProperty::isBase64() {
+   return encoding_ == Constants::ENCODING_BASE64;
+}
+
+std::string ClientProperty::getValueRaw() const {
+   return value_;
+}
+
+std::string ClientProperty::getStringValue() const {
+   if (value_ == "") return "";
+   if (Constants::ENCODING_BASE64 == encoding_) {
+      std::vector<unsigned char> vec = Base64::Decode(value_);
+      std::string str;
+      str.reserve(vec.size());
+      str.assign(vec.begin(),vec.end());
+      return str;
+   }
+   return value_;
+}
+
+std::vector<unsigned char> ClientProperty::getValue() const {
+   if (value_ == "") std::vector<unsigned char>();
+   if (Constants::ENCODING_BASE64 == encoding_) {
+      return Base64::Decode(value_);
+   }
+   std::vector<unsigned char>vec;
+   vec.reserve(value_.size());
+   copy(value_.begin(), value_.end(), vec.begin());
+   return vec;
+}
+
+void ClientProperty::setValue(const string& value) {
+   value_ = value;
+   if (needsEncoding()) {
+      std::vector<unsigned char>vec;
+      vec.reserve(value.size());
+      copy(value.begin(), value.end(), vec.begin());
+      value_ = Base64::Encode(vec);
+   }
+}
+
+void ClientProperty::setValueRaw(const string& value) {
+   value_ = value;
+}
+
+/**
+ * Dump state of this object into a XML ASCII string.
+ * <br>
+ * @param extraOffset indenting of tags for nice output
+ * @return internal state of the ClientProperty as a XML ASCII string
+ */
+std::string ClientProperty::toXml(std::string extraOffset) const {
+   std::string sb = std::string();
+   sb.reserve(256);
+   std::string offset = Constants::OFFSET + extraOffset;
+
+   sb += offset + "<clientProperty";
+   if (getName() != "") {
+      sb += " name='" + getName() + "'";
+   }
+   if (getType() != "") {
+      sb += " type='" + getType() + "'";
+   }
+   if (getEncoding() != "") {
+      sb += " encoding='" + getEncoding() + "'";
+   }
+
+   std::string val = getValueRaw();
+   if (val == "")
+      sb += "/>";
+   else {
+      sb += ">" + val + "</clientProperty>";
+   }
+
+   return sb;
+}
+
+}}}}
+
+//g++ -o ClientProperty -Wall -g -DCLIENTPROPERTY_MAIN ClientProperty.cpp ../Base64.cpp ../Constants.cpp -I ~/xmlBlaster/src/c++
+#ifdef CLIENTPROPERTY_MAIN
+# include <iostream>
+int main(int argc, char **argv) {
+   try {
+      {
+         ClientProperty cp("key", string("string"));
+         cout << "name=" << cp.getName() 
+              << ", valueB64=" << cp.getValueRaw()
+              << ", value=" << cp.getStringValue()
+              << ", type=" << cp.getType()
+              << ", isBase64=" << cp.isBase64()
+              << cp.toXml("")
+              << endl << endl;
+      }
+      {
+         ClientProperty cp("key", string("str]]>ing"));
+         cout << "name=" << cp.getName() 
+              << ", valueB64=" << cp.getValueRaw()
+              << ", value=" << cp.getStringValue()
+              << ", type=" << cp.getType()
+              << ", isBase64=" << cp.isBase64()
+              << cp.toXml("")
+              << endl << endl;
+      }
+      {
+         ClientProperty cp("key", string("str<<<ing"));
+         cout << "name=" << cp.getName() 
+              << ", valueB64=" << cp.getValueRaw()
+              << ", value=" << cp.getStringValue()
+              << ", type=" << cp.getType()
+              << ", isBase64=" << cp.isBase64()
+              << cp.toXml("")
+              << endl << endl;
+      }
+      {
+         ClientProperty cp("key", "const char *");
+         cout << "name=" << cp.getName() 
+              << ", valueB64=" << cp.getValueRaw()
+              << ", value=" << cp.getStringValue()
+              << ", type=" << cp.getType()
+              << ", isBase64=" << cp.isBase64()
+              << cp.toXml("")
+              << endl << endl;
+      }
+      {
+         long f=10L;
+         ClientProperty cp("key", f);
+         cout << "name=" << cp.getName() 
+              << ", valueB64=" << cp.getValueRaw()
+              << ", value=" << cp.getStringValue()
+              << ", type=" << cp.getType()
+              << ", isBase64=" << cp.isBase64()
+              << cp.toXml("")
+              << endl << endl;
+         float ff = lexical_cast<float>(cp.getStringValue());
+      }
+      {
+         float f=10.5;
+         ClientProperty cp("key", f);
+         cout << "name=" << cp.getName() 
+              << ", valueB64=" << cp.getValueRaw()
+              << ", value=" << cp.getStringValue()
+              << ", type=" << cp.getType()
+              << ", isBase64=" << cp.isBase64()
+              << cp.toXml("")
+              << endl << endl;
+      }
+      {
+         double f=20.6;
+         ClientProperty cp("key", f);
+         cout << "name=" << cp.getName() 
+              << ", valueB64=" << cp.getValueRaw()
+              << ", value=" << cp.getStringValue()
+              << ", type=" << cp.getType()
+              << ", encoding=" << cp.getEncoding()
+              << ", isBase64=" << cp.isBase64()
+              << cp.toXml("")
+              << endl;
+         cp.getValue(f);
+         cout << "getValue()=" << f
+              << endl << endl;
+      }
+      {
+         vector<unsigned char> v;
+         v.push_back('H');
+         v.push_back('a');
+         v.push_back('l');
+         v.push_back('l');
+         v.push_back('o');
+         ClientProperty cp("key", v);
+         cout << "name=" << cp.getName() 
+              << ", valueB64=" << cp.getValueRaw()
+              << ", value=" << cp.getStringValue()
+              << ", type=" << cp.getType()
+              << ", encoding=" << cp.getEncoding()
+              << ", isBase64=" << cp.isBase64()
+              << cp.toXml("")
+              << endl;
+         {
+            std::vector<unsigned char> ret = cp.getValue();
+            std::string str;
+            str.assign(v.begin(),v.end());
+            cout << "NEW=" << str << endl;
+         }
+      }
+   }
+   catch(bad_cast b) {
+      cout << "EXCEPTION: " << b.what() << endl;
+   }
+   return 0;
+}
+#endif
---
+++ xmlBlaster/src/c++/util/qos/ClientProperty.h
@@ -0,0 +1,247 @@
+/*------------------------------------------------------------------------------
+Name:      ClientProperty.h
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Handling one client property of QosData
+------------------------------------------------------------------------------*/
+#ifndef _UTIL_QOS_CLIENTPROPERTY_H
+#define _UTIL_QOS_CLIENTPROPERTY_H
+
+#include <util/xmlBlasterDef.h>
+#include <util/Constants.h>
+#include <util/Base64.h>
+#include <util/lexical_cast.h>
+#include <typeinfo>
+#include <vector>
+#include <string>
+
+namespace org { namespace xmlBlaster { namespace util { namespace qos {
+
+class Dll_Export bad_clientProperty_cast : public std::bad_cast
+{
+ public:
+   virtual const char * what() const throw() {
+      return "bad lexical cast for ClientProperty: source type value could not be interpreted as string";
+   }
+};
+
+/**
+ * This class encapsulates one client property in a QoS. 
+ * <p/>
+ * Examples:
+ * <pre>
+ *&lt;clientProperty name='transactionId' type='int'>120001&lt;/clientProperty>
+ *&lt;clientProperty name='myKey'>Hello World&lt;/clientProperty>
+ *&lt;clientProperty name='myBlob' type='byte[]' encoding='base64'>OKFKAL==&lt;/clientProperty>
+ * </pre>
+ * If the attribute <code>type</code> is missing we assume a 'String' property
+ *
+ * @author <a href="mailto:xmlBlaster@marcelruff.info">Marcel Ruff</a>
+ * @see <a href="http://www.xmlblaster.org/xmlBlaster/doc/requirements/engine.qos.clientProperty.html">The client.qos.clientProperty requirement</a>
+ * @see TestClientProperty
+ */
+class Dll_Export ClientProperty
+{
+private:
+   const std::string name_;
+   /** The value encoded as specified with encoding_ */
+   std::string value_;
+   std::string encoding_;
+   mutable std::string type_;
+
+   template <typename T_VALUE> void guessType(const T_VALUE& value) const;
+   bool ClientProperty::needsEncoding() const;
+
+public:
+
+   /**
+    * Standard constructor. 
+    * @param name  The unique property key
+    * @param value Your data . The type (like "float") is guessed from T_VALUE
+    *              NOTE: "vector<unsigned char>" "unsigned char*" are
+    *                    treated as BLOBs and will be transferred Base64 encoded.
+    * @param type The data type of the value, optional, e.g. "float"
+    */
+   template <typename T_VALUE> ClientProperty(
+                  const std::string& name,
+                  const T_VALUE& value
+                  );
+
+   /**
+    * Specialized ctor for literal data. 
+    * @param name  The unique property key
+    * @param value Your pointer
+    * @param type  Optionally you can force another type than "String",
+    *              for example Constant::TYPE_DOUBLE if the pointer contains
+    *              such a number as a string representation. 
+    */
+   ClientProperty(const std::string& name,
+                  const char *value,
+                  const std::string& type="");
+
+   /**
+    * Specialized ctor for blob data. 
+    * @param name  The unique property key
+    * @param value Your BLOB data.
+    * @param type  Optionally you can force another type than "byte[]",
+    *              for example Constant::TYPE_DOUBLE if the vector contains
+    *              such a number as a string representation. 
+    */
+   ClientProperty(const std::string& name,
+                  const std::vector<unsigned char>& value,
+                  const std::string& type="");
+
+   /**
+    * Internal constructor only, used by SAX parser in conjunction with setValueRaw()
+    */
+   ClientProperty(const std::string& name,
+                  const std::string& encoding,
+                  const std::string& type);
+
+   //virtual ~ClientProperty();
+
+   const std::string& getName() const;
+
+   /**
+    * @return The data type, for example "short" or "byte[]" for "vector<unsigned char>"
+    * @see Constants::TYPE_SHORT
+    * @see Constants::TYPE_BLOB
+    */
+   std::string getType() const;
+
+   /**
+    * @return The used encoding, for example "base64" or "" for none
+    * @see Constants::ENCODING_BASE64
+    */
+   std::string getEncoding() const;
+
+   /**
+    * Check if getValueRaw() is Base64 encoded
+    */
+   bool isBase64();
+
+   /**
+    * The raw, possibly still Base64 encoded value
+    */
+   std::string getValueRaw() const;
+   
+   /**
+    * Convenience method for getValue(T_VALUE&). 
+    * @return The value. It is decoded (readable) in case it was base64 encoded,
+    */
+   std::string getStringValue() const;
+   
+   /**
+    * @return The value. It is decoded (readable) in case it was base64 encoded,
+    */
+   std::vector<unsigned char> getValue() const;
+   
+   /**
+    * @param value OUT parameter: The value in the desired data type. 
+    */
+   template <typename T_VALUE> void getValue(T_VALUE& value) const;
+   
+   /**
+    * Set the value, it will be encoded with the encoding specified in the constructor. 
+    */
+   void setValue(const std::string& value);
+
+   /**
+    * Set the already correctly encoded value, used internally by SAX parser. 
+    */
+   void setValueRaw(const std::string& value);
+
+   /**
+    * Dump state of this object into a XML ASCII string.
+    * <br>
+    * @param extraOffset indenting of tags for nice output
+    * @return internal state of the ClientProperty as a XML ASCII string
+    */
+   std::string toXml(std::string extraOffset) const;
+};
+
+
+// All template based function definitions follow here
+// to be available outside the shared library
+template <typename T_VALUE> ClientProperty::ClientProperty(
+                     const std::string& name,
+                     const T_VALUE& value)
+   : name_(name),
+     value_(""),
+     encoding_(org::xmlBlaster::util::Constants::ENCODING_NONE),
+     type_("")
+{
+   if (type_ == "") {
+      guessType(value);  // guess type from T_VALUE
+   }
+
+   // Convert the given value type to a std::string value_
+   std::stringstream interpreter;
+   
+   int origPrecision = interpreter.precision(); // 6 digits
+   if (type_ == org::xmlBlaster::util::Constants::TYPE_DOUBLE)
+      interpreter.precision(15); // Otherwise it will be rounded to 6 digits
+                                 // Note that 'long double' has a typical size of 18
+
+   if(!(interpreter << value) || !(interpreter >> value_) || !(interpreter >> std::ws).eof())
+      throw bad_clientProperty_cast();
+
+   interpreter.precision(origPrecision);
+
+   if (needsEncoding()) {
+      std::vector<unsigned char> vec;
+      vec.reserve(value_.size());
+      encoding_ = org::xmlBlaster::util::Constants::ENCODING_BASE64;
+      copy(value_.begin(), value_.end(), back_inserter(vec));
+      value_ = org::xmlBlaster::util::Base64::Encode(vec);
+   }
+}
+
+template <typename T_VALUE> void ClientProperty::guessType(const T_VALUE& value) const {
+   const char *cPC=0;
+   char *cP=0;
+   unsigned char *cUP=0;
+   const unsigned char *cUPC=0;
+   long long ll=0LL;
+   std::vector<char> vc;
+   std::vector<unsigned char> vuc;
+
+   if (typeid(value) == typeid(std::string("")))
+      type_ = std::string(""); // "String"
+   else if (typeid(value) == typeid(true))
+      type_ = org::xmlBlaster::util::Constants::TYPE_BOOLEAN;
+   else if (typeid(value) == typeid(char(0)))
+      type_ = org::xmlBlaster::util::Constants::TYPE_BYTE;
+   else if (typeid(value) == typeid(cP) || typeid(value) == typeid(cPC) ||
+            typeid(value) == typeid("A") || typeid(value).name() == std::string("A13_c"))
+      type_ = std::string(""); // "String"
+   else if (typeid(value) == typeid(cUP) || typeid(value) == typeid(cUPC))
+      type_ = org::xmlBlaster::util::Constants::TYPE_BLOB;
+   else if (typeid(value) == typeid(vc) || typeid(value) == typeid(vuc))
+      type_ = org::xmlBlaster::util::Constants::TYPE_BLOB;
+   else if (typeid(value) == typeid(short(0)))
+      type_ = org::xmlBlaster::util::Constants::TYPE_SHORT;
+   else if (typeid(value) == typeid(int(0)))
+      type_ = org::xmlBlaster::util::Constants::TYPE_INT;
+   else if (typeid(value) == typeid(long(0L)))
+      type_ = org::xmlBlaster::util::Constants::TYPE_LONG;
+   else if (typeid(value) == typeid(ll))
+      type_ = org::xmlBlaster::util::Constants::TYPE_LONG;
+   else if (typeid(value) == typeid(float(1.10)))
+      type_ = org::xmlBlaster::util::Constants::TYPE_FLOAT;
+   else if (typeid(value) == typeid(double(1.7L)))
+      type_ = org::xmlBlaster::util::Constants::TYPE_DOUBLE;
+   else {
+      type_ = org::xmlBlaster::util::Constants::TYPE_BLOB;
+      std::cerr << "Warning: ClientProperty typeid=" << typeid(value).name() << " is unknown, we handle it as a blob" << std::endl;
+   }
+}
+
+template <typename T_VALUE> void ClientProperty::getValue(T_VALUE& value) const {
+   value = lexical_cast<T_VALUE>(value_);
+}
+
+}}}}
+
+#endif
+
