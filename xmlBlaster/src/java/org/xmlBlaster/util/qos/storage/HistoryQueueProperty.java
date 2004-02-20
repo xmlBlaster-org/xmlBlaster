d0cdfc783246ab5840978afec488e91d34b468c3
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-02-20 16:57:46 +0000

    Limited to 10 entries as a default
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@11352 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/util/qos/storage/HistoryQueueProperty.java
+++ xmlBlaster/src/java/org/xmlBlaster/util/qos/storage/HistoryQueueProperty.java
@@ -27,6 +27,8 @@ public class HistoryQueueProperty extends QueuePropertyBase
    public HistoryQueueProperty(Global glob, String nodeId) {
       super(glob, nodeId);
       this.log = glob.getLog("core");
+      this.maxEntries.setDefaultValue(10);
+      this.maxEntriesCache.setDefaultValue(10);
       setRelating(Constants.RELATING_HISTORY);
       super.initialize(Constants.RELATING_HISTORY); //related='history'--> -queue/history/maxEntries
    }
@@ -44,6 +46,36 @@ public class HistoryQueueProperty extends QueuePropertyBase
       if (Constants.ONOVERFLOW_DEADMESSAGE.equalsIgnoreCase(getOnOverflow()))
          return true;
       return false;
+   }
+
+   /**
+    * Get a usage string for queue configuration (in xmlBlaster.properties or on command line)
+    */
+   public String usage(String headerline) {
+      String prefix = getPrefix();
+      String text = "";
+      text += "\n" + headerline + "\n";
+      text += "   -"+prefix+"debug    Sets a debug flag on the queue [false].\n";
+      text += "                       Currently used for consistency asserts on jdbc queue.\n";
+      text += "   -"+prefix+"maxEntries\n";
+      text += "                       The maximum allowed number of messages [" + this.maxEntries.getDefaultValue() + "].\n";
+      text += "   -"+prefix+"maxEntriesCache\n";
+      text += "                       The maximum allowed number of messages in the cache [" + this.maxEntriesCache.getDefaultValue() + "].\n";
+      text += "   -"+prefix+"maxBytes\n";
+      text += "                       The maximum size in bytes of the storage [" + this.maxBytes.getDefaultValue() + "].\n";
+      text += "   -"+prefix+"maxBytesCache.\n";
+      text += "                       The maximum size in bytes in the cache [" + this.maxBytesCache.getDefaultValue() + "].\n";
+      //text += "   -"+prefix+"onOverflow\n";
+      //text += "                       What happens if storage is full [" + this.onOverflow.getDefaultValue() + "]\n";
+      //text += "   -"+prefix+"onFailure\n";
+      //text += "                       Error handling when storage failed [" + this.onFailure.getDefaultValue() + "]\n";
+      text += "   -"+prefix+"type\n";
+      text += "                       The plugin type [" + this.type.getDefaultValue() + "]\n";
+      text += "   -"+prefix+"version\n";
+      text += "                       The plugin version [" + this.version.getDefaultValue() + "]\n";
+      text += "   -"+prefix+"defaultPlugin\n";
+      text += "                       The plugin type,version (short form) [" + this.type.getDefaultValue()+","+this.version.getDefaultValue() + "]\n";
+      return text;
    }
 
    /** For testing: java org.xmlBlaster.engine.helper.HistoryQueueProperty */
