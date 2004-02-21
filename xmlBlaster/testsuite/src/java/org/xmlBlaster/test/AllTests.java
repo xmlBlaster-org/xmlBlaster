75a25caef52f136ef084ca14364c09ab2b1c6e4d
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-02-21 09:58:33 +0000

    Added missings
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@11369 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/src/java/org/xmlBlaster/test/AllTests.java
+++ xmlBlaster/testsuite/src/java/org/xmlBlaster/test/AllTests.java
@@ -27,13 +27,18 @@ public class AllTests extends junit.framework.TestCase
     suite.addTest(org.xmlBlaster.test.authentication.AllTests.suite());
     suite.addTest(org.xmlBlaster.test.mime.AllTests.suite());
     suite.addTest(org.xmlBlaster.test.dispatch.AllTests.suite());
+    suite.addTest(new TestSuite(org.xmlBlaster.test.distributor.TestConsumableQueue.class));
+    //suite.addTest(org.xmlBlaster.test.distributor.AllTests.suite());
+    suite.addTest(org.xmlBlaster.test.jms.AllTests.suite());
     suite.addTest(org.xmlBlaster.test.jdbc.AllTests.suite());
+    //suite.addTest(org.xmlBlaster.test.memoryleak.AllTests.suite());
     suite.addTest(org.xmlBlaster.test.snmp.AllTests.suite());
     suite.addTest(org.xmlBlaster.test.persistence.AllTests.suite());
     suite.addTest(org.xmlBlaster.test.cluster.AllTests.suite());
     suite.addTest(org.xmlBlaster.test.jmx.AllTests.suite());
     suite.addTest(org.xmlBlaster.test.j2ee.AllTests.suite());
     suite.addTest(org.xmlBlaster.test.stress.AllTests.suite());
+    suite.addTest(org.xmlBlaster.test.classloader.AllTests.suite());
     suite.addTest(new TestSuite(org.xmlBlaster.test.StopXmlBlaster.class));
     return suite;
   }
--- xmlBlaster/testsuite/src/java/org/xmlBlaster/test/test.sh
+++ xmlBlaster/testsuite/src/java/org/xmlBlaster/test/test.sh
@@ -11,6 +11,8 @@ xterm -geom 180x26 -e java org.xmlBlaster.Main&
 sleep 10
 echo "STARTING qos TESTS ..."
 java junit.swingui.TestRunner -noloading org.xmlBlaster.test.qos.AllTests
+echo "STARTING distributor TESTS ..."
+java junit.swingui.TestRunner -noloading org.xmlBlaster.test.distributor.AllTests
 echo "STARTING client TESTS ..."
 java junit.swingui.TestRunner -noloading org.xmlBlaster.test.client.AllTests
 echo "STARTING authentication TESTS ..."
@@ -19,6 +21,8 @@ echo "STARTING dispatch TESTS ..."
 java junit.swingui.TestRunner -noloading org.xmlBlaster.test.dispatch.AllTests
 echo "STARTING jdbc TESTS ..."
 java junit.swingui.TestRunner -noloading org.xmlBlaster.test.jdbc.AllTests
+echo "STARTING jms TESTS ..."
+java junit.swingui.TestRunner -noloading org.xmlBlaster.test.jms.AllTests
 echo "STARTING mime TESTS ..."
 java junit.swingui.TestRunner -noloading org.xmlBlaster.test.mime.AllTests
 echo "STARTING topic TESTS ..."
@@ -27,8 +31,16 @@ echo "STARTING persistence TESTS ..."
 java junit.swingui.TestRunner -noloading org.xmlBlaster.test.persistence.AllTests
 echo "STARTING j2ee TESTS ..."
 java junit.swingui.TestRunner -noloading org.xmlBlaster.test.j2ee.AllTests
+echo "STARTING memoryleak TESTS ..."
+java junit.swingui.TestRunner -noloading org.xmlBlaster.test.memoryleak.AllTests
 echo "STARTING jmx TESTS ..."
 java junit.swingui.TestRunner -noloading org.xmlBlaster.test.jmx.AllTests
 echo "STARTING cluster TESTS ..."
 java junit.swingui.TestRunner -noloading org.xmlBlaster.test.cluster.AllTests
+echo "STARTING stress TESTS ..."
+java junit.swingui.TestRunner -noloading org.xmlBlaster.test.stress.AllTests
+echo "STARTING classloader TESTS ..."
+java junit.swingui.TestRunner -noloading org.xmlBlaster.test.classloader.AllTests
+echo "STARTING snmp TESTS ..."
+java junit.swingui.TestRunner -noloading org.xmlBlaster.test.snmp.AllTests
 
