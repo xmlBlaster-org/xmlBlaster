899f09cb92a76af9cd66920c12da5b46db51997c
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-03-18 10:28:17 +0000

    added jm2e classes which should not be compiled
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@11507 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/client/protocol/http/j2me/UrlConnectionMicro.java
@@ -0,0 +1,78 @@
+/*------------------------------------------------------------------------------
+Name:      UrlConnectionMicro.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+
+package org.xmlBlaster.client.protocol.http.j2me;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.HttpConnection;
+
+import org.xmlBlaster.client.protocol.http.common.I_Connection;
+
+/**
+ * UrlConnectionMicro
+ * @author <a href="mailto:laghi@swissinfo.org">Michele Laghi</a>
+ */
+public class UrlConnectionMicro implements I_Connection {
+
+   private HttpConnection conn;
+
+   public UrlConnectionMicro(String url) throws IOException {
+      this.conn = (HttpConnection)Connector.open(url);
+   }
+
+   public InputStream getInputStream() throws IOException {
+      return this.conn.openInputStream();
+   }
+   
+   public OutputStream getOutputStream() throws IOException {
+      return conn.openOutputStream();
+   }
+   
+   public String getHeaderField(String key) {
+      try {
+         return this.conn.getHeaderField(key);
+      }
+      catch(IOException ex) {
+         ex.printStackTrace();
+         return null;
+      }
+   }
+   
+   public void setRequestProperty(String key, String val) {
+      try {
+         this.conn.setRequestProperty(key, val);
+      }
+      catch(IOException ex) {
+         ex.printStackTrace();
+      }
+   }
+   
+   public void setDoOutput(boolean doOutput) {
+      try {
+         if (doOutput) this.conn.setRequestMethod(HttpConnection.POST);
+      }
+      catch (IOException ex) {
+         ex.printStackTrace();
+      }
+   }
+   
+   public void setDoInput(boolean doInput) {
+      // TODO implement it
+   }
+   
+   public void setUseCaches(boolean useCaches) {
+      // TODO implement it
+   }
+   
+   public void connect() throws IOException {
+      int ret = this.conn.getResponseCode();
+   }
+
+}
---
+++ xmlBlaster/src/java/org/xmlBlaster/client/protocol/http/j2me/XmlBlasterAccessJ2ME.java
@@ -0,0 +1,102 @@
+package org.xmlBlaster.client.protocol.http.j2me;
+
+import java.util.Hashtable;
+
+import javax.microedition.midlet.MIDlet;
+import org.xmlBlaster.client.protocol.http.common.*;
+import org.kobjects.base64.Base64;
+
+/**
+ * A java client implementation to access xmlBlaster using a persistent http connection
+ * for instant callback messages. 
+ * <p>
+ * You can control logging with the Applet PARAM tag, logging output is put to the Java console
+ * of your browser:
+ * </p>
+ * <pre>
+ * &lt;PARAM name="xmlBlaster/logLevels" value="ERROR,WARN">
+ * with more logging:
+ * &lt;PARAM name="xmlBlaster/logLevels" value="ERROR,WARN,INFO,DEBUG">
+ * </pre>
+ * See the example applet {@link http.applet.HelloWorld3} on how to use it.
+ * @author <a href="mailto:xmlBlaster@marcelruff.info">Marcel Ruff</a>
+ * @see http.applet.HelloWorld3
+ */
+public class XmlBlasterAccessJ2ME extends XmlBlasterAccessRawBase {
+   
+   private Hashtable cookie;
+   /**
+    * Provides access to xmlBlaster server. 
+    * @param applet My environment
+    * @see #parseAppletParameter
+    */
+   public XmlBlasterAccessJ2ME(MIDlet midlet) {
+      super(parseMidletParameter(midlet));
+   }
+
+   /**
+    * Provides access to xmlBlaster server. 
+    * @see #parseAppletParameter
+    */
+   public XmlBlasterAccessJ2ME(Hashtable properties) {
+      super(properties);
+   }
+
+   /**
+    * Parse the applet parameter from the HTML page. 
+    * <p>
+    * As the applet class has no getAllParameters() method we expect a PARAM <i>deliveredParamKeys</i>
+    * which contains a list of all delivered PARAM in the HTML page.
+    * </p>
+    * @see #getHtmlProperties
+    */
+   private static Hashtable parseMidletParameter(MIDlet midlet) {
+      String deliveredParamKeys = midlet.getAppProperty("deliveredParamKeys"); // a comma seperated list of all param from html page
+      Hashtable properties = new Hashtable();
+      //log("DEBUG", "Reading HTML PARAM deliveredParamKeys=" + deliveredParamKeys);
+      if (deliveredParamKeys != null) {
+         /*
+         StringTokenizer st = new StringTokenizer(deliveredParamKeys, ",;:");
+         while (st.hasMoreTokens()) {
+            String key = st.nextToken();
+            if (key == null) continue;
+            key = key.trim();
+            String value = applet.getParameter(key);
+            if (value != null && value.length() > 0) {
+               properties.put(key, value);
+            }
+         }
+         */
+      }
+      String loginName = midlet.getAppProperty("xmlBlaster/loginName");
+      String passwd = midlet.getAppProperty("xmlBlaster/passwd");
+      String xmlBlasterServletUrl = midlet.getAppProperty("xmlBlaster/servletUrl"); //param from html page
+
+      if (xmlBlasterServletUrl != null) properties.put("xmlBlaster/servletUrl", xmlBlasterServletUrl);
+      if (midlet.getAppProperty("xmlBlaster/logLevels") != null)
+         properties.put("xmlBlaster/logLevels", midlet.getAppProperty("xmlBlaster/logLevels"));
+      return properties;
+   }
+
+   /**
+    * This was previously URLEncoder but j2me has no such support
+    */
+   public String encode(String s, String enc) {
+      return Base64.encode(s.getBytes());
+   }
+   
+   public byte[] encodeBase64(byte[] data) {
+      return Base64.encode(data).getBytes();
+   }
+
+   public byte[] decodeBase64(byte[] data) {
+      return Base64.decode(new String(data));
+   }
+
+   public I_Connection createConnection(String url) throws Exception {
+      return new UrlConnectionMicro(url);
+   }
+
+}
+
+
