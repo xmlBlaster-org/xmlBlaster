29ebd6681cbb0bbc5b06e8f9a571cdb7af36a595
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-04-01 17:13:28 +0000

    Bugfix for empty strings (they did throw a bad_cast_exception)
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@11596 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/c++/util/lexical_cast.h
+++ xmlBlaster/src/c++/util/lexical_cast.h
@@ -15,9 +15,18 @@
 #include <sstream>
 #include <typeinfo>
 #include <util/XmlBCfg.h>
+#if __GNUC__ == 2
+  // g++ 2.95.3 does only know limits.h
+#else
+#  include <limits>
+#endif
 
 namespace org { namespace xmlBlaster { namespace util {
 
+#if __GNUC__ == 2
+   // Marcel 2004-04-01:
+   // Is buggy for lexical_cast<string>(string("")): empty strings throw a bad_lexical_cast
+   // Newest version from boost handles it but did not compile on g++ 2.9x
     class Dll_Export bad_lexical_cast : public std::bad_cast
     {
     public:
@@ -64,6 +73,170 @@ namespace org { namespace xmlBlaster { namespace util {
 
         return result;
     }
+#else
+   // Marcel 2004-04-01:
+   // Copy from current boost.org cvs, slightly modified
+   // to be not depending on other boost headers,
+   // and added more expressive exception.what() text
+
+    // exception used to indicate runtime lexical_cast failure
+    class bad_lexical_cast : public std::bad_cast
+    {
+    public:
+        bad_lexical_cast() :
+        source(&typeid(void)), target(&typeid(void))
+        {
+        }
+        bad_lexical_cast(
+            const std::type_info &s,
+            const std::type_info &t) :
+            source(&s), target(&t)
+        {
+        }
+        const std::type_info &source_type() const
+        {
+            return *source;
+        }
+        const std::type_info &target_type() const
+        {
+            return *target;
+        }
+        virtual const char *what() const throw()
+        {
+            sprintf((char*)str_, "bad lexical cast: source type value '%.50s' could not be interpreted as target '%.50s'", 
+                     source->name(), target->name());
+            return str_;
+        }
+        virtual ~bad_lexical_cast() throw()
+        {
+        }
+    private:
+        const std::type_info *source;
+        const std::type_info *target;
+        char str_[256];
+    };
+
+    namespace detail // selectors for choosing stream character type
+    {
+        template<typename Type>
+        struct stream_char
+        {
+            typedef char type;
+        };
+
+        #ifndef DISABLE_WIDE_CHAR_SUPPORT
+        template<>
+        struct stream_char<wchar_t>
+        {
+            typedef wchar_t type;
+        };
+
+        template<>
+        struct stream_char<wchar_t *>
+        {
+            typedef wchar_t type;
+        };
+
+        template<>
+        struct stream_char<const wchar_t *>
+        {
+            typedef wchar_t type;
+        };
+
+        template<>
+        struct stream_char<std::wstring>
+        {
+            typedef wchar_t type;
+        };
+        #endif
+
+        template<typename TargetChar, typename SourceChar>
+        struct widest_char
+        {
+            typedef TargetChar type;
+        };
+
+        template<>
+        struct widest_char<char, wchar_t>
+        {
+            typedef wchar_t type;
+        };
+    }
+    
+    namespace detail // stream wrapper for handling lexical conversions
+    {
+        template<typename Target, typename Source>
+        class lexical_stream
+        {
+        public:
+            lexical_stream()
+            {
+                stream.unsetf(std::ios::skipws);
+
+                if(std::numeric_limits<Target>::is_specialized)
+                    stream.precision(std::numeric_limits<Target>::digits10 + 1);
+                else if(std::numeric_limits<Source>::is_specialized)
+                    stream.precision(std::numeric_limits<Source>::digits10 + 1);
+            }
+            ~lexical_stream()
+            {
+                #if defined(BOOST_NO_STRINGSTREAM)
+                stream.freeze(false);
+                #endif
+            }
+            bool operator<<(const Source &input)
+            {
+                return !(stream << input).fail();
+            }
+            template<typename InputStreamable>
+            bool operator>>(InputStreamable &output)
+            {
+                return /*!is_pointer<InputStreamable>::value &&*/
+                       stream >> output &&
+                       (stream >> std::ws).eof();
+            }
+            bool operator>>(std::string &output)
+            {
+                #if defined(BOOST_NO_STRINGSTREAM)
+                stream << '\0';
+                #endif
+                output = stream.str();
+                return true;
+            }
+            #ifndef DISABLE_WIDE_CHAR_SUPPORT
+            bool operator>>(std::wstring &output)
+            {
+                output = stream.str();
+                return true;
+            }
+            #endif
+        private:
+            typedef typename widest_char<
+                typename stream_char<Target>::type,
+                typename stream_char<Source>::type>::type char_type;
+
+            #if defined(BOOST_NO_STRINGSTREAM)
+            std::strstream stream;
+            #elif defined(BOOST_NO_STD_LOCALE)
+            std::stringstream stream;
+            #else
+            std::basic_stringstream<char_type> stream;
+            #endif
+        };
+    }
+
+    template<typename Target, typename Source>
+    Target lexical_cast(Source arg)
+    {
+        detail::lexical_stream<Target, Source> interpreter;
+        Target result;
+
+        if(!(interpreter << arg && interpreter >> result))
+            throw bad_lexical_cast(typeid(Target), typeid(Source));
+        return result;
+    }
+#endif
+
 #if __GNUC__ == 2 || defined(__sun)
   // Problems with g++ 2.95.3 and template<>
 #else
