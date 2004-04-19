7197c07bfa865158705ab070521e2e02bfabc60f
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-04-19 14:31:47 +0000

    Added __FILE__ and __LINE__ to asserts, added new assert variants
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@11736 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/src/c/test.h
+++ xmlBlaster/testsuite/src/c/test.h
@@ -3,14 +3,40 @@
  * Copyright: http://www.jera.com/techinfo/jtns/jtn002.html
  * See: http://www.eskimo.com/~scs/C-faq/q10.4.html
  */
-extern char MU_ASSERT_TEXT[1024];
-extern int tests_run;
+static char MU_ASSERT_TEXT[2048];
+static int tests_run = 0;
 
-#define mu_assert2(message, expected, was, test) do { if (!(test)) { sprintf(MU_ASSERT_TEXT, "%s: expected='%s' was='%s'", message, expected, was); return MU_ASSERT_TEXT; }} while (0)
-#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_fail(message) do { return message; } while (0)
-#define mu_run_test(test) do { const char *message = test(); tests_run++; \
+#define mu_assertException(file, line, message, exceptionStr) \
+   do { sprintf(MU_ASSERT_TEXT, "[TEST FAIL]%s:%d %.500s: Caught exception: '%.500s'", file, line, message, exceptionStr); return MU_ASSERT_TEXT; } while (0)
+
+#define mu_assertEqualsBool(file, line, message, expected, was) \
+   do { if (expected != was) { sprintf(MU_ASSERT_TEXT, "[TEST FAIL]%s:%d %.500s: expected='%.500s' was='%.500s'", file, line, message, expected?"true":"false", was?"true":"false"); return MU_ASSERT_TEXT; }} while (0)
+
+#define mu_assertEqualsInt(file, line, message, expected, was) \
+   do { if (expected != was) { sprintf(MU_ASSERT_TEXT, "[TEST FAIL]%s:%d %.500s: expected='%d' was='%d'", file, line, message, expected, was); return MU_ASSERT_TEXT; }} while (0)
+
+#define mu_assertEqualsShort(file, line, message, expected, was) \
+   do { if (expected != was) { sprintf(MU_ASSERT_TEXT, "[TEST FAIL]%s:%d %.500s: expected='%hd' was='%hd'", file, line, message, expected, was); return MU_ASSERT_TEXT; }} while (0)
+
+#define mu_assertEqualsLong(file, line, message, expected, was) \
+   do { if (expected != was) { sprintf(MU_ASSERT_TEXT, "[TEST FAIL]%s:%d %.500s: expected='%ld' was='%ld'", file, line, message, expected, was); return MU_ASSERT_TEXT; }} while (0)
+
+#define mu_assertEqualsString(file, line, message, expected, was) \
+   do { if (strcmp(expected,was) != 0) { sprintf(MU_ASSERT_TEXT, "[TEST FAIL]%s:%d %.500s: expected='%.500s' was='%.500s'", file, line, message, expected, was); return MU_ASSERT_TEXT; }} while (0)
+
+#define mu_assert2(file, line, message, expected, was, test) \
+   do { if (!(test)) { sprintf(MU_ASSERT_TEXT, "[TEST FAIL]%s:%d %.500s: expected='%.500s' was='%.500s'", file, line, message, expected, was); return MU_ASSERT_TEXT; }} while (0)
+
+#define mu_assert(file, line, message, test) \
+   do { if (!(test)) { sprintf(MU_ASSERT_TEXT, "[TEST FAIL]%s:%d %.500s", file, line, message); return MU_ASSERT_TEXT; }} while (0)
+
+#define mu_fail(message) \
+   do { return message; } while (0)
+
+#define mu_run_test(test) \
+   do { const char *message = test(); tests_run++; \
                               if (message) return message; } while (0)
-#define mu_run_test2(test, a, b) do { const char *message = test(a,b); tests_run++; \
+#define mu_run_test2(test, a, b) \
+   do { const char *message = test(a,b); tests_run++; \
                               if (message) return message; } while (0)
 
