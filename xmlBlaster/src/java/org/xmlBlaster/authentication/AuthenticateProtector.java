4324da87e6458f733203982bc551888e1b161ac0
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-04-26 15:37:02 +0000

    Changed to use AvailabilityChecker
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@11826 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/authentication/AuthenticateProtector.java
+++ xmlBlaster/src/java/org/xmlBlaster/authentication/AuthenticateProtector.java
@@ -12,13 +12,17 @@ import org.xmlBlaster.protocol.I_Authenticate;
 import org.xmlBlaster.protocol.I_XmlBlaster;
 import org.xmlBlaster.engine.qos.ConnectQosServer;
 import org.xmlBlaster.engine.qos.DisconnectQosServer;
+import org.xmlBlaster.util.qos.ConnectQosData;
 import org.xmlBlaster.util.def.Constants;
 import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.util.MsgUnit;
 import org.xmlBlaster.util.def.ErrorCode;
 import org.xmlBlaster.engine.qos.ConnectReturnQosServer;
 import org.xmlBlaster.util.SessionName;
+import org.xmlBlaster.util.def.MethodName;
 import org.xmlBlaster.engine.admin.I_AdminSubject;
 import org.xmlBlaster.engine.Global;
+import org.xmlBlaster.engine.AvailabilityChecker;
 
 
 /**
@@ -30,6 +34,7 @@ final public class AuthenticateProtector implements I_Authenticate
    private final Global glob;
    private final LogChannel log;
    private final Authenticate authenticate;
+   private final AvailabilityChecker availabilityChecker;
 
    public AuthenticateProtector(Global global, Authenticate authenticate) throws XmlBlasterException {
       this.glob = global;
@@ -38,6 +43,7 @@ final public class AuthenticateProtector implements I_Authenticate
       this.authenticate = authenticate;
       if (log.CALL) log.call(ME, "Entering constructor");
       this.glob.setAuthenticate(this);
+      this.availabilityChecker = new AvailabilityChecker(this.glob);
    }
 
    public Global getGlobal() {
@@ -59,10 +65,19 @@ final public class AuthenticateProtector implements I_Authenticate
 
    /** helper */
    public final ConnectReturnQosServer connect(ConnectQosServer xmlQos, String secretSessionId) throws XmlBlasterException {
-      // serialize first to have a clone for security reasons (and to guarantee our Global)
-      // Note: We throw away the ConnectQosServer facade and create a new one (no specific data enters the core)
-      ConnectReturnQosServer tmp = this.authenticate.connect(new ConnectQosServer(glob, xmlQos.toXml()), secretSessionId);
-      return new ConnectReturnQosServer(glob, tmp.toXml());
+
+      MsgUnit msgUnit = new MsgUnit(null, null, xmlQos.getData());
+      this.availabilityChecker.checkServerIsReady(xmlQos.getSessionName(), msgUnit, MethodName.CONNECT);
+
+      try {
+         // serialize first to have a clone for security reasons (and to guarantee our Global)
+         // Note: We throw away the ConnectQosServer facade and create a new one (no specific data enters the core)
+         ConnectReturnQosServer tmp = this.authenticate.connect(xmlQos.getClone(glob), secretSessionId);
+         return new ConnectReturnQosServer(glob, tmp.toXml());
+      }
+      catch (Throwable e) {
+         throw this.availabilityChecker.checkException(MethodName.CONNECT, e);
+      }
    }
 
    /** helper */
@@ -71,15 +86,40 @@ final public class AuthenticateProtector implements I_Authenticate
    }
 
    public final String connect(String connectQos_literal, String secretSessionId) throws XmlBlasterException {
-      //System.out.println("GOT Protector: " + connectQos_literal);
-      ConnectQosServer qos = new ConnectQosServer(glob, connectQos_literal);
-      //System.out.println("AFTER Protector: " + qos.toXml());
-      ConnectReturnQosServer ret = this.authenticate.connect(qos, secretSessionId);
-      return ret.toXml();
+
+      // Parse XML QoS
+      MsgUnit msgUnit = new MsgUnit(glob, null, null, connectQos_literal, MethodName.CONNECT);
+      ConnectQosServer qos = new ConnectQosServer(glob, (ConnectQosData)msgUnit.getQosData());
+
+      // Currently we have misused used the clientProperty to transport this information
+      if (qos.getData().getClientProperty(Constants.PERSISTENCE_ID) != null)
+         qos.isFromPersistenceRecovery(true);
+
+      this.availabilityChecker.checkServerIsReady(qos.getSessionName(), msgUnit, MethodName.CONNECT);
+
+      try {
+         //System.out.println("GOT Protector: " + connectQos_literal);
+         //System.out.println("AFTER Protector: " + qos.toXml());
+         ConnectReturnQosServer ret = this.authenticate.connect(qos, secretSessionId);
+         return ret.toXml();
+      }
+      catch (Throwable e) {
+         throw this.availabilityChecker.checkException(MethodName.CONNECT, e);
+      }
    }
 
    public final void disconnect(String secretSessionId, String qos_literal) throws XmlBlasterException {
-      this.authenticate.disconnect(secretSessionId, qos_literal);
+
+      // Parse XML QoS
+      MsgUnit msgUnit = new MsgUnit(glob, null, null, qos_literal, MethodName.DISCONNECT);
+      this.availabilityChecker.checkServerIsReady(null, msgUnit, MethodName.DISCONNECT);
+
+      try {
+         this.authenticate.disconnect(secretSessionId, qos_literal);
+      }
+      catch (Throwable e) {
+         throw this.availabilityChecker.checkException(MethodName.DISCONNECT, e);
+      }
    }
 
    /**
@@ -101,5 +141,9 @@ final public class AuthenticateProtector implements I_Authenticate
 
    public String toXml() throws XmlBlasterException {
       return this.authenticate.toXml();
+   }
+
+   public final void shutdown() {
+      this.availabilityChecker.shutdown();
    }
 }
