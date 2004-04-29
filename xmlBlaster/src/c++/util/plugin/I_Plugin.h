ce71c823bae23676c12fdb3e3c5a908737dd89a1
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-04-29 06:55:48 +0000

    General plugin interface
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@11872 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/c++/util/plugin/I_Plugin.h
@@ -0,0 +1,40 @@
+/*------------------------------------------------------------------------------
+Name:      I_Plugin.h
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+#ifndef _UTIL_PLUGIN_I_PLUGIN_H
+#define _UTIL_PLUGIN_I_PLUGIN_H
+
+#include <string>
+
+namespace org { namespace xmlBlaster { namespace util { namespace plugin {
+
+/**
+ * Interface for all plugins. 
+ * Note: Currently this interface marks plugins only, it is not
+ * yet used to dynamically load shared libraries or dlls.
+ * @author <a href='mailto:xmlblast@marcelruff.info'>Marcel Ruff</a>
+ */
+class Dll_Export I_Plugin
+{
+public:
+   virtual ~I_Plugin() {};
+    
+   /**
+    * Get the name of the plugin. 
+    * @return For example "SOCKET", "IOR", "SQLITE", "RAM", "XERCES", ...
+    */
+   virtual std::string getType() = 0;
+
+   /**
+    * Get the version of the plugin. 
+    * @return For example "1.0"
+    */
+   virtual std::string getVersion() = 0;
+};
+
+}}}} // namespace
+
+#endif
+
