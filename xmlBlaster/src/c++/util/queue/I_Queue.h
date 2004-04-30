62cf706b5cb3b911b357ab1715f3152447ea8f86
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-04-30 09:07:34 +0000

    Docu
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@11897 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/c++/util/queue/I_Queue.h
+++ xmlBlaster/src/c++/util/queue/I_Queue.h
@@ -3,13 +3,6 @@ Name:      I_Queue.h
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 ------------------------------------------------------------------------------*/
-
-/**
- * Interface for queue implementations (RAM, JDBC or CACHE). 
- *
- * @author <a href='mailto:laghi@swissinfo.org'>Michele Laghi</a>
- * @author <a href='mailto:xmlblast@marcelruff.info'>Marcel Ruff</a>
- */
 #ifndef _UTIL_QUEUE_I_QUEUE_H
 #define _UTIL_QUEUE_I_QUEUE_H
 
@@ -22,19 +15,28 @@ Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 
 namespace org { namespace xmlBlaster { namespace util { namespace queue {
 
-typedef ReferenceHolder<MsgQueueEntry>      EntryType;
+/**
+ * Smart pointer support. 
+ */
+typedef ReferenceHolder<MsgQueueEntry> EntryType;
 
+/**
+ * Interface for queue implementations (RAM, JDBC or CACHE). 
+ *
+ * @author <a href='mailto:laghi@swissinfo.org'>Michele Laghi</a>
+ * @author <a href='mailto:xmlblast@marcelruff.info'>Marcel Ruff</a>
+ */
 class Dll_Export I_Queue
 {
 public:
    virtual ~I_Queue() {};
     
    /**
-    * puts a new entry into the queue. 
-    * Note that this method takes the entry pointed to by the argument 
-    * and puts a reference to it into the queue. This means that you can not destroy the entry before the
-    * reference to it has been removed from the queue (which normally happens on a remove or when destroying
-    * the queue.
+    * Puts a new entry into the queue. 
+    * The put() method takes a clone of the passed entry, you can safely destroy
+    * your passed entry after this invocation.
+    * @param entry A message which is stored in the queue
+    * @throws XmlBlasterException on problems
     */
    virtual void put(const MsgQueueEntry &entry) = 0;
 
@@ -43,22 +45,31 @@ public:
     * this is the maximum number of entries to return. If maxNumOfBytes is positive, only the entries
     * which fit into the range specified are returned. If there are no such entries, an empty std::vector is
     * returned.
+    * @return A vector with EntryType, this smart pointer wraps the real message, you don't need to take
+    *         care on freeing memory.
+    * @throws XmlBlasterException on problems
     */
    virtual const std::vector<EntryType> peekWithSamePriority(long maxNumOfEntries=-1, long maxNumOfBytes=-1) const = 0;
 
    /**
     * Deletes the entries specified in the std::vector in the argument list. If this std::vector is empty or if
     * the queue is empty, zero (0) is returned, otherwise it returns the number of entries really deleted.
+    * @param start The inclusive beginning of the messages to remove
+    * @param end The exclusive ending
+    * @return Number of entries removed
+    * @throws XmlBlasterException on problems
     */
    virtual long randomRemove(const std::vector<EntryType>::const_iterator &start, const std::vector<EntryType>::const_iterator &end) = 0;
 
    /**
     * Clears (removes all entries) this queue
+    * @throws XmlBlasterException on problems
     */
    virtual void clear() = 0;
 
     /**
-     * returns true if the queue is empty, false otherwise
+     * @return true if the queue is empty, false otherwise
+     * @throws XmlBlasterException on problems
      */                                  
    virtual bool empty() const = 0;
 };
