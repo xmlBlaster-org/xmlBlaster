8db3898d544e1bde9d54d4af6ff8a01f860b98e0
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-04-30 08:10:45 +0000

    namespace for method name consts
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@11877 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/c++/util/MethodName.cpp
@@ -0,0 +1,30 @@
+/*-----------------------------------------------------------------------------
+Name:      MethodName.cpp
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Holding some constants, see MethodName.h for a description
+-----------------------------------------------------------------------------*/
+#include <util/MethodName.h>
+
+namespace org { namespace xmlBlaster {
+  namespace util { namespace MethodName {
+
+// See org.xmlBlaster.util.def.MethodName.java
+const std::string CONNECT = "connect";
+const std::string DISCONNECT = "disconnect";
+const std::string GET = "get";
+const std::string ERASE = "erase";
+const std::string PUBLISH = "publish";
+const std::string PUBLISH_ARR = "publishArr";
+const std::string PUBLISH_ONEWAY = "publishOneway";
+const std::string SUBSCRIBE = "subscribe";
+const std::string UNSUBSCRIBE = "unSubscribe";
+const std::string UPDATE = "update";
+const std::string UPDATE_ONEWAY = "updateOneway";
+const std::string PING = "ping";
+const std::string DUMMY = "dummy";
+const std::string UNKNOWN = "unknown";
+
+}}}} // namespace 
+
+
---
+++ xmlBlaster/src/c++/util/MethodName.h
@@ -0,0 +1,45 @@
+/*-----------------------------------------------------------------------------
+Name:      MethodName.h
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Method name constants
+-----------------------------------------------------------------------------*/
+#ifndef _UTIL_METHODNAME_H
+#define _UTIL_METHODNAME_H
+
+#include <util/xmlBlasterDef.h>
+#include <string>
+
+namespace org { namespace xmlBlaster {
+  namespace util { namespace MethodName {
+
+/**
+ * Holding the constant names of the supported remote methods. 
+ * <p />
+ * @see xmlBlaster/src/java/org/xmlBlaster/util/def/MethodName.java
+ */
+//class Dll_Export MethodName { // Using namespace instead of class here
+
+extern Dll_Export const std::string CONNECT;
+extern Dll_Export const std::string DISCONNECT;
+extern Dll_Export const std::string GET;
+extern Dll_Export const std::string ERASE;
+extern Dll_Export const std::string PUBLISH;
+extern Dll_Export const std::string PUBLISH_ARR;
+extern Dll_Export const std::string PUBLISH_ONEWAY;
+extern Dll_Export const std::string SUBSCRIBE;
+extern Dll_Export const std::string UNSUBSCRIBE;
+extern Dll_Export const std::string UPDATE;
+extern Dll_Export const std::string UPDATE_ONEWAY;
+extern Dll_Export const std::string PING;
+extern Dll_Export const std::string DUMMY;
+extern Dll_Export const std::string UNKNOWN;
+
+//}; // class MethodName
+
+
+}}}}; // namespace 
+
+#endif
+
+
