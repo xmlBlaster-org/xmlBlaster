9d3f40de4f078976e83fe67593a3c3109c28016e
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-05-04 16:05:09 +0000

    A simple CACHE implementation for our C++ queues
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@11952 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/c++/util/queue/CacheQueuePlugin.cpp
@@ -0,0 +1,212 @@
+/*------------------------------------------------------------------------------
+Name:      CacheQueuePlugin.cpp
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+#include <util/queue/CacheQueuePlugin.h>
+#include <util/queue/QueueFactory.h>
+#include <util/XmlBlasterException.h>
+#include <util/Global.h>
+#include <util/queue/SQLiteQueuePlugin.h> // temporary for usage -> remove again
+
+using namespace std;
+using namespace org::xmlBlaster::util;
+using namespace org::xmlBlaster::util::thread;
+using namespace org::xmlBlaster::util::qos::storage;
+
+namespace org { namespace xmlBlaster { namespace util { namespace queue {
+
+CacheQueuePlugin::CacheQueuePlugin(Global& global, const ClientQueueProperty& property)
+   : ME("CacheQueuePlugin"), 
+     global_(global), 
+     log_(global.getLog("org.xmlBlaster.util.queue")), 
+     property_(property), 
+     transientQueueP_(0), 
+     persistentQueueP_(0), 
+     accessMutex_()
+{
+   // TODO: type/version should be set from outside!!!
+
+   transientQueueP_ = &QueueFactory::getFactory(global_).getPlugin(property, "RAM", "1.0");
+
+   try {
+      persistentQueueP_ = &QueueFactory::getFactory(global_).getPlugin(property, "SQLite", "1.0");
+
+      // Note: On startup we can only load the highest priority in a bulk, peekWithSamePriority() does not support to get all!
+      reloadFromPersistentStore();
+   }
+   catch (const XmlBlasterException &e) {
+      log_.warn(ME, "No persistent queue is available, we continue RAM based. Reason: " + e.getMessage());
+   }
+   log_.info(ME, "Created queue [" + getType() + "][" + getVersion() + "]");
+}
+
+/*
+CacheQueuePlugin::CacheQueuePlugin(const CacheQueuePlugin& queue)
+   : ME("CacheQueuePlugin"), 
+     global_(queue.global_), 
+     log_(queue.log_), 
+     property_(queue.property_), 
+     storage_(queue.storage_), 
+     accessMutex_()
+{
+   numOfBytes_ = queue.numOfBytes_;
+}
+
+CacheQueuePlugin& CacheQueuePlugin::operator =(const CacheQueuePlugin& queue)
+{
+   Lock lock(queue.accessMutex_);
+   property_   = queue.property_;
+   storage_    = queue.storage_;
+   numOfBytes_ = queue.numOfBytes_;
+   return *this;
+
+}
+*/
+
+CacheQueuePlugin::~CacheQueuePlugin()
+{
+   if (log_.call()) log_.call(ME, "destructor");
+   QueueFactory::getFactory(global_).releasePlugin(transientQueueP_);
+   if (persistentQueueP_) QueueFactory::getFactory(global_).releasePlugin(persistentQueueP_);
+} 
+
+void CacheQueuePlugin::put(const MsgQueueEntry &entry)
+{
+   if (log_.call()) log_.call(ME, "::put");
+
+   Lock lock(accessMutex_);
+   transientQueueP_->put(entry);
+   if (persistentQueueP_) {
+      if (entry.isPersistent()) {
+         try {
+           persistentQueueP_->put(entry);
+         }
+         catch (const XmlBlasterException &e) {
+            log_.warn(ME, "Ignoring problem to put entry into persistent queue, we are handling it transient: " + e.getMessage());
+         }
+      }
+   }
+}
+
+long CacheQueuePlugin::reloadFromPersistentStore() const
+{
+   if (persistentQueueP_ && transientQueueP_->getNumOfEntries() == 0 && persistentQueueP_->getNumOfEntries() > 0) {
+      // On startup shuffle them to the transient queue (only the highest priority is accessible with our I_Queue API)
+      const vector<EntryType> vec = persistentQueueP_->peekWithSamePriority(-1, -1);
+      long count = 0;
+      vector<EntryType>::const_iterator iter = vec.begin();
+      for (; iter != vec.end(); ++iter) {
+         const EntryType &entryType = (*iter);
+         transientQueueP_->put(*entryType);
+         count++;
+      }
+      return count;
+   }
+   return 0;
+}
+
+const vector<EntryType> CacheQueuePlugin::peekWithSamePriority(long maxNumOfEntries, long maxNumOfBytes) const
+{
+   Lock lock(accessMutex_);
+   vector<EntryType> vec = transientQueueP_->peekWithSamePriority(maxNumOfEntries, maxNumOfBytes);
+
+   if (vec.size() == 0) {
+      long count = reloadFromPersistentStore();
+      if (count > 0) {
+         return transientQueueP_->peekWithSamePriority(maxNumOfEntries, maxNumOfBytes);
+      }
+   }
+
+   return vec;
+}
+
+
+long CacheQueuePlugin::randomRemove(const vector<EntryType>::const_iterator &start, const vector<EntryType>::const_iterator &end) 
+{
+   Lock lock(accessMutex_);
+   long count = transientQueueP_->randomRemove(start, end);
+
+   if (persistentQueueP_) {
+      vector<EntryType> persistents;
+      vector<EntryType>::const_iterator iter = start;
+      while (iter != end) {
+         const EntryType &entryType = (*iter);
+         if (entryType->isPersistent()) {
+            persistents.push_back(entryType);
+         }
+         iter++;
+      }
+      try {
+         persistentQueueP_->randomRemove(persistents.begin(), persistents.end());
+      }
+      catch (const XmlBlasterException &e) {
+         log_.warn(ME, "Ignoring problem to remove entry from persistent queue, we remove it from the transient queue only: " + e.getMessage());
+      }
+   }
+   return count;
+}
+
+long CacheQueuePlugin::getNumOfEntries() const
+{
+   return transientQueueP_->getNumOfEntries();
+}
+
+long CacheQueuePlugin::getMaxNumOfEntries() const
+{
+   return transientQueueP_->getMaxNumOfEntries();
+}
+
+int64_t CacheQueuePlugin::getNumOfBytes() const
+{
+   return transientQueueP_->getNumOfBytes();
+}
+
+int64_t CacheQueuePlugin::getMaxNumOfBytes() const
+{
+   return transientQueueP_->getMaxNumOfBytes();
+}
+
+void CacheQueuePlugin::clear()
+{
+   Lock lock(accessMutex_);
+   transientQueueP_->clear();
+   if (persistentQueueP_) {
+      try {
+         persistentQueueP_->clear();
+      }
+      catch (const XmlBlasterException &e) {
+         log_.warn(ME, "Ignoring problem to put entry into persistent queue, we are handling it transient: " + e.getMessage());
+      }
+   }
+}
+
+bool CacheQueuePlugin::empty() const
+{
+   return transientQueueP_->empty();
+}
+
+void CacheQueuePlugin::destroy()
+{
+   transientQueueP_->destroy();
+   if (persistentQueueP_) {
+      try {
+         persistentQueueP_->destroy();
+      }
+      catch (const XmlBlasterException &e) {
+         log_.warn(ME, "Ignoring problem to destroy the persistent queue: " + e.getMessage());
+      }
+   }
+}
+
+string CacheQueuePlugin::usage()
+{
+   std::string text = string("");
+   text += string("\nThe CACHE queue plugin configuration:");
+   text += SQLiteQueuePlugin::usage();   // TODO: depending on persistency
+   return text;
+}
+}}}} // namespace
+
+
+
---
+++ xmlBlaster/src/c++/util/queue/CacheQueuePlugin.h
@@ -0,0 +1,151 @@
+/*------------------------------------------------------------------------------
+Name:      CacheQueuePlugin.h
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+#ifndef _UTIL_QUEUE_CACHEQUEUE_H
+#define _UTIL_QUEUE_CACHEQUEUE_H
+
+#include <util/xmlBlasterDef.h>
+#include <util/ReferenceHolder.h>
+#include <util/queue/I_Queue.h>
+#include <util/queue/MsgQueueEntry.h>
+#include <util/thread/ThreadImpl.h>
+#include <util/I_Log.h>
+#include <set>
+
+namespace org { namespace xmlBlaster { namespace util { namespace queue {
+
+typedef std::set<EntryType, std::greater<EntryType> > StorageType;
+
+/**
+ * This class implements a very simple cache around the RAM and SQLite queue. 
+ * Note there is no swapping support for transient or persistent messages
+ * all transient messages are hold in RAM, and all persistent messages are
+ * duplicated to harddisk.
+ * When time permits we will add swapping support similar to the Java CACHE
+ * implementation. 
+ * <br />
+ * If you have mainly persistent messages and need to take care on
+ * your RAM consumption with many messages in queue consider to use
+ * the "SQLite" persistent queue directly (without any RAM or CACHE)
+ * with the option <code>-connection/queue/type SQLite</code> instead of the default
+ * <code>-connection/queue/type CACHE</code>.
+ * <br />
+ * On the other hand if you use only transient message consider using the RAM queue directly
+ * with the option <code>-connection/queue/type RAM</code> instead of the default
+ * <code>-connection/queue/type CACHE</code>.
+ * @author <a href="mailto:xmlBlaster@marcelruff.info">Marcel Ruff</a>
+ * @see <a href="http://www.xmlBlaster.org/xmlBlaster/doc/requirements/client.cpp.queue.html">The client.cpp.queue requirement</a>
+ */
+class Dll_Export CacheQueuePlugin : public I_Queue
+{
+private:
+   CacheQueuePlugin(const CacheQueuePlugin& queue);
+   CacheQueuePlugin& operator =(const CacheQueuePlugin& queue);
+
+   /**
+    * Shuffle messages from persistent store to transient store. 
+    * On startup we can only load the highest priority in a bulk
+    * NOTE: This must be called from inside a synchronization lock.
+    * @return Number of entries loaded
+    */
+   long CacheQueuePlugin::reloadFromPersistentStore() const;
+
+protected:
+   std::string ME;
+   org::xmlBlaster::util::Global& global_;
+   org::xmlBlaster::util::I_Log& log_;
+   org::xmlBlaster::util::qos::storage::ClientQueueProperty property_;
+   mutable I_Queue *transientQueueP_;
+   I_Queue *persistentQueueP_;
+   org::xmlBlaster::util::thread::Mutex accessMutex_;
+
+public:
+   CacheQueuePlugin(org::xmlBlaster::util::Global& global, const org::xmlBlaster::util::qos::storage::ClientQueueProperty& property);
+
+   virtual ~CacheQueuePlugin();
+    
+   /**
+    * puts a new entry into the queue. 
+    * Note that this method takes the entry pointed to by the argument 
+    * and puts a reference to it into the queue. This means that you can not destroy the entry before the
+    * reference to it has been removed from the queue (which normally happens on a remove or when destroying
+    * the queue.
+    */
+   void put(const MsgQueueEntry &entry);
+
+   /**
+    * Returns the entries with the highest priority in the queue. If 'maxNumOfEntries' is positive,
+    * this is the maximum number of entries to return. If maxNumOfBytes is positive, only the entries
+    * which fit into the range specified are returned. If there are no such entries, an empty std::vector is
+    * returned.
+    */
+   const std::vector<EntryType> peekWithSamePriority(long maxNumOfEntries=-1, long maxNumOfBytes=-1) const;
+
+   /**
+    * Deletes the entries specified in the std::vector in the argument list. If this std::vector is empty or if
+    * the queue is empty, zero (0) is returned, otherwise it returns the number of entries really deleted.
+    */
+   long randomRemove(const std::vector<EntryType>::const_iterator &start, const std::vector<EntryType>::const_iterator &end);
+
+   /**
+    * Access the current number of entries. 
+    * @return The number of entries in the queue
+    */                                  
+   long getNumOfEntries() const;
+
+   /**
+    * Access the configured maximum number of elements for this queue. 
+    * @return The maximum number of elements in the queue
+    */
+   long getMaxNumOfEntries() const;
+
+   /**
+    * Returns the amount of bytes currently in the queue. 
+    * If the implementation of this interface is not able to return the correct
+    * number of entries (for example if the implementation must make a remote
+    * call to a DB which is temporarly not available) it will return -1.
+    * @return The amount of bytes currently in the queue, returns -1 on error
+    */
+   int64_t getNumOfBytes() const;
+
+   /**
+    * Access the configured capacity (maximum bytes) for this queue. 
+    * @return The maximum capacity for the queue in bytes
+    */
+   int64_t getMaxNumOfBytes() const;
+
+   /**
+    * Clears (removes all entries) this queue
+    */
+   void clear();
+
+    /**
+     * returns true if the queue is empty, false otherwise
+     */                                  
+    bool empty() const;
+
+    static std::string usage();
+
+   /**
+    * Get the name of the plugin. 
+    * @return "CACHE"
+    * @enforcedBy I_Plugin
+    */
+   std::string getType() { static std::string type = "CACHE"; return type; }
+
+   /**
+    * Get the version of the plugin. 
+    * @return "1.0"
+    * @enforcedBy I_Plugin
+    */
+   std::string getVersion() { static std::string version = "1.0"; return version; }
+
+   void destroy();
+};
+
+}}}} // namespace
+
+#endif
+
