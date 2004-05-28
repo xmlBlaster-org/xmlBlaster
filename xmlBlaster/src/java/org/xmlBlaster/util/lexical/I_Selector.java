61574fe2822431de3195b272ecadc7ebeca23223
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-05-28 11:56:51 +0000

    added interface for lexical mime plugin
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@12057 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/util/lexical/I_Selector.java
@@ -0,0 +1,22 @@
+/*------------------------------------------------------------------------------
+Name:      I_Selector.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+
+package org.xmlBlaster.util.lexical;
+
+import java.util.Map;
+
+import org.xmlBlaster.util.XmlBlasterException;
+
+
+/**
+ * I_Selector
+ * @author <a href="mailto:laghi@swissinfo.org">Michele Laghi</a>
+ */
+public interface I_Selector {
+   
+   public boolean select(String query, Map clientProperties) throws XmlBlasterException;
+
+}
