689167aadbc3e6c6e0fd3520f682fe32c64f492f
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-05-28 14:21:21 +0000

    files for Sql92 Message Selector
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@12061 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/util/lexical/LikeOpWrapper.java
@@ -0,0 +1,152 @@
+/*------------------------------------------------------------------------------
+Name:      LikeOpWrapper.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+
+package org.xmlBlaster.util.lexical;
+
+import gnu.regexp.RE;
+import gnu.regexp.REException;
+
+import java.util.StringTokenizer;
+
+import org.jutils.log.LogChannel;
+import org.xmlBlaster.util.Global;
+import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.util.def.ErrorCode;
+
+
+/**
+ * LikeOpWrapper
+ * @author <a href="mailto:laghi@swissinfo.org">Michele Laghi</a>
+ */
+public final class LikeOpWrapper {
+
+   private static final String ME = "LikeOpWrapper";
+   private String regexPattern;
+   private RE expression;
+   private Global global;
+   private LogChannel log;
+
+   public LikeOpWrapper(Global global, String pattern) throws XmlBlasterException {
+      this(global, pattern, (char)0);
+   }
+
+   public LikeOpWrapper(Global global, String pattern, char escape) throws XmlBlasterException {
+      this(global, pattern, escape, true);
+   }
+
+/*
+   public LikeOpWrapper(Global global, String pattern, char escape, boolean doReducedSyntax) 
+      throws XmlBlasterException {
+      this.global = global;
+      this.log = this.global.getLog("lexical");
+      
+      String tmp = pattern;
+      if (doReducedSyntax) {
+         tmp = replace(tmp, escape, "_", ".");
+         if (this.log.TRACE) this.log.trace(ME, "constructor regexPattern (transitional)='" + tmp + "'");
+         tmp = replace(tmp, escape, "%", ".*");
+         if (this.log.TRACE) this.log.trace(ME, "constructor regexPattern='" + this.regexPattern + "'");
+      }
+
+      try {
+         this.regexPattern = tmp;
+         this.expression = new RE(tmp);
+      }
+      catch (REException ex) {
+         if (this.log.TRACE) this.log.trace(ME, "constructor " + Global.getStackTraceAsString());
+         throw new XmlBlasterException(this.global, ErrorCode.USER_ILLEGALARGUMENT, ME + " constructor: could not generate a Regex from the string '" + pattern + "' reason: " + ex.getMessage());
+      }
+   }
+*/
+   public LikeOpWrapper(Global global, String pattern, char escape, boolean doReducedSyntax) 
+   throws XmlBlasterException {
+   this.global = global;
+   this.log = this.global.getLog("lexical");
+   
+   String tmp = pattern;
+   if (doReducedSyntax) {
+      StringBuffer buf = new StringBuffer();
+      boolean isEscape = false;
+      for (int i=0; i < pattern.length(); i++) {
+         char ch = pattern.charAt(i);
+         switch (ch) {
+            case '_' :
+               if (isEscape) buf.append(ch);
+               else buf.append('.');
+               isEscape = false;
+               break;
+            case '%' : 
+               if (isEscape) buf.append(ch);
+               else buf.append('.').append('*');
+               isEscape = false;
+               break;
+            default: 
+               if (ch == escape) isEscape = true;
+               else {
+                  isEscape = false;
+                  buf.append(ch);
+               }
+         }
+      }
+      tmp = buf.toString();
+      if (this.log.TRACE) this.log.trace(ME, "constructor regexPattern='" + tmp + "'");
+   }
+
+   try {
+      this.regexPattern = tmp;
+      this.expression = new RE(tmp);
+   }
+   catch (REException ex) {
+      if (this.log.TRACE) this.log.trace(ME, "constructor " + Global.getStackTraceAsString());
+      throw new XmlBlasterException(this.global, ErrorCode.USER_ILLEGALARGUMENT, ME + " constructor: could not generate a Regex from the string '" + pattern + "' reason: " + ex.getMessage());
+   }
+}
+
+   
+   public boolean match(String inputString) {
+      return this.expression.isMatch(inputString);
+   }
+   
+   /**
+    * 
+    * @param pattern The input String to modify (the initial pattern used)
+    * @param escape The character to use as the escape char. If 0 then it works as no escape was defined
+    * @param token the token to be replaced
+    * @param replacement the string to use to replace the given token.
+    * @return
+    */
+   public static String replace(String pattern, char escape, String token, String replacement) {
+      StringTokenizer tokenizer = new StringTokenizer(pattern, token);
+      StringBuffer buf = new StringBuffer();
+      while (tokenizer.hasMoreTokens()) {
+         String tmp = tokenizer.nextToken();
+         buf.append(tmp);
+         if (tokenizer.hasMoreTokens()) {
+            if (tmp.charAt(tmp.length()-1) == escape) {
+               buf.append(token);
+            }
+            else {
+               buf.append(replacement);
+            }
+         }
+      }
+      return buf.toString();
+   }
+   
+   public static void main(String[] args) {
+      if (args.length != 2) {
+         System.err.println("usage: java org.xmlBlaster.util.lexical.LikeOpWrapper pattern inputString");
+         System.exit(-1);
+      }
+      try {
+         LikeOpWrapper wrapper = new LikeOpWrapper(new Global((String[])null), args[0], (char)0, false);
+         System.out.println("result: " + wrapper.match(args[1]));
+      }
+      catch (Exception ex) {
+         ex.printStackTrace();
+      }
+   }
+}
---
+++ xmlBlaster/src/java/org/xmlBlaster/util/lexical/Sql92Selector.java
@@ -0,0 +1,58 @@
+/*------------------------------------------------------------------------------
+Name:      Sql92Selector.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+
+package org.xmlBlaster.util.lexical;
+   
+import org.jutils.log.LogChannel;
+import org.xmlBlaster.util.Global;
+import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.util.def.ErrorCode;
+
+import java.io.StringReader;
+import java.util.Map;
+
+public class Sql92Selector implements I_Selector {
+   
+   private Global global;
+   private LogChannel log;
+   private Sql92Scanner scanner;
+   private Sql92Parser parser;
+   
+   /**
+    * 
+    * @param global
+    * @param map The map containing the client properties to match against.
+    */
+   public Sql92Selector(Global global) {
+      this.global = global;
+      this.log = this.global.getLog("lexical");
+      if (this.log.CALL) this.log.call("Sql92Selector", "constructor");
+      this.scanner = new Sql92Scanner(global);
+      this.parser = new Sql92Parser(this.global, this.scanner);
+   }
+   
+   public boolean select(String query, Map clientProperties) throws XmlBlasterException {
+      try {
+         if (this.log.CALL) this.log.call("Sql92Selector", "select \"" + query + "\"");
+         this.scanner.yyreset(new StringReader(query));
+         this.scanner.setClientPropertyMap(clientProperties);
+         
+         if (this.log.DUMP) {
+            return ((Boolean)this.parser.debug_parse().value).booleanValue();      
+         }
+         return ((Boolean)this.parser.parse().value).booleanValue();      
+      }
+      catch (Throwable ex) {
+         if (this.log.TRACE) {
+            ex.printStackTrace();
+         }
+         throw new XmlBlasterException(this.global,ErrorCode.USER_ILLEGALARGUMENT, "Selector.select: could not interpret the query '" + query + "'");
+      }
+   }
+   
+}
+
+
