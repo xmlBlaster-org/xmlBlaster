feaac72bab8cba2c1e95f1924655ea4e13e98185
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-08-22 22:47:00 +0000

    Added PluginInfo attributes as defaults
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@12229 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/engine/qos/AddressServer.java
+++ xmlBlaster/src/java/org/xmlBlaster/engine/qos/AddressServer.java
@@ -8,17 +8,14 @@ package org.xmlBlaster.engine.qos;
 
 import org.xmlBlaster.util.Global;
 import org.xmlBlaster.util.qos.address.AddressBase;
+import java.util.Properties;
 
 /**
- * Helper class holding address string, protocol string and client side connection properties.
+ * Helper class holding address string, protocol string of server side protocol plugins. 
  * <p />
  * <pre>
- * &lt;address type='XMLRPC' sessionId='4e56890ghdFzj0'
- *           pingInterval='60000' retries='5' delay='10000'
- *           oneway='false'>
- *    http://server:8080/cb
- *    &lt;compress type='gzip' minSize='1000'/>
- *    &lt;burstMode collectTime='400'/> <!-- for publishOneway() calls -->
+ * &lt;address type='XMLRPC'>
+ *    http://server:8080/
  * &lt;/address>
  * </pre>
  */
@@ -28,18 +25,15 @@ public class AddressServer extends AddressBase
 
    /**
     * @param type    The protocol type, e.g. "IOR", "EMAIL", "XMLRPC"
-    * @param nodeId  A unique string (typically the cluster node id we connect to).<br />
-    *   This is used for extended env-variable support, e.g. for a given
-    *    <code>nodeId="heron"</ code>
-    *   the command line argument (or xmlBlaster.property entry)
-    *    <code>-retries[heron] 20</code>
-    *   is precedence over
-    *    <code>-retries 10</code>
+    * @param nodeId  A unique string (typically the cluster node id we connect to).
+    * @param pluginInfoParameters Attributes from xmlBlasterPlugins.xml
     */
-   public AddressServer(Global glob, String type, String nodeId) {
+   public AddressServer(Global glob, String type, String nodeId, Properties pluginInfoParameters) {
       super(glob, "address");
       this.nodeId = nodeId;
       this.instanceName = null;
+      if (pluginInfoParameters != null)
+         setPluginInfoParameters(pluginInfoParameters);
       setType(type);
       initialize();
    }
