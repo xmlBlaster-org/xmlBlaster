bd57be30e243124cf5a95ab47e548b72a7058f00
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-08-29 22:13:14 +0000

    Removed icc compiler warning
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@12316 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/src/c++/TestSub.cpp
+++ xmlBlaster/testsuite/src/c++/TestSub.cpp
@@ -3,7 +3,7 @@ Name:      TestSub.cpp
 Project:   xmlBlaster.org
 Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 Comment:   Demo code for a client using xmlBlaster
-Version:   $Id: TestSub.cpp,v 1.32 2004/08/28 17:23:28 ruff Exp $
+Version:   $Id: TestSub.cpp,v 1.33 2004/08/29 22:13:14 ruff Exp $
 -----------------------------------------------------------------------------*/
 #include "TestSuite.h"
 #include <iostream>
@@ -47,7 +47,7 @@ public:
 
    string update(const string &sessionId,
                UpdateKey &updateKey,
-               const unsigned char *content, long contentSize,
+               const unsigned char */*content*/, long /*contentSize*/,
                UpdateQos &updateQos) 
    {
       log_.info("update", string("Receiving update on callback '") + name_ + "' of message oid=" +
@@ -220,8 +220,8 @@ private:
       subscribeOid_ = "";
       try {
          subscribeOid_ = connection_.subscribe(subKey1, subQos, cb1_).getSubscriptionId();
-         string sub1 = connection_.subscribe(subKey2, subQos, cb2_).getSubscriptionId();
-         string sub2 = connection_.subscribe(subKey3, subQos, cb3_).getSubscriptionId();
+         //string sub1 = connection_.subscribe(subKey2, subQos, cb2_).getSubscriptionId();
+         //string sub2 = connection_.subscribe(subKey3, subQos, cb3_).getSubscriptionId();
 
          log_.info(ME, string("Success: Subscribe subscription-id=") + subscribeOid_ + " done");
 
