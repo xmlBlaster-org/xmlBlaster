8ccc8f4544d22d0db3f6d79f8fb5a4762f996b03
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-08-30 07:30:11 +0000

    Extensions added by Jason Martin <jhmartin@toger.us>
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@12320 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/demo/perl/xmlrpc/server.pl
+++ xmlBlaster/demo/perl/xmlrpc/server.pl
@@ -1,9 +1,12 @@
 #!/usr/bin/perl -w
 
 # Perl callback server example, xmlBlaster.org
-# @author David Kelly davidk@navahonetworks.com
-# @author Russell Chan russ@navahonetworks.com
+# @author David Kelly <davidk@navahonetworks.com>
+# @author Russell Chan <russ@navahonetworks.com>
+# @author Jason Martin <jhmartin@toger.us>
+use strict;
 
+use MIME::Base64;
 use Frontier::Daemon;
 use Frontier::Client;
 use xmlBlaster::Exception ;
@@ -13,7 +16,12 @@ use xmlBlaster::EraseReturnQos ;
 
 
 sub do_update {
-        print "Received update ...\n";
+        print "***\nReceived update ...\n";
+        print "Header:" . $_[1] . "\n";
+        print "Message:" .decode_base64($_[2]->value) . "\n";
+        print "QoS:". $_[3] . "\n***\n";
+
+# Acknowledge receipt of the update
         return "<qos><state>OK</state></qos>";
 }
 
@@ -29,8 +37,10 @@ my $server = Frontier::Client->new(url => $server_url);
 print "Connected to xmlBlaster server on $server_url \n";
 
 # Call the remote server and get our result.
+# Retries and delay set to cover a race between subscribing and
+# xmlBlaster attempting to communicate with the xmlrpc server created below.
 my $sessionId = $server->call('authenticate.login', "dk2", "dk2",
-                "<qos><callback type='XMLRPC'>$local_url</callback>".
+                "<qos><callback type='XMLRPC' retries='2' delay='2000'>$local_url</callback>".
                 "<local>false</local></qos>", "");
 print "\nLogin success with sessionId=$sessionId \n";
 
@@ -39,10 +49,15 @@ $server->call('xmlBlaster.subscribe',
                          "<key oid='' queryType='XPATH'>//service</key>",
                         "<qos><duplicateUpdates>false</duplicateUpdates></qos>");
 
-Frontier::Daemon->new(
+# ReuseAddr is an option to the IO::Socket class of which Frontier::Daemon is a
+# subclass.  It prevents an 'Address already in use' error that occurs when this
+# script is interrupted and restarted quickly.
+my $result = Frontier::Daemon->new(
+	    ReuseAddr => 1,
             LocalPort => 9091,
             methods => {
                         'update'   => \&do_update,
                         'ping'   => \&do_ping
             });
 
+die "Unable to spawn daemon: $!" unless $result;
