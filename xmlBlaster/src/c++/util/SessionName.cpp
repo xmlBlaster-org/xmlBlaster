350417c5f670ad46101fd6fe747504d925e5917d
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-09-23 07:15:03 +0000

    The missing SessionName class added to cleanup SessionQos, it is reference counted
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@12409 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/c++/util/SessionName.cpp
@@ -0,0 +1,186 @@
+/*------------------------------------------------------------------------------
+Name:      SessionName.cpp
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   
+------------------------------------------------------------------------------*/
+#include <util/SessionName.h>
+#include <stdlib.h>
+#include <util/lexical_cast.h>
+#include <util/StringStripper.h>
+#include <util/StringTrim.h>
+#include <util/Global.h>
+
+namespace org { namespace xmlBlaster { namespace util {
+
+using namespace org::xmlBlaster::util;
+using namespace org::xmlBlaster::util::parser;
+using namespace std;
+
+
+/*---------------------------- SessionName --------------------------------*/
+
+SessionName::SessionName(Global& global, const string& defaultUserName, long publicSessionId)
+    : ReferenceCounterBase(), ME("SessionName"), global_(global)
+{
+   initialize("", defaultUserName, publicSessionId);   
+}
+
+SessionName::SessionName(Global& global, const string& absoluteName)
+    : ReferenceCounterBase(), ME("SessionName"), global_(global)
+{
+   initialize(absoluteName, "", 0);
+}
+
+
+void SessionName::copy(const SessionName& data)
+{
+   clusterNodeId_ = data.clusterNodeId_;
+   subjectId_     = data.subjectId_;
+   pubSessionId_  = data.pubSessionId_;
+}
+
+
+void SessionName::initialize(const string& absoluteName, const string& defaultUserName, long publicSessionId)
+{
+   pubSessionId_ = publicSessionId;
+
+   if (!absoluteName.empty()) {
+      setAbsoluteName(absoluteName);
+      return;
+   }
+
+   string name = global_.getProperty().getStringProperty("session.name", "");
+   if (!name.empty()) {
+      setAbsoluteName(name);
+      return;
+   }
+
+   clusterNodeId_ = global_.getProperty().getStringProperty("session.clusterNodeId", "");
+
+   if (!defaultUserName.empty()) {
+      subjectId_ = defaultUserName;
+      return;
+   }
+
+   string subjectId = global_.getProperty().getStringProperty("USER", "guest");
+   subjectId_ = global_.getProperty().getStringProperty("user", subjectId);
+}
+
+
+SessionName::SessionName(const SessionName& data) : ReferenceCounterBase(), ME(data.ME), global_(data.global_)
+{
+   copy(data);
+}
+
+SessionName& SessionName::operator =(const SessionName& data)
+{
+  copy(data);
+  return *this;
+}
+
+
+void SessionName::setAbsoluteName(const string& name)
+{
+   pubSessionId_ = 0; // resets the value if previously set
+   string relative = "";
+   if (name.empty())
+      throw XmlBlasterException(USER_ILLEGALARGUMENT, ME + "::setAbsoluteName", "the absolute name is empty");
+
+   if (name[0] == '/') { // then it is an absolute name
+      StringStripper stripper("/");
+      vector<std::string> help = stripper.strip(name);
+      help.erase(help.begin()); // since it is empty for sure.
+      if (help.size() < 4) 
+         throw XmlBlasterException(USER_ILLEGALARGUMENT, ME + "::setAbsoluteName", "the absolute name '" + name + "' is not allowed");
+      if (help[0] == "node") clusterNodeId_ = help[1];
+      else throw XmlBlasterException(USER_ILLEGALARGUMENT, ME + "::setAbsoluteName", "the absolute name '" + name + "' is not allowed. It should start with '/node'");
+      if (help[2] != "client") 
+         throw XmlBlasterException(USER_ILLEGALARGUMENT, ME + "::setAbsoluteName", "the absolute name '" + name + "' is not allowed. '/client' is missing");
+   
+      for (size_t i=3; i < help.size(); i++) {
+         relative += help[i];
+        if ( i < help.size()-1) relative += "/";
+      }
+   }
+   else relative = name;
+
+   StringStripper relStripper("/");
+   vector<std::string> relHelp = relStripper.strip(relative);
+   if (relHelp.empty()) {
+      throw XmlBlasterException(USER_ILLEGALARGUMENT, ME + "::setAbsoluteName", "there is no relative name information: '" + name + "' is not allowed");
+   }
+
+   size_t ii = 0;
+   if ( relHelp.size() > ii ) {
+      string tmp = relHelp[ii++];
+      if ( tmp == "client" ) {
+         if ( relHelp.size() > ii ) {
+            subjectId_ = relHelp[ii++];
+         }
+         else {
+            throw XmlBlasterException(USER_ILLEGALARGUMENT, ME + "::setAbsoluteName", "there is no relative name information: '" + name + "' is not allowed");
+         }
+      }
+      else subjectId_ = tmp;
+   }
+   if ( relHelp.size() > ii ) {
+      pubSessionId_ = lexical_cast<long>(relHelp[ii]);
+   }
+}
+
+string SessionName::getRelativeName() const
+{
+   string ret = string("client/") + subjectId_;
+   if (pubSessionId_ != 0) ret += string("/") + lexical_cast<std::string>(pubSessionId_);
+   return ret;
+}
+
+string SessionName::getAbsoluteName() const
+{
+   if (clusterNodeId_.empty()) return getRelativeName();
+   return string("/node/") + clusterNodeId_ + string("/") + getRelativeName();
+}
+
+string SessionName::getClusterNodeId() const
+{
+   return clusterNodeId_;
+}
+
+void SessionName::setClusterNodeId(const string& clusterNodeId)
+{
+   clusterNodeId_ = clusterNodeId;
+}
+
+string SessionName::getSubjectId() const
+{
+   return subjectId_;
+}
+
+void SessionName::setSubjectId(const string& subjectId)
+{
+    subjectId_ = subjectId;
+}
+
+long SessionName::getPubSessionId() const
+{
+   return pubSessionId_;
+}
+
+void SessionName::setPubSessionId(const long pubSessionId)
+{
+   pubSessionId_ = pubSessionId;
+}
+
+string SessionName::toXml(const string& extraOffset) const
+{
+   string offset = Constants::OFFSET + extraOffset;
+   string ret;
+   
+   ret += offset + string("<session");
+   ret += string(" name='")  + getAbsoluteName() + string("'");
+   ret += string("/>\n");
+   return ret;
+}
+
+}}}
---
+++ xmlBlaster/src/c++/util/SessionName.h
@@ -0,0 +1,86 @@
+/*------------------------------------------------------------------------------
+Name:      SessionName.h
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   
+------------------------------------------------------------------------------*/
+
+#ifndef XMLBLASTER_UTIL_SessionName_H
+#define XMLBLASTER_UTIL_SessionName_H
+
+#include <util/xmlBlasterDef.h>
+#include <string>
+#include <util/ReferenceCounterBase.h>
+#include <util/ReferenceHolder.h>
+#include <util/StringTrim.h>
+
+namespace org { namespace xmlBlaster { namespace util {
+
+class Dll_Export SessionName : public org::xmlBlaster::util::ReferenceCounterBase
+{
+private:
+   const std::string ME;
+   std::string  sessionId_;
+   std::string  clusterNodeId_;
+   std::string  subjectId_;
+   long         pubSessionId_;
+   org::xmlBlaster::util::Global& global_;
+
+   void copy(const SessionName& data);
+
+   void initialize(const std::string& absoluteName, const std::string& defaultUserName, long publicSessionId);
+
+public:
+   /**
+    * When using this constructor you can let it assign the defaults by passing an empty std::string as the 
+    * 'absoluteName' argument, or you force the SessionName to set the SessionName to what you specify in the
+    * 'absoluteName' argument. 
+    * @param absoluteName the sessionId to assign to this SessionName. You can either pass an absolute name,
+    * or a relative name or an empty std::string.
+    */
+   SessionName(org::xmlBlaster::util::Global& global, const std::string& absoluteName="");
+
+   /**
+    * @defaultUserName is the name to use as a default for the user (the subjectId). It is stronger than the
+    * properties set but if an empty std::string is used, then the default name is taken from the 'user' property. 
+    * This is just a default and as such it is weaker than the property 'session.name'. In other words, even
+    * if defaultUserName is not empty, it will be overwritten by eventual setting of the property 'session.name'.
+    *
+    * @publicSessionId is the public sessionId to be used. Note that this is just a suggestion, so if you 
+    * have the 'session.name' property set, it will overwrite this. If the publicSessionId is '0', then it
+    * is omitted (the server will assign one for you).
+    */
+   SessionName(org::xmlBlaster::util::Global& global, const std::string& defaultUserName, long publicSessionId);
+   SessionName(const SessionName& data);
+   SessionName& operator =(const SessionName& data);
+
+   /**
+    * Sets the absolute name. It checks if it really is an absolute name,
+    * i.e. if it contains the well known structure '/node/....' it parses it,
+    * otherwise it leaves it untouched (i.e. it will not parse it).
+    */
+
+   /**
+    * Sets the absolute name. Note that you can overwrite the nodeId here. It returns 'true' if the
+    * name was absolute, 'false' otherwise.
+    */
+   void setAbsoluteName(/*const std::string nodeId="",*/ const std::string& name);
+   std::string getRelativeName() const;
+   std::string getAbsoluteName() const;
+   std::string getClusterNodeId() const;
+   void setClusterNodeId(const std::string& clusterNodeId);
+   std::string getSubjectId() const;
+   void setSubjectId(const std::string& subjectId);
+   long getPubSessionId() const;
+   void setPubSessionId(const long pubSessionId);
+
+   std::string getSecretSessionId() const;
+   void setSecretSessionId(const std::string& sessionId);
+   std::string toXml(const std::string& extraOffset="") const;
+};
+
+typedef org::xmlBlaster::util::ReferenceHolder<SessionName> SessionNameRef;
+
+}}} // namespaces
+
+#endif
