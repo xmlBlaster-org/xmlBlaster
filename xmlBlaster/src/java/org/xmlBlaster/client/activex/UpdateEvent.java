2a15e8a80e782f45aa5096aa02edf569f3c6d588
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-10-29 20:36:36 +0000

    Added Base64 accessor because of ActiveX bridge problems with byte[]
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@12774 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/client/activex/UpdateEvent.java
+++ xmlBlaster/src/java/org/xmlBlaster/client/activex/UpdateEvent.java
@@ -8,6 +8,7 @@ package org.xmlBlaster.client.activex;
 import org.xmlBlaster.client.I_Callback;
 import org.xmlBlaster.client.key.UpdateKey;
 import org.xmlBlaster.client.qos.UpdateQos;
+import org.xmlBlaster.client.protocol.http.j2me.Base64; // org.apache.commons.codec.binary.Base64;
 
 /**
  * Event object used to transport a callback message back to ActiveX (C#, VisualBasic). 
@@ -25,18 +26,45 @@ public class UpdateEvent extends java.util.EventObject {
       this.content = content;
       this.qos = qos;
    }
+   
    public String getCbSessionId() {
       return this.cbSessionId;
    }
+   
    public UpdateKey getKey() {
       return this.key;
    }
+
+   /**
+    * NOTE: Passing byte[] seems to be broken with Java bridge! 
+    *
+    * If you have binary data use #getContentBase64() as a work around
+    * you need to decode it yourself in Basic/C#.<br />
+    * If you have a string content (like xml markup) you can call
+    * #getContentStr() directly.
+    *
+    * @see http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4887461
+    */ 
    public byte[] getContent() {
       return this.content;
    }
+
    public String getContentStr() {
       return new String(this.content);
    }
+
+   public int getContentLength() {
+      return this.content.length;
+   }
+
+   /**
+    * Access binary content encoded with Base64. 
+    */
+   public String getContentBase64() {
+      String encoded = Base64.encode(this.content);
+      return encoded;
+   }
+
    public UpdateQos getQos() {
       return this.qos;
    }
