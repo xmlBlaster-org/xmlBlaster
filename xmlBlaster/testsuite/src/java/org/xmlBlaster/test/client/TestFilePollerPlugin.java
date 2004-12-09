600d021b9336e9c4528c8c1542843d9c82bbf01f
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2004-12-09 18:46:20 +0000

    added some testing
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@13012 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/src/java/org/xmlBlaster/test/client/TestFilePollerPlugin.java
+++ xmlBlaster/testsuite/src/java/org/xmlBlaster/test/client/TestFilePollerPlugin.java
@@ -6,17 +6,25 @@ Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 package org.xmlBlaster.test.client;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 import org.jutils.log.LogChannel;
 import org.xmlBlaster.client.I_Callback;
+import org.xmlBlaster.client.filepoller.Publisher;
+import org.xmlBlaster.client.key.SubscribeKey;
+import org.xmlBlaster.client.key.UnSubscribeKey;
 import org.xmlBlaster.client.key.UpdateKey;
+import org.xmlBlaster.client.qos.ConnectQos;
+import org.xmlBlaster.client.qos.DisconnectQos;
+import org.xmlBlaster.client.qos.SubscribeQos;
+import org.xmlBlaster.client.qos.UnSubscribeQos;
 import org.xmlBlaster.client.qos.UpdateQos;
 import org.xmlBlaster.test.MsgInterceptor;
 import org.xmlBlaster.util.Global;
 import org.xmlBlaster.util.XmlBlasterException;
-import org.xmlBlaster.util.def.ErrorCode;
+import org.xmlBlaster.util.qos.address.Address;
 
 import junit.framework.TestCase;
 
@@ -38,8 +46,13 @@ public class TestFilePollerPlugin extends TestCase implements I_Callback {
    
    private Global global;
    private LogChannel log;
-
-   private MsgInterceptor[] updateInterceptors;
+   private Global connGlobal;
+   private String oid = "filepollerTest";
+   private String dirName = "/tmp/testsuitePoller";
+   private String dirNameSent = "/tmp/testsuitePoller/Sent";
+   private String dirNameDiscarded = "/tmp/testsuitePoller/Discarded";
+   
+   private MsgInterceptor updateInterceptor;
 
    public TestFilePollerPlugin() {
       this(null);
@@ -53,98 +66,24 @@ public class TestFilePollerPlugin extends TestCase implements I_Callback {
          this.global.init((String[])null);
       }
       this.log = this.global.getLog("test");
-      this.updateInterceptors = new MsgInterceptor[1];
    }
 
-   private void writeToFileSlowly(String filename, int numChunks, int chunkSize, long delayBetweenChunks) throws XmlBlasterException {
-      char[] chunk = new char[chunkSize];
-      int sizeChanges = 0;
-      int timeChanges = 0;
-      long time = -1L;
-      long size = -1L;
-      this.log.info(ME, "writeToFileSlowly: '" + filename + "' numChunks=" + numChunks + " chunkSize=" + chunkSize);
-      for (int i=0; i < chunkSize; i++) 
-         chunk[i] = 'a';
-      try {
-         File file = new File(filename);
-         if (file.exists()) {
-            boolean ret = file.delete();
-            if (!ret)
-               throw new XmlBlasterException(this.global, ErrorCode.RESOURCE_FILEIO, ME + ".writeToFileSlowly", "can not remove old file '" + filename + "'");
-         }
-      }
-      catch (SecurityException ex) {
-         throw new XmlBlasterException(this.global, ErrorCode.RESOURCE_FILEIO, ME + ".writeToFileSlowly", "can not remove old file '" + filename + "' because of security exception: " + ex.getMessage());
-      }
-      int i = 0;
-      try {
-         FileWriter fileWriter = new FileWriter(filename);
-         for (i=0; i < numChunks; i++) {
-            fileWriter.write(chunk);
-            File file = new File(filename);
-            
-            if (this.log.DUMP) {
-               this.log.dump(ME, "writeToFileSlowly: chunk nr. " + i + " file exists   :'" + file.exists() + "'");
-               this.log.dump(ME, "writeToFileSlowly: chunk nr. " + i + " file can read :'" + file.canRead() + "'");
-               this.log.dump(ME, "writeToFileSlowly: chunk nr. " + i + " file can write:'" + file.canWrite() + "'");
-            }
-            
-            long newSize = file.length();
-            if (this.log.DUMP) {
-               this.log.dump(ME, "writeToFileSlowly: chunk nr. " + i + " size='" + newSize + "'");
-            }
-            if (size > -1L) {
-               if (newSize != size)
-                  sizeChanges++;
-            }
-            size = newSize;
-            
-            long newTime = file.lastModified();
-            if (this.log.DUMP) {
-               this.log.dump(ME, "writeToFileSlowly: chunk nr. " + i + " time='" + newTime + "'");
-            }
-            if (time > -1L) {
-               if (newTime != time)
-                  timeChanges++;
-            }
-            time = newTime;
-            
-            try {
-               Thread.sleep(delayBetweenChunks);
-            }
-            catch (Exception ex) {
-            }
-         }
-         fileWriter.close();
-
-         File file = new File(filename);
-         long newSize = file.length();
-         if (this.log.DUMP) {
-            this.log.dump(ME, "writeToFileSlowly: closed file size='" + newSize + "'");
-         }
-         long newTime = file.lastModified();
-         if (this.log.DUMP) {
-            this.log.dump(ME, "writeToFileSlowly: closed file time='" + newTime + "'");
-         }
-      }
-      catch (IOException ex) {
-         throw new XmlBlasterException(this.global, ErrorCode.RESOURCE_FILEIO, ME + ".writeToFileSlowly", "IOException when writing chunk '" + i + "' to '" + filename + "' reason: " + ex.getMessage());
-      }
-      if (this.log.TRACE) {
-         this.log.trace(ME, ""); 
-         this.log.trace(ME, "writeToFileSlowly: size changes = " + sizeChanges); 
-         this.log.trace(ME, "writeToFileSlowly: time changes = " + timeChanges); 
-         this.log.trace(ME, "writeToFileSlowly: chunks       = " + numChunks); 
-         this.log.trace(ME, ""); 
-      }
-   }
-   
    /**
     * Sets up the fixture.
     * <p />
     * Connect to xmlBlaster and login
     */
    protected void setUp() {
+      try {
+         this.connGlobal = this.global.getClone(null);
+         this.updateInterceptor = new MsgInterceptor(this.connGlobal, this.log, null);
+         this.connGlobal.getXmlBlasterAccess().connect(new ConnectQos(this.connGlobal), this.updateInterceptor);
+         this.connGlobal.getXmlBlasterAccess().subscribe(new SubscribeKey(this.connGlobal, this.oid), new SubscribeQos(this.connGlobal));
+      }
+      catch (XmlBlasterException ex) {
+         ex.printStackTrace();
+         fail("aborting since exception ex: " + ex.getMessage());
+      }
    }
    
    
@@ -155,6 +94,17 @@ public class TestFilePollerPlugin extends TestCase implements I_Callback {
     */
    protected void tearDown() {
       log.info(ME, "Entering tearDown(), test is finished");
+      cleanUpDirs();
+      try {
+         this.connGlobal.getXmlBlasterAccess().unSubscribe(new UnSubscribeKey(this.connGlobal, this.oid), new UnSubscribeQos(this.connGlobal));
+         this.connGlobal.getXmlBlasterAccess().disconnect(new DisconnectQos(this.connGlobal));
+         this.connGlobal.shutdown();
+         this.connGlobal = null;
+      }
+      catch (XmlBlasterException ex) {
+         ex.printStackTrace();
+         fail("aborting since exception ex: " + ex.getMessage());
+      }
    }
 
    public String update(String cbSessionId, UpdateKey updateKey, byte[] content, UpdateQos updateQos) throws XmlBlasterException {
@@ -171,17 +121,273 @@ public class TestFilePollerPlugin extends TestCase implements I_Callback {
       return "OK";
    }
 
-   public void testSystem() {
+   /**
+    * Tests the creation of the necessary directories
+    *
+    */
+   public void testDirectories() {
+      // absolute path
+      Properties prop = new Properties();
+      prop.put("topicName", "dummy");
+      prop.put("directoryName", this.dirName);
+      prop.put("sent", this.dirNameSent);
+      prop.put("discarded", this.dirNameDiscarded);
       try {
-         int numChunks = 10;
-         int chunkSize = 10000;
-         long delayBetweenChunks = 2000L;
-         writeToFileSlowly("testFilePollerPlugin.dat", numChunks, chunkSize, delayBetweenChunks);
+         Publisher publisher = new Publisher(this.global, "test", prop);
       }
       catch (XmlBlasterException ex) {
          ex.printStackTrace();
-         assertTrue("Exception occured when it should not " + ex.getMessage(), false);
+         assertTrue("An exception should not occur here " + ex.getMessage(), false);
       }
+      checkDirs();
+
+      // repeat that with already existing directories
+      prop = new Properties();
+      prop.put("topicName", "dummy");
+      prop.put("directoryName", this.dirName);
+      prop.put("sent", this.dirNameSent);
+      prop.put("discarded", this.dirNameDiscarded);
+      try {
+         Publisher publisher = new Publisher(this.global, "test", prop);
+      }
+      catch (XmlBlasterException ex) {
+         ex.printStackTrace();
+         assertTrue("An exception should not occur here " + ex.getMessage(), false);
+      }
+      checkDirs();
+      cleanUpDirs();
+
+      // relative path are added to the 'directoryName'
+      prop = new Properties();
+      prop.put("topicName", "dummy");
+      prop.put("directoryName", this.dirName);
+      prop.put("sent", "Sent");
+      prop.put("discarded", "Discarded");
+      try {
+         Publisher publisher = new Publisher(this.global, "test", prop);
+      }
+      catch (XmlBlasterException ex) {
+         ex.printStackTrace();
+         assertTrue("An exception should not occur here " + ex.getMessage(), false);
+      }
+      checkDirs();
+      // relative path are added to the 'directoryName' repeat with existing directories
+      prop = new Properties();
+      prop.put("topicName", "dummy");
+      prop.put("directoryName", this.dirName);
+      prop.put("sent", "Sent");
+      prop.put("discarded", "Discarded");
+      try {
+         Publisher publisher = new Publisher(this.global, "test", prop);
+      }
+      catch (XmlBlasterException ex) {
+         ex.printStackTrace();
+         assertTrue("An exception should not occur here " + ex.getMessage(), false);
+      }
+      checkDirs();
+      
+      cleanUpDirs();
+      
+      // now some which should fail:
+      // existing file but not a directory
+      File file = new File(this.dirName);
+      try {
+         file.createNewFile();
+      }
+      catch (IOException ex) {
+         assertTrue("could not create the file '" + this.dirName + "'", false);
+      }
+      
+      prop = new Properties();
+      prop.put("topicName", "dummy");
+      prop.put("directoryName", this.dirName);
+      prop.put("sent", "Sent");
+      prop.put("discarded", "Discarded");
+      try {
+         Publisher publisher = new Publisher(this.global, "test", prop);
+         assertTrue("an exception should occur since '" + this.dirName + "' is a file and should be a directory", false);
+      }
+      catch (XmlBlasterException ex) {
+         this.log.info(ME, "Exception is OK here");
+      }
+      cleanUpDirs();
+      
+      try {
+         file = new File(this.dirName);
+         boolean ret = file.mkdir();
+         assertTrue("could not create directory '" + this.dirName + "'", ret);
+         file = new File(this.dirNameSent);
+         file.createNewFile();
+      }
+      catch (IOException ex) {
+         assertTrue("could not create the file '" + this.dirNameSent + "'", false);
+      }
+      
+      prop = new Properties();
+      prop.put("topicName", "dummy");
+      prop.put("directoryName", this.dirName);
+      prop.put("sent", "Sent");
+      prop.put("discarded", "Discarded");
+      try {
+         Publisher publisher = new Publisher(this.global, "test", prop);
+         assertTrue("an exception should occur since '" + this.dirName + "' is a file and should be a directory", false);
+      }
+      catch (XmlBlasterException ex) {
+         this.log.info(ME, "Exception is OK here");
+      }
+      cleanUpDirs();
+   }
+
+   
+   
+   public void testSimplePublish() {
+      Properties prop = new Properties();
+      prop.put("topicName", this.oid);
+      prop.put("directoryName", this.dirName);
+/*
+      prop.put("sent", "Sent");
+      prop.put("discarded", "Discarded");
+      prop.put("publishKey", "");
+      prop.put("publishQos", "");
+      prop.put("connectQos", "");
+      prop.put("loginName", "");
+      prop.put("password", "");
+      prop.put("fileFilter", "");
+      prop.put("lockExtention", ".lck");
+*/
+      int maximumSize = 10000;
+      long delaySinceLastChange = 500L;
+      long pollInterval = 250L;
+      prop.put("maximumFileSize", "" + maximumSize);
+      prop.put("delaySinceLastFileChange", "" + delaySinceLastChange);
+      prop.put("pollInterval", "" + pollInterval);
+      prop.put("warnOnEmptyFileDelay", "1000L");
+      
+      org.xmlBlaster.engine.Global engineGlobal = new org.xmlBlaster.engine.Global();
+      prop.put("connectQos", this.getConnectQos(engineGlobal));
+      
+      Publisher publisher = null;
+      try {
+         publisher = new Publisher(engineGlobal, "test", prop);
+         publisher.init();
+
+         this.updateInterceptor.clear();
+         // too big
+         String tooBig = this.dirName + File.separator + "tooBig.dat";
+         writeFile(tooBig, maximumSize+1);
+         int ret = this.updateInterceptor.waitOnUpdate(delaySinceLastChange* 2);
+         assertEquals("expected no updates", 0, ret);
+         File tmp = new File(tooBig);
+         assertFalse("the file '" + tooBig + "' should have been removed", tmp.exists());
+         
+         this.updateInterceptor.clear();
+         // too big
+         String okFile = this.dirName + File.separator + "ok.dat";
+         byte[] okBuf = writeFile(okFile, maximumSize-1);
+         ret = this.updateInterceptor.waitOnUpdate(delaySinceLastChange* 2);
+         assertEquals("expected one update", 1, ret);
+         tmp = new File(okFile);
+         assertFalse("the file '" + okFile + "' should have been removed", tmp.exists());
+         
+         boolean sameContent = compareContent(okBuf, this.updateInterceptor.getMsgs()[0].getContent());
+         assertTrue("the content of the file is not the same as the arrived content of the update method", sameContent);
+         String fileName = this.updateInterceptor.getMsgs()[0].getUpdateQos().getClientProperty("_fileName", (String)null);
+         assertNotNull("The fileName is null", fileName);
+         assertEquals("", "ok.dat", fileName);
+      }
+      catch (XmlBlasterException ex) {
+         ex.printStackTrace();
+         assertTrue("An exception should not occur here " + ex.getMessage(), false);
+      }
+      finally {
+         if (publisher != null) {
+            try {
+               publisher.shutdown();
+            }
+            catch (Throwable ex) {
+               ex.printStackTrace();
+               fail("exception when shutting down the poller " + ex.getMessage());
+            }
+         }
+      }
+      
+   }
+
+   private boolean compareContent(byte[] buf1, byte[] buf2) {
+      if (buf1 == null && buf2 == null)
+         return true;
+
+      if (buf1 == null || buf2 == null)
+         return false;
+      
+      if (buf1.length != buf2.length)
+         return false;
+      for (int i=0; i < buf1.length; i++) {
+         if (buf1[i] != buf2[i])
+            return false;
+      }
+      return true;
+   }
+   
+   private byte[] writeFile(String filename, int size) {
+      try {
+         byte[] buf = new byte[size];
+         for (int i=0; i < size; i++) {
+            buf[i] = (byte)i;
+         }
+         FileOutputStream fos = new FileOutputStream(filename);
+         fos.write(buf);
+         fos.close();
+         return buf;
+      }
+      catch (IOException ex) {
+         ex.printStackTrace();
+         fail("could not write to file '" + filename + "'");
+         return null; // fake return to make compiler happy
+      }
+   }
+   
+   
+   private void checkDirs() {
+      File file = new File(this.dirName);
+      assertTrue("file '" + this.dirName + "' does not exist", file.exists());
+      file = new File(this.dirNameSent);
+      assertTrue("file '" + this.dirNameSent + "' does not exist", file.exists());
+      file = new File(this.dirNameDiscarded);
+      assertTrue("file '" + this.dirNameDiscarded + "' does not exist", file.exists());
+   }
+   
+   private String getConnectQos(Global glob) {
+      try {
+         ConnectQos connQos = new ConnectQos(glob, "filePollerTestUser", "secret");
+         connQos.setMaxSessions(100);
+         Address address = connQos.getAddress();
+         address.setPingInterval(0L);
+         address.setCollectTime(0L);
+         connQos.getClientQueueProperty().setType("RAM");
+         connQos.getClientQueueProperty().setVersion("1.0");
+         return connQos.toXml();
+      }
+      catch (XmlBlasterException ex) {
+         fail("an exception when building the connect qos: " + ex.getMessage());
+         return null;
+      }
+   }
+
+   private void delete(String filename) {
+      try {
+         (new File(filename)).delete();
+      }
+      catch (Throwable ex) {
+      }
+   }
+   
+   private void cleanUpDirs() {
+      delete(this.dirNameSent);
+      delete(this.dirNameDiscarded);
+      delete(this.dirName + File.separator + "ok.dat");
+      delete(this.dirName + File.separator + "tooBig.dat");
+      delete(this.dirName);
    }
    
    
@@ -201,7 +407,11 @@ public class TestFilePollerPlugin extends TestCase implements I_Callback {
       TestFilePollerPlugin test = new TestFilePollerPlugin(global);
 
       test.setUp();
-      test.testSystem();
+      test.testDirectories();
+      test.tearDown();
+
+      test.setUp();
+      test.testSimplePublish();
       test.tearDown();
 
    }
