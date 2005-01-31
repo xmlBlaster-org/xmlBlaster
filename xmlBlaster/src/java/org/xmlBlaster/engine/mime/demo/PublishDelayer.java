6f99b7df2a26be033c0724a229d1ffc739540cb9
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2005-01-31 00:02:19 +0000

    Delay published messages, useful for testing
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@13140 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/engine/mime/demo/PublishDelayer.java
@@ -0,0 +1,143 @@
+/*------------------------------------------------------------------------------
+Name:      PublishDelayer.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   Interface hiding the real callback protocol
+Version:   $Id: PublishDelayer.java 12936 2004-11-24 20:15:11Z ruff $
+Author:    xmlBlaster@marcelruff.info
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.engine.mime.demo;
+
+import org.jutils.log.LogChannel;
+import org.xmlBlaster.util.plugin.I_Plugin;
+import org.xmlBlaster.util.plugin.PluginInfo;
+import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.util.def.ErrorCode;
+import org.xmlBlaster.authentication.SubjectInfo;
+import org.xmlBlaster.util.def.Constants;
+import org.xmlBlaster.util.MsgUnit;
+import org.xmlBlaster.engine.mime.I_PublishFilter;
+import org.xmlBlaster.engine.Global;
+
+
+/**
+ * This demo plugin delays incoming (published) messages. 
+ * <p />
+ * Please register this plugin in xmlBlaster.properties:
+ * <pre>
+ * MimePublishPlugin[PublishDelayer][1.0]=org.xmlBlaster.engine.mime.demo.PublishDelayer,delayMillis=2000
+ * </pre>
+ * Plugins must implement the I_Plugin interface to be loadable by the PluginManager
+ * and must implement the I_PublishFilter interface to be usable as a filter.
+ * @author xmlBlaster@marcelruff.info
+ */
+public class PublishDelayer implements I_Plugin, I_PublishFilter
+{
+   private final String ME = "PublishDelayer";
+   private Global glob;
+   private LogChannel log;
+   /** How long to delay an incoming publish message */
+   private long delayMillis = 0;
+
+   /**
+    * This is called after instantiation of the plugin 
+    * @param glob The Global handle of this xmlBlaster server instance.
+    */
+   public void initialize(Global glob) {
+      this.glob = glob;
+      this.log = glob.getLog("mime");
+      log.info(ME, "Filter is initialized, we check all mime types if content is not too long");
+   }
+
+   /**
+    * This method is called by the PluginManager (enforced by I_Plugin). 
+    * @see org.xmlBlaster.util.plugin.I_Plugin#init(org.xmlBlaster.util.Global,org.xmlBlaster.util.plugin.PluginInfo)
+    */
+   public void init(org.xmlBlaster.util.Global glob, org.xmlBlaster.util.plugin.PluginInfo pluginInfo) {
+      this.log = glob.getLog("mime");
+
+      java.util.Properties props = pluginInfo.getParameters();
+
+      String lenStr = (String)props.get("delayMillis");
+      if (lenStr != null) {
+         delayMillis = (new Long(lenStr)).longValue();
+         log.info(ME, "Setting delayMillis=" + delayMillis + " as configured in xmlBlaster.properties");
+      }
+   }
+
+   /**
+    * Return plugin type for Plugin loader
+    * @return "PublishDelayer"
+    */
+   public String getType() {
+      return "PublishDelayer";
+   }
+
+   /**
+    * Return plugin version for Plugin loader
+    * @return "1.0"
+    */
+   public String getVersion() {
+      return "1.0";
+   }
+
+   /**
+    * Get a human readable name of this filter implementation
+    * @return "PublishDelayer"
+    */
+   public String getName() {
+      return "PublishDelayer";
+   }
+
+   /**
+    * Get the content MIME type for which this plugin applies
+    * @return "*" This plugin handles all mime types
+    */
+   public String[] getMimeTypes() {
+      String[] mimeTypes = { "*" };
+      return mimeTypes;
+   }
+
+   /**
+    * Get the content MIME version number for which this plugin applies
+    * @return "1.0" (this is the default version number)
+    */
+   public String[] getMimeExtended() {
+      String[] mimeExtended = { Constants.DEFAULT_CONTENT_MIME_EXTENDED }; // "1.0"
+      return mimeExtended;
+   }
+
+   /**
+    * Delay the message for the configured amount of time. 
+    * Please read the I_PublisheFilter.intercept() Javadoc.
+    * @return "" or "OK": The message is accepted<br />
+    * @see I_PublisheFilter#intercept(SubjectInfo, MsgUnit)
+    */
+   public String intercept(SubjectInfo publisher, MsgUnit msgUnit) throws XmlBlasterException {
+      if (msgUnit == null) {
+         Thread.currentThread().dumpStack();
+         throw new XmlBlasterException(glob, ErrorCode.INTERNAL_ILLEGALARGUMENT, ME, "Illegal argument in intercept() call - msgUnit is null");
+      }
+
+      if (msgUnit.getKeyData().isInternal())
+         return "";  // ignore internal messages
+
+      try {
+         if (delayMillis > 0) {
+            Thread.sleep(delayMillis);
+            log.info(ME, "Waking up after delaying message for " + delayMillis + " milli seconds");
+         }
+         return Constants.STATE_OK; // "OK" message is accepted
+      }
+      catch (Throwable e) {
+         String tmp = "Can't delay message because of an unexpected problem: " + e.toString();
+         log.error(ME, tmp);
+         throw new XmlBlasterException(glob, ErrorCode.INTERNAL_UNKNOWN, ME, tmp);
+      }
+   }
+
+   public void shutdown() {
+   }
+
+}
+
