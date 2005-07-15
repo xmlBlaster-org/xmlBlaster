45ca234428822227d061d3cc1b5ea8dec8117d21
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2005-07-15 01:57:27 +0000

    Added test case for recursive locks
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@13447 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/src/c++/TestThread.cpp
+++ xmlBlaster/testsuite/src/c++/TestThread.cpp
@@ -29,11 +29,13 @@ private:
    I_Log&  log_;
    bool    blocking_;
    bool    doRun_;
+   org::xmlBlaster::util::thread::Mutex *invocationMutex_;
 public:
    TestThread(Global& global, string name, bool blocking) 
       : ME(name), 
         global_(global),
-        log_(global.getLog("test")) 
+        log_(global.getLog("test")),
+        invocationMutex_(0)
    {
       blocking_ = blocking;
       doRun_    = true;
@@ -73,6 +75,27 @@ public:
       log_.info(ME, "testThread() end");
    }
 
+   void testRecursiveThread()
+   {
+      log_.info(ME, "testRecursiveThread() start");
+      int count = 0;
+      bool recursive = global_.getProperty().get("xmlBlaster/invocationMutex/recursive", true);
+      invocationMutex_ = new Mutex(recursive);
+      callRecursive(count);
+      delete invocationMutex_;
+      log_.info(ME, "testRecursiveThread() end");
+   }
+
+   void callRecursive(int count) {
+      Lock lock(*invocationMutex_);
+      count++;
+      log_.info(ME, "testRecursiveThread() count=" + lexical_cast<string>(count));
+      if (count > 4) {
+         return;
+      }
+      callRecursive(count);
+   }
+
    void setUp(int args=0, char *argc[]=0) {
       if (log_.trace()) {
          for (int i=0; i < args; i++) {
@@ -91,21 +114,26 @@ using namespace org::xmlBlaster::test;
 
 int main(int args, char *argc[]) 
 {
+   org::xmlBlaster::util::Object_Lifetime_Manager::init();
+
    Global& glob = Global::getInstance();
    glob.initialize(args, argc);
 
    TestThread *testObj = new TestThread(glob, "TestThread", false);
    testObj->setUp(args, argc);
+   testObj->testRecursiveThread();
    testObj->testThread();
    testObj->tearDown();
    delete testObj;
-
+   
    testObj = new TestThread(glob, "TestThread", true);
    testObj->setUp(args, argc);
    testObj->testThread();
    testObj->tearDown();
    delete testObj;
    testObj = NULL;
+   
+   org::xmlBlaster::util::Object_Lifetime_Manager::fini();
    return 0;
 }
 
