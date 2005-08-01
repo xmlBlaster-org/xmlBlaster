3b3ab88b5152ae0b16f561e866ae5b87122570eb
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2005-08-01 12:04:25 +0000

    Extended tests
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@13626 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/testsuite/src/java/org/xmlBlaster/test/classtest/ContextNodeTest.java
+++ xmlBlaster/testsuite/src/java/org/xmlBlaster/test/classtest/ContextNodeTest.java
@@ -1,8 +1,6 @@
 package org.xmlBlaster.test.classtest;
 
-import org.jutils.log.LogChannel;
 import org.xmlBlaster.util.Global;
-import org.xmlBlaster.util.XmlBlasterException;
 import org.xmlBlaster.util.context.ContextNode;
 
 import junit.framework.*;
@@ -29,6 +27,7 @@ public class ContextNodeTest extends TestCase {
          String[] urls = { "/xmlBlaster/node/heron", "/node/heron" };
          for (int i=0; i<urls.length; i++) {
             String url = urls[i];
+            System.out.println("Testing syntax parsing: " + url);
             ContextNode contextNode = ContextNode.valueOf(glob, url);
             assertEquals("", "heron", contextNode.getInstanceName());
             assertEquals("", ContextNode.CLUSTER_MARKER_TAG, contextNode.getClassName());
@@ -40,8 +39,50 @@ public class ContextNodeTest extends TestCase {
       catch (IllegalArgumentException e) {
          fail("testParse failed: " + e.toString());
       }
+
+      try {
+         String[] urls = { "/xmlBlaster/node/heron/client/joe/session/1", "/node/heron" };
+         for (int i=0; i<urls.length; i++) {
+            String url = urls[i];
+            System.out.println("Testing syntax parsing: " + url);
+            ContextNode contextNode = ContextNode.valueOf(glob, url);
+            ContextNode cluster = contextNode.getParent(ContextNode.CLUSTER_MARKER_TAG);
+            assertEquals("", "heron", cluster.getInstanceName());
+         }
+      }
+      catch (IllegalArgumentException e) {
+         fail("testParse failed: " + e.toString());
+      }
          
-         
+      try {
+         String[] urls = { "org.xmlBlaster:nodeClass=node,node=heron",
+                           "org.xmlBlaster:nodeClass=node,node=heron,connectionClass=connection,connection=jack,queueClass=queue,queue=connection-99" };
+         for (int i=0; i<urls.length; i++) {
+            String url = urls[i];
+            System.out.println("Testing JMX syntax parsing: " + url);
+            ContextNode contextNode = ContextNode.valueOf(glob, url);
+            ContextNode cluster = contextNode.getParent(ContextNode.CLUSTER_MARKER_TAG);
+            assertEquals("", "heron", cluster.getInstanceName());
+         }
+      }
+      catch (IllegalArgumentException e) {
+         fail("testParse failed: " + e.toString());
+      }
+
+      try {
+         String url = "org.xmlBlaster:nodeClass=node,node=\"avalon\",connectionClass=connection,connection=\"jack\",queueClass=queue,queue=\"connection-99\"";
+         ContextNode newParentNode = ContextNode.valueOf(glob, "org.xmlBlaster:nodeClass=node,node=heron");
+         System.out.println("Testing JMX syntax parsing: " + url);
+         ContextNode contextNode = ContextNode.valueOf(glob, url);
+         contextNode.changeParentName(newParentNode);
+         String newString = contextNode.getAbsoluteName(ContextNode.SCHEMA_JMX);
+         assertEquals("", "org.xmlBlaster:nodeClass=node,node=\"heron\",connectionClass=connection,connection=\"jack\",queueClass=queue,queue=\"connection-99\"",
+                          newString);
+      }
+      catch (IllegalArgumentException e) {
+         fail("testParse failed: " + e.toString());
+      }
+
       try {
          ContextNode contextNode = ContextNode.valueOf(glob, null);
          assertTrue("Expected null", contextNode==ContextNode.ROOT_NODE);
