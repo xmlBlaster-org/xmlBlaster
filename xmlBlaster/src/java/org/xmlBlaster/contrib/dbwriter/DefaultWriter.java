024e3c874ef6c78cbfc48ec00d601a04b74d517a
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2005-09-09 14:20:21 +0000

    added witer stuff
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@13775 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/src/java/org/xmlBlaster/contrib/dbwriter/DefaultWriter.java
@@ -0,0 +1,28 @@
+/*------------------------------------------------------------------------------
+Name:      Storer.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+
+package org.xmlBlaster.contrib.dbwriter;
+
+import org.xmlBlaster.contrib.I_Info;
+import org.xmlBlaster.contrib.dbwriter.info.DbUpdateInfo;
+
+public class DefaultWriter implements I_Writer {
+
+   public DefaultWriter() {
+      
+   }
+   
+   public void store(DbUpdateInfo info) throws Exception {
+      System.out.println("=========== NEW MESSAGE ============\n" + info.toXml("") + "\n");
+   }
+
+   public void init(I_Info info) throws Exception {
+   }
+
+   public void shutdown() throws Exception {
+   }
+
+}
