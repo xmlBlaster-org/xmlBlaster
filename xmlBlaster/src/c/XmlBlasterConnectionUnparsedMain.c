efbd0ec30500e6a0ee0ba6cc9f9a2d2bee1ca5ba
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2005-10-22 23:00:33 +0000

    Updated to compile
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@13853 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/c/XmlBlasterConnectionUnparsedMain.c
+++ xmlBlaster/src/c/XmlBlasterConnectionUnparsedMain.c
@@ -8,9 +8,9 @@ Comment:   Demo client for synchronous access.
            multi threading in this case.
 Author:    "Marcel Ruff" <xmlBlaster@marcelruff.info>
 Compile:
-  LINUX:   gcc -Wall -g -I. -o XmlBlasterConnectionUnparsedMain XmlBlasterConnectionUnparsedMain.c util/msgUtil.c util/Properties.c socket/XmlBlasterConnectionUnparsed.c socket/xmlBlasterSocket.c
-  WIN:     cl /MT /W3 /Wp64 -D_WINDOWS -I. XmlBlasterConnectionUnparsedMain.c util\msgUtil.c util\Properties.c socket\XmlBlasterConnectionUnparsed.c socket\xmlBlasterSocket.c ws2_32.lib
-  Solaris: cc -Xc -g -I. -o XmlBlasterConnectionUnparsedMain XmlBlasterConnectionUnparsedMain.c util/msgUtil.c util/Properties.c socket/XmlBlasterConnectionUnparsed.c socket/xmlBlasterSocket.c -lsocket -lnsl 
+  LINUX:   gcc -Wall -g -Wno-long-long -D_REENTRANT -I. -o XmlBlasterConnectionUnparsedMain XmlBlasterConnectionUnparsedMain.c util/helper.c util/msgUtil.c util/Properties.c socket/xmlBlasterSocket.c socket/xmlBlasterZlib.c socket/XmlBlasterConnectionUnparsed.c
+  WIN:     cl /MT /W3 /Wp64 -D_WINDOWS -I. XmlBlasterConnectionUnparsedMain.c util\msgUtil.c util\Properties.c socket\XmlBlasterConnectionUnparsed.c socket\xmlBlasterSocket.c socket\xmlBlasterZlib.c ws2_32.lib
+  Solaris: cc -Xc -g -I. -o XmlBlasterConnectionUnparsedMain XmlBlasterConnectionUnparsedMain.c util/msgUtil.c util/Properties.c socket/XmlBlasterConnectionUnparsed.c socket/xmlBlasterSocket.c socket/xmlBlasterZlib.c -lsocket -lnsl 
 Invoke:    XmlBlasterConnectionUnparsedMain -dispatch/callback/plugin/socket/hostname develop -dispatch/callback/plugin/socket/port 7607
 See:       http://www.xmlblaster.org/xmlBlaster/doc/requirements/protocol.socket.html
 Date:      05/2003
@@ -46,9 +46,9 @@ int main(int argc, char** argv)
       exit(EXIT_FAILURE);
    }
 
-   xb = getXmlBlasterConnectionUnparsed(argc, argv);
+   xb = getXmlBlasterConnectionUnparsed(argc, (const char* const*)argv);
 
-   if (xb->ping(xb, 0) == (char *)0) {
+   if (xb->ping(xb, 0, &xmlBlasterException) == (char *)0) {
       printf("[XmlBlasterConnectionUnparsedMain] Pinging a not connected server failed -> this is OK\n");
    }
    else {
@@ -121,13 +121,14 @@ int main(int argc, char** argv)
    }
 
    {  /* unSubscribe ... */
+   	QosArr *resp;
       const char *key = "<key oid='HelloWorld'/>";
       const char *qos = "<qos/>";
       printf("[XmlBlasterConnectionUnparsedMain] UnSubscribe message 'HelloWorld' ...\n");
-      response = xb->unSubscribe(xb, key, qos, &xmlBlasterException);
-      if (response) {
-         printf("[XmlBlasterConnectionUnparsedMain] Unsbscribe success, returned status is '%s'\n", response);
-         free(response);
+      resp = xb->unSubscribe(xb, key, qos, &xmlBlasterException);
+      if (resp) {
+         printf("[XmlBlasterConnectionUnparsedMain] Unsbscribe success\n");
+         free(resp);
       }
       else {
          printf("[XmlBlasterConnectionUnparsedMain] Caught exception in unSubscribe errorCode=%s, message=%s\n", xmlBlasterException.errorCode, xmlBlasterException.message);
@@ -173,17 +174,18 @@ int main(int argc, char** argv)
 
 
    {  /* erase ... */
+   	QosArr *resp;
       const char *key = "<key oid='HelloWorld'/>";
       const char *qos = "<qos/>";
       printf("[XmlBlasterConnectionUnparsedMain] Erasing message 'HelloWorld' ...\n");
-      response = xb->erase(xb, key, qos, &xmlBlasterException);
+      resp = xb->erase(xb, key, qos, &xmlBlasterException);
       if (*xmlBlasterException.errorCode != '\0') {
          printf("[XmlBlasterConnectionUnparsedMain] Caught exception in erase errorCode=%s, message=%s", xmlBlasterException.errorCode, xmlBlasterException.message);
          freeXmlBlasterConnectionUnparsed(xb);
          exit(EXIT_FAILURE);
       }
       printf("[XmlBlasterConnectionUnparsedMain] Erase success, returned status is '%s'\n", response);
-      free(response);
+      free(resp);
    }
 
    {  /* disconnect ... */
