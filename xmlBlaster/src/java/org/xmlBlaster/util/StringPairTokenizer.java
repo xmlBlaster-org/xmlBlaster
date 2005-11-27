ad35fc2971b53117bad201a9ad75aa33a15aefe6
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2005-11-27 17:04:55 +0000

    Added parseLine() method which repsects quotes when making tokens
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@14194 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/util/StringPairTokenizer.java
+++ xmlBlaster/src/java/org/xmlBlaster/util/StringPairTokenizer.java
@@ -6,9 +6,13 @@ Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 
 package org.xmlBlaster.util;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
 
 import org.xmlBlaster.util.qos.ClientProperty;
 
@@ -18,7 +22,121 @@ import org.xmlBlaster.util.qos.ClientProperty;
  * 
  * @author <a href="mailto:laghi@swissinfo.org">Michele Laghi</a>
  */
+
+/**
+ * Helper class to split Strings. 
+ */
 public class StringPairTokenizer {
+
+
+   public static String[] parseLine(String nextLine, char separator, char quotechar, boolean trimEmpty) {
+      if (nextLine == null || nextLine.length()==0) return new String[0];
+      String[] nextLines = new String[1];
+      nextLines[0] = nextLine;
+      return parseLine(nextLines, separator, quotechar, trimEmpty);
+   }
+   
+   /**
+    * Split string to tokens and respect quotes. 
+    * 
+    *<pre>  
+    * /node/heron/client/\"joe/the/great\"
+    * 
+    *  'node'
+    *  'heron'
+    *  'client'
+    *  'joe/the/great'
+    *</pre>  
+    *
+    * Thanks to http://opencsv.sourceforge.net/ (under Apache license)
+    */
+   public static String[] parseLine(String[] nextLines, char separator, char quotechar, boolean trimEmpty) {
+      List tokensOnThisLine = new ArrayList();
+      StringBuffer sb = new StringBuffer();
+      boolean inQuotes = false;
+      int jj=0;
+      String nextLine = nextLines[jj];
+      do {
+         if (sb.length() > 0) {
+            // continuing a quoted section, reappend newline
+            sb.append("\n");
+            jj++;
+            if (jj >= nextLines.length)
+               break;
+            nextLine = nextLines[jj];
+         }
+         for (int i = 0; i < nextLine.length(); i++) {
+            char c = nextLine.charAt(i);
+            if (c == quotechar) {
+               inQuotes = !inQuotes;
+            } else if (c == separator && !inQuotes) {
+               if (trimEmpty && sb.toString().trim().length() == 0) {
+                  ;
+               }
+               else {
+                  tokensOnThisLine.add(sb.toString());
+               }
+               sb = new StringBuffer(); // start work on next token
+            } else {
+               sb.append(c);
+            }
+         }
+      } while (inQuotes);
+      tokensOnThisLine.add(sb.toString());
+      return (String[]) tokensOnThisLine.toArray(new String[0]);
+   }
+   
+   /*
+    * Split a string (similar to StringTokenizer) but respect escape quotes. 
+    * <br />
+    *<pre>  
+    * String[] arr = split("now,DATE,\"dd,MM,yyyy\"", ",", "\"");
+    *  now
+    *  DATE
+    *  "dd,MM,yyyy"
+    *</pre>  
+    *<pre>  
+    *  ,node,\"h,,eron\",client,\"X,\,[],X,X\"
+    *    'node'
+    *    '"h,,eron"'
+    *    'client'
+    *    '"X,,[],X,X"'
+    *</pre>  
+    *  
+    *  Caution: I don't think this is very stable
+    *  We should change to something like http://opencsv.sourceforge.net/
+    *  
+    *  @see http://www.creativyst.com/Doc/Articles/CSV/CSV01.htm#FileFormat
+    *  @see http://www.jguru.com/faq/view.jsp?EID=809266
+   public static String[] split(String field, String separator, String quotechar) {
+      // This can't handle leading ',' see hack below
+      //String regex="\\s*\"(.*?)\"\\s*|(?<=^|,)[^,]*"
+      StringBuffer sb = new StringBuffer();
+      sb.append("\\s*"+quotechar+".*?"+quotechar+"\\s*");  //all matches enclosed in double quotes (\s*".*?"\s*) with leading/trailing white spaces
+      sb.append("|");        //or
+      sb.append("(?<=^|"+separator+")"); //beginning of line or comma (^|,) via zero-width positive lookbehind ((?<=)) followed by
+      sb.append("[^"+separator+"]*");    //any character except comma ([^,]) zero or more times (*)
+      String regex = sb.toString();
+      while (field.startsWith(separator))
+         field = field.substring(1); // hack
+      
+      //  DOES NOT WORK STABLE, for exampel '/node/heron/client/joe' fails
+      //  whereas '/node/heron/client/\"joe/xx\"' works
+      //String regex = "(?:[^\",]+?(?=,))|(\".+\")";
+      //String regex = "(?:[^\""+separator+"]+?(?="+separator+"))|(\".+\")";
+      //String regex = "(?:[^"+quotechar+separator+"]+?(?="+separator+"))|("+quotechar+".+"+quotechar+")";
+
+      Pattern pat = Pattern.compile(regex);
+      Matcher mat = pat.matcher(field);
+   
+      ArrayList list = new ArrayList();
+      while (mat.find()) {
+         String s = mat.group();
+         list.add(s);
+       }
+      return (String[])list.toArray(new String[list.size()]);
+   }
+    */
 
    /**
     * If a value is missing then a null object will be put into the map as value.
@@ -35,7 +153,7 @@ public class StringPairTokenizer {
       Map ret = new HashMap();
       StringTokenizer st = new StringTokenizer(rawString, outerToken);
       while(st.hasMoreTokens()) {
-         String tok = (String)st.nextToken().trim();
+         String tok = st.nextToken().trim();
          int pos = tok.indexOf(innerToken);
          if (pos < 0) {
             ret.put(tok.trim(), null);
@@ -74,5 +192,29 @@ public class StringPairTokenizer {
     */
    public static Map parseToStringStringPairs(String rawString, String outerToken, String innerToken) {
       return parseStringProperties(rawString, outerToken, innerToken, false);
+   }
+   
+   // java org.xmlBlaster.util.StringPairTokenizer
+   public static void main(String[] args) {
+      String test = (args.length > 0) ? args[0] : "/node/heron/client/\"joe/the/great\"";
+      String separator = (args.length > 1) ? args[1] : "/";
+      String quotechar = (args.length > 2) ? args[2] : "\"";
+      
+      System.out.println("From '" + test + "' we get");
+      /*
+      {
+         System.out.println("with split():");
+         String[] result = split(test, separator, quotechar);
+         for (int i=0; i<result.length; i++)
+            System.out.println("'" + result[i] + "'");
+      }
+      */
+
+      {
+         System.out.println("with parseLine():");
+         String[] result = parseLine(test, separator.charAt(0), quotechar.charAt(0), true);
+         for (int i=0; i<result.length; i++)
+            System.out.println("'" + result[i] + "'");
+      }
    }
 }
