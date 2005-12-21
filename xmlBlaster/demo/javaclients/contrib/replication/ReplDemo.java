e13380f8f38017c6363ef2c76cd6cde4bcb5b2ed
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2005-12-21 14:35:10 +0000

    added replication demo
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@14400 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ xmlBlaster/demo/javaclients/contrib/replication/ReplDemo.java
@@ -0,0 +1,31 @@
+/*------------------------------------------------------------------------------
+ Name:      ReplDemo.java
+ Project:   org.xmlBlasterProject:   xmlBlaster.org
+ Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+ ------------------------------------------------------------------------------*/
+package javaclients.contrib.replication;
+
+// import java.util.logging.Logger;
+
+import org.xmlBlaster.contrib.replication.ReplicationAgent;
+
+/**
+ * @author <a href="mailto:laghi@swissinfo.org">Michele Laghi</a>
+ */
+public class ReplDemo {
+   // private static Logger log = Logger.getLogger(ReplDemo.class.getName());
+
+   /**
+    */
+   public static void main(String[] args) {
+      ReplicationAgent.main(args);
+   }
+
+   /**
+    * Default ctor.
+    */
+   public ReplDemo() {
+      super();
+   }
+
+}
