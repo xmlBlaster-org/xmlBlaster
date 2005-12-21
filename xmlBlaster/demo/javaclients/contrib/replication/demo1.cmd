9675b52ef01067ced2526683b437a72466a27618
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2005-12-21 14:36:58 +0000

    removed error
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@14402 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/demo/javaclients/contrib/replication/demo1.cmd
+++
@@ -1,5 +0,0 @@
-create table demo_users (account varchar2(100), name varchar2(100), firstname varchar2(100), primary key(account))
-insert into demo_users values ('account1', 'Topo', 'Gigio')
-insert into demo_users values ('account2', 'Mister', 'Bean')
-select * from demo_users
-
