c7df995270a1e215367faa09e8f93322312d14d9
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2005-12-22 13:34:45 +0000

    fixed the error and split it in different methods
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@14418 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/client/protocol/http/common/BufferedInputStreamMicro.java
+++ xmlBlaster/src/java/org/xmlBlaster/client/protocol/http/common/BufferedInputStreamMicro.java
@@ -16,55 +16,81 @@ import java.io.IOException;
 public class BufferedInputStreamMicro implements I_ObjectStream {
 
    private InputStream in;
+   /** The buffer holding the rest of the data coming after a previous EOL */
    private String rest = "";
 
    public BufferedInputStreamMicro(InputStream inStream) throws IOException {
       this.in = inStream;
    }
 
-   synchronized public String readLine() throws IOException {
+   /**
+    * 
+    * @return null if the stream is closed (finished to read data), a String otherwise. It will always wait until there is data available.
+    * @throws IOException
+    */
+   private final String readNewData() throws IOException {
+      int first = this.in.read();
+      if (first < 0)
+         return null;
+      int nmax = this.in.available() + 1;
+      if (nmax < 2) {
+         byte[] tmp = new byte[1];
+         tmp[0] = (byte)first;
+         return new String(tmp);
+      }
+
+      byte[] buffer = new byte[nmax];
+      buffer[0] = (byte)first;
+      int read = 1;
+      while(read < nmax) {
+         read += this.in.read(buffer, read, nmax-read);
+      }
+      return new String(buffer);
+   }
+   
+   /**
+    * It reads until at least one EOL or an EOF is reached.  
+    * @return
+    * @throws IOException
+    */
+   private final String readNewDataUntilEolOrEof() throws IOException {
+      while (true) {
+         String newData = readNewData();
+         if (newData == null)
+            return null;
+         this.rest += newData;
+         String tmp = getLineFromBuffer();
+         if (tmp != null)
+            return tmp;
+      }
+   }
+
+   private final String getLineFromBuffer() {
       int pos = this.rest.indexOf("\n");
       if (pos > -1) {
-         String ret = this.rest.substring(0, pos);
-         if (pos == 0)
+         String firstPart = this.rest.substring(0, pos);
+         int tmp = this.rest.length() - pos - "\n".length();
+         if (tmp > 0)
             this.rest = this.rest.substring(pos+1);
          else
             this.rest = "";
-         return ret;
+         return firstPart;
       }
-      StringBuffer ret = new StringBuffer(this.rest);
-
-      this.rest = "";
-      boolean doRun = true;
-      while (doRun) {
-         int first = this.in.read();
-         if (first < 0) 
-            return null;
-         int nmax = this.in.available();
-         if (nmax < 0)
-            return null;
-         byte[] buffer = new byte[nmax+1];
-         buffer[0] = (byte)first;
-         if (nmax > 0) {
-            int length = this.in.read(buffer, 1, nmax);
-            if (length < nmax) doRun = false;
-         }
-         String tmp = new String(buffer);
-         pos = tmp.indexOf("\n");
-         if (pos < 0) 
-            ret.append(tmp);
-         else {
-            if (pos > 0) {
-               this.rest = tmp.substring(pos+1);
-               tmp = tmp.substring(0, pos);
-               ret.append(tmp);
-            }
-            else 
-               this.rest = "";
-            break;
-         }
-      }
-      return ret.toString();      
+      return null;
    }
    
+   synchronized public String readLine() throws IOException {
+      String ret = getLineFromBuffer();
+      if (ret != null)
+         return ret;
+
+      ret = readNewDataUntilEolOrEof();
+      if (ret != null)
+         return ret;
+      
+      if (this.rest.length() > 0)
+         return this.rest;
+      else
+         return null;
+   }
 }
