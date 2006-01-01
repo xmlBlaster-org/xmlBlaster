a2504472e227247ddcc244175974a951b8c6a1b2
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2006-01-01 14:55:48 +0000

    BUGFIX: Fixed unregister() call; added boolean return for register() and unregister()
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@14473 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- xmlBlaster/src/java/org/xmlBlaster/util/log/LogNotifierDeviceFactory.java
+++ xmlBlaster/src/java/org/xmlBlaster/util/log/LogNotifierDeviceFactory.java
@@ -5,18 +5,11 @@ Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.util.log;
 
-import org.xmlBlaster.protocol.I_CallbackDriver;
-import org.xmlBlaster.util.MsgUnitRaw;
 import org.xmlBlaster.util.XmlBlasterException;
-import org.xmlBlaster.util.def.ErrorCode;
-import org.xmlBlaster.util.log.I_LogDeviceFactory;
 import org.xmlBlaster.util.plugin.PluginInfo;
-import org.xmlBlaster.util.qos.address.CallbackAddress;
-import org.jutils.init.Property;
 import org.jutils.log.LogableDevice;
 import org.jutils.log.LogChannel;
 
-import java.util.Properties;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -25,8 +18,6 @@ import java.util.Set;
  * @see <a href="http://www.xmlBlaster.org/xmlBlaster/doc/requirements/util.log.plugin.html">The util.log.plugin requirement</a>
  */
 public class LogNotifierDeviceFactory implements I_LogDeviceFactory {
-   private static final String ME = "LogNotifierDeviceFactory";
-   private org.xmlBlaster.util.Global glob;
    private PluginInfo info;
    private LogNotifierDevice logNotifierDevice; // This factory has exactly one device instance
    
@@ -41,8 +32,7 @@ public class LogNotifierDeviceFactory implements I_LogDeviceFactory {
     */
    public void init(org.xmlBlaster.util.Global glob, PluginInfo pluginInfo) throws XmlBlasterException {
       this.info = pluginInfo;
-      this.glob = glob;
-      Properties pluginProps = pluginInfo.getParameters();
+      //Properties pluginProps = pluginInfo.getParameters();
       this.logNotifierDevice = new LogNotifierDevice(this);
    }
    
@@ -60,7 +50,7 @@ public class LogNotifierDeviceFactory implements I_LogDeviceFactory {
    }
 
    /**
-      * Register a listener (currently max one)
+      * Register a listener. 
       * @param level Which levels you are interested in. Currently ignored, only ERROR are forwarded.
       * @param logNotification The interface to send the logging
       */
@@ -69,7 +59,7 @@ public class LogNotifierDeviceFactory implements I_LogDeviceFactory {
    }
 
    public void unregister(int level, LogableDevice logNotification) {
-      this.logNotifierDevice.register(level, logNotification);
+      this.logNotifierDevice.unregister(level, logNotification);
    }
 
    public  LogableDevice getLogDevice(LogChannel channel) {
@@ -83,7 +73,7 @@ public class LogNotifierDeviceFactory implements I_LogDeviceFactory {
     */
    class LogNotifierDevice implements LogableDevice {
       
-      private LogNotifierDeviceFactory factory;
+      //private LogNotifierDeviceFactory factory;
       private Set errorListenerSet = new HashSet();
       private LogableDevice[] errorCache;
       private Set warnListenerSet = new HashSet();
@@ -93,39 +83,46 @@ public class LogNotifierDeviceFactory implements I_LogDeviceFactory {
        * Constructor. 
        */
       LogNotifierDevice(LogNotifierDeviceFactory factory) {
-         this.factory = factory;
+         //this.factory = factory;
       }
 
       /**
-       * Register a listener (currently max one). 
+       * Register a listener. 
        * This listener may NOT use logging himself to avoid recursion
+       * If this set already contains the specified element, the call leaves this set unchanged and returns false
        * @param level Which levels you are interested in. Pass LogConstants.LOG_ERROR|LogConstants.LOG_WARN to register for all available levels.
        * @param logNotification The interface to send the logging
+       * @return true if the given logNotification is added
        */
-      public synchronized void register(int level, LogableDevice logNotification) {
+      public synchronized boolean register(int level, LogableDevice logNotification) {
+         boolean ret = false;
          if ((LogChannel.LOG_WARN & level) != 0) {
-            this.warnListenerSet.add(logNotification);
+            ret = this.warnListenerSet.add(logNotification);
             this.warnCache = null;
          }
          if ((LogChannel.LOG_ERROR & level) != 0) {
-            this.errorListenerSet.add(logNotification);
+            ret = this.errorListenerSet.add(logNotification);
             this.errorCache = null;
          }
+         return ret;
       }
 
       /**
        * Remove the listener. 
        * @param level Which levels you want to remove. Pass LogConstants.LOG_ERROR|LogConstants.LOG_WARN to unregister for all available levels.
+       * @return true if the set contained the specified element.
        */
-      public synchronized void unregister(int level, LogableDevice logNotification) {
+      public synchronized boolean unregister(int level, LogableDevice logNotification) {
+         boolean ret = false;
          if ((LogChannel.LOG_WARN & level) != 0) {
-            this.warnListenerSet.remove(logNotification);
+            ret = this.warnListenerSet.remove(logNotification);
             this.warnCache = null;
          }
          if ((LogChannel.LOG_ERROR & level) != 0) {
-            this.errorListenerSet.remove(logNotification);
+            ret = this.errorListenerSet.remove(logNotification);
             this.errorCache = null;
          }
+         return ret;
       }
 
       /**
