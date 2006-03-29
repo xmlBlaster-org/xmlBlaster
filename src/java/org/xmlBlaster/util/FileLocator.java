6e933343cd05da67ba20f3c2e556cbcabd73dd66
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2006-03-29 20:45:51 +0000

    deleteDir
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@14974 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/util/FileLocator.java
+++ src/java/org/xmlBlaster/util/FileLocator.java
@@ -489,6 +489,27 @@ public class FileLocator
          return body;
       return body.substring(sep + 1);
    }
+   
+   /**
+    * Deletes all files and subdirectories under dir.
+    * Returns true if all deletions were successful.
+    * If a deletion fails, the method stops attempting to delete and returns false.
+    * Thanks to "The Java Developers Almanac 1.4"
+    */
+   public static boolean deleteDir(File dir) {
+       if (dir.isDirectory()) {
+           String[] children = dir.list();
+           for (int i=0; i<children.length; i++) {
+               boolean success = deleteDir(new File(dir, children[i]));
+               if (!success) {
+                   return false;
+               }
+           }
+       }
+   
+       // The directory is now empty so delete it
+       return dir.delete();
+   }   
 
    /**
    * Convert some file extensions to MIME types.
