5878d2f0d4b3ceb9e6ee2c4f000aace9725df22c
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2006-07-03 18:52:20 +0000

    Added JMS approach for request/reply as a comment
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@15225 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/client/XmlBlasterAccess.java
+++ src/java/org/xmlBlaster/client/XmlBlasterAccess.java
@@ -1770,5 +1770,87 @@ public /*final*/ class XmlBlasterAccess extends AbstractCallbackExtended
    public String getBuildJavaVersion() {
       return glob.getBuildJavaVersion();
    }
+   
+   
+   /* JMS approach for request/reply
+   public class TopicRequestor {
+
+      TopicSession    session;    // The topic session the topic belongs to.
+      Topic           topic;      // The topic to perform the request/reply on.  
+      TemporaryTopic  tempTopic;
+      TopicPublisher  publisher;
+      TopicSubscriber subscriber;
+
+
+       Constructor for the <CODE>TopicRequestor</CODE> class.
+        * 
+        * <P>This implementation assumes the session parameter to be non-transacted,
+        * with a delivery mode of either <CODE>AUTO_ACKNOWLEDGE</CODE> or 
+        * <CODE>DUPS_OK_ACKNOWLEDGE</CODE>.
+        *
+        * @param session the <CODE>TopicSession</CODE> the topic belongs to
+        * @param topic the topic to perform the request/reply call on
+        *
+        * @exception JMSException if the JMS provider fails to create the
+        *                         <CODE>TopicRequestor</CODE> due to some internal
+        *                         error.
+        * @exception InvalidDestinationException if an invalid topic is specified.
+       
+
+      public 
+      TopicRequestor(TopicSession session, Topic topic) throws JMSException {
+   this.session = session;
+   this.topic   = topic;
+          tempTopic    = session.createTemporaryTopic();
+          publisher    = session.createPublisher(topic);
+          subscriber   = session.createSubscriber(tempTopic);
+      }
+
+
+       Sends a request and waits for a reply. The temporary topic is used for
+        * the <CODE>JMSReplyTo</CODE> destination; the first reply is returned, 
+        * and any following replies are discarded.
+        *
+        * @param message the message to send
+        *  
+        * @return the reply message
+        *  
+        * @exception JMSException if the JMS provider fails to complete the
+        *                         request due to some internal error.
+      
+
+      public Message
+      request(Message message) throws JMSException {
+   message.setJMSReplyTo(tempTopic);
+          publisher.publish(message);
+   return(subscriber.receive());
+      }
+
+
+      * Closes the <CODE>TopicRequestor</CODE> and its session.
+        *
+        * <P>Since a provider may allocate some resources on behalf of a 
+        * <CODE>TopicRequestor</CODE> outside the Java virtual machine, clients 
+        * should close them when they 
+        * are not needed. Relying on garbage collection to eventually reclaim 
+        * these resources may not be timely enough.
+        *
+        * <P>Note that this method closes the <CODE>TopicSession</CODE> object 
+        * passed to the <CODE>TopicRequestor</CODE> constructor.
+        *  
+        * @exception JMSException if the JMS provider fails to close the
+        *                         <CODE>TopicRequestor</CODE> due to some internal
+        *                         error.
+      
+
+      public void
+      close() throws JMSException {
+
+   // publisher and consumer created by constructor are implicitly closed.
+   session.close();
+   tempTopic.delete();
+      }
+  }
+  */
 }
 
