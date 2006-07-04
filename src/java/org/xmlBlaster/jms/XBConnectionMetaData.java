ac85b94b84422253f6324bcb5848d5c869b21ac2
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2006-07-04 13:05:29 +0000

    changed intern/extern properties (intern have a prefix)
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@15230 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/jms/XBConnectionMetaData.java
+++ src/java/org/xmlBlaster/jms/XBConnectionMetaData.java
@@ -23,7 +23,7 @@ import org.xmlBlaster.util.def.ErrorCode;
  * @author <a href="mailto:laghi@swissinfo.org">Michele Laghi</a>
  * 
  */
-public class XBConnectionMetaData implements ConnectionMetaData, Serializable, Cloneable {
+public class XBConnectionMetaData implements ConnectionMetaData, Serializable, Cloneable, XBPropertyNames {
 
    /**
     * 
@@ -35,49 +35,72 @@ public class XBConnectionMetaData implements ConnectionMetaData, Serializable, C
    final static int MINOR_VERSION = 1;
 
    /** The receiver Timestamp which is a property specific to XmlBlaster */
-   public final static String JMSX_RCV_TIMESTAMP = "JMSXRcvTimestamp"; 
+   public final static String JMSX_RCV_TIMESTAMP_EXTERN = "JMSXRcvTimestamp"; 
    
    /** The maximum size for the data to be transmitted in one single message */
-   public final static String JMSX_MAX_CHUNK_SIZE = "JMSXMaxChunkSize"; 
+   public final static String JMSX_MAX_CHUNK_SIZE_EXTERN = "JMSXMaxChunkSize"; 
    
-   public final static String JMSX_PTP_DESTINATION = "JMSXPtPDest";
+   public final static String JMSX_PTP_DESTINATION_EXTERN = "JMSXPtPDest";
    
    /**
     * When using chunked messages this belongs to a specified group.
     */
-   public final static String JMSX_GROUP_ID = "JMSXGroupID";
+   public final static String JMSX_GROUP_ID_EXTERN = "JMSXGroupID";
    /**
     * The internal sequence number within a group. If this is set, then JMSXGroupID must
     * be set too.
     */
-   public final static String JMSX_GROUP_SEQ = "JMSXGroupSeq";
+   public final static String JMSX_GROUP_SEQ_EXTERN = "JMSXGroupSeq";
    
    /** 
     * If this exists it is always set to boolean 'true' and tells this is the EOF
     * message of a sequence (a group)
     */
-   public final static String JMSX_GROUP_EOF = "JMSXGroupEof";
+   public final static String JMSX_GROUP_EOF_EXTERN = "JMSXGroupEof";
    
    /** If set, an exception occured in this chunk. It contains the 
     * exception. It is used to perform clean up in case of exceptions.
     */
-   public final static String JMSX_GROUP_EX = "JMSXGroupEx";
-   
-
+   public final static String JMSX_GROUP_EX_EXTERN = "JMSXGroupEx";
    
    
+   /** The receiver Timestamp which is a property specific to XmlBlaster */
+   public final static String JMSX_RCV_TIMESTAMP = JMS_PREFIX + JMSX_RCV_TIMESTAMP_EXTERN; 
    
+   /** The maximum size for the data to be transmitted in one single message */
+   public final static String JMSX_MAX_CHUNK_SIZE = JMS_PREFIX + JMSX_MAX_CHUNK_SIZE_EXTERN;
    
+   public final static String JMSX_PTP_DESTINATION = JMS_PREFIX + JMSX_PTP_DESTINATION_EXTERN;
    
+   /**
+    * When using chunked messages this belongs to a specified group.
+    */
+   public final static String JMSX_GROUP_ID = JMS_PREFIX + JMSX_GROUP_ID_EXTERN;
+   /**
+    * The internal sequence number within a group. If this is set, then JMSXGroupID must
+    * be set too.
+    */
+   public final static String JMSX_GROUP_SEQ = JMS_PREFIX + JMSX_GROUP_SEQ_EXTERN;
+   
+   /** 
+    * If this exists it is always set to boolean 'true' and tells this is the EOF
+    * message of a sequence (a group)
+    */
+   public final static String JMSX_GROUP_EOF = JMS_PREFIX + JMSX_GROUP_EOF_EXTERN;
+   
+   /** If set, an exception occured in this chunk. It contains the 
+    * exception. It is used to perform clean up in case of exceptions.
+    */
+   public final static String JMSX_GROUP_EX = JMS_PREFIX + JMSX_GROUP_EX_EXTERN;
    
    /** These properties are specific to our implementation and must start with the prefix JMSX */
    private final static String[] propNames = new String[] {
-      JMSX_GROUP_ID, 
-      JMSX_GROUP_SEQ,
-      JMSX_GROUP_EOF,
-      JMSX_GROUP_EX,
-      JMSX_RCV_TIMESTAMP,
-      JMSX_MAX_CHUNK_SIZE,
+      JMSX_GROUP_ID_EXTERN, 
+      JMSX_GROUP_SEQ_EXTERN,
+      JMSX_GROUP_EOF_EXTERN,
+      JMSX_GROUP_EX_EXTERN,
+      JMSX_RCV_TIMESTAMP_EXTERN,
+      JMSX_MAX_CHUNK_SIZE_EXTERN,
       "JMSX"
    };   
    
