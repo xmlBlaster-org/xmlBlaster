79da9d9e70cc6d8bcbf96b0cded9c255164eec8f
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2006-09-10 10:59:27 +0000

    javadoc only
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@15494 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/client/I_Callback.java
+++ src/java/org/xmlBlaster/client/I_Callback.java
@@ -12,14 +12,13 @@ import org.xmlBlaster.client.key.UpdateKey;
 import org.xmlBlaster.client.qos.UpdateQos;
 
 /**
- * This is a little helper class wraps the different, protocol specific
- * update() methods, and delivers the client a nicer update() method.
+ * Interface to receive asynchronously send callback messages from xmlBlaster. 
  * <p>
- * You may use this, if you don't want to program with the rawer CORBA BlasterCallback.update()
- * or RMI or XMLRPC.
+ * Please implement this to receive your messages.
  *
  * @version $Revision: 1.13 $
  * @author <a href="mailto:xmlBlaster@marcelruff.info">Marcel Ruff</a>.
+ * @see <a href="http://www.xmlBlaster.org/xmlBlaster/demo/HelloWorld3.java.html">HelloWorld8.java</a>
  */
 public interface I_Callback
 {
@@ -27,15 +26,17 @@ public interface I_Callback
     * This is the callback method invoked from I_XmlBlasterAccess
     * informing the client in an asynchronous mode about a new message.
     * <p />
-    * So you should implement in your client the I_Callback interface -
+    * So you should implement in your client code the I_Callback interface -
     * suppling the update() method where you can do with the message whatever you want.
     * <p />
-    * The raw driver specific update() method (e.g. CORBA-BlasterCallback.update())
+    * The raw protocol driver specific update() method (e.g. CORBA-BlasterCallback.update())
     * is unpacked and for each arrived message this update is called.
     *
-    * @param cbSessionId The session ID specified by the client which registered the callback
-    * @param updateKey   The arrived key
-    * @param content     The arrived message content
+    * @param cbSessionId The session ID specified by the client which registered the callback.
+    *                    You can specify a cbSessionId during connection (with ConnectQos)
+    *                    and this is bounced back here so you can authenticate the message.
+    * @param updateKey   The arrived key containing the topic name
+    * @param content     The arrived message content. This is your payload.
     * @param qos         Quality of Service of the MsgUnit
     * @see org.xmlBlaster.client.I_XmlBlasterAccess
     */
