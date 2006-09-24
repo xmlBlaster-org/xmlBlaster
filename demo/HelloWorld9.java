dd0f106c0fa52194e14b3729c59cda6783055651
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2006-09-24 09:49:38 +0000

    Simplified
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@15532 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- demo/HelloWorld9.java
+++ demo/HelloWorld9.java
@@ -5,10 +5,6 @@ import org.xmlBlaster.util.MsgUnit;
 import org.xmlBlaster.util.XmlBlasterException;
 import org.xmlBlaster.client.qos.ConnectQos;
 import org.xmlBlaster.client.qos.DisconnectQos;
-import org.xmlBlaster.client.I_Callback;
-import org.xmlBlaster.client.key.UpdateKey;
-import org.xmlBlaster.client.qos.UpdateQos;
-import org.xmlBlaster.client.qos.UpdateReturnQos;
 import org.xmlBlaster.client.I_XmlBlasterAccess;
 
 
@@ -17,8 +13,9 @@ import org.xmlBlaster.client.I_XmlBlasterAccess;
  * <p />
  * Please invoke a publisher first to test it.
  * <pre>
- * Invoke: 
- *   java javaclients.HelloWorldPublish -oid hello -numPublish 10
+ * Invoke:
+ *   java org.xmlBlaster.Main 
+ *   java javaclients.HelloWorldPublish -numPublish 10
  *   java HelloWorld9
  * </pre>
  * 
