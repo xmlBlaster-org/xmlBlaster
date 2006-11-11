590dd9935db00312c117215aaf5853f3edede4db
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2006-11-11 23:16:57 +0000

    added interceptor for streaming publishes
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@15667 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/util/I_ReplaceContent.java
+++ src/java/org/xmlBlaster/util/I_ReplaceContent.java
@@ -5,6 +5,8 @@ Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.util;
 
+import java.util.Map;
+
 /**
  * Callback interface to replace a byte[], useful for example when publishing in 
  * stream mode where you need to modify each chunk. 
@@ -12,9 +14,13 @@ package org.xmlBlaster.util;
 public interface I_ReplaceContent {
    /**
     * Replaces or modifies the oldcontent. The returned instance can be the same as 
-    * passed as the oldContent or it can be a new instance. 
+    * passed as the oldContent or it can be a new instance.
+    * @param oldContent
+    * @param clientProperties the map can either be used as attributes or it can be modified in
+    * this method.
+    *  
     * @return value
     */
-   byte[] get(byte[] oldContent);
+   byte[] replace(byte[] oldContent, Map clientProperties);
 }
 
