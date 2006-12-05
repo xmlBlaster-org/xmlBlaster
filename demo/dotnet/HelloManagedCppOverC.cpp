e2f05d27a764d8230c29c70671ec9d9dbb2ecf21
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2006-12-05 17:06:10 +0000

    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@15762 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ demo/dotnet/HelloManagedCppOverC.cpp
@@ -0,0 +1,69 @@
+/*----------------------------------------------------------------------------
+Name:      HelloManagedCppOverC.cpp
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Author:    "Marcel Ruff" <xmlBlaster@marcelruff.info>
+Note:      Example xmlBlaster access with a managed .net C++ client
+-----------------------------------------------------------------------------*/
+#include "stdafx.h"
+#using <xmlBlasterClientCmanaged.dll>
+
+using namespace System;
+using namespace System::Collections;
+using namespace org::xmlBlaster::client;
+
+/**
+ * @example  HelloManagedCppOverC --help
+ * @example  HelloManagedCppOverC.exe -dispatch/connection/plugin/socket/hostname myserverIP
+ */
+int main(array<System::String ^> ^args)
+{
+   String^ callbackSessionId = L"secret";
+   String^ connectQos = String::Format(
+         L"<qos>\n"+
+         " <securityService type='htpasswd' version='1.0'>\n"+
+         "  <![CDATA[\n"+
+         "   <user>fritz</user>\n"+
+         "   <passwd>secret</passwd>\n"+
+         "  ]]>\n"+
+         " </securityService>\n"+
+         " <queue relating='callback' maxEntries='50000' maxEntriesCache='10000'>\n"+
+         "   <callback type='SOCKET' sessionId='{0}'>\n"+
+         "   </callback>\n"+
+         " </queue>\n"+
+         "</qos>", callbackSessionId);
+   try {
+      Console::WriteLine(L"C++ xmlBlaster client using managed wrapper around C client DLL");
+      Hashtable^ props = gcnew Hashtable();
+      //props->Add(L"dispatch/connection/plugin/socket/hostname", L"localhost");
+      bool help = false;
+      IEnumerator^ myEnum = args->GetEnumerator();
+      while ( myEnum->MoveNext() ) {
+         String^ key = safe_cast<String^>(myEnum->Current);
+         if (key->Equals(L"--help")||key->Equals(L"-help"))
+            help = true;
+         if (myEnum->MoveNext()) {
+            String^ value = safe_cast<String^>(myEnum->Current);
+            if (key->StartsWith("-"))
+               key = key->Substring(1);
+            props->Add(key, value);
+         }
+      }
+      XmlBlasterAccessM xb(props);
+      if (help) {
+         Console::WriteLine("Usage:\nXmlBlaster C SOCKET client {0}\n{1}\n",
+                  xb.getVersion(), xb.getUsage());
+         return 1;
+      }
+
+      xb.connect(connectQos);
+      Console::WriteLine("Connected to xmlBlaster");
+
+      xb.disconnect(L"");
+      Console::WriteLine("Disconnected from xmlBlaster");
+      return 0;
+   }
+   catch (XmlBlasterExceptionM^ e) {
+      Console::WriteLine("Caught an exception:" + e->getMessage());
+   }
+}
