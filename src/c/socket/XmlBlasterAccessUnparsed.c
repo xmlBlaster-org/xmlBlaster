0c9619b6c27b1a5a85c2f8393c6bfd9825f973a3
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2006-12-15 13:58:30 +0000

    Now initializing connectionP and callbackP to 0
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@15805 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/c/socket/XmlBlasterAccessUnparsed.c
+++ src/c/socket/XmlBlasterAccessUnparsed.c
@@ -83,6 +83,8 @@ Dll_Export XmlBlasterAccessUnparsed *getXmlBlasterAccessUnparsed(int argc, const
    }
    xa->isInitialized = false;
    xa->isShutdown = false;
+   xa->connectionP = 0;
+   xa->callbackP = 0;
    xa->userObject = 0; /* A client can use this pointer to point to any client specific information */
    xa->userFp = 0;
    xa->connect = xmlBlasterConnect;
