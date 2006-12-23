25717c1556f60e7b32f51ab7b7b6877c65d771c7
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2006-12-23 15:32:08 +0000

    Code cleanup
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@15877 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/protocol/http/simple/PullServlet.java
+++ src/java/org/xmlBlaster/protocol/http/simple/PullServlet.java
@@ -2,6 +2,9 @@ package org.xmlBlaster.protocol.http.simple;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Enumeration;
+import java.util.Properties;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -23,9 +26,9 @@ import org.xmlBlaster.util.StringPairTokenizer;
 import org.xmlBlaster.util.XmlBlasterException;
 
 /**
- * This servlet supports requests from a browser, it queries the topic
- * "vehicle.location" which needs to contain GPS coordinates (published for
- * example by a PDA).
+ * This servlet supports requests from a browser, it queries the topic given by
+ * "gpsTopicId" configuration which needs to contain GPS coordinates (published
+ * for example by a PDA).
  * 
  * <p>
  * Use xmlBlaster/demo/http/gpsmap.html to display the coordinates in a map.
@@ -36,6 +39,8 @@ import org.xmlBlaster.util.XmlBlasterException;
 public class PullServlet extends HttpServlet {
 	private static final long serialVersionUID = -8094289301696678030L;
 
+	private Properties props = new Properties();
+
 	private I_XmlBlasterAccess xmlBlasterAccess;
 
 	private String loginName = "pullServlet";
@@ -44,80 +49,86 @@ public class PullServlet extends HttpServlet {
 
 	public void init(ServletConfig conf) throws ServletException {
 		super.init(conf);
+		// Add the web.xml parameters to our environment settings:
+		Enumeration enumer = conf.getInitParameterNames();
+		while (enumer.hasMoreElements()) {
+			String name = (String) enumer.nextElement();
+			if (name != null && name.length() > 0)
+				props.setProperty(name, conf.getInitParameter(name));
+		}
 	}
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
 		String host = req.getRemoteAddr();
-		String ME = "PullServlet.doGet("+host+"): ";
+		String ME = "PullServlet.doGet(" + host + "): ";
 		boolean forceLoad = req.getParameter("forceLoad") != null;
-		System.out.println(ME+"forceLoad=" + forceLoad);
+		if (forceLoad)
+			System.out.println(ME + "forceLoad=" + forceLoad);
+		boolean newBrowser = false;
+		if (req.getSession(false) == null) {
+			req.getSession(true);
+			newBrowser = true;
+			System.out.println(ME + "new browser arrived");
+		}
+
+		// set header field first
+		res.setContentType("text/plain");
+		PrintWriter out = res.getWriter();
 
 		try {
-			I_XmlBlasterAccess xmlBlaster = getXmlBlaster(req);
-
-			GetKey gk = new GetKey(xmlBlaster.getGlobal(), "vehicle.location");
-			GetQos gq = new GetQos(xmlBlaster.getGlobal());
-			MsgUnit[] msgs = xmlBlaster.getCached(gk, gq);
-			String data = "";
-			if (msgs.length > 0) {
-				System.out
-						.println(ME+ msgs.length + " msgs found ...");
-				byte[] content = msgs[msgs.length - 1].getContent();
-				data = new String(content, "UTF-8");
-			} else {
-				System.out.println(ME+"no data found ...");
-				PrintWriter out = res.getWriter();
-				out.close();
+			Position pos = getCurrentPosition(req);
+			if (pos == null) {
+				// System.out.println(ME+"no data found ...");
+				if (newBrowser) {
+					out.write(getStartupUrl());
+					System.out.println(ME + getStartupUrl());
+				}
 				return;
 			}
 
-			// Expecting a PDA sending me Gran'mas position
-			// with simplified NMEA:
-			// >Gran'mas place,047°46'55.86",N,009°10'00.95",E,0<
-			String[] tokens = StringPairTokenizer.toArray(data, ",");
-			String label = tokens[0];
-			String latitude = tokens[1];
-			// String latitudeL = tokens[2]; // N S
-			String longitude = tokens[3];
-			// String longitudeL = tokens[4]; // E W
-
-			// System.out.println("label=" + label + " latitude=" + latitude + "
-			// longitude=" + longitude);
-			String url = "http://maps.google.com/maps?q=";
-			url += latitude + ",+" + longitude;
-			url += "+(" + label + ")&iwloc=A&hl=en";
-
-			HttpSession session = req.getSession(true);
-			if (!forceLoad) {
-				String lastUrl = (String) session.getAttribute("lastUrl");
-				if (lastUrl != null && lastUrl.equals(url)) {
-					PrintWriter out = res.getWriter();
-					out.close();
-					System.out.println(ME+"Ignoring identical");
-					return;
-				}
+			if (forceLoad || positionHasChanged(req, pos)) {
+				String url = getUrl(pos);
+				out.print(url);
+				System.out.println(ME + "Send new url=" + url);
 			}
-			session.setAttribute("lastUrl", url);
+			// System.out.println(ME+"Ignoring identical");
 
-			String encoded = url;
-			// String encoded = Global.encode(url, null);
-
-			// set header field first
-			res.setContentType("text/plain");
-
-			// then get the writer and write the response data
-			PrintWriter out = res.getWriter();
-			out.print(encoded);
-			out.close();
-			System.out.println(ME+"Send new url=" + encoded);
 		} catch (XmlBlasterException e) {
-			System.err.println(e.toString());
-			// set header field first
-			res.setContentType("text/html");
-			PrintWriter out = res.getWriter();
+			System.err.println("newBrowser=" + newBrowser + " forceLoad="
+					+ forceLoad + ": " + e.toString());
+			if (newBrowser || forceLoad)
+				out.write(getStartupUrl());
+		} finally {
 			out.close();
+
 		}
+	}
+
+	private Position getCurrentPosition(HttpServletRequest req)
+			throws XmlBlasterException {
+		I_XmlBlasterAccess xmlBlaster = getXmlBlaster(req);
+		String topicId = props.getProperty("gpsTopicId", "vehicle.location");
+		GetKey gk = new GetKey(xmlBlaster.getGlobal(), topicId);
+		GetQos gq = new GetQos(xmlBlaster.getGlobal());
+		MsgUnit[] msgs = xmlBlaster.getCached(gk, gq);
+		if (msgs.length > 0) {
+			// System.out.println(ME+ msgs.length + " msgs found ...");
+			byte[] content = msgs[msgs.length - 1].getContent();
+			return parsePosition(content);
+		}
+		return null;
+	}
+
+	// Only call once for each request
+	private boolean positionHasChanged(HttpServletRequest req, Position pos) {
+		HttpSession session = req.getSession(true);
+		Position lastPos = (Position) session.getAttribute("lastPosition");
+		if (lastPos != null && lastPos.equals(pos)) {
+			return false;
+		}
+		session.setAttribute("lastPosition", pos);
+		return true;
 	}
 
 	private synchronized I_XmlBlasterAccess getXmlBlaster(HttpServletRequest req)
@@ -125,21 +136,97 @@ public class PullServlet extends HttpServlet {
 		// HttpSession session = req.getSession(true);
 		// I_XmlBlasterAccess xmlBlaster = (I_XmlBlasterAccess) session
 		// .getAttribute("xmlBlaster");
-		if (xmlBlasterAccess == null) {
-			Global glob = new Global();
-			xmlBlasterAccess = glob.getXmlBlasterAccess();
-			ConnectQos qos = new ConnectQos(glob, loginName + "/1", passwd);
-			xmlBlasterAccess.connect(qos, new I_Callback() {
+		if (this.xmlBlasterAccess == null) {
+			if (!props.contains("dispatch/callback/retries"))
+				props.put("dispatch/callback/retries", "-1");
+			String sessionName = props.getProperty("sessionName", loginName
+					+ "/1");
+			String password = props.getProperty("password", passwd);
+			Global glob = new Global(props);
+			this.xmlBlasterAccess = glob.getXmlBlasterAccess();
+			ConnectQos qos = new ConnectQos(glob, sessionName, password);
+			this.xmlBlasterAccess.connect(qos, new I_Callback() {
 				public String update(String cbSessionId, UpdateKey updateKey,
 						byte[] content, UpdateQos updateQos) {
-					System.out.println("PullServler.update(): Ignoring update " + updateKey.getOid());
+					System.out.println("PullServler.update(): Ignoring update "
+							+ updateKey.getOid());
 					return "";
 				}
 			});
-			xmlBlasterAccess.createSynchronousCache(20);
+			this.xmlBlasterAccess.createSynchronousCache(20);
 			// session.setAttribute("xmlBlaster", xmlBlaster);
 		}
-		return xmlBlasterAccess;
+		return this.xmlBlasterAccess;
+	}
+
+	public String getUrl(Position pos) {
+		StringBuffer url = new StringBuffer();
+		url.append("http://maps.google.com/maps?q=");
+		url.append(pos.latitude).append(",+").append(pos.longitude);
+		url.append("+(").append(pos.label).append(")&iwloc=A&hl=en");
+		// String encoded = Global.encode(url.toString(), null);
+		return url.toString();
+	}
+
+	public String getStartupUrl() {
+		// http://maps.google.com/maps?q=047%C2%B042'20.41%22,+009%C2%B012'06.80%22+(Gran'ma%20on%20tour)&iwloc=A&hl=en
+		// "047°42'20.41\"", "009°12'06.80\""
+		Position pos = new Position();
+		pos.label = "Gran'ma is at home";
+		pos.latitude = "047%C2%B042'20.41%22";
+		pos.longitude = "009%C2%B012'06.80%22";
+		return getUrl(pos);
+	}
+
+	// Expecting a PDA sending me Gran'mas position
+	// with simplified NMEA:
+	// >Gran'mas place,047°46'55.86",N,009°10'00.95",E,0<
+	public Position parsePosition(byte[] data_) {
+
+		String data;
+		try {
+			data = new String(data_, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException(e.toString());
+		}
+		Position pos = new Position();
+		String[] tokens = StringPairTokenizer.toArray(data, ",");
+		if (tokens.length < 6)
+			throw new IllegalArgumentException("Illegal data: " + data);
+		pos.label = tokens[0];
+		pos.latitude = tokens[1];
+		pos.latitudeL = tokens[2];
+		pos.longitude = tokens[3];
+		pos.longitudeL = tokens[4];
+		pos.heading = tokens[5];
+		return pos;
+	}
+
+	class Position {
+		String label = ""; // "Gran'ma on tour
+
+		String latitude = "";
+
+		String latitudeL = ""; // N S
+
+		String longitude = "";
+
+		String longitudeL = ""; // E W
+
+		String heading = ""; // direction degree
+
+		public String toString() {
+			return "label=" + label + " latitude=" + latitude + " longitude="
+					+ longitude;
+		}
+
+		public boolean equals(Position other) {
+			if (label.equals(other.label) && latitude.equals(other.latitude)
+					&& longitude.equals(other.longitude))
+				return true;
+			return false;
+		}
 	}
 
 	public String getServletInfo() {
