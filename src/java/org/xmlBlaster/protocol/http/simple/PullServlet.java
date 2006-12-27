9e3ab7295624d39ea598c4c5cfc91ccfe97bb7b1
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2006-12-27 17:40:22 +0000

    Added simple chat for Granny (such SMS costs below one Cent)
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@15902 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/protocol/http/simple/PullServlet.java
+++ src/java/org/xmlBlaster/protocol/http/simple/PullServlet.java
@@ -16,14 +16,18 @@ import javax.servlet.http.HttpSession;
 import org.xmlBlaster.client.I_Callback;
 import org.xmlBlaster.client.I_XmlBlasterAccess;
 import org.xmlBlaster.client.key.GetKey;
+import org.xmlBlaster.client.key.PublishKey;
 import org.xmlBlaster.client.key.UpdateKey;
 import org.xmlBlaster.client.qos.ConnectQos;
 import org.xmlBlaster.client.qos.GetQos;
+import org.xmlBlaster.client.qos.PublishQos;
 import org.xmlBlaster.client.qos.UpdateQos;
 import org.xmlBlaster.util.Global;
 import org.xmlBlaster.util.MsgUnit;
 import org.xmlBlaster.util.StringPairTokenizer;
 import org.xmlBlaster.util.XmlBlasterException;
+
+//Debug: ~/apache-tomcat-5.5.15/bin/catalina.sh jpda start
 
 /**
  * This servlet supports requests from a browser, it queries the topic given by
@@ -58,31 +62,48 @@ public class PullServlet extends HttpServlet {
 		}
 	}
 
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		doGet(req, res);
+	}
+
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
 		String host = req.getRemoteAddr();
 		String ME = "PullServlet.doGet(" + host + "): ";
+		// log(ME + "Entering doGet ...");
 		boolean forceLoad = req.getParameter("forceLoad") != null;
 		if (forceLoad)
-			System.out.println(ME + "forceLoad=" + forceLoad);
+			log(ME + "forceLoad=" + forceLoad);
 		boolean newBrowser = false;
 		if (req.getSession(false) == null) {
 			req.getSession(true);
 			newBrowser = true;
-			System.out.println(ME + "new browser arrived");
+			log(ME + "new browser arrived, charset="
+					+ req.getCharacterEncoding());
 		}
 
 		// set header field first
-		res.setContentType("text/plain");
+		res.setContentType("text/plain; charset=UTF-8");
 		PrintWriter out = res.getWriter();
 
 		try {
+			if (isAction(ME, req, res))
+				return;
+
+			String feedback = getFeedback(req);
+			if (feedbackHasChanged(req, feedback)) {
+				out.print(feedback);
+				log(ME + feedback);
+				return;
+			}
+
 			Position pos = getCurrentPosition(req);
 			if (pos == null) {
-				// System.out.println(ME+"no data found ...");
+				// log(ME+"no data found ...");
 				if (newBrowser || forceLoad) {
-					out.write(getStartupUrl());
-					System.out.println(ME + getStartupUrl());
+					out.print(getStartupUrl());
+					log(ME + getStartupUrl());
 				}
 				return;
 			}
@@ -90,19 +111,59 @@ public class PullServlet extends HttpServlet {
 			if (forceLoad || positionHasChanged(req, pos)) {
 				String url = getUrl(pos);
 				out.print(url);
-				System.out.println(ME + "Send new url=" + url);
+				log(ME + "Send new url=" + url);
 			}
-			// System.out.println(ME+"Ignoring identical");
+			// log(ME+"Ignoring identical");
 
 		} catch (XmlBlasterException e) {
-			System.err.println("newBrowser=" + newBrowser + " forceLoad="
-					+ forceLoad + ": " + e.toString());
+			log("newBrowser=" + newBrowser + " forceLoad=" + forceLoad + ": "
+					+ e.toString());
 			if (newBrowser || forceLoad)
 				out.write(getStartupUrl());
 		} finally {
 			out.close();
 
 		}
+	}
+
+	private boolean isAction(String ME, HttpServletRequest req,
+			HttpServletResponse res) throws ServletException, IOException,
+			XmlBlasterException {
+		String actionType = (String) req.getParameter("action");
+		String smsText = (String) req.getParameter("cheapSmsSend");
+		if (actionType != null && "publish".equals(actionType)) {
+			log(ME + "Got action = '" + actionType + "' -> '" + smsText + "'");
+			I_XmlBlasterAccess xmlBlaster = getXmlBlaster(req);
+			String topicId = props.getProperty("smsBroadcastTopicId",
+					"vehicle.sms.broadcast");
+			PublishKey pk = new PublishKey(xmlBlaster.getGlobal(), topicId,
+					"text/xml", "1.0");
+			PublishQos pq = new PublishQos(xmlBlaster.getGlobal());
+			pq.addClientProperty("sender", req.getSession().getId()); // TODO:
+																		// nice
+																		// name
+			MsgUnit msgUnit = new MsgUnit(pk, smsText.getBytes("UTF-8"), pq);
+			xmlBlaster.publish(msgUnit);
+			return true;
+		}
+		return false;
+	}
+
+	private String getFeedback(HttpServletRequest req)
+			throws XmlBlasterException, UnsupportedEncodingException {
+		I_XmlBlasterAccess xmlBlaster = getXmlBlaster(req);
+		String topicId = props.getProperty("smsFeedbackTopicId",
+				"vehicle.sms.feedback");
+		GetKey gk = new GetKey(xmlBlaster.getGlobal(), topicId);
+		GetQos gq = new GetQos(xmlBlaster.getGlobal());
+		MsgUnit[] msgs = xmlBlaster.getCached(gk, gq);
+		if (msgs.length > 0) {
+			// log(ME+ msgs.length + " msgs found ...");
+			byte[] content = msgs[msgs.length - 1].getContent();
+			String sms = new String(content, "UTF-8");
+			return sms.trim();
+		}
+		return null;
 	}
 
 	private Position getCurrentPosition(HttpServletRequest req)
@@ -113,11 +174,24 @@ public class PullServlet extends HttpServlet {
 		GetQos gq = new GetQos(xmlBlaster.getGlobal());
 		MsgUnit[] msgs = xmlBlaster.getCached(gk, gq);
 		if (msgs.length > 0) {
-			// System.out.println(ME+ msgs.length + " msgs found ...");
+			// log(ME+ msgs.length + " msgs found ...");
 			byte[] content = msgs[msgs.length - 1].getContent();
 			return parsePosition(content);
 		}
 		return null;
+	}
+
+	// Only call once for each request
+	private boolean feedbackHasChanged(HttpServletRequest req, String sms) {
+		if (sms == null || sms.length() < 1)
+			return false;
+		HttpSession session = req.getSession(true);
+		String lastFeedback = (String) session.getAttribute("lastFeedback");
+		if (lastFeedback != null && lastFeedback.equals(sms)) {
+			return false;
+		}
+		session.setAttribute("lastFeedback", sms);
+		return true;
 	}
 
 	// Only call once for each request
@@ -148,7 +222,7 @@ public class PullServlet extends HttpServlet {
 			this.xmlBlasterAccess.connect(qos, new I_Callback() {
 				public String update(String cbSessionId, UpdateKey updateKey,
 						byte[] content, UpdateQos updateQos) {
-					System.out.println("PullServler.update(): Ignoring update "
+					log("PullServler.update(): Ignoring update "
 							+ updateKey.getOid());
 					return "";
 				}
@@ -238,11 +312,16 @@ public class PullServlet extends HttpServlet {
 			try {
 				xmlBlasterAccess.disconnect(null);
 			} catch (Throwable e) {
-				System.out.println("Ignoring disconnect problem "
-						+ e.toString());
+				log("Ignoring disconnect problem " + e.toString());
 			} finally {
 				xmlBlasterAccess = null;
 			}
 		}
+	}
+
+	public void log(String text) {
+		// System.err.println("ERR"+text);
+		// System.out.println("OUT"+text);
+		super.log(text);
 	}
 }
