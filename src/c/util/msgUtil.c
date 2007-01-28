cd7596ef6b47e007c011b18b6bf26d6c61d067fd
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2007-01-28 14:57:09 +0000

    Release 1.5
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@15961 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/c/util/msgUtil.c
+++ src/c/util/msgUtil.c
@@ -55,7 +55,7 @@ Dll_Export const char *getXmlBlasterVersion(void)
    if (strstr(p1, "@") == 0) { /* Verify that version replacement worked fine */
        return p1;
    }
-   return "1.4";
+   return "1.5";
 }
 
 /**
