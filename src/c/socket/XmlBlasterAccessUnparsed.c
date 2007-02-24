xmlBlaster-org/xmlBlaster

commit 89b44b8d9d9ec4c98b498c5ad07402d4fab429e9

Disconnect is oneway, return value fixed
git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@15972 00fa6119-6a0f-0410-b6da-8b96a2b62773

src/c/socket/XmlBlasterConnectionUnparsed.c
@@ -661,7 +661,7 @@ static bool sendData(XmlBlasterConnectionUnparsed *xb,
    free(rawMsg);
    rawMsg = 0;
 
-   if (msgType==MSG_TYPE_RESPONSE || msgType==MSG_TYPE_EXCEPTION || !strcmp(XMLBLASTER_PUBLISH_ONEWAY, methodName))
+   if (msgType==MSG_TYPE_RESPONSE || msgType==MSG_TYPE_EXCEPTION || !strcmp(XMLBLASTER_PUBLISH_ONEWAY, methodName) || !strcmp(XMLBLASTER_DISCONNECT, methodName))
       return true; /* Responses and exceptions are oneway */
 
    if (responseSocketDataHolder) { /* if not oneway read the response message */

commit 05bf1692ebd8c8a3e54e0b7a876f99c7310641bb

The disconnect is not oneway (see protocol.socket specification) this bug was introduced in r15972 2007-02-07. Increased max. waiting on pending update threads to 10 sec (during shutdown).
git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@16042 00fa6119-6a0f-0410-b6da-8b96a2b62773

src/c/socket/XmlBlasterAccessUnparsed.c
@@ -218,7 +218,7 @@ Dll_Export void freeXmlBlasterAccessUnparsed(XmlBlasterAccessUnparsed *xa)
 
    {  /* Wait for any pending update() dispatcher threads to die */
       int i;
-      int num = 100;
+      int num = 1000;
       int interval = 10;
       for (i=0; i<num; i++) {
          if ((int)xa->threadCounter < 1)
@@ -226,6 +226,10 @@ Dll_Export void freeXmlBlasterAccessUnparsed(XmlBlasterAccessUnparsed *xa)
          sleepMillis(interval);
          if (xa->logLevel>=XMLBLASTER_LOG_TRACE) xa->log(xa->logUserP, xa->logLevel, XMLBLASTER_LOG_TRACE, __FILE__,
              "freeXmlBlasterAccessUnparsed(): Sleeping %d millis for update thread to join. %d/%d", interval, i, num);
+      }
+      if (i >= num) {
+         if (xa->logLevel>=XMLBLASTER_LOG_ERROR) xa->log(xa->logUserP, xa->logLevel, XMLBLASTER_LOG_ERROR, __FILE__,
+             "freeXmlBlasterAccessUnparsed(): There are active callback threads in user code which didn't return after sleeping for %ld millis, we continue now to shutdown ...", (long)interval*num);
       }
    }
 
@@ -396,7 +400,8 @@ static MsgRequestInfo *preSendEvent(MsgRequestInfo *msgRequestInfoP, XmlBlasterE
    bool retVal;
    XmlBlasterAccessUnparsed * const xa = (XmlBlasterAccessUnparsed *)msgRequestInfoP->xa;
 
-   if (!strcmp(XMLBLASTER_PUBLISH_ONEWAY, msgRequestInfoP->methodName))
+   /* if (!strcmp(XMLBLASTER_PUBLISH_ONEWAY, msgRequestInfoP->methodName)) */
+   if (xbl_isOneway(MSG_TYPE_INVOKE, msgRequestInfoP->methodName))
       return msgRequestInfoP;
 
    /* ======== Initialize threading ====== */

src/c/socket/XmlBlasterConnectionUnparsed.c
@@ -661,7 +661,7 @@ static bool sendData(XmlBlasterConnectionUnparsed *xb,
    free(rawMsg);
    rawMsg = 0;
 
-   if (msgType==MSG_TYPE_RESPONSE || msgType==MSG_TYPE_EXCEPTION || !strcmp(XMLBLASTER_PUBLISH_ONEWAY, methodName) || !strcmp(XMLBLASTER_DISCONNECT, methodName))
+   if (xbl_isOneway(msgType, methodName))
       return true; /* Responses and exceptions are oneway */
 
    if (responseSocketDataHolder) { /* if not oneway read the response message */
@@ -820,7 +820,7 @@ static bool xmlBlasterDisconnect(XmlBlasterConnectionUnparsed *xb, const char * 
 {
    SocketDataHolder responseSocketDataHolder;
 
-   if (checkArgs(xb, "disconnect", true, exception) == false ) return 0;
+   if (checkArgs(xb, XMLBLASTER_DISCONNECT, true, exception) == false ) return 0;
 
    if (sendData(xb, XMLBLASTER_DISCONNECT, MSG_TYPE_INVOKE, (const char *)qos, 
                 (qos == (const char *)0) ? 0 : strlen(qos),

src/c/socket/xmlBlasterSocket.c
@@ -92,6 +92,24 @@ ssize_t readn(const int fd, char *ptr, const size_t nbytes, XmlBlasterNumReadFun
 }
 
 /**
+ * Check if the given arguments mark a oneway message. 
+ * @param msgType The message type like MSG_TYPE_INVOKE
+ * @param methodName The name of the invoked message like "publish", can be null
+ * @return if true it is treated as oneway
+ */
+bool xbl_isOneway(XMLBLASTER_MSG_TYPE msgType, const char *const methodName) {
+   if (msgType==MSG_TYPE_RESPONSE || msgType==MSG_TYPE_EXCEPTION)
+      return true; /* Responses and exceptions are oneway */
+   if (methodName == 0)
+      return false;
+   if (   
+       !strcmp(XMLBLASTER_PUBLISH_ONEWAY, methodName)
+       /*|| !strcmp(XMLBLASTER_DISCONNECT, methodName)*/ /* according to protocol.socket it returns an ACK: shall we remove it? */
+       ) return true;
+   return false;
+}
+
+/**
  * Creates a raw blob to push over a socket as described in protocol.socket
  * @param msgUnit The message which we need to send
  * @param debug Pass true for debugging output to stdout

src/c/socket/xmlBlasterSocket.h
@@ -94,6 +94,8 @@ extern void closeSocket(int fd);
 extern ssize_t writen(const int fd, const char *ptr, const size_t nbytes);
 extern ssize_t readn(const int fd, char *ptr, const size_t nbytes, XmlBlasterNumReadFunc fpNumRead, void *userP);
 
+extern bool xbl_isOneway(XMLBLASTER_MSG_TYPE msgType, const char *const methodName);
+
 /**
  * Creates a raw blob to push over a socket as described in the protocol.socket requirement. 
  * @param msgType invoke, response or exception