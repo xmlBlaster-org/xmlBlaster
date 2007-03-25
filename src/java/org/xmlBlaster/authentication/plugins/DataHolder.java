f6c86de049f64ad16432af0563e4670b4d69388d
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2007-03-25 13:52:26 +0000

    toString()
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@16085 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/authentication/plugins/DataHolder.java
+++ src/java/org/xmlBlaster/authentication/plugins/DataHolder.java
@@ -4,13 +4,13 @@ import org.xmlBlaster.util.MsgUnit;
 import org.xmlBlaster.util.def.MethodName;
 
 /**
- * Container to transport information to the isAuthorized() method. 
+ * Container to transport information to the isAuthorized() method.
  * @author xmlblast@marcelruff.info
  */
 public class DataHolder {
    private final MethodName action;
    private final MsgUnit msgUnit;
-   
+
    /**
     * @param action May not be null
     * @param msgUnit May not be null
@@ -28,7 +28,7 @@ public class DataHolder {
    public MethodName getAction() {
       return this.action;
    }
-   
+
    /**
     * @return Returns the msgUnit, is never null
     */
@@ -43,12 +43,16 @@ public class DataHolder {
    public String getKeyOid() {
       return this.msgUnit.getKeyOid() == null ? "" : this.msgUnit.getKeyOid();
    }
-   
+
    /**
     * The key url notation like "exact:hello", "xpath://key", "domain:sport"
     * @return Never null, but can be empty
     */
    public String getKeyUrl() {
       return this.msgUnit.getKeyData() == null ? "" : this.msgUnit.getKeyData().getUrl();
+   }
+
+   public String toString() {
+      return this.action + " " + this.msgUnit.getKeyOid();
    }
 }
