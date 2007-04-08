7da25b6fe746f589838b4b8352640a91a7b628e0
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2007-04-08 16:18:02 +0000

    Forcing XML dump to be UTF-8
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@16103 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/csharp/util/Serialization.cs
+++ src/csharp/util/Serialization.cs
@@ -2,20 +2,63 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml;
 using System.Xml.Serialization;
 
-namespace org.xmlBlaster.client.util {
+namespace org.xmlBlaster.util {
    public static class Serialization {
+
+      /// <summary>
+      /// Parse UTF-8 xml string
+      /// </summary>
+      /// <typeparam name="type"></typeparam>
+      /// <param name="data"></param>
+      /// <returns></returns>
       public static type Deserialize<type>(string data) {
+         XmlSerializer xs = new XmlSerializer(typeof(type));
+         MemoryStream memoryStream = new MemoryStream(StringToUTF8ByteArray(data));
+         XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);
+         return (type)xs.Deserialize(memoryStream);
+         /*
          XmlSerializer ser = new XmlSerializer(typeof(type), "");
          return (type)ser.Deserialize(new StringReader(data));
+         */
       }
 
+      /// <summary>
+      /// Returns the UTF-8 XML representation
+      /// </summary>
+      /// <typeparam name="type"></typeparam>
+      /// <param name="data"></param>
+      /// <returns></returns>
       public static string Serialize<type>(type data) {
+         MemoryStream memoryStream = new MemoryStream();
+         XmlSerializer xs = new XmlSerializer(data.GetType(), "");
+         XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);
+         xs.Serialize(xmlTextWriter, data);
+         memoryStream = (MemoryStream)xmlTextWriter.BaseStream;
+         string xml = UTF8ByteArrayToString(memoryStream.ToArray());
+         return xml;
+         /*
          StringWriter wr = new StringWriter();
          XmlSerializer ser = new XmlSerializer(data.GetType(), "");
          ser.Serialize(wr, data);
          return wr.ToString();
+          */
       }
+
+      public static String UTF8ByteArrayToString(Byte[] characters) {
+
+         UTF8Encoding encoding = new UTF8Encoding();
+         String constructedString = encoding.GetString(characters);
+         return (constructedString);
+      }
+
+      public static Byte[] StringToUTF8ByteArray(String pXmlString) {
+         UTF8Encoding encoding = new UTF8Encoding();
+         Byte[] byteArray = encoding.GetBytes(pXmlString);
+         return byteArray;
+      }
+
    }
 }
