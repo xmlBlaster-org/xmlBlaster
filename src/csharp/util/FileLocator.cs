26129bf8af657747cb49da70930314aa4d55fae3
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2007-04-20 16:06:54 +0000

    Added XML serializer and directory reader to object!
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@16137 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/csharp/util/FileLocator.cs
+++ src/csharp/util/FileLocator.cs
@@ -8,6 +8,115 @@ namespace org.xmlBlaster.util
 {
    public class FileLocator
    {
+      public static byte[] getFileAsBytes(string fileName) {
+         BinaryReader sReader = null;
+         byte[] contents = null;
+         try {
+            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            //sReader = new BinaryReader(fileStream);
+            contents = ReadFully(fileStream, 0);
+         }
+         finally {
+            if (sReader != null) {
+               sReader.Close();
+            }
+         }
+         return contents;
+      }
+
+      public class FileContainer {
+         string directoy;
+         string fileName;
+
+         public FileContainer(string d, string f) {
+            this.directoy = d;
+            this.fileName = f;
+         }
+
+         public string FileName {
+            get { return fileName; }
+            set { fileName = value; }
+         }
+
+         byte[] xmlContent;
+
+         public byte[] GetXmlContent() {
+            if (xmlContent == null) {
+               string name = Path.Combine(this.directoy, this.fileName);
+               if (name != null)
+                  xmlContent = org.xmlBlaster.util.FileLocator.getFileAsBytes(name);
+            }
+            if (xmlContent == null) return new byte[0];
+            return xmlContent;
+         }
+
+         public string GetXmlContentUtf8Str() {
+            return org.xmlBlaster.util.Serialization.UTF8ByteArrayToString(GetXmlContent());
+         }
+
+         public type Deserialize<type>() {
+            return org.xmlBlaster.util.Serialization.Deserialize<type>(GetXmlContent());
+         }
+      }
+
+      public static FileContainer[] GetFiles(string directory, string fileName) {
+         string[] fileNames = Directory.GetFiles(directory, fileName);
+         FileContainer[] fileContainers = new FileContainer[fileNames.Length];
+         for (int i = 0; i < fileNames.Length; i++) {
+            fileContainers[i] = new FileContainer(directory, fileNames[i]);
+         }
+         return fileContainers;
+      }
+
+      public static type[] GetObjectsFromFiles<type>(string directory, string fileName) {
+         FileContainer[] ff = GetFiles(directory, fileName);
+         type[] types = new type[ff.Length];
+         for (int i=0; i<types.Length; i++) {
+            types[i] = ff[i].Deserialize<type>();
+         }
+         return types;
+      }
+
+      public static byte[] ReadFully(Stream stream, int initialLength) {
+         // If we've been passed an unhelpful initial length, just
+         // use 32K.
+         if (initialLength < 1) {
+            initialLength = 32768;
+         }
+
+         byte[] buffer = new byte[initialLength];
+         int read = 0;
+
+         int chunk;
+         while ((chunk = stream.Read(buffer, read, buffer.Length - read)) > 0) {
+            read += chunk;
+
+            // If we've reached the end of our buffer, check to see if there's
+            // any more information
+            if (read == buffer.Length) {
+               int nextByte = stream.ReadByte();
+
+               // End of stream? If so, we're done
+               if (nextByte == -1) {
+                  return buffer;
+               }
+
+               // Nope. Resize the buffer, put in the byte we've just
+               // read, and continue
+               byte[] newBuffer = new byte[buffer.Length * 2];
+               Array.Copy(buffer, newBuffer, buffer.Length);
+               newBuffer[read] = (byte)nextByte;
+               buffer = newBuffer;
+               read++;
+            }
+         }
+         // Buffer is now too big. Shrink it.
+         byte[] ret = new byte[read];
+         Array.Copy(buffer, ret, read);
+         return ret;
+      }
+
+
       public static string getFileAsString(string fileName)
       {
          StreamReader sReader = null;
@@ -98,6 +207,7 @@ namespace org.xmlBlaster.util
       /// <param name="text">The data to write</param>
       public static bool writeBinaryFile(string fileName, byte[] text)
       {
+         //FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
          FileStream fs = File.Create(fileName);
          if (fs == null) return false;
          try
