1eba6e2dcea23c7d48cb960c08808b4d22e33cdd
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2007-05-12 08:57:44 +0000

    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@16166 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- testsuite/src/csharp/Start.cs
+++ testsuite/src/csharp/Start.cs
@@ -11,17 +11,44 @@ gacutil -l
 csc /unsafe /r:nunit.framework /t:library -debug+ -out:xmlBlasterClient.dll *.cs ..\..\..\src\csharp\XmlBlasterAccess.cs ..\..\..\src\csharp\Key.cs ..\..\..\src\csharp\Qos.cs
 */
 using System;
+using System.IO;
 using org.xmlBlaster.client;
+using org.xmlBlaster.util;
 using org.xmlBlaster.contrib.service;
 
-public class Start
-{
-   static void Main(string[] argv)
-   {
+public class Start {
+   static void Main(string[] argv) {
       Console.WriteLine("Startup");
+
+      TestSer testSer = new TestSer("William", 54);
+      //testSer.name = "Jack";
+      //testSer.age = 23;
+
+      string path = @"C:\tmp\in";
+      string name = Path.Combine(path, "2007-04-20T12-10-44.xml");
+      FileLocator.writeBinaryFile(name, Serialization.Serialize<TestSer>(testSer));
+
+      TestSer[] arr = FileLocator.GetObjectsFromFiles<TestSer>(path, "*.xml");
+      foreach (TestSer f in arr) {
+         Console.WriteLine("File: -> " + f.ToString());
+      }
       //testNmea();
       //testXmlBlaster();
       testService();
+   }
+
+   public class TestSer {
+      public TestSer() {
+      }
+      public TestSer(string n, int a) {
+         this.name = n;
+         this.age = a;
+      }
+      public string name;
+      public int age;
+      public override string ToString() {
+         return "name=" + this.name + " age=" + this.age;
+      }
    }
 
    private static void testService() {
@@ -34,8 +61,7 @@ public class Start
       //t.CheckXmlBase64Parsing();
    }
 
-   private static void testNmea()
-   {
+   private static void testNmea() {
       NmeaTest n = new NmeaTest();
       n.CheckSerialInput();
    }
