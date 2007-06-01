28a0459ea1f876414e9a6d8a253c3e3ca43ef38d
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2007-06-01 21:06:26 +0000

    Use for readonly DbWatcher mode
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@16217 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ src/java/org/xmlBlaster/contrib/replication/impl/SpecificDbReadonly.java
@@ -0,0 +1,81 @@
+package org.xmlBlaster.contrib.replication.impl;
+
+import java.sql.Connection;
+
+import org.xmlBlaster.contrib.I_Info;
+import org.xmlBlaster.contrib.dbwriter.info.SqlColumn;
+import org.xmlBlaster.contrib.dbwriter.info.SqlDescription;
+import org.xmlBlaster.contrib.replication.TableToWatchInfo;
+
+/**
+ * @author laghi, ruff
+ */
+public class SpecificDbReadonly extends SpecificDefault {
+
+   public synchronized void init(I_Info info) throws Exception {
+      super.isDbWriteable = false;
+      super.init(info);
+   }
+   
+   public void checkTriggerConsistency(boolean doFix) {
+   }
+   
+   public void bootstrap(Connection conn, boolean doWarn, boolean force) {
+   }
+
+   public final void cleanup(Connection conn, boolean doWarn) {
+   }
+
+   protected boolean sequenceExists(Connection conn, String sequenceName)
+         throws Exception {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   protected boolean triggerExists(Connection conn, String triggerName)
+         throws Exception {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   public void addSchemaToWatch(Connection conn, String catalog, String schema)
+         throws Exception {
+      // TODO Auto-generated method stub
+
+   }
+
+   public String createTableTrigger(SqlDescription infoDescription,
+         TableToWatchInfo tableToWatch) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public StringBuffer getColumnStatement(SqlColumn colInfoDescription) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public String getContentFromGuid(String guid, String catalog, String schema,
+         String table) throws Exception {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public boolean removeTrigger(String triggerName, String tableName,
+         boolean isSchemaTrigger) {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   public boolean triggerExists(Connection conn, TableToWatchInfo tableToWatch)
+         throws Exception {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   public int wipeoutSchema(String catalog, String schema,
+         boolean[] objectsToWipeout) throws Exception {
+      // TODO Auto-generated method stub
+      return 0;
+   }
+}
