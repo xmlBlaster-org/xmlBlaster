dffe342cf494517ac208bad271681b5ee2116f86
laghi <laghi@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2007-07-18 14:13:17 +0000

    refactoring from I_Queue to I_Storage stuff
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@16356 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ src/java/org/xmlBlaster/util/queue/StorageSizeListenerHelper.java
@@ -0,0 +1,85 @@
+/*------------------------------------------------------------------------------
+Name:      StorageSizeListenerHelper.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+
+package org.xmlBlaster.util.queue;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * StorageSizeListenerHelper
+ * @author <a href="mailto:michele@laghi.eu">Michele Laghi</a>
+ */
+public class StorageSizeListenerHelper {
+   
+   private ArrayList storageSizeListeners;
+   private Object storageSizeListenersSync = new Object();
+   private I_Storage storage;
+   private final static Logger log = Logger.getLogger(StorageSizeListenerHelper.class.getName());
+   
+   public StorageSizeListenerHelper(I_Storage storage) {
+      this.storage = storage;
+   }
+   
+   /**
+    * @see I_Queue#addStorageSizeListener(I_StorageSizeListener)
+    */
+   public void addStorageSizeListener(I_StorageSizeListener listener) {
+      if (listener == null) 
+         throw new IllegalArgumentException(storage.getStorageId().getId() + ": addStorageSizeListener(null) is not allowed");
+      synchronized(this.storageSizeListenersSync) {
+         if (this.storageSizeListeners == null)
+            this.storageSizeListeners = new ArrayList();
+         this.storageSizeListeners.add(listener);
+      }
+   }
+   
+   /**
+    * @see I_Queue#removeStorageSizeListener(I_StorageSizeListener)
+    */
+   public void removeStorageSizeListener(I_StorageSizeListener listener) {
+      synchronized(this.storageSizeListenersSync) {
+         if (listener == null) this.storageSizeListeners = null;
+         else {
+            if (!this.storageSizeListeners.remove(listener))
+               log.warning("removeStorageSizeListener: could not remove listener '" + listener.toString() + "' since not registered");
+            if (this.storageSizeListeners.size() == 0) this.storageSizeListeners = null;
+         }
+      }
+   }
+   
+   public final void invokeStorageSizeListener() {
+      if (this.storageSizeListeners != null) {
+         I_StorageSizeListener[] listeners = null;
+         synchronized(this.storageSizeListenersSync) {
+             listeners = (I_StorageSizeListener[])this.storageSizeListeners.toArray(new I_StorageSizeListener[this.storageSizeListeners.size()]);
+         }
+         for (int i=0; i < listeners.length; i++) {
+            try {
+               listeners[i].changed(storage, storage.getNumOfEntries(), storage.getNumOfBytes(), storage.isShutdown());
+            }
+            catch (NullPointerException e) {
+               if (log.isLoggable(Level.FINE)) log.fine("invokeStorageSizeListener() call is not possible as another thread has removed storageSizeListeners, this is OK to prevent a synchronize.");
+            }
+         }
+      }
+   }
+
+   /**
+    * @see I_Queue#hasStorageSizeListener(I_StorageSizeListener)
+    */
+   public boolean hasStorageSizeListener(I_StorageSizeListener listener) {
+      if (listener == null)
+         return this.storageSizeListeners != null;
+      else
+         return this.storageSizeListeners.contains(listener);
+   }
+
+   
+   
+
+}
