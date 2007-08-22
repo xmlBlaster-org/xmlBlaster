e0a9d74f396ba38f0c48191f7f84aca1b01f7a22
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2007-08-22 14:07:36 +0000

    Compile WinCE C client dll
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@16440 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ Projects/WinCE/VC8/xmlBlasterClientC/xmlBlasterC.cpp
@@ -0,0 +1,15 @@
+// xmlBlasterC.cpp : Defines the entry point for the DLL application.
+//
+
+#include "stdafx.h"
+#include <windows.h>
+#include <commctrl.h>
+
+BOOL APIENTRY DllMain( HANDLE hModule, 
+                       DWORD  ul_reason_for_call, 
+                       LPVOID lpReserved
+					 )
+{
+    return TRUE;
+}
+
