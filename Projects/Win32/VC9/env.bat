013bd8c6ce3fc4e46f7725ef3c1cf6030a5b49ee
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2008-02-25 16:22:05 +0000

    Visual Studio 2008 release
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@16752 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ Projects/Win32/VC9/env.bat
@@ -0,0 +1,13 @@
+REM Environment settings for Visual Studio 2005 C/C++ Win32 compilation
+REM Please customize to your environment
+REM http://www.xmlBlaster.org
+REM zlib and sqlite are optional (if not needed remove preprocessor XMLBLASTER_PERSISTENT_QUEUE=1;XMLBLASTER_ZLIB=1 in VS2005)
+
+set XMLBLASTER_HOME=C:\xmlBlaster
+
+set xerces_include_dir=C:\xerces-c-src_2_7_0\src
+set xerces_lib_dir=C:\xerces-c-src_2_7_0\Build\Win32\VC7.1\Release
+set zlib_lib_dir=C:\zlib\lib
+set zlib_include_dir=C:\zlib\include
+set sqlite_include_dir=C:\sqlite
+set sqlite_lib_dir=C:\sqlite
