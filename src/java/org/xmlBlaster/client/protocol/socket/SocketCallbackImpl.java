9e368d99bf7cda0e30fb4e55f471ed13b2fa49d4
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2008-05-14 13:54:20 +0000

    Logging only
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@16921 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/client/protocol/socket/SocketCallbackImpl.java
+++ src/java/org/xmlBlaster/client/protocol/socket/SocketCallbackImpl.java
@@ -134,12 +134,12 @@ public class SocketCallbackImpl extends SocketExecutor implements Runnable, I_Ca
       obj = glob.getObjectEntry("ClusterManager[cluster]/I_Authenticate");
       if (obj != null) {
          setAuthenticateCore((I_Authenticate)obj);
-         log.severe("TODO: Handle I_Authenticate");
+         log.info("Setting I_Authenticate for useRemoteLoginAsTunnel");
       }
       obj = glob.getObjectEntry("ClusterManager[cluster]/I_XmlBlaster");
       if (obj != null) {
          super.setXmlBlasterCore((I_XmlBlaster)obj);
-         log.severe("TODO: Handle I_XmlBlaster");
+         log.info("Setting I_XmlBlaster for useRemoteLoginAsTunnel");
       }
       
       // Lookup SocketConnection instance in the NameService
