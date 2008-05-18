5a9f54f6b618708676be976ef09726be0f77aae8
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2008-05-18 19:27:44 +0000

    Monodevelop for C# 0.15
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@16974 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ Projects/Mono/xmlBlasterClientCsh/xmlBlasterClientCsh/Main.cs
@@ -0,0 +1,18 @@
+// Main.cs created with MonoDevelop
+// User: xmlblast at 8:36 PM 5/18/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+// project created on 5/18/2008 at 8:36 PM
+using System;
+
+namespace xmlBlasterClientCsh
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
