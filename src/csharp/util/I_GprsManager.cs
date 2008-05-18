aa91e7eb3d902b7e0f32571804459dd1fadb748a
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2008-05-18 22:20:59 +0000

    Interface to control GPRS
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@16978 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ src/csharp/util/I_GprsManager.cs
@@ -0,0 +1,17 @@
+﻿/*------------------------------------------------------------------------------
+Name:      I_GprsManager.cs
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+------------------------------------------------------------------------------*/
+
+using System.Text;
+using System.Collections;
+
+namespace org.xmlBlaster.util
+{
+   public interface I_GprsManager
+   {
+      bool Connect(bool callbackGui);
+      /// <returns>true on success</returns>
+      bool Disconnect(bool callbackGui);
+   }
+} // namespace
