786753d977347d73ce91ce0e83dea6a11c93c933
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2008-07-17 17:00:22 +0000

    Ported to PHP 5 and phpxmlrpc 2.2.1 http://phpxmlrpc.sourceforge.net/
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@17036 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- demo/php/xmlrpc/index.php
+++ demo/php/xmlrpc/index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<?
+<?php
+
 /**
  *	look at xmlBlanster.inc to know more
  *
  *	08/07/02 21:51 cyrille@ktaland.com 
  */
+
 
 // ===============
 $DEBUG = 1 ;
@@ -25,11 +27,12 @@ function svar_dump($data) {
 
 include( 'xmlBlaster.inc' );
 
-$server = getHttpVar('server');
-$port = getHttpVar('port');
-$user = getHttpVar('user');
-$password = getHttpVar('password');
-$xpathQuery = getHttpVar('xpathQuery');
+$server = getHttpVar('server', 'localhost');
+$port = getHttpVar('port', 8080);
+$user = getHttpVar('user', 'joe');
+$password = getHttpVar('password','secret');
+$securityPlugin = getHttpVar('securityPlugin','htpasswd');
+$xpathQuery = getHttpVar('xpathQuery', '//key');
 $xbMethod = getHttpVar('xbMethod');
 $publishKey     = getHttpVar('publishKey');
 $publishQos     = getHttpVar('publishQos');
@@ -52,6 +55,7 @@ if ($publishQos != "") {
  */
 
 $xb = new xmlBlaster( $server, $port, $user, $password );
+$xb->securityService_type = $securityPlugin;
 
 $res = $xb->connect();
 if( $res[0] != 'OK' ){
@@ -66,6 +70,7 @@ $sysInternal = array() ;
 if( $xb->isConnected() ){
 
 	$sysInternalVariables = array( 'nodeId','version','uptime','totalMem','usedMem','freeMem','clientList' );
+	//$sysInternalVariables = array( 'uptime' );
 	//$sysInternalVariables = array( 'freeMem','syspropList','toto' );
 
 	foreach( $sysInternalVariables as $sysVar ){
@@ -82,11 +87,11 @@ if( $xb->isConnected() ){
 
 		}else{
 			$messages = $res[1] ;
-			$message_count = count( $message );
+			$message_count = count( $messages );
 			// should be only one because it's admin value
 
 			foreach( $messages as $msg ){
-				//dbgPrint( "MSG:" .$msg->content() );
+				dbgPrint( "MSG:" .$msg->content() );
 				$sysInternal[ $sysVar ] = $msg->content() ;
 			}
 		}
@@ -97,7 +102,7 @@ if( $xb->isConnected() ){
 
 <html>
 <head>
-	<title>PHP XmlBlaster client - running on <? echo $server ?> </title>
+	<title>PHP XmlBlaster client - running on <?php echo $server ?> </title>
 
 <style type="text/css">
 <!--
@@ -118,16 +123,16 @@ if( $xb->isConnected() ){
 <center>
 <table>
   <tr><th></th><th></th></tr>
-  <tr><td>server</td><td><? echo $server; ?></td></tr>
-  <tr><td>port</td><td><? echo $port; ?></td></tr>
-  <tr><td>user</td><td><? echo $user; ?></td></tr>
-  <tr><td>password</td><td><? echo $password; ?></td></tr>
-  <tr><td>xpathQuery</td><td><? echo $xpathQuery; ?></td></tr>
-  <tr><td>xbMethod</td><td><? echo $xbMethod; ?></td></tr>
-  <tr><td>publishKey (html)</td><td><? echo htmlentities($publishKey); ?></td></tr>
-  <tr><td>publishQos (html)</td><td><? echo htmlentities($publishQos); ?></td></tr>
-  <tr><td>publishContent</td><td><? echo $publishContent; ?></td></tr>
-  <tr><td>publishRes</td><td><? echo $publishRes; ?></td></tr>
+  <tr><td>server</td><td><?php echo $server; ?></td></tr>
+  <tr><td>port</td><td><?php echo $port; ?></td></tr>
+  <tr><td>user</td><td><?php echo $user; ?></td></tr>
+  <tr><td>password</td><td><?php echo $password; ?></td></tr>
+  <tr><td>xpathQuery</td><td><?php echo $xpathQuery; ?></td></tr>
+  <tr><td>xbMethod</td><td><?php echo $xbMethod; ?></td></tr>
+  <tr><td>publishKey (html)</td><td><?php echo htmlentities($publishKey); ?></td></tr>
+  <tr><td>publishQos (html)</td><td><?php echo htmlentities($publishQos); ?></td></tr>
+  <tr><td>publishContent</td><td><?php echo $publishContent; ?></td></tr>
+  <tr><td>publishRes</td><td><?php echo $publishRes; ?></td></tr>
 </table>
 </center>
 -->
@@ -135,16 +140,16 @@ if( $xb->isConnected() ){
 <table>
 <tr>
 <td class="title"><a href="http://xmlBlaster.org/"><img src="logo_xmlBlaster_2.gif" border="0"></a></td>
-<td class="title"><a href="http://xmlBlaster.org/">xmlBlaster</a> client for <a href="http://php.net/">PHP</a>,<br>using <a href="http://xml-rpc.org/">xml-rpc</a> protocol</td>
+<td class="title"><a href="http://xmlBlaster.org/">xmlBlaster</a> client for <a href="http://php.net/">php</a>,<br>using <a href="http://xml-rpc.org/">xml-rpc</a> protocol</td>
 </tr>
 </table>
 
-<? if( isset($error_message) ){ ?>
+<?php if( isset($error_message) ){ ?>
 <p>&nbsp;</p>
 <table border="1"><tr><td class="error">
-ERROR : <? echo $error_message; ?>
+ERROR : <?php echo $error_message; ?>
 </td></tr></table>
-<? } ?>
+<?php } ?>
 
 <p>&nbsp;</p>
 <form method="GET">
@@ -154,26 +159,27 @@ ERROR : <? echo $error_message; ?>
   <tr>
    <td class="text">server :</td>
    <td class="text">
-	<input type="text" name="server" value="<? echo $server; ?>">
+	<input type="text" name="server" value="<?php echo $server; ?>">
    </td>
 
    <td class="text" rowspan="4">
 
 	<table>
-	<?
+	<?php
 	foreach( $sysInternal as $k=>$v ){
 	?>
-	<tr><td class="text"> &nbsp; <? echo htmlentities($k); ?></td><td class="text"><? echo htmlentities($v); ?></td></tr>
-	<?
+	<tr><td class="text"> &nbsp; <?php echo htmlentities($k); ?></td><td class="text"><?php echo htmlentities($v); ?></td></tr>
+	<?php
 	}
 	?>
 	</table>
 
    </td>
   </tr>
-  <tr><td class="text">port :</td><td class="text"><input type="text" name="port" value="<? echo $port; ?>"></td></tr>
-  <tr><td class="text">user :</td><td class="text"><input type="text" name="user" value="<? echo $user; ?>"></td></tr>
-  <tr><td class="text">password :</td><td class="text"><input type="text" name="password" value="<? echo $password; ?>"></td></tr>
+  <tr><td class="text">port :</td><td class="text"><input type="text" name="port" value="<?php echo $port; ?>"></td></tr>
+  <tr><td class="text">user :</td><td class="text"><input type="text" name="user" value="<?php echo $user; ?>"></td></tr>
+  <tr><td class="text">password :</td><td class="text"><input type="text" name="password" value="<?php echo $password; ?>"></td></tr>
+  <tr><td class="text">securityPlugin :</td><td class="text"><input type="text" name="securityPlugin" value="<?php echo $securityPlugin; ?>"></td></tr>
   <tr><td class="text" colspan="3" align="center"><input type="submit" value=" Connect "></td></tr>
  </table>
 </form>
@@ -182,17 +188,18 @@ ERROR : <? echo $error_message; ?>
 <form method="GET">
 <div class="text">After connection, you can query the server for messages :</div>
 	<input type="hidden" name="xbMethod" value="get">
-	<input type="hidden" name="server" value="<? echo $server; ?>"><input type="hidden" name="port" value="<? echo $port; ?>">
-	<input type="hidden" name="user" value="<? echo $user; ?>"><input type="hidden" name="password" value="<? echo $password; ?>">
+	<input type="hidden" name="server" value="<?php echo $server; ?>"><input type="hidden" name="port" value="<?php echo $port; ?>">
+	<input type="hidden" name="user" value="<?php echo $user; ?>"><input type="hidden" name="password" value="<?php echo $password; ?>">
+	<input type="hidden" name="securityPlugin" value="<?php echo $securityPlugin; ?>">
  <table border="1">
   <tr>
    <td class="text">
-	XPath Query :<br><input type="text" name="xpathQuery" value="<? echo $xpathQuery; ?>" size="50">
+	XPath Query :<br><input type="text" name="xpathQuery" value="<?php echo $xpathQuery; ?>" size="50">
    </td>
   </tr>
   <tr><td><input type="submit" value=" Query "></td></tr>
 
-<?
+<?php
 if( $xb->isConnected() && isset($xpathQuery) && ($xbMethod == 'get')){
 
 	// $key = "<key oid='' queryType='XPATH'>/xmlBlaster/key[starts-with(\@oid,'myHel')]</key>" ;
@@ -202,13 +209,12 @@ if( $xb->isConnected() && isset($xpathQuery) && ($xbMethod == 'get')){
 		$error_message = $res[1];
 		?>
 		<tr><td>
-			<? echo $error_message; ?>
+			<?php echo $error_message; ?>
 		</td></tr>
-		<?
+		<?php
 
 	}else{
 		$messages = $res[1] ;
-		$message_count = count( $message );
 
 		foreach( $messages as $msg ){
 			//dbgPrint( "MSG.type:" .gettype($msg) );
@@ -218,15 +224,15 @@ if( $xb->isConnected() && isset($xpathQuery) && ($xbMethod == 'get')){
 				<tr><td>
 					No Result.
 				</td></tr>
-				<?
+				<?php
 			}else{
 				?>
 				<tr><td>
-					<? echo htmlentities($msg->keyOid()); ?>
+					<?php echo htmlentities($msg->keyOid()); ?>
 					<br>
-					<? echo htmlentities($msg->content()); ?>
+					<?php echo htmlentities($msg->content()); ?>
 				</td></tr>
-				<?
+				<?php
 			}
 		}
 	}
@@ -241,20 +247,28 @@ if( $xb->isConnected() && isset($xpathQuery) && ($xbMethod == 'get')){
 <form method="GET">
 <div class="text">A Publish Test :</div>
  <input type="hidden" name="xbMethod" value="publish">
-<?
+<?php
   if ($xbMethod == 'publish') {
-     $pubRes = $xb->publish($publishKey, $publishContent, $publishQos);
+     $arr = $xb->publish($publishKey, $publishContent, $publishQos);
+     $okstr = $arr[0];
+     $publishReturnQos = $arr[1];
+     $publishRes = $publishReturnQos->state();
+     dbgprint("PublishReturnQos::state=" . $publishRes);
   }
 ?> 
 
-  <input type="hidden" name="server" value="<? echo $server; ?>"><input type="hidden" name="port" value="<? echo $port; ?>">
-  <input type="hidden" name="user" value="<? echo $user; ?>"><input type="hidden" name="password" value="<? echo $password; ?>">
+  <input type="hidden" name="server" value="<?php echo $server; ?>"><input type="hidden" name="port" value="<?php echo $port; ?>">
+  <input type="hidden" name="user" value="<?php echo $user; ?>"><input type="hidden" name="password" value="<?php echo $password; ?>">
+  <input type="hidden" name="securityPlugin" value="<?php echo $securityPlugin; ?>">
+
+
+  <input type="hidden" name="$publishRes" value="<?php echo $publishRes; ?>">
 
  <table border="1">
   <tr><td class="text">key :</td><td class="text"><input type="text" id='publishKey' name="publishKey" value="<key oid='phpPublish'/>" size="50"></td></tr>
-  <tr><td class="text">content :</td><td class="text"><input type="text" id='publishContent' name="publishContent" value="<? echo $publishContent; ?>" size="50"></td></tr>
+  <tr><td class="text">content :</td><td class="text"><input type="text" id='publishContent' name="publishContent" value="<?php echo $publishContent; ?>" size="50"></td></tr>
   <tr><td class="text">qos :</td><td class="text"><input type="text" id='publishQos' name="publishQos" value="&lt;qos/>" size="50"></td></tr>
-  <tr><td class="text">Result :</td><td class="text"><input type="text" id='publishRes' name="publishRes" value="<? echo $publishRes; ?>" size="50"></td></tr>
+  <tr><td class="text">Result :</td><td class="text"><input type="text" id='publishRes' name="publishRes" value="<?php echo $publishRes; ?>" size="50"></td></tr>
   <tr><td class="text" colspan="2" align="center"><input type="submit" value=" Publish "></td></tr>
  </table>
 </form>
@@ -263,26 +277,26 @@ if( $xb->isConnected() && isset($xpathQuery) && ($xbMethod == 'get')){
 </body>
 </html>
 
-<?
+<?php
 
 $xb->logout();
 ?>
 
-<?
+<?php
 // ============
-function getHttpVar( $name ){
+function getHttpVar( $name, $defaultVal=null ){
 	// 04/06/02 mad@ktaland.com
-	global $HTTP_GET_VARS, $HTTP_POST_VARS, $HTTP_SERVER_VARS ;
-	if( $HTTP_SERVER_VARS['REQUEST_METHOD'] == 'POST' ){
-		if( isset($HTTP_POST_VARS[$name]) ){
-			return $HTTP_POST_VARS[$name] ;
+	//global $HTTP_GET_VARS, $HTTP_POST_VARS, $HTTP_SERVER_VARS ;
+	if( $_SERVER['REQUEST_METHOD'] == 'POST' ){
+		if( isset($_POST[$name]) ){
+			return $_POST[$name] ;
 		}
 	}else{
-		if( isset($HTTP_GET_VARS[$name]) ){
-			return $HTTP_GET_VARS[$name] ;
+		if( isset($_GET[$name]) ){
+			return $_GET[$name] ;
 		}
 	}
-	return null ;
+	return $defaultVal ;
 }
 
 ?>
