7260afc2576312444ab2c0e904f5a2f2fbd2dbaa
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2008-11-10 10:21:51 +0000

    Testing performance
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@17386 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ doc/jdbc/perform.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Start 10 subscribers in separate xterms (Unix only)
+
+echo "Try publish:"
+echo "java -Dcom.sun.management.jmxremote javaclients.HelloWorldPublish -numPublish 500 -session.name pubisher/1 -persistent true -oid Hello -interactive false -sleep 0 -contentSize 300000"
+
+
+export COUNT=10
+if [ "$1" != "" ] ; then
+   export COUNT=$1
+fi
+
+echo "Starting $COUNT subscribers ..."
+
+export PROG="java -Dcom.sun.management.jmxremote javaclients.HelloWorldSubscribe"
+export ARGS=" -oid Hello -session.name subscriber$i/1 -persistentSession true -persistentSubscribe true -multiSubscribe false -dispatch/callback/retries -1 -autoSubscribe true"
+#export SERV=" -dispatch/connection/plugin/socket/hostname localhost -dispatch/connection/plugin/socket/port 7607"
+
+for ((i=1;i<=$COUNT;i+=1)); do
+	echo "subscriber$i/1 ..."
+	xterm -geom 180x26 -e $PROG $ARGS $SERV -session.name subscriber$i/1 &
+#	xterm -geom 180x26 -e java -Dcom.sun.management.jmxremote javaclients.HelloWorldSubscribe -oid Hello -session.name subscriber$i/1 -persistentSubscribe true -multiSubscribe false -dispatch/callback/retries -1 -autoSubscribe true
+	sleep 1
+done
