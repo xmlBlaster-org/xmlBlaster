fb62a83c94624d1fb51a36f80c93d307b16fc165
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2008-11-18 11:23:51 +0000

    leak protection!
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@17522 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- testsuite/src/c/TestMethods.c
+++ testsuite/src/c/TestMethods.c
@@ -34,6 +34,10 @@ static bool myUpdate(MsgUnitArr *msgUnitArr, void *userData, XmlBlasterException
    if (xmlBlasterException != 0) ;  /* Supress compiler warning */
    updateUserData = xa;
    for (i=0; i<msgUnitArr->len; i++) {
+      if (updateContent != 0) {
+         xmlBlasterFree(updateContent);
+         updateContent = 0;
+      }
       MsgUnit *msg = &msgUnitArr->msgUnitArr[i];
       printf("[client] CALLBACK update(): Asynchronous message update arrived\n");
       updateContent = strFromBlobAlloc(msg->content, msg->contentLen);
