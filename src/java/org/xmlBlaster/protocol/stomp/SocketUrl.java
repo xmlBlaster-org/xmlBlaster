3a991524fcc5d274efcd4317bcdbe4c43723f92d
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2010-05-01 12:20:30 +0000

    JMX , config and shutdown
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@17793 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ src/java/org/xmlBlaster/protocol/stomp/SocketUrl.java
@@ -0,0 +1,227 @@
+/*------------------------------------------------------------------------------
+Name:      SocketUrl.java
+Project:   xmlBlaster.org
+Copyright: xmlBlaster.org, see xmlBlaster-LICENSE file
+Comment:   SocketUrl knows how to parse the URL notation of our STOMP protocol
+------------------------------------------------------------------------------*/
+package org.xmlBlaster.protocol.stomp;
+
+import java.net.InetAddress;
+import java.util.logging.Logger;
+
+import org.xmlBlaster.util.Global;
+import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.util.def.ErrorCode;
+import org.xmlBlaster.util.property.PropInt;
+import org.xmlBlaster.util.property.PropString;
+import org.xmlBlaster.util.qos.address.AddressBase;
+
+/**
+ * This knows how to parse the URL notation of our SOCKET protocol. 
+ * It holds the hostname and the port.
+ * @see <a href="http://www.xmlBlaster.org/xmlBlaster/doc/requirements/protocol.socket.html">The protocol.socket requirement</a>
+ * @see org.xmlBlaster.test.classtest.SocketUrlTest
+ */
+public class SocketUrl
+{
+   private String ME = "SocketUrl";
+   private Global glob;
+   private static Logger log = Logger.getLogger(SocketUrl.class.getName());
+   /** The string representation like "192.168.1.1", useful if multihomed computer */
+   private String hostname = null;
+   private PropString hostnameProp;
+   /** xmlBlaster server host */
+   private java.net.InetAddress inetAddress = null;
+   /** The port */
+   private int port = SocketUrl.DEFAULT_SERVER_PORT;
+   private PropInt portProp;
+   private boolean isLocal = false;
+   /** Flag to use TCP/IP */
+   public static final boolean SOCKET_TCP = false;
+   /** Flag to use UDP */
+   public static final boolean SOCKET_UDP = true;
+   // Default port of xmlBlaster socket local side is 0 (choosen by OS) */
+   //public static final int DEFAULT_SERVER_CBPORT = 0;
+   /** Default port of xmlBlaster stomp server is 61613 */
+   public static final int DEFAULT_SERVER_PORT = 61613;
+   private boolean isEnforced = false;
+
+   /**
+    * @param hostname if null or empty the local IP will be used
+    * @param port any port, not checked
+    */
+   public SocketUrl(Global glob, String hostname, int port) throws XmlBlasterException {
+      this.glob = glob;
+
+      this.hostname = hostname;
+      if (this.hostname == null || this.hostname.length() < 1) {
+         this.hostname = glob.getLocalIP();
+      }
+      this.port = port;
+      createInetAddress(); // first check
+   }
+
+   /**
+    * Parse the given url. 
+    * @param url e.g. "socket://127.168.1.1:61613" or only "127.168.1.1:61613" or "" (choose default settings)
+    * @exception XmlBlasterException if url is null or invalid
+    */
+   public SocketUrl(Global glob, String url) throws XmlBlasterException {
+      this.glob = glob;
+
+      parse(url);
+      createInetAddress(); // first check
+   }
+
+   /**
+    * Extract "hostname" and "port" from environment, if not found use the local host
+    * for hostname and the default port 61613. 
+    * <br />
+    * Updates the raw address from AddressBase
+    */
+   public SocketUrl(Global glob, AddressBase address) throws XmlBlasterException {
+      this(glob, address, false, SocketUrl.DEFAULT_SERVER_PORT);
+   }
+
+   /**
+    * Extract hostname and port from environment, if not found use the local host
+    * for hostname and the given default port (usually ExecutorBase.DEFAULT_SERVER_PORT=61613). 
+    * <br />
+    * Updates the raw address from AddressBase if isLocal==false
+    * @param isLocal If local is set to true "localHostname" and "localPort" will be extracted
+    */
+   public SocketUrl(Global glob, AddressBase address, boolean isLocal, int defaultServerPort) throws XmlBlasterException {
+      this.glob = glob;
+
+
+      if (isLocal) {
+         this.isLocal = true;
+         this.portProp = address.getEnv("localPort", defaultServerPort); 
+         this.port = this.portProp.getValue();
+         this.hostnameProp = address.getEnv("localHostname", glob.getLocalIP());
+         this.hostname = this.hostnameProp.getValue();
+      }
+      else {
+         if (address.getRawAddress() != null && address.getRawAddress().length() > 2) {
+            parse(address.getRawAddress());
+            createInetAddress(); // first check
+            this.isEnforced = true;
+            return;
+         }
+         this.portProp = address.getEnv("port", defaultServerPort); 
+         this.port = this.portProp.getValue();
+         this.hostnameProp = address.getEnv("hostname", glob.getLocalIP());
+         this.hostname = this.hostnameProp.getValue();
+         address.setRawAddress(getUrl());
+      }
+      createInetAddress(); // first check
+   }
+   
+   /** @return true if host or port was given by user configuration, false if default is chosen */
+   public boolean isEnforced() {
+      if (this.isEnforced) return true;
+      boolean enforcedHost = (this.hostnameProp == null) ? false : !this.hostnameProp.isDefault();
+      boolean enforcedPort = (this.portProp == null) ? false : !this.portProp.isDefault();
+      return enforcedHost || enforcedPort;
+   }
+
+   public String getHostname() {
+      return this.hostname;
+   }
+
+   public int getPort() {
+      return this.port;
+   }
+
+   /**
+    * @return for example "socket://myServer.com:61613"
+    */
+   public String getUrl() {
+      return "stomp://" + this.hostname + ":" + this.port;
+   }
+
+   public String toString() {
+      return getUrl();
+   }
+
+   private void parse(String url) throws XmlBlasterException {
+      if (url == null) {
+         throw new XmlBlasterException(this.glob, ErrorCode.RESOURCE_CONFIGURATION_ADDRESS, ME, "Your given stomp url '" + url + "' is invalid");
+      }
+
+      String urlLowerCase = url.toLowerCase();
+      if (urlLowerCase.startsWith("stomp://")) {
+         url = url.substring("stomp://".length());
+      }
+      else if (urlLowerCase.startsWith("stomp:")) {
+         url = url.substring("stomp:".length());
+      }
+
+      int pos = url.indexOf(":");
+      String portStr = null;
+      if (pos > -1) {
+         this.hostname = url.substring(0, pos);
+         portStr = url.substring(pos+1);
+         if (portStr != null && portStr.length() > 0) {
+            pos = portStr.indexOf("/");
+            if (pos > -1) {
+               portStr = portStr.substring(0, pos); // strip path e.g. "stomp://myHost:8000/path/subpath"
+            }
+            try {
+               this.port = (new Integer(portStr)).intValue();
+            }
+            catch (NumberFormatException e) {
+               throw new XmlBlasterException(this.glob, ErrorCode.RESOURCE_CONFIGURATION_ADDRESS, ME, "Your given stomp url '" + url + "' port '" + portStr + "' is invalid");
+            }
+         }
+      }
+      else {
+         this.hostname = url;
+      }
+
+      if (this.hostname == null || this.hostname.length() < 1) {
+         this.hostname = glob.getLocalIP();
+      }
+   }
+
+   public InetAddress getInetAddress() {
+      return this.inetAddress;
+   }
+
+   private void createInetAddress() throws XmlBlasterException {
+      try {
+         this.inetAddress = java.net.InetAddress.getByName(this.hostname);
+      } catch(java.net.UnknownHostException e) {
+         Thread.dumpStack();
+         String txt = "The hostname [" + this.hostname + "] of url '" + getUrl() + "' is invalid, check your '-plugin/stomp/" +
+                       (isLocal ? "localHostname" : "hostname") + " <ip>' setting: " + e.toString();
+         log.warning(txt);
+         // throw new XmlBlasterException(glob, ErrorCode.RESOURCE_CONFIGURATION_ADDRESS, ME, txt);
+         throw new XmlBlasterException(glob, ErrorCode.COMMUNICATION_NOCONNECTION, ME, txt);
+      }
+   }
+
+   public boolean equals(SocketUrl other) {
+      if (this.port == other.getPort() && getInetAddress().equals(other.getInetAddress())) {
+         //log.error(ME, "DEBUG ONLY: EQUAL: " + getUrl() + " - " + other.getUrl());
+         return true;
+      }
+      //log.error(ME, "DEBUG ONLY: NOT EQUAL: " + getUrl() + " - " + other.getUrl());
+      return false;
+   }
+
+
+   /** java org.xmlBlaster.protocol.stomp.SocketUrl stomp://localhost:7609 */
+   public static void main(String[] args) {
+      try {
+         if (args.length > 0) {
+            SocketUrl s = new SocketUrl(Global.instance(), args[0]);
+            System.out.println(args[0] + " -> " + s.getUrl() + " hostname=" + s.getHostname() + " port=" + s.getPort());
+         }
+      }
+      catch (Throwable e) {
+         System.out.println("ERROR: " + e.toString());
+      }
+   }
+}
+
--- src/java/org/xmlBlaster/protocol/stomp/XbStompDriver.java
+++ src/java/org/xmlBlaster/protocol/stomp/XbStompDriver.java
@@ -3,6 +3,7 @@ package org.xmlBlaster.protocol.stomp;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.net.ServerSocketFactory;
@@ -10,49 +11,69 @@ import javax.net.ServerSocketFactory;
 import org.codehaus.stomp.StompHandler;
 import org.codehaus.stomp.StompHandlerFactory;
 import org.codehaus.stomp.tcp.TcpTransportServer;
+import org.xmlBlaster.engine.qos.AddressServer;
+import org.xmlBlaster.protocol.I_Authenticate;
 import org.xmlBlaster.protocol.I_Driver;
+import org.xmlBlaster.protocol.I_XmlBlaster;
 import org.xmlBlaster.util.Global;
 import org.xmlBlaster.util.XmlBlasterException;
+import org.xmlBlaster.util.context.ContextNode;
+import org.xmlBlaster.util.def.Constants;
 import org.xmlBlaster.util.def.ErrorCode;
 import org.xmlBlaster.util.plugin.PluginInfo;
 
 /**
  * XbStomp driver class to invoke the xmlBlaster server over STOMP protocol.
  * <p />
+ * 
  * @see <a href="http://stomp.codehaus.org/">Website</a>
  * @see <a href="http://stomp.codehaus.org/Protocol">Protocol describtion</a>
  * @author Dieter Saken
  */
 
-
-
-
 public class XbStompDriver implements I_Driver, StompHandlerFactory {
 	private static Logger log = Logger.getLogger(XbStompDriver.class.getName());
-	  private String ME = "XbStompDriver";
-	   /** The global handle */
-	   private Global glob;
-	   private String uri = "tcp://localhost:11111";
-	   private URI location;
-	   private ServerSocketFactory serverSocketFactory;
-	   private TcpTransportServer tcpServer;
-	 
+	private String ME = "XbStompDriver";
+	/** The global handle */
+	private Global glob;
+	public static final int DEFAULT_SERVER_PORT = 61613;
+	/**
+	 * The socket address info object holding hostname (useful for multi homed
+	 * hosts) and port
+	 */
+	private SocketUrl socketUrl;
+	private URI location;
+	private ServerSocketFactory serverSocketFactory;
+	private TcpTransportServer tcpServer;
+	private AddressServer addressServer;
+	/** The singleton handle for this authentication server */
+	private I_Authenticate authenticate;
+	/* The singleton handle for this xmlBlaster server */
+	//private I_XmlBlaster xmlBlasterImpl;
+	/** My JMX registration */
+	protected Object mbeanHandle;
+	protected ContextNode contextNode;
+	protected boolean isShutdown;
+
 	public void activate() throws XmlBlasterException {
-	     log.info("activate");
-         try {
+		log.info("activate");
+		try {
 			doStart();
 		} catch (Exception e) {
-			throw new XmlBlasterException(glob, ErrorCode.COMMUNICATION, "activate", e.getMessage(),e);
+			throw new XmlBlasterException(glob, ErrorCode.COMMUNICATION,
+					"activate", e.getMessage(), e);
 		}
 
 	}
 
 	public void deActivate() throws XmlBlasterException {
 		try {
-		     log.info("deActivate");
-		     			doStop();
+			log.info("deActivate");
+			doStop();
 		} catch (Throwable ex) {
-	         throw new XmlBlasterException(this.glob, ErrorCode.INTERNAL_UNKNOWN, ME + ".init", "init. Could'nt shutdown the driver.", ex);
+			throw new XmlBlasterException(this.glob,
+					ErrorCode.INTERNAL_UNKNOWN, ME + ".init",
+					"init. Could'nt shutdown the driver.", ex);
 		}
 
 	}
@@ -66,16 +87,61 @@ public class XbStompDriver implements I_Driver, StompHandlerFactory {
 	}
 
 	public String getRawAddress() {
+		// return this.socketUrl.getUrl(); // this.socketUrl.getHostname() + ":"
+		// + this.socketUrl.getPort();
 		try {
 			return getTcpServer().getBindLocation().toString();
 		} catch (Throwable e) {
-		   ;
+			;
 		}
 		return null;
 	}
 
+	/**
+	 * The command line key prefix
+	 * 
+	 * @return The configured type in xmlBlasterPlugins.xml, defaults to
+	 *         "plugin/socket"
+	 */
+	public String getEnvPrefix() {
+		return (addressServer != null) ? addressServer.getEnvPrefix()
+				: "plugin/" + getType().toLowerCase();
+	}
+
+	/**
+	 * Command line usage.
+	 * <p />
+	 * <ul>
+	 * <li><i>-plugin/socket/port</i> The SOCKET web server port [61613]</li>
+	 * <li><i>-plugin/socket/hostname</i> Specify a hostname where the SOCKET
+	 * web server runs Default is the localhost.</li>
+	 * <li><i>-plugin/socket/backlog</i> Queue size for incoming connection
+	 * request [50]</li>
+	 * <li><i>-dump[socket]</i> true switches on detailed SOCKET debugging
+	 * [false]</li>
+	 * </ul>
+	 * <p />
+	 * Enforced by interface I_Driver.
+	 */
 	public String usage() {
-		return "Usage info";
+		String text = "\n";
+		text += "StompDriver options:\n";
+		text += "   -" + getEnvPrefix() + "port\n";
+		text += "                       The server port [61613].\n";
+		text += "   -" + getEnvPrefix() + "hostname\n";
+		text += "                       Specify a hostname where the stomp server runs.\n";
+		text += "                       Default is the localhost.\n";
+		text += "   -" + getEnvPrefix() + "responseTimeout\n";
+		text += "                       Max wait for the method return value/exception in msec.\n";
+		// text += "                       The default is "
+		// +getDefaultResponseTimeout() + ".\n";
+		text += "                       Defaults to 'forever', the value to pass is milli seconds.\n";
+		text += "   -dump[socket]       true switches on detailed " + getType()
+				+ " debugging [false].\n";
+		text += "   "
+				+ Global.getJmxUsageLinkInfo(this.getClass().getName(), null);
+		text += "\n";
+		return text;
 	}
 
 	public String getType() {
@@ -89,107 +155,163 @@ public class XbStompDriver implements I_Driver, StompHandlerFactory {
 	public void init(Global glob, PluginInfo pluginInfo)
 			throws XmlBlasterException {
 		this.glob = glob;
-	    try{
-	        activate();
-		}
-		catch (XmlBlasterException ex) {
-		         throw ex;
-	    }
-		catch (Throwable ex) {
-		         throw new XmlBlasterException(this.glob, ErrorCode.INTERNAL_UNKNOWN, ME + ".init", "init. Could'nt initialize the driver.", ex);
+        this.ME = "StompDriver" + this.glob.getLogPrefixDashed() + "-" + getType();
+		try {
+
+			org.xmlBlaster.engine.ServerScope engineGlob = (org.xmlBlaster.engine.ServerScope) glob
+					.getObjectEntry(Constants.OBJECT_ENTRY_ServerScope);
+			if (engineGlob == null)
+				throw new XmlBlasterException(this.glob,
+						ErrorCode.INTERNAL_UNKNOWN, ME + ".init",
+						"could not retreive the ServerNodeScope. Am I really on the server side ?");
+
+			// For JMX instanceName may not contain ","
+			String vers = ("1.0".equals(getVersion())) ? "" : getVersion();
+			this.contextNode = new ContextNode(ContextNode.SERVICE_MARKER_TAG,
+					"StompDriver[" + getType() + vers + "]", glob
+							.getContextNode());
+			this.mbeanHandle = this.glob.registerMBean(this.contextNode, this);
+
+			this.authenticate = engineGlob.getAuthenticate();
+			if (this.authenticate == null) {
+				throw new XmlBlasterException(this.glob,
+						ErrorCode.INTERNAL_UNKNOWN, ME + ".init",
+						"authenticate object is null");
+			}
+			I_XmlBlaster xmlBlasterImpl = this.authenticate.getXmlBlaster();
+			if (xmlBlasterImpl == null) {
+				throw new XmlBlasterException(this.glob,
+						ErrorCode.INTERNAL_UNKNOWN, ME + ".init",
+						"xmlBlasterImpl object is null");
+			}
+
+			this.addressServer = new AddressServer(glob, getType(), glob
+					.getId(), pluginInfo.getParameters());
+
+			this.socketUrl = new SocketUrl(glob, this.addressServer);
+
+			if (this.socketUrl.getPort() < 1) {
+				log.info(ME + "Option protocol/socket/port set to "
+						+ this.socketUrl.getPort()
+						+ ", stomp server not started");
+				return;
+			}
+
+			activate();
+		} catch (XmlBlasterException ex) {
+			throw ex;
+		} catch (Throwable ex) {
+			throw new XmlBlasterException(this.glob,
+					ErrorCode.INTERNAL_UNKNOWN, ME + ".init",
+					"init. Could'nt initialize the driver.", ex);
 		}
 
 	}
 
 	public void shutdown() throws XmlBlasterException {
+		if (log.isLoggable(Level.FINER))
+			log.finer("Entering shutdown");
 		try {
 			doStop();
 			this.tcpServer = null;
 		} catch (Throwable ex) {
-	         throw new XmlBlasterException(this.glob, ErrorCode.INTERNAL_UNKNOWN, ME + ".init", "init. Could'nt shutdown the driver.", ex);
+			throw new XmlBlasterException(this.glob,
+					ErrorCode.INTERNAL_UNKNOWN, ME + ".init",
+					"init. Could'nt shutdown the driver.", ex);
 		}
+
+		try {
+			deActivate();
+		} catch (Exception e) {
+			log.severe(e.toString());
+		}
+
+		this.glob.unregisterMBean(this.mbeanHandle);
+
+		this.isShutdown = true;
+
+		log.info("Stomp driver '" + getType()
+				+ "' stopped, all resources released.");
 	}
-	
- /////////// STOMP
-    
-    public StompHandler createStompHandler(StompHandler outputHandler)  {
-         return new XbStompInOutBridge(this.glob, this, outputHandler);
-    }
 
-    /**
-     * Joins with the background thread until the transport is stopped
-     */
-    public void join() throws IOException, URISyntaxException, InterruptedException {
-        getTcpServer().join();
-    }
+	public boolean isShutdown() {
+		return this.isShutdown;
+	}
 
+	// ///////// STOMP
 
+	public StompHandler createStompHandler(StompHandler outputHandler) {
+		return new XbStompInOutBridge(this.glob, this, outputHandler);
+	}
 
-    public String getUri() {
-        return uri;
-    }
+	/**
+	 * Joins with the background thread until the transport is stopped
+	 */
+	public void join() throws IOException, URISyntaxException,
+			InterruptedException {
+		getTcpServer().join();
+	}
 
-    /**
-     * Sets the URI string for the hostname/IP address and port to listen on for STOMP frames
-     */
-    public void setUri(String uri) {
-        this.uri = uri;
-    }
+	public URI getLocation() throws URISyntaxException {
+		if (location == null) {
+			// "tcp://localhost:11111";
+			String uri = "tcp://" + this.socketUrl.getHostname() + ":"
+					+ this.socketUrl.getPort();
+			location = new URI(uri);
+		}
+		return location;
+	}
 
-    public URI getLocation() throws URISyntaxException {
-        if (location == null) {
-            location = new URI(uri);
-        }
-        return location;
-    }
+	/**
+	 * Sets the URI for the hostname/IP address and port to listen on for STOMP
+	 * frames
+	 */
+	public void setLocation(URI location) {
+		this.location = location;
+	}
 
-    /**
-     * Sets the URI for the hostname/IP address and port to listen on for STOMP frames
-     */
-    public void setLocation(URI location) {
-        this.location = location;
-    }
+	public ServerSocketFactory getServerSocketFactory() {
+		if (serverSocketFactory == null) {
+			serverSocketFactory = ServerSocketFactory.getDefault();
+		}
+		return serverSocketFactory;
+	}
 
-    public ServerSocketFactory getServerSocketFactory() {
-        if (serverSocketFactory == null) {
-            serverSocketFactory = ServerSocketFactory.getDefault();
-        }
-        return serverSocketFactory;
-    }
+	/**
+	 * Sets the {@link ServerSocketFactory} to use to listen for STOMP frames
+	 */
+	public void setServerSocketFactory(ServerSocketFactory serverSocketFactory) {
+		this.serverSocketFactory = serverSocketFactory;
+	}
 
-    /**
-     * Sets the {@link ServerSocketFactory} to use to listen for STOMP frames
-     */
-    public void setServerSocketFactory(ServerSocketFactory serverSocketFactory) {
-        this.serverSocketFactory = serverSocketFactory;
-    }
+	public TcpTransportServer getTcpServer() throws IOException,
+			URISyntaxException {
+		if (tcpServer == null) {
+			tcpServer = createTcpServer();
+		}
+		return tcpServer;
+	}
 
-    public TcpTransportServer getTcpServer() throws IOException, URISyntaxException {
-        if (tcpServer == null) {
-            tcpServer = createTcpServer();
-        }
-        return tcpServer;
-    }
+	public void setTcpServer(TcpTransportServer tcpServer) {
+		this.tcpServer = tcpServer;
+	}
 
-    public void setTcpServer(TcpTransportServer tcpServer) {
-        this.tcpServer = tcpServer;
-    }
+	// Implementation methods
+	// -------------------------------------------------------------------------
+	protected void doStart() throws Exception {
+		getTcpServer().start();
+        log.info("Started successfully " + getType() + " driver on '" + this.socketUrl.getUrl() + "'");
+	}
 
+	protected void doStop() throws Exception {
+		log.info("stop tcpserver");
+		getTcpServer().stop();
+	}
 
-
-    // Implementation methods
-    //-------------------------------------------------------------------------
-    protected void doStart() throws Exception {
-          getTcpServer().start();
-    }
-
-    protected void doStop() throws Exception {
-        log.info("stop tcpserver");
-     	getTcpServer().stop();
-    }
-
-    protected TcpTransportServer createTcpServer() throws IOException, URISyntaxException {
-        return new TcpTransportServer(this, getLocation(), getServerSocketFactory());
-    }
+	protected TcpTransportServer createTcpServer() throws IOException,
+			URISyntaxException {
+		return new TcpTransportServer(this, getLocation(),
+				getServerSocketFactory());
+	}
 
 }
