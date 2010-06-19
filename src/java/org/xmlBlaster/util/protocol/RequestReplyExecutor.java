xmlBlaster-org/xmlBlaster

commit e677c2cf031bda92625d405731ca3792307f9a39

handling the exceptions of email when configuring with xmlScript to be handled consistently with the configuration, i.e. with the script syntax too
git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@17746 00fa6119-6a0f-0410-b6da-8b96a2b62773

src/java/org/xmlBlaster/util/protocol/RequestReplyExecutor.java
@@ -433,6 +433,8 @@ public abstract class RequestReplyExecutor implements RequestReplyExecutorMBean
    public boolean receiveReply(MsgInfo receiver, boolean udp) throws XmlBlasterException, IOException {
       if (log.isLoggable(Level.FINE)) log.fine("Receiving '" + receiver.getTypeStr() + "' message " + receiver.getMethodName() + "(" + receiver.getRequestId() + ")");
 
+      top: // used for the break in case we are on the client side when an invocation is done
+      
       if (receiver.isInvoke()) {
          // handling invocations ...
 
@@ -445,6 +447,10 @@ public abstract class RequestReplyExecutor implements RequestReplyExecutorMBean
             xmlBlasterImpl.publishOneway(getAddressServer(), receiver.getSecretSessionId(), arr);
          }
          else if (MethodName.PUBLISH == receiver.getMethodName()) {
+            if (!glob.isServerSide()) {
+               log.severe("We are on client side and no "+getType()+" callback driver is available, can't process the remote invocation " + receiver.getMethodName());
+               break top;
+            }
             MsgUnitRaw[] arr = receiver.getMessageArr();
             if (arr == null || arr.length < 1)
                throw new XmlBlasterException(glob, ErrorCode.INTERNAL_ILLEGALARGUMENT, ME, "Invocation of " + receiver.getMethodName() + "() failed, missing arguments");
@@ -498,6 +504,10 @@ public abstract class RequestReplyExecutor implements RequestReplyExecutorMBean
             }
          }
          else if (MethodName.GET == receiver.getMethodName()) {
+            if (!glob.isServerSide()) {
+               log.severe("We are on client side and no "+getType()+" callback driver is available, can't process the remote invocation " + receiver.getMethodName());
+               break top;
+            }
             MsgUnitRaw[] arr = receiver.getMessageArr();
             if (arr == null || arr.length != 1)
                throw new XmlBlasterException(glob, ErrorCode.INTERNAL_ILLEGALARGUMENT, ME, "Invocation of " + receiver.getMethodName() + "() failed, wrong arguments");
@@ -522,6 +532,10 @@ public abstract class RequestReplyExecutor implements RequestReplyExecutorMBean
             }
          }
          else if (MethodName.SUBSCRIBE == receiver.getMethodName()) {
+            if (!glob.isServerSide()) {
+               log.severe("We are on client side and no "+getType()+" callback driver is available, can't process the remote invocation " + receiver.getMethodName());
+               break top;
+            }
             MsgUnitRaw[] arr = receiver.getMessageArr();
             if (arr == null || arr.length != 1)
                throw new XmlBlasterException(glob, ErrorCode.INTERNAL_ILLEGALARGUMENT, ME, "Invocation of " + receiver.getMethodName() + "() failed, wrong arguments");
@@ -529,6 +543,10 @@ public abstract class RequestReplyExecutor implements RequestReplyExecutorMBean
             executeResponse(receiver, response, udp);
          }
          else if (MethodName.UNSUBSCRIBE == receiver.getMethodName()) {
+            if (!glob.isServerSide()) {
+               log.severe("We are on client side and no "+getType()+" callback driver is available, can't process the remote invocation " + receiver.getMethodName());
+               break top;
+            }
             MsgUnitRaw[] arr = receiver.getMessageArr();
             if (arr == null || arr.length != 1)
                throw new XmlBlasterException(glob, ErrorCode.INTERNAL_ILLEGALARGUMENT, ME, "Invocation of " + receiver.getMethodName() + "() failed, wrong arguments");
@@ -536,6 +554,10 @@ public abstract class RequestReplyExecutor implements RequestReplyExecutorMBean
             executeResponse(receiver, response, udp);
          }
          else if (MethodName.ERASE == receiver.getMethodName()) {
+            if (!glob.isServerSide()) {
+               log.severe("We are on client side and no "+getType()+" callback driver is available, can't process the remote invocation " + receiver.getMethodName());
+               break top;
+            }
             MsgUnitRaw[] arr = receiver.getMessageArr();
             if (arr == null || arr.length != 1)
                throw new XmlBlasterException(glob, ErrorCode.INTERNAL_ILLEGALARGUMENT, ME, "Invocation of " + receiver.getMethodName() + "() failed, wrong arguments");
@@ -543,9 +565,17 @@ public abstract class RequestReplyExecutor implements RequestReplyExecutorMBean
             executeResponse(receiver, response, udp);
          }
          else if (MethodName.CONNECT == receiver.getMethodName()) {
+            if (!glob.isServerSide()) {
+               log.severe("We are on client side and no "+getType()+" callback driver is available, can't process the remote invocation " + receiver.getMethodName());
+               break top;
+            }
             return false;
          }
          else if (MethodName.DISCONNECT == receiver.getMethodName()) {
+            if (!glob.isServerSide()) {
+               log.severe("We are on client side and no "+getType()+" callback driver is available, can't process the remote invocation " + receiver.getMethodName());
+               break top;
+            }
             return false;
          }
          else {

src/java/org/xmlBlaster/util/protocol/email/EmailExecutor.java
@@ -442,6 +442,8 @@ public abstract class EmailExecutor extends  RequestReplyExecutor implements I_R
                   msgInfos[k].setSecretSessionId(msgInfo.getSecretSessionId());
             }
          } catch (Throwable e) {
+            if (log.isLoggable(Level.FINE))
+               e.printStackTrace();
             log.warning("Can't process email data from "
                   + pop3Url + ": " + e.toString());
             return;

commit 8307b5ff4578864319969e4237651058eeac7496

Bugfix for cluster callback tunnel (bug introduced in r17746)
git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@17846 00fa6119-6a0f-0410-b6da-8b96a2b62773

src/java/org/xmlBlaster/util/protocol/RequestReplyExecutor.java
@@ -447,7 +447,7 @@ public abstract class RequestReplyExecutor implements RequestReplyExecutorMBean
             xmlBlasterImpl.publishOneway(getAddressServer(), receiver.getSecretSessionId(), arr);
          }
          else if (MethodName.PUBLISH == receiver.getMethodName()) {
-            if (!glob.isServerSide()) {
+            if (!glob.isServerSide() && this.xmlBlasterImpl==null) { // Fixed again 2010-06-19: As for acceptRemoteLoginAsTunnel=true it is allowed
                log.severe("We are on client side and no "+getType()+" callback driver is available, can't process the remote invocation " + receiver.getMethodName());
                break top;
             }