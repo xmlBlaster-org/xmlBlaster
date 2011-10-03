51e3da588208a2bed3d7c201300a7cd655d3088d
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2011-10-03 10:19:08 +0000

    Protect on digit
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@17996 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/util/ReplaceVariable.java
+++ src/java/org/xmlBlaster/util/ReplaceVariable.java
@@ -69,6 +69,14 @@ public final class ReplaceVariable
          int fromIndex = text.indexOf(this.startToken, minIndex);
          if (fromIndex == -1) return text;
          minIndex = 0;
+         
+         if (fromIndex+1 >= text.length()) {
+            if (this.throwException) {
+                throw new IllegalArgumentException("Invalid variable '" + text.substring(fromIndex) +
+                         "', expecting " + this.startToken + this.endToken + " syntax.");
+             }
+             return text;
+         }
 
          int to = text.indexOf(this.endToken, fromIndex+1);
          //System.out.println("ReplaceVariable: Trying fromIndex=" + fromIndex + " toIndex=" + to + " '" + text.substring(fromIndex,to+1) + "'");
@@ -295,20 +303,28 @@ public final class ReplaceVariable
     * <br />
     * Invoke:
     * java org.xmlBlaster.util.ReplaceVariable -template 'Hello ${A} and ${B}, ${A${B}}' -A ZZXX -B OO -AOO OK
-    * java org.xmlBlaster.util.ReplaceVariable -template '${A${B}}' -A aa -B bb -Abb WRONG -aabb OK
+    * java org.xmlBlaster.util.ReplaceVariable -template 'Hello ${A} and ${B}, ${A${B}}' -A ZZXX -B OO -AOO OK
+    * java org.xmlBlaster.util.ReplaceVariable -startToken @ -endToken @ -template '@A@' -A aa -B bb -Abb WRONG -aabb OK
     */
    public static void main(String args[]) {
       String template = "Hello ${A} and ${B}, ${A${B}}";
+      String startToken = "${";
+      String endToken = "}";
       for (int i=0; i<args.length-1; i++) { // Add all "-key value" command line
          String key = args[i].substring(1);
          if (key.equals("template"))
             template = args[i+1];
+         else if (key.equals("startToken"))
+        	 startToken = args[i+1];
+         else if (key.equals("endToken"))
+        	 endToken = args[i+1];
          else
             System.setProperty(key, args[i+1]);
          i++;
       }
+      System.out.println("Using startToken=" + startToken + " and endToken=" + endToken);
 
-      ReplaceVariable r = new ReplaceVariable();
+      ReplaceVariable r = new ReplaceVariable(startToken, endToken);
       String result = r.replace(template,
          new I_ReplaceVariable() {
             public String get(String key) {
