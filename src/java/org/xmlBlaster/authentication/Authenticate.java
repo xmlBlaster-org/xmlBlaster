27f6efcd1548cc0dbd017ed2e876ecb9a8e125ff
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2012-01-16 13:35:14 +0000

    NPE check on disconnect§
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@18023 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/authentication/Authenticate.java
+++ src/java/org/xmlBlaster/authentication/Authenticate.java
@@ -568,6 +568,10 @@ final public class Authenticate implements I_RunlevelListener
          }
 
          SessionInfo sessionInfo = getSessionInfo(secretSessionId);
+         if (sessionInfo != null) {
+        	 log.warning("Given secretSessionId=" + secretSessionId + " is not known, no disconnect possible: " + qos_literal);
+        	 return;
+         }
          if (sessionInfo.getCbQueueNumMsgs() > 0) {
             long sleep = glob.getProperty().get("cb.disconnect.pending.sleep", 1000L); // TODO: allow configuration over DisconnectQos
             log.info("Sleeping cb.disconnect.pending.sleep=" + sleep + " millis in disconnect(" + sessionInfo.getId() + ") to deliver " + sessionInfo.getCbQueueNumMsgs() + " pending messages ...");
