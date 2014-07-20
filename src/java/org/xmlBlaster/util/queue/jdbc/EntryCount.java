f5ad2e1937fe819cc929290fdfcf93849009f454
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2014-07-20 18:10:45 +0000

    public accessor
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@18171 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/util/queue/jdbc/EntryCount.java
+++ src/java/org/xmlBlaster/util/queue/jdbc/EntryCount.java
@@ -1,10 +1,10 @@
 package org.xmlBlaster.util.queue.jdbc;
 
-class EntryCount {
-   long numOfEntries = 0L;
-   long numOfPersistentEntries = 0L;
-   long numOfBytes = 0L;
-   long numOfPersistentBytes = 0L;
+public class EntryCount {
+   public long numOfEntries = 0L;
+   public long numOfPersistentEntries = 0L;
+   public long numOfBytes = 0L;
+   public long numOfPersistentBytes = 0L;
    public String toString() {
       return "numOfEntries=" + numOfEntries +
       " numOfPersistentEntries=" + numOfPersistentEntries +
