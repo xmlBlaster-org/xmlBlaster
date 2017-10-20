c68ecd3ba59260a060c761d3b77c9fccbdbbb380
bjoern <bjoern@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2017-10-20 15:09:42 +0000

    build with Visual Studio 2012
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@18285 00fa6119-6a0f-0410-b6da-8b96a2b62773

---
+++ Projects/Win32/VC11/env_fw.bat
@@ -0,0 +1,15 @@
+REM Environment settings for Visual Studio 2012 C/C++ Win32 compilation
+REM Please customize to your environment
+REM http://www.xmlBlaster.org
+REM zlib and sqlite are optional (if not needed remove preprocessor XMLBLASTER_PERSISTENT_QUEUE=1;XMLBLASTER_ZLIB=1 in VS2005)
+
+set XMLBLASTER_HOME=C:\xmlBlaster
+
+set xerces_include_dir=C:\xerces-c-src_2_8_0\src
+set xerces_lib_dir=C:\xerces-c-src_2_8_0\lib
+
+set zlib_lib_dir=C:\pialibs
+set zlib_include_dir=C:\c\zlib
+
+set sqlite_include_dir=C:\c\sqlite3
+set sqlite_lib_dir=C:\c\sqlite
