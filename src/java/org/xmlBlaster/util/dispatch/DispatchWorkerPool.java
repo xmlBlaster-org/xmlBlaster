bd52bf2378dbfc005e55a144253598f21e47762c
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2018-02-06 17:03:24 +0000

    Change default pool thread size from createThreads=5 to 20(server side) and 2(client side)
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@18298 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/util/dispatch/DispatchWorkerPool.java
+++ src/java/org/xmlBlaster/util/dispatch/DispatchWorkerPool.java
@@ -7,7 +7,6 @@ Author:    xmlBlaster@marcelruff.info
 ------------------------------------------------------------------------------*/
 package org.xmlBlaster.util.dispatch;
 
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.RejectedExecutionHandler;
@@ -40,23 +39,31 @@ public class DispatchWorkerPool //implements I_RunlevelListener
    private PropLong threadLifetime = new PropLong(180 * 1000L);
    private PropInt maximumPoolSize = new PropInt(Integer.MAX_VALUE);
    //private PropInt minimumPoolSize = new PropInt(20);
-   private PropInt createThreads = new PropInt(5);
+   // 2018-02-06 Marcel Ruff increased from 5 to 20 threads
+   private PropInt createThreads;// = new PropInt(5);
    private boolean isShutdown = false;
    private final String poolId = "dispatch";
    // 2017-03-21 Michele Laghi
    private PropLong maxWaitTime = new PropLong(60000L);
+   private static int counterPool = 0;
 
    protected static class DeamonThreadFactory implements ThreadFactory {
+      private Global glob;
       private final String id;
       private final int priority;
       private int count; // to have a nice logging
        
-      DeamonThreadFactory(String id, int priority) {
+      DeamonThreadFactory(Global glob, String id, int priority) {
+         this.glob = glob;
          this.id = id;
          this.priority = priority;
       }
       public Thread newThread(Runnable command) {
-         String threadName = "XmlBlaster.DispatchWorkerPool."+id + "-" + this.count++;
+         String threadName = "";
+         synchronized (DispatchWorkerPool.class) {
+            String scope = glob.isServerSide() ? "ServerScope" : "ClientGlobal";
+            threadName = "XmlBlaster." + scope + ".DispatchWorkerPool." +id + "-pool#" + counterPool + "-thread#" + this.count++;
+         }
          log.fine("Created a new thread '" + threadName + "'");
          Thread t = new Thread(command, threadName);
          t.setDaemon(true);
@@ -70,6 +77,16 @@ public class DispatchWorkerPool //implements I_RunlevelListener
     */
    public DispatchWorkerPool(Global glob) {
       this.glob = glob;
+      if (this.glob.isServerSide()) {
+    	  this.createThreads = new PropInt(20);
+      }
+      else {
+    	  this.createThreads = new PropInt(2);
+      }
+      
+      synchronized (this.getClass()) {
+         counterPool++;
+      }
 
       initialize();
       // Currently not used - on client side there is no RunlevelManager
@@ -89,7 +106,10 @@ public class DispatchWorkerPool //implements I_RunlevelListener
       
       this.maximumPoolSize.setFromEnv(glob, glob.getStrippedId(), context, this.poolId, instanceName, "maximumPoolSize");
       //this.minimumPoolSize.setFromEnv(glob, glob.getStrippedId(), context, this.poolId, instanceName, "minimumPoolSize");
-      this.createThreads.setFromEnv(glob, glob.getStrippedId(), context, this.poolId, instanceName, "createThreads");
+      if (this.glob.isServerSide())
+    	  this.createThreads.setFromEnv(glob, glob.getStrippedId(), context, this.poolId, instanceName, "createThreads");
+      else
+    	  this.createThreads.setFromEnv(glob, glob.getStrippedId(), context, this.poolId, instanceName, "createThreadsClient");
       this.threadLifetime.setFromEnv(glob, glob.getStrippedId(), context, this.poolId, instanceName, "threadLifetime");
       this.maxWaitTime.setFromEnv(glob, glob.getStrippedId(), context, this.poolId, instanceName, "maxWaitTime");
       if (log.isLoggable(Level.FINE)) log.fine("maximumPoolSize=" + this.maximumPoolSize.getValue()/* + " minimumPoolSize=" +
@@ -97,14 +117,16 @@ public class DispatchWorkerPool //implements I_RunlevelListener
 
       //if (this.minimumPoolSize.getValue() < 3)
       //   log.warning("The minimumPoolSize of '" + this.minimumPoolSize.getValue() + "' is less than 2: if one single callback blocks it could block all other callbacks");
-      ThreadFactory threadFactory = new DeamonThreadFactory(glob.getId(), this.threadPrio.getValue());
+      ThreadFactory threadFactory = new DeamonThreadFactory(this.glob, glob.getId(), this.threadPrio.getValue());
       // Default: corePoolSize=0, maximumPoolSize=Integer.MAX_VALUE, keepAliveTime=60L, TimeUnit.SECONDS
       // SynchronousQueue} that hands off tasks to threads without otherwise holding them. Here, an attempt to queue a task will fail if no threads are immediately available to run it, so a new thread will be constructed.
 
       // BlockingQueue<Runnable> blockingQueue = new SynchronousQueue<Runnable>(); 
       BlockingQueue<Runnable> blockingQueue = null;
-      if (maxWaitTime.getValue() > 0L)
+      if (maxWaitTime.getValue() > 0L) {
+    	  // 1 is just queue size, the pool has still createThreads threads
     	  blockingQueue = new BlockingOnOfferQueue<Runnable>(1, maxWaitTime.getValue());
+      }
       else
     	  blockingQueue = new SynchronousQueue<Runnable>();
 
@@ -148,13 +170,26 @@ public class DispatchWorkerPool //implements I_RunlevelListener
          log.severe("The pool is shudown, ignoring execute()");
          return false;
       }
-      this.pool.execute(command);
+      try {
+         this.pool.execute(command);
+      }
+      catch (RejectedExecutionException e) {
+    	  log.severe("Can't gather new thread " + getStatistic() + ": " + e.toString());
+    	  return false;
+      }
+      catch (Throwable e) {
+    	  e.printStackTrace();
+    	  log.severe("Can't gather new thread " + getStatistic() + ": " + e.toString());
+    	  return false;
+      }
       return true;
    }
 
    public String getStatistic() {
 	  //return "None statistic";
-      return "Active threads=" + this.pool.getPoolSize() + " of max=" + this.pool.getMaximumPoolSize();
+	  String prefix = this.glob.isServerSide() ? this.glob.getLogPrefix() : "ClientGlobal";
+      return prefix + " Threads activeCount=" + this.pool.getActiveCount() + " poolSize=" + this.pool.getPoolSize() + " of max=" + this.pool.getMaximumPoolSize()
+      + " maxWaitTime=" + maxWaitTime.getValue();
    }
 
    /**
