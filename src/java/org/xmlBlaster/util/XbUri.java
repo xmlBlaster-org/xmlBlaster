ca936daa0c30bdcb5abbd6135e9bf735a303a7b9
ruff <ruff@00fa6119-6a0f-0410-b6da-8b96a2b62773>
2018-05-21 06:26:43 +0000

    getUrlWithoutPassword() fixed to ommit password
    git-svn-id: svn://www.xmlblaster.org/xmlBlaster/trunk@18315 00fa6119-6a0f-0410-b6da-8b96a2b62773

--- src/java/org/xmlBlaster/util/XbUri.java
+++ src/java/org/xmlBlaster/util/XbUri.java
@@ -205,7 +205,8 @@ public class XbUri
     * @return
     */
    public String getUrlWithoutPassword() {
-      return getScheme() + "://" + getUserInfo() + "@"
+      String userInfo = (this.user != null) ? this.user : "";
+      return getScheme() + "://" + userInfo + "@"
             + getHost()
             + ((getPort() > 0) ? (":" + getPort()) : "");
       // TODO: Add the path
