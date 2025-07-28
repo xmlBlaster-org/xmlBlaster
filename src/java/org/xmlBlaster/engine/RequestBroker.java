08317c8a2d8556f522788eab96c10830075c813e
Marcel Ruff <mr@marcelruff.info>
2025-07-28 21:17:58 +0200

    nicer logging

--- src/java/org/xmlBlaster/engine/RequestBroker.java
+++ src/java/org/xmlBlaster/engine/RequestBroker.java
@@ -737,10 +737,12 @@ public final class RequestBroker extends NotificationBroadcasterSupport
 
          if (subscribeQos.getMultiSubscribe() == false) {
             ArrayList<SubscriptionInfo> vec =  clientSubscriptions.getSubscription(sessionInfo, xmlKey, subscribeQos.getData());
+            String subscriptionId = "";
             if (vec != null && vec.size() > 0) {
 
                for (int i=0; i<vec.size(); i++) {
                   SubscriptionInfo sub = vec.get(i);
+                  subscriptionId = sub.getSubscriptionId();
                   sub.update(subscribeQos);
 
                   if (subscribeQos.getWantInitialUpdate() && xmlKey.isExact()) {
@@ -767,7 +769,7 @@ public final class RequestBroker extends NotificationBroadcasterSupport
 
                log.info("Ignoring duplicate subscription '" +
                        ((xmlKey.getOid()==null)?((xmlKey.getDomain()==null)?xmlKey.getQueryString():xmlKey.getDomain()):xmlKey.getOid()) +
-                        "' as you have set multiSubscribe to false" + (subscribeQos.isRecoveredFromPersistenceStore() ? ", recovered from persistenceStore=true" : ""));
+                        "' subId=" + subscriptionId + " from '" + sessionInfo.getSessionName().getAbsoluteName() + "' as you have set multiSubscribe to false" + (subscribeQos.isRecoveredFromPersistenceStore() ? ", recovered from persistenceStore=true" : ""));
 
                StatusQosData qos = new StatusQosData(glob, MethodName.SUBSCRIBE);
                SubscriptionInfo i = vec.get(0);
