d297f22bf88a3a5d9ba56a91d039d2a9186b4406
Marcel Ruff <xmlblaster@netwake.com>
2025-11-09 18:24:59 +0100

    Extended example

--- src/java/org/xmlBlaster/util/dispatch/plugins/DispatchPluginContentManipulator.java
+++ src/java/org/xmlBlaster/util/dispatch/plugins/DispatchPluginContentManipulator.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
 
+import org.xmlBlaster.engine.MsgUnitWrapper;
 import org.xmlBlaster.engine.dispatch.ServerDispatchManager;
+import org.xmlBlaster.engine.queuemsg.MsgQueueUpdateEntry;
 import org.xmlBlaster.util.Global;
 import org.xmlBlaster.util.MsgUnit;
 import org.xmlBlaster.util.XmlBlasterException;
@@ -13,7 +15,6 @@ import org.xmlBlaster.util.dispatch.I_DispatchManager;
 import org.xmlBlaster.util.plugin.I_Plugin;
 import org.xmlBlaster.util.plugin.PluginInfo;
 import org.xmlBlaster.util.queue.I_Entry;
-import org.xmlBlaster.util.queuemsg.MsgQueueEntry;
 
 /**
  * A minimal message interceptor demo code which changes a message content just
@@ -26,7 +27,8 @@ import org.xmlBlaster.util.queuemsg.MsgQueueEntry;
  * DispatchPlugin[ContentManipulator][1.0]=org.xmlBlaster.util.dispatch.plugins.DispatchPluginContentManipulator,someTestContent=dummyContent
  * </pre>
  * 
- * Normally a client activates this plugin during connection in ConnectQos:
+ * Normally a client activates this plugin during connection in ConnectQos (see
+ * HelloWorldSubscribe.java):
  * 
  * <pre>
  * <connect>
@@ -38,20 +40,29 @@ import org.xmlBlaster.util.queuemsg.MsgQueueEntry;
  * </connect>
  * </pre>
  * 
- * In case ALL clients shall use this plugin, add on server side to
- * <code>xmlBlaster.properties</code>:
+ * In case ALL clients shall use this plugin, add on server side following line
+ * to <code>xmlBlaster.properties</code>:
  * 
  * <pre>
  *   DispatchPlugin/defaultPlugin=ContentManipulator,1.0
  * </pre>
  * 
- * Test example (run in four different shells):
+ * Test example (run commands in four different shells):
  * 
  * <pre>
- * java org.xmlBlaster.Main
- * java javaclienst.HelloWorldSubscribe                                 (and hit key to subscribe)
- * java javaclients.HelloWorldSubscribe -session.name "Subscriber/2"    (and hit key to subscribe)
- * java javaclients.HelloWorldPublish                                   (and hit enter to publish next message)
+export XMLBLASTER_HOME=$HOME/xmlBlaster
+export CLASSPATH=$XMLBLASTER_HOME/build.tmp/classes:$XMLBLASTER_HOME/lib/xmlBlaster.jar
+ * 
+java org.xmlBlaster.Main
+ * 
+ * # launch and hit enter to subscribe ... receives manipulated content:
+java javaclients.HelloWorldSubscribe  -session.name Subscriber/1 -dispatchPlugin ContentManipulator,1.0
+ * 
+ * # launch and hit enter to subscribe ... receives original content:
+java javaclients.HelloWorldSubscribe -session.name "Subscriber/2"
+ * 
+ * # and hit enter to publish next message
+java javaclients.HelloWorldPublish -contentMimeExtended "jsonContentToBeFiltered"
  * </pre>
  * 
  * @see http://www.xmlblaster.org/xmlBlaster/doc/requirements/dispatch.plugin.priorizedDispatch.html
@@ -170,18 +181,23 @@ public class DispatchPluginContentManipulator implements I_Plugin, I_MsgDispatch
       entryList = dispatchManager.prepareMsgsFromQueue(entryList);
       log.info("... " + mgr.getSessionName().getRelativeName() + " -> num messages=" + entryList.size());
 
-      // manipulate entries
       for (I_Entry ientry : entryList) {
-         MsgQueueEntry entry = (MsgQueueEntry) ientry;
+         MsgQueueUpdateEntry entry = (MsgQueueUpdateEntry) ientry;
+         MsgUnitWrapper msgUnitWrapper = entry.getMsgUnitWrapper().getClone();
+         entry.setMsgUnitWrapper(msgUnitWrapper);
          MsgUnit msgUnit = entry.getMsgUnit();
-         String contentStr = msgUnit.getContentStr();
-         log.info("Processing " + contentStr);
-         String testContent = this.pluginProperties.getProperty("someTestContent");
-         contentStr = "Manipulated " + testContent + " random=" + UUID.randomUUID(); // RandomGenerator.getDefault().nextInt(1,
-                                                                                     // 101);
-         msgUnit.setContentStr(contentStr);
-      }
+         String topicId = msgUnit.getKeyData().getOid();
+         String contentMimeExtended = msgUnit.getKeyData().getContentMimeExtended();
 
+         String contentStr = msgUnit.getContentStr();
+
+         log.info("topicId=" + topicId + ",contentMimeExtended=" + contentMimeExtended
+               + ": Manipulate content for this subscriber, original=" + contentStr);
+         String testContent = this.pluginProperties.getProperty("someTestContent"); // from plugin registration
+         contentStr = "Manipulated " + testContent + " random=" + UUID.randomUUID();
+         msgUnit.setContentStr(contentStr);
+
+      }
       return entryList;
    }
 
